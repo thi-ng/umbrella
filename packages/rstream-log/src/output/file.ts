bc26d097c18f3182c6f2b3fb688356acf5e258ca
Karsten Schmidt <k@postspectacular.com>
2018-01-25 14:08:38 +0000

    feat(rstream-log): add node check for writeFile()

--- packages/rstream-log/src/output/file.ts
+++ packages/rstream-log/src/output/file.ts
@@ -1,14 +1,18 @@
-import * as fs from "fs";
+import { isNode } from "@thi.ng/checks/is-node";
 import { ISubscriber } from "@thi.ng/rstream/api";
 
 export function writeFile(path: string): ISubscriber<string> {
-    return {
-        next(msg) {
-            fs.appendFile(path, msg + "\n", (e) => {
-                if (e) {
-                    process.stderr.write(e.message);
-                }
-            });
-        }
-    };
+    if (isNode()) {
+        const fs = require("fs");
+        return {
+            next(msg) {
+                fs.appendFile(path, msg + "\n", (e) => {
+                    if (e) {
+                        process.stderr.write(e.message);
+                    }
+                });
+            }
+        };
+    }
+    throw new Error("only available in NodeJS");
 }
