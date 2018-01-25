4e5a2ee19f632ef7b385d99d88b62defb8f5a345
Karsten Schmidt <k@postspectacular.com>
2018-01-25 14:05:48 +0000

    feat(rstream): add fromRAF() fallback for node, add docs

--- packages/rstream/src/from/raf.ts
+++ packages/rstream/src/from/raf.ts
@@ -1,14 +1,29 @@
-import { Stream } from "../stream";
+import { isNode } from "@thi.ng/checks/is-node";
 
+import { Stream } from "../stream";
+import { fromInterval } from "./interval";
+
+/**
+ * Yields a stream of monotonically increasing counter,
+ * triggered by a `requestAnimationFrame()` loop.
+ * Only available in browser environments. In NodeJS,
+ * this function falls back to `fromInterval(16)`, yielding
+ * a similar (approximately 60fps) stream.
+ *
+ * Subscribers to this stream will be processed during
+ * that same loop iteration.
+ */
 export function fromRAF() {
-    return new Stream<number>((o) => {
-        let i = 0, id,
-            isActive = true,
-            loop = () => {
-                isActive && o.next(i++);
-                isActive && (id = requestAnimationFrame(loop));
-            };
-        id = requestAnimationFrame(loop);
-        return () => (isActive = false, cancelAnimationFrame(id));
-    }, `raf-${Stream.NEXT_ID++}`);
+    return isNode() ?
+        fromInterval(16) :
+        new Stream<number>((o) => {
+            let i = 0, id,
+                isActive = true,
+                loop = () => {
+                    isActive && o.next(i++);
+                    isActive && (id = requestAnimationFrame(loop));
+                };
+            id = requestAnimationFrame(loop);
+            return () => (isActive = false, cancelAnimationFrame(id));
+        }, `raf-${Stream.NEXT_ID++}`);
 }
