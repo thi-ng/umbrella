d36a9ff8acf823b90813e53456e3c1ead4a0e303
Karsten Schmidt <k@postspectacular.com>
2018-01-26 13:36:25 +0000

    feat(rstream): add fromIterableSync()

--- packages/rstream/src/from/iterable.ts
+++ packages/rstream/src/from/iterable.ts
@@ -1,17 +1,33 @@
 import { Stream } from "../stream";
 
 export function fromIterable<T>(src: Iterable<T>, delay = 0) {
-    return new Stream<T>((stream) => {
-        const iter = src[Symbol.iterator](),
-            id = setInterval(() => {
-                let val: IteratorResult<T>;
-                if ((val = iter.next()).done) {
-                    clearInterval(id);
-                    stream.done();
-                } else {
-                    stream.next(val.value);
-                }
-            }, delay);
-        return () => clearInterval(id);
-    }, `iterable-${Stream.NEXT_ID++}`);
+    return new Stream<T>(
+        (stream) => {
+            const iter = src[Symbol.iterator](),
+                id = setInterval(() => {
+                    let val: IteratorResult<T>;
+                    if ((val = iter.next()).done) {
+                        clearInterval(id);
+                        stream.done();
+                    } else {
+                        stream.next(val.value);
+                    }
+                }, delay);
+            return () => clearInterval(id);
+        },
+        `iterable-${Stream.NEXT_ID++}`
+    );
+}
+
+export function fromIterableSync<T>(src: Iterable<T>) {
+    return new Stream<T>(
+        (stream) => {
+            for (let s of src) {
+                stream.next(s);
+            }
+            stream.done();
+            return null;
+        },
+        `iterable-${Stream.NEXT_ID++}`
+    );
 }
