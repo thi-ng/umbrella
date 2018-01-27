1f32fc0c5e7c9ffe08c57b56d35183f617bc947c
Karsten Schmidt <k@postspectacular.com>
2018-01-27 14:54:32 +0000

    feat(transducers): update step() to support multiple results
    - respect reduced value termination
    - internal use of push() vs. last() to support multiple results per transduction step
    - add docs
    
    BREAKING CHANGE:
    now possibly returns array instead of single value if wrapped transducer produced multiple results

--- packages/transducers/src/step.ts
+++ packages/transducers/src/step.ts
@@ -1,7 +1,56 @@
 import { Transducer } from "./api";
-import { last } from "./rfn/last";
+import { push } from "./rfn/push";
+import { isReduced } from "./index";
 
-export function step<A, B>(tx: Transducer<A, B>): (x: A) => B {
-    const rfn = tx(last())[2];
-    return (x: A) => rfn(undefined, x);
+/**
+ * Single-step transducer execution wrapper.
+ * Returns array if transducer produces multiple results
+ * and undefined if there was no output. Else returns single
+ * result value.
+ *
+ * Likewise, once a transducer has produced a final / reduced
+ * value, all further invocations of the stepper function will
+ * return undefined.
+ *
+ * ```
+ * // single result
+ * step(map(x => x * 10))(1);
+ * // 10
+ *
+ * // multiple results
+ * step(mapcat(x => [x, x + 1, x + 2]))(1)
+ * // [ 1, 2, 3 ]
+ *
+ * // no result
+ * f = step(filter(even))
+ * f(1); // undefined
+ * f(2); // 2
+ *
+ * // reduced value termination
+ * f = step(take(2));
+ * f(1); // 1
+ * f(1); // 1
+ * f(1); // undefined
+ * f(1); // undefined
+ * ```
+ *
+ * @param tx
+ */
+export function step<A, B>(tx: Transducer<A, B>): (x: A) => B | B[] {
+    const [_, complete, reduce] = tx(push()); _;
+    let done = false;
+    return (x: A) => {
+        if (!done) {
+            let acc = reduce([], x);
+            done = isReduced(acc);
+            if (done) {
+                acc = complete(acc.deref());
+            }
+            return acc.length === 1 ?
+                acc[0] :
+                acc.length > 0 ?
+                    acc :
+                    undefined;
+        }
+    };
 }
