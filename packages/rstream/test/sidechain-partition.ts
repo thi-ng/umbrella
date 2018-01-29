4c242c93539567c3e45237561e4bb5cdd102fb46
Karsten Schmidt <k@postspectacular.com>
2018-01-29 20:20:55 +0000

    test(rstream): remove sidechainPartition interval test due to unpredictable timing

--- packages/rstream/test/sidechain-partition.ts
+++ packages/rstream/test/sidechain-partition.ts
@@ -29,34 +29,6 @@ describe("SidechainPartition", function () {
         side.done();
     });
 
-    it("partitions (interval)", (done) => {
-        let src = rs.fromInterval(10);
-        let side = rs.fromInterval(27);
-        let buf = [];
-        src.subscribe(rs.sidechainPartition(side))
-            .subscribe({
-                next(x) {
-                    buf.push(x);
-                },
-                done() {
-                    try {
-                        assert.deepEqual(
-                            buf,
-                            [[0, 1], [2, 3, 4], [5, 6]]
-                        );
-                    } catch (_) {
-                        // possible alternative due to timing issues
-                        assert.deepEqual(
-                            buf,
-                            [[0, 1], [2, 3], [4, 5, 6]]
-                        );
-                    }
-                    done();
-                }
-            });
-        setTimeout(() => src.done(), 100);
-    });
-
     it("partitions w/ predicate", (done) => {
         let src = new rs.Stream();
         let side = new rs.Stream();
