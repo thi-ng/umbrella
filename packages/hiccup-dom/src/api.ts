d134d5b7ba7b2c37b09fd1d8ca38145830c1eec8
Karsten Schmidt <k@postspectacular.com>
2018-02-02 15:58:59 +0000

    refactor(hiccup-dom): add interfaces (still unused)

--- packages/hiccup-dom/src/api.ts
+++ packages/hiccup-dom/src/api.ts
@@ -1,1 +1,19 @@
+import { IObjectOf } from "@thi.ng/api/api";
+
+export interface ILifecycle {
+    init(el: Element, ...args: any[]);
+    render(...args: any[]): any;
+    release(...args: any[]);
+}
+
+export interface ComponentAttribs {
+    class?: string;
+    disabled?: boolean;
+    href?: string;
+    id?: string;
+    key?: string;
+    style?: string | IObjectOf<string | number>;
+    [_: string]: any;
+}
+
 export const DEBUG = false;
