d38f47e07df9a067b5f52ad2a039d21c01bf4c0d
Karsten Schmidt <k@postspectacular.com>
2018-02-16 11:45:09 +0000

    test(transducers): add range2d tests

---
+++ packages/transducers/test/range2d.ts
@@ -0,0 +1,48 @@
+import { range2d } from "../src/iter/range2d";
+
+import * as assert from "assert";
+
+describe("range2d", () => {
+    it("forward", () => {
+        assert.deepEqual(
+            [...range2d(0, 3, 1, 3)],
+            [[0, 1], [1, 1], [2, 1], [0, 2], [1, 2], [2, 2]]
+        );
+    });
+    it("forward w/ step", () => {
+        assert.deepEqual(
+            [...range2d(0, 5, 1, 6, 2, 3)],
+            [[0, 1], [2, 1], [4, 1], [0, 4], [2, 4], [4, 4]]
+        );
+    });
+    it("reverse", () => {
+        assert.deepEqual(
+            [...range2d(3, 0, 3, 1)],
+            [[3, 3], [2, 3], [1, 3], [3, 2], [2, 2], [1, 2]]
+        );
+    });
+    it("reverse w/ step", () => {
+        assert.deepEqual(
+            [...range2d(5, 0, 6, 1, -2, -3)],
+            [[5, 6], [3, 6], [1, 6], [5, 3], [3, 3], [1, 3]]
+        );
+    });
+    it("empty w/ wrong step sign (x)", () => {
+        assert.deepEqual(
+            [...range2d(0, 1, 0, 1, -1, 1)],
+            []
+        );
+    });
+    it("empty w/ wrong step sign (y)", () => {
+        assert.deepEqual(
+            [...range2d(0, 1, 0, 1, 1, -1)],
+            []
+        );
+    });
+    it("single output", () => {
+        assert.deepEqual(
+            [...range2d(0, 1, 0, 1)],
+            [[0, 0]]
+        );
+    });
+});
