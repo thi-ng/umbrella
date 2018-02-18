d7b1d0dfaae2af7d4c0d93eecde79cdc5f008a92
Karsten Schmidt <k@postspectacular.com>
2018-02-18 19:53:48 +0000

    feat(transducers): add movingMedian() xform

---
+++ packages/transducers/src/xform/moving-median.ts
@@ -0,0 +1,14 @@
+import { Comparator } from "@thi.ng/api/api";
+import { compare } from "@thi.ng/api/compare";
+
+import { Transducer } from "../api";
+import { comp } from "../func/comp";
+import { identity } from "../func/identity";
+
+import { map } from "./map";
+import { partition } from "./partition";
+
+export function movingMedian<A, B>(n: number, key: ((x: A) => B) = <any>identity, cmp: Comparator<B> = compare): Transducer<A, A> {
+    const m = n >> 1;
+    return comp(partition(n, 1, true), map((window) => [...window].sort((a, b) => cmp(key(a), key(b)))[m]));
+}
