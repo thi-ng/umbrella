3d8aa324d0ad3f2f3c3f9031ff932b6b0f41466e
Karsten Schmidt <k@postspectacular.com>
2018-03-02 15:42:05 +0000

    fix(transducers): flattenWith()

--- packages/transducers/src/xform/flatten-with.ts
+++ packages/transducers/src/xform/flatten-with.ts
@@ -6,9 +6,9 @@ export function flattenWith<T>(fn: (x: T) => Iterable<T>): Transducer<T | Iterab
     return (rfn: Reducer<any, T>) => {
         const r = rfn[2];
         const flatten = (acc, x) => {
-            x = fn(x);
-            if (x) {
-                for (let y of x) {
+            const xx = fn(x);
+            if (xx) {
+                for (let y of xx) {
                     acc = flatten(acc, y);
                     if (isReduced(acc)) {
                         break;
@@ -18,6 +18,6 @@ export function flattenWith<T>(fn: (x: T) => Iterable<T>): Transducer<T | Iterab
             }
             return r(acc, x);
         };
-        return compR(rfn, fn);
+        return compR(rfn, flatten);
     };
 }
