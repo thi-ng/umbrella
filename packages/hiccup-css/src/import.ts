e347c2924e3fedf00b6de59e30cd42bbd0f7880d
Karsten Schmidt <k@postspectacular.com>
2018-03-04 20:19:02 +0000

    fix(hiccup-css): @import query separator

--- packages/hiccup-css/src/import.ts
+++ packages/hiccup-css/src/import.ts
@@ -1,10 +1,10 @@
 import { RuleFn } from "./api";
 
 export function at_import(url: string, ...queries: string[]): RuleFn {
-    return (acc, _) => (
+    return (acc, opts) => (
         acc.push(
             queries.length ?
-                `@import url(${url}) ${queries.join(" ")};` :
+                `@import url(${url}) ${queries.join(opts.format.ruleSep)};` :
                 `@import url(${url});`
         ),
         acc
