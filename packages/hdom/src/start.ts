a200beb88199066c97c8f1e3f059e2f132d09fad
Karsten Schmidt <k@postspectacular.com>
2018-03-05 22:47:04 +0000

    feat(hdom): add support for frame skipping, add docs

--- packages/hdom/src/start.ts
+++ packages/hdom/src/start.ts
@@ -8,6 +8,18 @@ import { normalizeTree } from "./normalize";
  * computing diff to previous frame's tree and applying
  * any changes to the real DOM.
  *
+ * **Selective updates**: No updates will be applied
+ * if the given hiccup tree is `undefined` or `null` or
+ * a root component function returns no value. This way
+ * a given root function can do some state handling of its own
+ * and implement fail-fast checks to determine no DOM updates
+ * are necessary, save effort re-creating a new hiccup tree and
+ * request skipping DOM updates via this function. In this case,
+ * the previous DOM tree is kept around until the root function
+ * returns a tree again, which then is diffed and applied against
+ * the previous tree kept as usual. Any number of frames can be
+ * skipped this way.
+ *
  * Important: The parent element given is assumed to have NO
  * children at the time when `start()` is called. Since
  * hdom does NOT track the real DOM, the resulting
@@ -29,7 +41,11 @@ export function start(parent: Element | string, tree: any, spans = true) {
         parent;
     function update() {
         if (isActive) {
-            diffElement(<Element>parent, prev, prev = normalizeTree(tree, [0], true, spans));
+            const curr = normalizeTree(tree, [0], true, spans);
+            if (curr != null) {
+                diffElement(<Element>parent, prev, curr);
+                prev = curr;
+            }
             // check again in case one of the components called cancel
             isActive && requestAnimationFrame(update);
         }
