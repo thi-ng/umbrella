a48b776297c5fa322e573c859ee277445f1be5b9
Karsten Schmidt <k@postspectacular.com>
2018-03-08 21:54:32 +0000

    refactor(examples): update async-effect

--- examples/async-effect/src/index.ts
+++ examples/async-effect/src/index.ts
@@ -1,13 +1,29 @@
 import { IObjectOf } from "@thi.ng/api/api";
 import { EventDef, EffectDef } from "@thi.ng/atom/api";
-import { FX_DISPATCH_ASYNC, FX_DISPATCH_NOW, FX_STATE } from "@thi.ng/atom/api";
+import { FX_DISPATCH_ASYNC, FX_DISPATCH_NOW } from "@thi.ng/atom/api";
 import { Atom } from "@thi.ng/atom/atom";
 import { EventBus } from "@thi.ng/atom/event-bus";
-import { setIn } from "@thi.ng/atom/path";
+import { valueSetter } from "@thi.ng/atom/interceptors";
 import { start } from "@thi.ng/hdom/start";
 
+// best practice tip:
+// define event & effect names as consts or enums and
+// avoid hardcoded strings
+// additionally/alternatively define dispatch functions
+
+const EV_SET_STATUS = "set-status";
+const EV_LOAD_JSON = "load-json";
+const EV_RECEIVE_JSON = "receive-json";
+const EV_ERROR = "error";
+
+const FX_JSON = "load-json";
+const FX_DELAY = "delay";
+
+// event handler definitions
+
 const events: IObjectOf<EventDef> = {
-    setStatus: (state, [_, s]) => ({ [FX_STATE]: setIn(state, "status", s) }),
+    // valueSetter() produces an interceptor to set value at given path
+    [EV_SET_STATUS]: valueSetter("status"),
 
     // this event is the initial trigger for starting an async IO operation
     // via the FX_DISPATCH_ASYNC side effect, which takes this general definition:
@@ -16,42 +32,49 @@ const events: IObjectOf<EventDef> = {
     // FX_DISPATCH_ASYNC acts as a wrapper for the actual side effect to be executed,
     // in this case it's the "json" side effect defined below
     // the last items in the array are the event IDs for success & error outcomes
-    loadJSON: (_, [__, url]) => ({
-        [FX_DISPATCH_NOW]: ["setStatus", ["idle", `loading: ${url}...`]],
-        [FX_DISPATCH_ASYNC]: ["json", url, "receiveJSON", "error"]
+    [EV_LOAD_JSON]: (_, [__, url]) => ({
+        [FX_DISPATCH_NOW]: [EV_SET_STATUS, ["idle", `loading: ${url}...`]],
+        [FX_DISPATCH_ASYNC]: [FX_JSON, url, EV_RECEIVE_JSON, EV_ERROR]
     }),
 
     // this event will be triggered after JSON data has been successfully loaded
-    // sets status message, `json` state value and triggers another,
-    // delayed invocation of "setStatus" event to reset message after 1sec
-    receiveJSON: (state, [_, json]) => ({
-        [FX_STATE]: setIn(state, "json", JSON.stringify(json, null, 2)),
-        [FX_DISPATCH_NOW]: ["setStatus", ["success", "JSON succesfully loaded"]],
-        [FX_DISPATCH_ASYNC]: ["delay", [1000, ["idle", "done."]], "setStatus", "error"],
-    }),
+    // sets `json` state value, status and triggers another, delayed invocation
+    // of EV_SET_STATUS event to reset message after 1sec
+
+    // as with the EV_SET_STATUS event, we're using the higher-order valueSetter()
+    // to produce an interceptor, here with additional value transformer to
+    // create a formatted JSON string
+    [EV_RECEIVE_JSON]: [
+        valueSetter("json", (json) => JSON.stringify(json, null, 2)),
+        () => ({
+            [FX_DISPATCH_NOW]: [EV_SET_STATUS, ["success", "JSON succesfully loaded"]],
+            [FX_DISPATCH_ASYNC]: [FX_DELAY, [1000, ["idle", "done."]], EV_SET_STATUS, EV_ERROR],
+        })
+    ],
 
     // error event handler
-    error: (_, [__, err]) => ({
-        [FX_DISPATCH_NOW]: ["setStatus", ["error", err.message]],
+    [EV_ERROR]: (_, [__, err]) => ({
+        [FX_DISPATCH_NOW]: [EV_SET_STATUS, ["error", err.message]],
     }),
 };
 
 const effects: IObjectOf<EffectDef> = {
     // these are async side effects. ALWAYS MUST RETURN A PROMISE
-    json: (url) => fetch(url).then((res) => res.json()),
-    delay: ([x, msg]) => new Promise((res) => setTimeout(() => res(msg), x))
+    [FX_JSON]: (url) => fetch(url).then((res) => res.json()),
+    [FX_DELAY]: ([x, msg]) => new Promise((res) => setTimeout(() => res(msg), x))
 };
 
+// main app component
 const app = () => {
     const bus = new EventBus(new Atom<any>({}), events, effects);
 
     // kick off JSON request
-    bus.dispatch(["loadJSON", "foo.json"]);
+    bus.dispatch([EV_LOAD_JSON, "foo.json"]);
 
     // root component function
     return () => {
         if (bus.processQueue()) {
-            let { json, status } = bus.state.deref();
+            const { json, status } = bus.state.deref();
             return ["div",
                 ["p#status", { class: status[0] }, `status: ${status[1]}`],
                 ["pre", json]
