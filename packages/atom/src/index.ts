59085e0ced82d8f0578c5379f42b6a77351049d6
Karsten Schmidt <k@postspectacular.com>
2018-03-09 14:45:23 +0000

    fix(atom): add interceptors to re-exports

--- packages/atom/src/index.ts
+++ packages/atom/src/index.ts
@@ -3,5 +3,6 @@ export * from "./atom";
 export * from "./cursor";
 export * from "./event-bus";
 export * from "./history";
+export * from "./interceptors";
 export * from "./path";
 export * from "./view";
