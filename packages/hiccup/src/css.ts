93343d638e349f811612a7d66cb9eae2dad483ec
Karsten Schmidt <k@postspectacular.com>
2018-03-14 17:54:57 +0000

    feat(hiccup): support fn values in style objects

--- packages/hiccup/src/css.ts
+++ packages/hiccup/src/css.ts
@@ -1,8 +1,14 @@
+import { isFunction } from "@thi.ng/checks/is-function";
+
 export const css = (rules: any) => {
     const css = [];
     for (let r in rules) {
         if (rules.hasOwnProperty(r)) {
-            css.push(r + ":" + rules[r] + ";");
+            let v = rules[r];
+            if (isFunction(v)) {
+                v = v(rules);
+            }
+            css.push(r + ":" + v + ";");
         }
     }
     return css.join("");
