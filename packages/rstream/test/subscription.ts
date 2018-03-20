42b343aaec935de16132f60861c02a78f7ec5ff1
Karsten Schmidt <k@postspectacular.com>
2018-03-20 20:41:03 +0000

    test(rstream): add StreamSync & Subscription tests

--- packages/rstream/test/subscription.ts
+++ packages/rstream/test/subscription.ts
@@ -1,4 +1,4 @@
-// import * as tx from "@thi.ng/transducers";
+import * as tx from "@thi.ng/transducers";
 import * as assert from "assert";
 
 import * as rs from "../src/index";
@@ -22,5 +22,58 @@ describe("Subscription", () => {
                 }
             }),
             25);
+    });
+
+    it("unsub does not trigger Subscription.done()", (done) => {
+        let buf = [];
+        let called = false;
+        src = rs.fromIterable([1, 2, 3], 10);
+        const sub = src.subscribe({
+            next(x) { buf.push(x); },
+            done() { called = true; }
+        });
+        setTimeout(() => sub.unsubscribe(), 15);
+        setTimeout(() => {
+            assert.deepEqual(buf, [1]);
+            assert.equal(src.getState(), rs.State.DONE);
+            assert(!called);
+            done();
+        }, 30);
+    });
+
+    it("no new values after unsub", (done) => {
+        let buf = [];
+        let called = false;
+        src = rs.fromIterable([1, 2, 3], 10);
+        const sub = src.subscribe(
+            {
+                next(x) { buf.push(x); },
+                done() { called = true; }
+            },
+            tx.partition(2, true)
+        );
+        setTimeout(() => sub.unsubscribe(), 25);
+        setTimeout(() => {
+            assert.deepEqual(buf, [[1, 2]]);
+            assert.equal(src.getState(), rs.State.DONE);
+            assert(!called);
+            done();
+        }, 50);
+    });
+
+    it("completing transducer sends all values", (done) => {
+        let buf = [];
+        src = rs.fromIterable([1, 2, 3], 10);
+        src.subscribe(
+            {
+                next(x) { buf.push(x); },
+                done() {
+                    assert.deepEqual(buf, [[1, 2], [3]]);
+                    assert.equal(src.getState(), rs.State.DONE);
+                    done();
+                }
+            },
+            tx.partition(2, true)
+        );
     });
 });
