2ad2f485b37c14683b1186391a6d5051d62b4594
Karsten Schmidt <k@postspectacular.com>
2018-03-20 10:48:16 +0000

    fix(rstream): bisect() add downstream impl checks, add tests

---
+++ packages/rstream/test/bisect.ts
@@ -0,0 +1,51 @@
+import * as tx from "@thi.ng/transducers";
+import * as assert from "assert";
+
+import * as rs from "../src/index";
+import { Subscription } from "../src/index";
+
+describe("bisect", () => {
+    let src: rs.Stream<number>;
+
+    beforeEach(() => {
+        src = rs.fromIterable([1, 2, 3, 4]);
+    });
+
+    it("raw subscribers", (done) => {
+        const odds = [], evens = [];
+        src.subscribe(
+            rs.bisect((x) => !!(x & 1),
+                { next(x) { odds.push(x) } },
+                { next(x) { evens.push(x) } }
+            )
+        ).subscribe({
+            done() {
+                assert.deepEqual(odds, [1, 3]);
+                assert.deepEqual(evens, [2, 4]);
+                done();
+            }
+        });
+    });
+
+    it("subs", (done) => {
+        const odds = [], evens = [];
+        const subo = new Subscription<number, number>(
+            { next(x) { odds.push(x) }, done() { doneCount++; } },
+            tx.map<number, number>(x => x * 10)
+        );
+        const sube = new Subscription<number, number>(
+            { next(x) { evens.push(x) }, done() { doneCount++; } },
+            tx.map<number, number>(x => x * 100)
+        );
+        let doneCount = 0;
+        src.subscribe(rs.bisect((x) => !!(x & 1), subo, sube))
+            .subscribe({
+                done() {
+                    assert.deepEqual(odds, [10, 30]);
+                    assert.deepEqual(evens, [200, 400]);
+                    assert.equal(doneCount, 2);
+                    done();
+                }
+            });
+    });
+});
