910b7187a00f01ddf414c8b4af2dbfeede6f7303
Karsten Schmidt <k@postspectacular.com>
2018-03-31 17:59:22 +0100

    feat(examples): add pointfree-svg demo

---
+++ examples/pointfree-svg/README.md
@@ -0,0 +1,18 @@
+# pointfree-svg
+
+This is a non-interactive demo combining the following packages to generate the SVG graphic below:
+
+- [@thi.ng/hiccup](https://github.com/thi-ng/umbrella/tree/master/packages/hiccup)
+- [@thi.ng/hdom-components](https://github.com/thi-ng/umbrella/tree/master/packages/hdom-components)
+- [@thi.ng/pointfree](https://github.com/thi-ng/umbrella/tree/master/packages/pointfree)
+- [@thi.ng/pointfree-lang](https://github.com/thi-ng/umbrella/tree/master/packages/pointfree-lang)
+
+![generated result](./output.svg)
+
+The generated SVG file will be written in this example's directory...
+
+```
+git clone https://github.com/thi-ng/umbrella.git
+cd umbrella/examples/pointfree-svg
+yarn start
+```
---
+++ examples/pointfree-svg/src/index.ts
@@ -0,0 +1,97 @@
+import * as fs from "fs";
+
+import * as svg from "@thi.ng/hdom-components/svg";
+import { serialize } from "@thi.ng/hiccup";
+import { ensureStack, maptos } from "@thi.ng/pointfree";
+import { ffi, run } from "@thi.ng/pointfree-lang";
+
+/**
+ * Graphics library / helper words
+ */
+const libsrc = `
+( helper words for forming 2D vectors )
+: xy ( x y -- [x y] ) vec2 ;
+: yx ( x y -- [y x] ) swap vec2 ;
+
+( appends a hiccup shape element to @shapes array )
+: addshape ( s -- )
+  @shapes pushl drop ;
+
+( creates hiccup element with 2 args & shape type )
+: shape2 ( a b type -- )
+  -rot vec3 addshape;
+
+( transforms 2 points into a svg line )
+: line ( a b -- )
+@svg.line shape2 ;
+
+( transforms point and radius into a svg circle )
+: circle ( p r -- )
+  @svg.circle shape2 ;
+
+( creates a horizontal line )
+: hline ( y width -- )
+over 0 yx -rot yx line ;
+
+( creates a vertical line )
+: vline ( x height -- )
+  over 0 xy -rot xy line ;
+
+(
+    2D grid loop construct
+    executes body quot for each iteration
+)
+: loop2 ( cols rows body -- )
+  >r [ over [ over cprd exec ] dotimes drop ] dotimes
+  drop rdrop ;
+`;
+
+/**
+ * User code to generate SVG graphic and write as file
+ * The whole block has this stack effect:
+ *
+ * ( filename res -- )
+ */
+const usersrc = `
+( creates grid of lines with given grid res )
+: grid ( res -- )
+  dup [10 * 100 dup2 hline vline] dotimes ;
+
+(
+    creates triangular grid of circles with given grid res
+    only creates circles for grid cells where x <= y
+)
+: circlegrid ( res -- )
+  dup [dup2 lteq [xy 10 v* 3 circle] [drop2] if] loop2 ;
+
+grid circlegrid
+
+( create SVG root element in hiccup format )
+@svg.svgdoc [{width: 200, height: 200, stroke: "#f04", fill: "none"}] pushl
+( concat with generated shapes )
+@shapes cat
+( serialize hiccup format to SVG and write to disk )
+serialize swap write-file
+`;
+
+// initialize environment and pre-compile library source
+const env = ffi(
+    {
+        "svg.line": svg.line,
+        "svg.circle": svg.circle,
+        "svg.svgdoc": svg.svgdoc,
+        shapes: [],
+    },
+    {
+        "serialize": maptos(serialize),
+        "write-file": (ctx) => {
+            const stack = ctx[0];
+            ensureStack(stack, 2);
+            fs.writeFileSync(stack.pop(), stack.pop());
+            return ctx;
+        }
+    });
+run(libsrc, env);
+
+// compile & execute user code with given stack params
+run(usersrc, env, ["output.svg", 21]);
