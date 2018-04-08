70cfe06cac9dbd5fdecfbcb1dab6d8e8f5d4a300
Karsten Schmidt <k@postspectacular.com>
2018-04-08 01:45:16 +0100

    feat(hdom): fix #13, add support for user context and pass to components
    - add optional context arg for start()
    - update normalizeTree(), diffElement() to inject provided context for
      all component functions and objects w/ lifecycle methods
    - context is always injected as 1st arg to comp fns, with only exception of
      `init` lifecycle hook, where (for perf reasons) the created DOM element
      is passed as the first arg (context 2nd)
    - various minor optimizations
    - add doc strings
    
    BREAKING CHANGE: component functions & lifecycle hooks now receive user
    context object as their first arg. All components accepting arguments must
    be updated, but can potentially be simplified at the same time.

--- packages/hdom/src/start.ts
+++ packages/hdom/src/start.ts
@@ -3,37 +3,60 @@ import { diffElement } from "./diff";
 import { normalizeTree } from "./normalize";
 
 /**
- * Takes a parent DOM element (or ID) and hiccup tree
- * (array or function) and starts RAF update loop,
- * computing diff to previous frame's tree and applying
- * any changes to the real DOM.
+ * Takes a parent DOM element (or ID), hiccup tree (array, function or
+ * component object w/ lifecycle methods) and an optional context
+ * object. Starts RAF update loop, computing diff to previous frame's
+ * tree and applying any changes to the real DOM.
  *
- * **Selective updates**: No updates will be applied
- * if the given hiccup tree is `undefined` or `null` or
- * a root component function returns no value. This way
- * a given root function can do some state handling of its own
- * and implement fail-fast checks to determine no DOM updates
- * are necessary, save effort re-creating a new hiccup tree and
- * request skipping DOM updates via this function. In this case,
- * the previous DOM tree is kept around until the root function
- * returns a tree again, which then is diffed and applied against
- * the previous tree kept as usual. Any number of frames can be
- * skipped this way.
+ * The optional `context` arg can be used for passing global config data
+ * or state down into the hiccup component tree. Any embedded component
+ * function in the tree will receive this context object as first
+ * argument, as will life cycle methods in component objects:
  *
- * Important: The parent element given is assumed to have NO
- * children at the time when `start()` is called. Since
- * hdom does NOT track the real DOM, the resulting
- * changes will result in potentially undefined behavior
- * if the parent element wasn't empty.
+ * ```
+ * const link = (ctx, evt, ...body) =>
+ *   ["a",
+ *     {...ctx.ui.link, onclick: () => ctx.app.dispatch(evt) },
+ *     body];
  *
- * Returns a function, which when called, immediately
- * cancels the update loop.
+ * const foo = {
+ *   init: (ctx, el) => { ... },
+ *   render: (ctx, ...args) => ["section", ctx.ui.foo, args.map(x => [link, x])],
+ *   release: (ctx) => { ... }
+ * };
+ *
+ * const ctx = {
+ *   app: ...
+ *   ui: { link: ..., foo: ...},
+ * };
+ *
+ * start("app", foo, ctx)
+ * ```
+ *
+ * **Selective updates**: No updates will be applied if the given hiccup
+ * tree is `undefined` or `null` or a root component function returns no
+ * value. This way a given root function can do some state handling of
+ * its own and implement fail-fast checks to determine no DOM updates
+ * are necessary, save effort re-creating a new hiccup tree and request
+ * skipping DOM updates via this function. In this case, the previous
+ * DOM tree is kept around until the root function returns a tree again,
+ * which then is diffed and applied against the previous tree kept as
+ * usual. Any number of frames may be skipped this way.
+ *
+ * Important: The parent element given is assumed to have NO children at
+ * the time when `start()` is called. Since hdom does NOT track the real
+ * DOM, the resulting changes will result in potentially undefined
+ * behavior if the parent element wasn't empty.
+ *
+ * Returns a function, which when called, immediately cancels the update
+ * loop.
  *
  * @param parent root element or ID
  * @param tree hiccup DOM tree
+ * @param ctx arbitrary user context object
  * @param spans true (default), if text should be wrapped in `<span>`
  */
-export function start(parent: Element | string, tree: any, spans = true) {
+export function start(parent: Element | string, tree: any, ctx?: any, spans = true) {
     let prev = [];
     let isActive = true;
     parent = isString(parent) ?
@@ -41,7 +64,7 @@ export function start(parent: Element | string, tree: any, spans = true) {
         parent;
     function update() {
         if (isActive) {
-            const curr = normalizeTree(tree, [0], true, spans);
+            const curr = normalizeTree(tree, ctx, [0], true, spans);
             if (curr != null) {
                 diffElement(<Element>parent, prev, curr);
                 prev = curr;
