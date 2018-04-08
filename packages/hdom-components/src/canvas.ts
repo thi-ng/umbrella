86d1f0d9ea58e7f6b4a2610fa5a8cc6ac163a183
Karsten Schmidt <k@postspectacular.com>
2018-04-08 01:57:17 +0100

    refactor(hdom-components): remove svg, update canvas (hdom context support)
    - remove entire svg.ts
    - remove @thi.ng/hiccup dependency
    - update readme
    
    BREAKING CHANGE: SVG functionality has been moved to new
    @thi.ng/hiccup-svg package. Canvas component user fns have new args

--- packages/hdom-components/src/canvas.ts
+++ packages/hdom-components/src/canvas.ts
@@ -4,31 +4,37 @@ export interface CanvasOpts {
     [id: string]: any;
 }
 
+export type WebGLInitFn = (el: HTMLCanvasElement, gl: WebGLRenderingContext, hctx?: any) => void;
+export type WebGLUpdateFn = (el: HTMLCanvasElement, gl: WebGLRenderingContext, hctx?: any, frame?: number) => void;
+
+export type Canvas2DInitFn = (el: HTMLCanvasElement, gl: CanvasRenderingContext2D, hctx?: any) => void;
+export type Canvas2DUpdateFn = (el: HTMLCanvasElement, gl: CanvasRenderingContext2D, hctx?: any, frame?: number) => void;
+
 const _canvas = (type, init, update, attribs, opts) => {
-    let ctx;
+    let el, ctx;
     let frame = 0;
     return [{
-        init(el: HTMLCanvasElement) {
+        init(el: HTMLCanvasElement, hctx: any) {
             ctx = el.getContext(type, opts);
-            init(ctx);
+            init(el, ctx, hctx);
         },
-        render() {
-            ctx && update(ctx, frame++);
+        render(hctx: any) {
+            ctx && update(el, ctx, hctx, frame++);
             return ["canvas", attribs]
         }
     }];
 };
 
 export const canvasWebGL = (
-    init: (gl: WebGLRenderingContext) => void,
-    update: (gl: WebGLRenderingContext, frame: number) => void,
+    init: WebGLInitFn,
+    update: WebGLUpdateFn,
     attribs: CanvasOpts,
     glopts?: WebGLContextAttributes) =>
     _canvas("webgl", init, update, attribs, glopts);
 
 export const canvas2D = (
-    init: (gl: CanvasRenderingContext2D) => void,
-    update: (gl: CanvasRenderingContext2D, frame: number) => void,
+    init: Canvas2DInitFn,
+    update: Canvas2DUpdateFn,
     attribs: CanvasOpts,
     ctxopts?: Canvas2DContextAttributes) =>
     _canvas("2d", init, update, attribs, ctxopts);
