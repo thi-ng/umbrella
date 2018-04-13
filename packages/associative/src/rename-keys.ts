bfabe8025682f5a392d3951f07d5ae5683638a8a
Karsten Schmidt <k@postspectacular.com>
2018-04-13 16:57:33 +0100

    feat(associative): add renameKeysMap

--- packages/associative/src/rename-keys.ts
+++ packages/associative/src/rename-keys.ts
@@ -1,10 +1,22 @@
 import { IObjectOf } from "@thi.ng/api/api";
 
+import { empty } from "./utils";
+
+export function renameKeysMap<T>(src: Map<any, T>, km: IObjectOf<T>): Map<any, T> {
+    const dest = empty(src, Map);
+    for (let p of src) {
+        const k = p[0];
+        const kk = km[k];
+        dest.set(kk !== undefined ? kk : k, p[1]);
+    }
+    return dest;
+}
+
 export function renameKeysObj(src: any, km: IObjectOf<PropertyKey>) {
     const dest = {};
     for (let k in src) {
         const kk = km[k];
-        dest[kk !== undefined ? kk : k] = src[k];
+        dest[kk != null ? kk : k] = src[k];
     }
     return dest;
 }
