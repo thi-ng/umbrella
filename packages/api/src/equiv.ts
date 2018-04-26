2ac8bff3d1afe21e3536d03abb1f3550dc190864
Karsten Schmidt <k@postspectacular.com>
2018-04-26 15:36:42 +0100

    feat(api): support more types in equiv(), add tests
    - add equivSetLike() for ES6 Set/Map
    - add checks for Date, RegExp & NaN

--- packages/api/src/equiv.ts
+++ packages/api/src/equiv.ts
@@ -1,5 +1,9 @@
 import { isArrayLike } from "@thi.ng/checks/is-arraylike";
+import { isDate } from "@thi.ng/checks/is-date";
+import { isMap } from "@thi.ng/checks/is-map";
 import { isPlainObject } from "@thi.ng/checks/is-plain-object";
+import { isRegExp } from "@thi.ng/checks/is-regexp";
+import { isSet } from "@thi.ng/checks/is-set";
 
 export function equiv(a, b): boolean {
     if (a === b) {
@@ -28,7 +32,17 @@ export function equiv(a, b): boolean {
     if (isArrayLike(a) && isArrayLike(b)) {
         return equivArrayLike(a, b);
     }
-    return false;
+    if ((isSet(a) && isSet(b)) || (isMap(a) && isMap(b))) {
+        return equivSetLike(a, b);
+    }
+    if (isDate(a) && isDate(b)) {
+        return a.getTime() === b.getTime();
+    }
+    if (isRegExp(a) && isRegExp(b)) {
+        return a.toString() === b.toString();
+    }
+    // NaN
+    return (a !== a && b !== b);
 }
 
 function equivArrayLike(a: ArrayLike<any>, b: ArrayLike<any>) {
@@ -37,6 +51,11 @@ function equivArrayLike(a: ArrayLike<any>, b: ArrayLike<any>) {
         while (--l >= 0 && equiv(a[l], b[l]));
     }
     return l < 0;
+}
+
+function equivSetLike(a: Set<any>, b: Set<any>) {
+    if (a.size !== b.size) return false;
+    return equiv([...a].sort(), [...b].sort());
 }
 
 function equivObject(a, b) {
