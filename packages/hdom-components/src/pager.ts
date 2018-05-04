334a4d24174acfacd5d26d7053bcf83ee84f41a1
Karsten Schmidt <k@postspectacular.com>
2018-05-04 13:15:18 +0100

    refactor(hdom-components): make pager more customizable

--- packages/hdom-components/src/pager.ts
+++ packages/hdom-components/src/pager.ts
@@ -7,7 +7,7 @@ import { map } from "@thi.ng/iterators/map";
 export interface PagerOpts {
     /**
      * Function producing a single page nav or counter element. MUST be
-     * provided by user. All other opts are optional.
+     * provided by user.
      *
      * The function is called with:
      *
@@ -18,9 +18,10 @@ export interface PagerOpts {
      * - disabled flag as determined by the pager
      *
      * If `disabled` is true, the function should return a version of
-     * the button component reflecting this state to the user. I.e. the
+     * the button component reflecting this state to the user. E.g. the
      * "prev page" buttons should be disabled if the first page is
-     * currently active.
+     * currently active. Likewise, the currently selected page button
+     * will be set to disabled as well.
      *
      * Page IDs are zero-based indices, whereas page number labels are
      * one-based. The currently active page ID is only provided for
@@ -28,29 +29,30 @@ export interface PagerOpts {
      */
     button: (page: number, curr: number, max: number, label: any, disabled: boolean) => any;
     /**
-     * Attribs for pager root element
+     * Pager root component function. Receives all 3 button groups as
+     * arguments. Optional. Default: `["div.pager", ...body]`
      */
-    attribs?: any;
+    root: (ctx: any, ...body: any[]) => any;
     /**
-     * Attribs for prev page button group
+     * Component function to provide wrapper for the first / prev nav
+     * button group. The `first` / `prev` args are button components.
+     * Optional. Default: `["div.pager-prev", first, prev]`
      */
-    prevAttribs?: any;
+    groupPrev: (ctx: any, first: any, prev: any) => any;
     /**
-     * Attribs for next page button group
+     * Component function to provide wrapper for the page buttons group.
+     * The `buttons` argument is an array of button components.
+     * Optional. Default: `["div.pager-pages", ...buttons]`
      */
-    nextAttribs?: any;
+    groupPages: (ctx: any, buttons: any[]) => any;
     /**
-     * Number of numbered page buttons to show. If zero, only the
-     * prev/next and first/last buttons will be shown. Default: 5
+     * Component function to provide wrapper for the next / last nav
+     * button group. The `next` /  `last` args are button components.
+     * Optional. Default: `["div.pager-next", next, last]`
      */
-    maxButtons?: number;
+    groupNext: (ctx: any, next: any, last: any) => any;
     /**
-     * Number of items per page. Used to compute max page count.
-     * Default: 10
-     */
-    pageLen?: number;
-    /**
-     * Page offset for prev / next pages. Default: 1
+     * Page increment for prev / next page buttons. Default: 1
      */
     navStep?: number;
     /**
@@ -72,59 +74,77 @@ export interface PagerOpts {
 }
 
 /**
- * Higher order component for paged navigation buttons. The returned
- * component function takes two arguments: current number of items and
- * current page ID (zero-based) and yields a component of page buttons
- * and prev / next and first / last navigation buttons. The actual
- * button components are defined via the user supplied `button` option.
- * The first / prev and next / last nav buttons are paired within inner
- * `div` elements (one per pair) and can be styled (or hidden)
- * separately.
+ * Higher order container component for paged navigation buttons. The
+ * returned component function takes these arguments:
+ *
+ * - `ctx` - hdom context object
+ * - `id` - current page ID (zero-based)
+ * - `numItems` - current number of items
+ * - `pageLen` - number of items per page (only used for calculation)
+ * - `maxButtons` - number of page buttons to show (default: 5). If
+ *   zero, only the prev / next and first / last buttons will be shown.
+ *
+ * If there are more pages than the specified number, only the
+ * neighboring page IDs (relative to the current page) are shown. E.g.
+ * If there are 10 pages, the current page ID is 5 and 3 visible page
+ * buttons then the pager will look like this (the `|` character here
+ * indicates button group boundaries):
+ *
+ * ```
+ * << < | 4 5 6 | > >>
+ * ```
+ *
+ * Providing `pageLen` and `maxButtons` as arguments allows to
+ * dynamically control the number of page buttons at runtime, e.g. in
+ * response to window resizing.
+ *
+ * Yields a component of page buttons and prev / next and first / last
+ * navigation buttons. The actual button and button group components are
+ * defined via the user supplied options. The first / prev and next /
+ * last nav buttons are paired within inner `div` elements (one per
+ * pair) and can be styled (or hidden) separately.
  *
  * ```
  * // initialization
  * const mypager = pager({
- *   button: (i, label, disabled) => ["a", {href: `/page/${i}`}, label],
- *   attribs: { class: "pager w-100 tc mv3" },
- *   prevAttribs: { class: "pager-nav fl mr3" },
- *   nextAttribs: { class: "pager-nav fr ml3" },
+ *   button: (i, curr, max, label, disabled) =>
+ *     ["a", {href: `/page/${i}`, disabled}, label]
  * });
  *
  * // usage
- * [mypager, currNumItems, currPage]
+ * [mypager, currPage, currNumItems, 10, 5]
  * ```
  *
  * @param opts
  */
 export const pager = (opts: PagerOpts) => {
-    opts = Object.assign({
-        maxButtons: 5,
-        pageLen: 10,
+    opts = Object.assign(<PagerOpts>{
+        root: (_, ...body: any[]) => ["div.pager", ...body],
+        groupPrev: (_, ...bts: any[]) => ["div.pager-prev", ...bts],
+        groupNext: (_, ...bts: any[]) => ["div.pager-next", ...bts],
+        groupPages: (_, ...bts: any[]) => ["div.pager-pages", ...bts],
         navStep: 1,
-        attribs: {},
-        prevAttribs: {},
-        nextAttribs: {},
         labelFirst: "<<",
         labelPrev: "<",
         labelNext: ">",
         labelLast: ">>",
     }, opts);
-    return (_, num: number, id: number) => {
+    return (_, id: number, num: number, pageLen = 10, maxBts = 5) => {
         const bt = opts.button;
         const step = opts.navStep;
-        const maxID = Math.floor(Math.max(0, num - 1) / opts.pageLen);
+        const maxID = Math.floor(Math.max(0, num - 1) / pageLen);
         id = Math.max(Math.min(id, maxID), 0);
-        return ["div", opts.attribs,
-            ["div", opts.prevAttribs,
-                bt(0, id, maxID, opts.labelFirst, !id),
-                bt(Math.max(id - step, 0), id, maxID, opts.labelPrev, !id)],
-            map(
-                (i: number) => bt(i, id, maxID, i + 1, i === id),
-                pageRange(id, maxID, opts.maxButtons)
-            ),
-            ["div", opts.nextAttribs,
-                bt(Math.min(id + step, maxID), id, maxID, opts.labelNext, id >= maxID),
-                bt(maxID, id, maxID, opts.labelLast, id >= maxID)],
+        return [
+            opts.root,
+            [opts.groupPrev,
+            bt(0, id, maxID, opts.labelFirst, !id),
+            bt(Math.max(id - step, 0), id, maxID, opts.labelPrev, !id)],
+            [opts.groupPages,
+            map((i: number) => bt(i, id, maxID, i + 1, i === id),
+                pageRange(id, maxID, maxBts))],
+            [opts.groupNext,
+            bt(Math.min(id + step, maxID), id, maxID, opts.labelNext, id >= maxID),
+            bt(maxID, id, maxID, opts.labelLast, id >= maxID)],
         ];
     };
 };
