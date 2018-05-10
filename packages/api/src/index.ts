f051ca3c8ea8cc015750db4cb2712b7f030ff619
Karsten Schmidt <k@postspectacular.com>
2018-05-10 14:59:59 +0100

    refactor(api): remove obsolete files from package
    BREAKING CHANGE: @thi.ng/api now only contains type declarations,
    decorators and mixins. All other features have been moved
    to new dedicated packages:
    
    - @thi.ng/bench
    - @thi.ng/compare
    - @thi.ng/equiv
    - @thi.ng/errors

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,13 +1,8 @@
 import * as decorators from "./decorators";
 import * as mixins from "./mixins";
-
-export * from "./api";
-export * from "./bench";
-export * from "./compare";
-export * from "./error";
-export * from "./equiv";
-
 export {
     decorators,
     mixins,
 }
+
+export * from "./api";
