b1cb7d9a2192558ebc1f083ca0781d78882eba70
Karsten Schmidt <k@postspectacular.com>
2018-05-12 23:05:07 +0100

    perf(hiccup): update css()
    - use string concat (~2.5x faster)
    - skip null values

--- packages/hiccup/src/css.ts
+++ packages/hiccup/src/css.ts
@@ -1,15 +1,13 @@
 import { isFunction } from "@thi.ng/checks/is-function";
 
 export const css = (rules: any) => {
-    const css = [];
+    let css = "", v;
     for (let r in rules) {
-        if (rules.hasOwnProperty(r)) {
-            let v = rules[r];
-            if (isFunction(v)) {
-                v = v(rules);
-            }
-            css.push(r + ":" + v + ";");
+        v = rules[r];
+        if (isFunction(v)) {
+            v = v(rules);
         }
+        v != null && (css += `${r}:${v};`);
     }
-    return css.join("");
+    return css;
 };
