74f7d02ef65faef75d1ddcd1f1469772b98e6a18
Karsten Schmidt <k@postspectacular.com>
2018-06-17 12:02:19 +0100

    refactor(sax): extract parser sub-states

--- packages/sax/src/index.ts
+++ packages/sax/src/index.ts
@@ -6,7 +6,7 @@ export interface FSMState {
 }
 
 export type FSMStateMap<T extends FSMState, A, B> = IObjectOf<FSMHandler<T, A, B>>;
-export type FSMHandler<T extends FSMState, A, B> = (state: T, input: A) => B;
+export type FSMHandler<T extends FSMState, A, B> = (state: T, input: A) => B | void;
 
 export function fsm<T extends FSMState, A, B>(states: FSMStateMap<T, A, B>, initial: () => T): Transducer<A, B> {
     return (rfn: Reducer<any, B>) => {
@@ -37,167 +37,155 @@ const isTagChar = (x: string) =>
     x == "_" ||
     x == ":";
 
+const unexpected = (x) => { throw new Error(`unexpected char: ${x}`); };
+
 export interface ParseState extends FSMState {
     scope: string[];
     tag: string;
     body: string;
     attribs: any;
-    phase: number;
     name: string;
     val: string;
 }
 
-export enum State {
+enum State {
     WAIT,
-    ELEM,
+    MAYBE_ELEM,
+    ELEM_START,
+    ELEM_END,
+    ELEM_SINGLE,
     ELEM_BODY,
-    ATTRIB,
+    MAYBE_ATTRIB,
+    ATTRIB_NAME,
+    ATTRIB_VAL_START,
+    ATTRIB_VALUE
 }
 
-const unexpected = (x) => { throw new Error(`unexpected char: ${x}`); };
 
 export const PARSER: FSMStateMap<ParseState, string, Event> = {
     [State.WAIT]: (s: ParseState, x: string) => {
-        if (isWS(x)) {
-            return null;
-        } else if (x == "<") {
-            s.state = State.ELEM;
-            s.phase = 0;
+        if (!isWS(x)) {
+            if (x == "<") {
+                s.state = State.MAYBE_ELEM;
+            } else {
+                unexpected(x);
+            }
+        }
+    },
+
+    [State.MAYBE_ELEM]: (s: ParseState, x: string) => {
+        if (x == "/") {
+            if (s.scope.length == 0) {
+                unexpected(x);
+            }
+            s.state = State.ELEM_END;
+            s.tag = "";
+        } else if (isTagChar(x)) {
+            s.state = State.ELEM_START;
+            s.tag = x;
+            s.attribs = {};
         } else {
             unexpected(x);
         }
     },
 
-    [State.ELEM]: (s: ParseState, x: string) => {
-        switch (s.phase) {
-            case 0:
-                if (x == "/") {
-                    if (s.scope.length == 0) {
-                        unexpected(x);
-                    }
-                    s.tag = "";
-                    s.phase = 1;
-                } else if (isTagChar(x)) {
-                    s.tag = x;
-                    s.attribs = {};
-                    s.phase = 2;
-                } else {
-                    unexpected(x);
-                }
-                break;
-            // end
-            case 1:
-                if (isTagChar(x)) {
-                    s.tag += x;
-                } else if (x == ">") {
-                    if (s.tag == s.scope[s.scope.length - 1]) {
-                        const res = { id: "end", value: { tag: s.tag } };
-                        s.scope.pop();
-                        s.state = State.WAIT;
-                        return res;
-                    } else {
-                        throw new Error(`unmatched end tag: ${s.tag}`);
-                    }
-                }
-                break;
-            // start
-            case 2:
-                if (isTagChar(x)) {
-                    s.tag += x;
-                } else if (isWS(x)) {
-                    s.state = State.ATTRIB;
-                    s.phase = 0;
-                } else if (x == ">") {
-                    const res = { id: "elem", value: { tag: s.tag, attribs: s.attribs } };
-                    s.scope.push(s.tag);
-                    s.state = State.ELEM_BODY;
-                    s.body = "";
-                    return res;
-                } else if (x == "/") {
-                    s.phase = 3;
-                } else {
-                    unexpected(x);
-                }
-                break;
-            case 3:
-                if (x == ">") {
-                    s.state = State.WAIT;
-                    return { id: "start", value: { tag: s.tag, attribs: s.attribs } };
-                } else {
-                    unexpected(x);
-                }
-                break;
+    [State.ELEM_START]: (s: ParseState, x: string) => {
+        if (isTagChar(x)) {
+            s.tag += x;
+        } else if (isWS(x)) {
+            s.state = State.MAYBE_ATTRIB;
+        } else if (x == ">") {
+            const res = { id: "elem", value: { tag: s.tag, attribs: s.attribs } };
+            s.state = State.ELEM_BODY;
+            s.scope.push(s.tag);
+            s.body = "";
+            return res;
+        } else if (x == "/") {
+            s.state = State.ELEM_SINGLE;
+        } else {
+            unexpected(x);
         }
     },
 
+    [State.ELEM_END]: (s: ParseState, x: string) => {
+        if (isTagChar(x)) {
+            s.tag += x;
+        } else if (x == ">") {
+            if (s.tag == s.scope[s.scope.length - 1]) {
+                const res = { id: "end", value: { tag: s.tag } };
+                s.scope.pop();
+                s.state = State.WAIT;
+                return res;
+            } else {
+                throw new Error(`unmatched end tag: ${s.tag}`);
+            }
+        }
+    },
+
+    [State.ELEM_SINGLE]: (s: ParseState, x: string) => {
+        if (x == ">") {
+            s.state = State.WAIT;
+            return { id: "start", value: { tag: s.tag, attribs: s.attribs } };
+        } else {
+            unexpected(x);
+        }
+    },
 
     [State.ELEM_BODY]: (s: ParseState, x: string) => {
         if (x == "<") {
             const res = s.body.length > 0 ?
                 { id: "body", value: s.body } :
                 undefined;
-            s.state = State.ELEM;
+            s.state = State.MAYBE_ELEM;
             s.tag = "";
-            s.phase = 0;
             return res;
         } else {
             s.body += x;
         }
     },
 
-    [State.ATTRIB]: (s: ParseState, x: string) => {
-        switch (s.phase) {
-            // ws
-            case 0:
-                if (isTagChar(x)) {
-                    s.name = x;
-                    s.val = "";
-                    s.phase = 2;
-                } else if (x == ">") {
-                    const res = { id: "elem", value: { tag: s.tag, attribs: s.attribs } };
-                    s.scope.push(s.tag);
-                    s.state = State.ELEM_BODY;
-                    s.body = "";
-                    return res;
-                } else if (x == "/") {
-                    s.phase = 1;
-                } else if (!isWS(x)) {
-                    unexpected(x);
-                }
-                break;
-            // self-closing
-            case 1:
-                if (x == ">") {
-                    s.state = State.WAIT;
-                    return { id: "elem", value: { tag: s.tag, attribs: s.attribs } };
-                } else {
-                    unexpected(x);
-                }
-                break;
-            // attrib name
-            case 2:
-                if (isTagChar(x)) {
-                    s.name += x;
-                } else if (x == "=") {
-                    s.phase = 3;
-                } else {
-                    unexpected(x);
-                }
-                break;
-            case 3:
-                if (x == "\"") {
-                    s.phase = 4;
-                } else {
-                    unexpected(x);
-                }
-                break;
-            case 4:
-                if (x == "\"") {
-                    s.attribs[s.name] = s.val;
-                    s.phase = 0;
-                } else {
-                    s.val += x;
-                }
+    [State.MAYBE_ATTRIB]: (s: ParseState, x: string) => {
+        if (isTagChar(x)) {
+            s.state = State.ATTRIB_NAME;
+            s.name = x;
+            s.val = "";
+        } else if (x == ">") {
+            const res = { id: "elem", value: { tag: s.tag, attribs: s.attribs } };
+            s.state = State.ELEM_BODY;
+            s.scope.push(s.tag);
+            s.body = "";
+            return res;
+        } else if (x == "/") {
+            s.state = State.ELEM_SINGLE;
+        } else if (!isWS(x)) {
+            unexpected(x);
         }
-    }
+    },
+
+    [State.ATTRIB_NAME]: (s: ParseState, x: string) => {
+        if (isTagChar(x)) {
+            s.name += x;
+        } else if (x == "=") {
+            s.state = State.ATTRIB_VAL_START;
+        } else {
+            unexpected(x);
+        }
+    },
+
+    [State.ATTRIB_VAL_START]: (s: ParseState, x: string) => {
+        if (x == "\"") {
+            s.state = State.ATTRIB_VALUE;
+        } else {
+            unexpected(x);
+        }
+    },
+
+    [State.ATTRIB_VALUE]: (s: ParseState, x: string) => {
+        if (x == "\"") {
+            s.attribs[s.name] = s.val;
+            s.state = State.MAYBE_ATTRIB;
+        } else {
+            s.val += x;
+        }
+    },
 }
