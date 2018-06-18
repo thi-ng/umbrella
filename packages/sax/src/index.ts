0f2fcdffa8d8207d0ecebe3d58bdd788536a9ed3
Karsten Schmidt <k@postspectacular.com>
2018-06-18 01:46:28 +0100

    feat(sax): add entity support, update result format, update states
    - add ParseOpts, ParseElement, ParseEvent, Type
    - add XML entity replacement (optional)
    - update error handling (add `error()` helper)
    - update PROC_DECL state to support arbitrary instruction tags
    -

--- packages/sax/src/index.ts
+++ packages/sax/src/index.ts
@@ -1,17 +1,44 @@
-import { Event } from "@thi.ng/api";
+import { IObjectOf } from "@thi.ng/api";
 import * as fsm from "@thi.ng/transducers-fsm";
 import { Transducer } from "@thi.ng/transducers/api";
 
-export interface ParseState extends fsm.FSMState {
-    pos: number;
+export interface ParseOpts {
+    entities: boolean;
+}
+
+export interface ParseElement {
+    tag: string;
+    attribs: IObjectOf<string>;
+    body: string;
+    children: ParseElement[];
+}
+
+export interface ParseEvent extends Partial<ParseElement> {
+    type: Type;
+}
+
+export enum Type {
+    PROC,
+    DOCTYPE,
+    COMMENT,
+    ELEM_START,
+    ELEM_END,
+    ELEM_BODY,
+    ERROR,
+}
+
+interface ParseState extends fsm.FSMState {
     scope: any[];
     tag: string;
-    body: string;
     attribs: any;
+    body: string;
     name: string;
     val: string;
+    pos: number;
     quote: string;
     phase: number;
+    isProc: boolean;
+    opts: Partial<ParseOpts>;
 }
 
 enum State {
@@ -34,13 +61,24 @@ enum State {
     PROC_END,
 }
 
-export const parse = (): Transducer<string, Event> =>
+const ENTITIES = {
+    "&amp;": "&",
+    "&lt;": "<",
+    "&gt;": ">",
+    "&quot;": '"',
+    "&apos;": "'",
+};
+
+const ENTITY_RE = new RegExp(`(${Object.keys(ENTITIES).join("|")})`, "g");
+
+export const parse = (opts: Partial<ParseOpts> = {}): Transducer<string, ParseEvent> =>
     fsm.fsm({
         states: PARSER,
         init: () => (<ParseState>{
             state: State.WAIT,
             scope: [],
-            pos: 0
+            pos: 0,
+            opts,
         }),
         terminate: State.ERROR
     });
@@ -56,245 +94,265 @@ const isTagChar = (x: string) =>
     x == "_" ||
     x == ":";
 
-const unexpected = (s: ParseState, x: string) => {
+const error = (s: ParseState, body: string) => {
     s.state = State.ERROR;
-    return { id: "error", value: `unexpected char: '${x}' @ pos: ${s.pos}` };
+    return { type: Type.ERROR, body };
 };
 
-const PARSER: fsm.FSMStateMap<ParseState, string, Event> = {
-    [State.ERROR]: () => {
-    },
+const unexpected = (s: ParseState, x: string) =>
+    error(s, `unexpected char: '${x}' @ pos ${s.pos}`);
 
-    [State.WAIT]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (!isWS(x)) {
-            if (x === "<") {
-                s.state = State.MAYBE_ELEM;
+const replaceEntities = (x: string) => x.replace(ENTITY_RE, (y) => ENTITIES[y]);
+
+const PARSER: fsm.FSMStateMap<ParseState, string, ParseEvent> = {
+
+    [State.ERROR]: () => { },
+
+    [State.WAIT]: (state, ch) => {
+        state.pos++;
+        if (!isWS(ch)) {
+            if (ch === "<") {
+                state.state = State.MAYBE_ELEM;
             } else {
-                return unexpected(s, x);
+                return unexpected(state, ch);
             }
         }
     },
 
-    [State.MAYBE_ELEM]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (x === "/") {
-            if (s.scope.length == 0) {
-                return unexpected(s, x);
+    [State.MAYBE_ELEM]: (state, ch) => {
+        state.pos++;
+        if (ch === "/") {
+            if (state.scope.length == 0) {
+                return unexpected(state, ch);
             }
-            s.state = State.ELEM_END;
-            s.tag = "";
-        } else if (isTagChar(x)) {
-            s.state = State.ELEM_START;
-            s.tag = x;
-            s.attribs = {};
-        } else if (x === "!") {
-            s.state = State.MAYBE_INSTRUCTION;
-        } else if (x === "?") {
-            s.state = State.PROC_DECL;
-            s.phase = 0;
-            s.body = "";
+            state.state = State.ELEM_END;
+            state.tag = "";
+        } else if (isTagChar(ch)) {
+            state.state = State.ELEM_START;
+            state.tag = ch;
+            state.attribs = {};
+        } else if (ch === "!") {
+            state.state = State.MAYBE_INSTRUCTION;
+        } else if (ch === "?") {
+            state.state = State.PROC_DECL;
+            state.phase = 0;
+            state.tag = "";
+            state.body = "";
         } else {
-            return unexpected(s, x);
+            return unexpected(state, ch);
         }
     },
 
-    [State.ELEM_START]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (isTagChar(x)) {
-            s.tag += x;
-        } else if (isWS(x)) {
-            s.state = State.MAYBE_ATTRIB;
-        } else if (x === ">") {
-            const res = { id: "elem", value: { tag: s.tag, attribs: s.attribs } };
-            s.state = State.ELEM_BODY;
-            s.scope.push({ tag: s.tag, attribs: s.attribs, children: [] });
-            s.body = "";
+    [State.ELEM_START]: (state, ch) => {
+        state.pos++;
+        if (isTagChar(ch)) {
+            state.tag += ch;
+        } else if (isWS(ch)) {
+            state.state = State.MAYBE_ATTRIB;
+        } else if (ch === ">") {
+            const res = { type: Type.ELEM_START, tag: state.tag, attribs: state.attribs };
+            state.state = State.ELEM_BODY;
+            state.scope.push({ tag: state.tag, attribs: state.attribs, children: [] });
+            state.body = "";
             return res;
-        } else if (x === "/") {
-            s.state = State.ELEM_SINGLE;
+        } else if (ch === "/") {
+            state.state = State.ELEM_SINGLE;
         } else {
-            return unexpected(s, x);
+            return unexpected(state, ch);
         }
     },
 
-    [State.ELEM_END]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (isTagChar(x)) {
-            s.tag += x;
-        } else if (x === ">") {
-            const n = s.scope.length;
-            if (n > 0 && s.tag === s.scope[n - 1].tag) {
-                const res = { id: "end", value: s.scope[n - 1] };
-                s.scope.pop();
+    [State.ELEM_END]: (state, ch) => {
+        state.pos++;
+        if (isTagChar(ch)) {
+            state.tag += ch;
+        } else if (ch === ">") {
+            const n = state.scope.length;
+            if (n > 0 && state.tag === state.scope[n - 1].tag) {
+                const res = state.scope[n - 1];
+                state.scope.pop();
                 if (n > 1) {
-                    s.scope[n - 2].children.push(res.value);
+                    state.scope[n - 2].children.push(res);
                 }
-                s.state = State.WAIT;
-                return res;
+                state.state = State.WAIT;
+                return { type: Type.ELEM_END, ...res };
             } else {
-                throw new Error(`unmatched end tag: ${s.tag}`);
+                error(state, state.tag);
             }
         }
     },
 
-    [State.ELEM_SINGLE]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (x === ">") {
-            s.state = State.WAIT;
-            const n = s.scope.length;
-            const res = { tag: s.tag, attribs: s.attribs };
+    [State.ELEM_SINGLE]: (state, ch) => {
+        state.pos++;
+        if (ch === ">") {
+            state.state = State.WAIT;
+            const n = state.scope.length;
+            const res = { tag: state.tag, attribs: state.attribs };
             if (n > 0) {
-                s.scope[n - 1].children.push(res);
+                state.scope[n - 1].children.push(res);
             }
-            return { id: "elem", value: res };
+            return { type: Type.ELEM_END, ...res };
         } else {
-            return unexpected(s, x);
+            return unexpected(state, ch);
         }
     },
 
-    [State.ELEM_BODY]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (x === "<") {
+    [State.ELEM_BODY]: (state, ch) => {
+        state.pos++;
+        if (ch === "<") {
             let res;
-            if (s.body.length > 0) {
-                s.scope[s.scope.length - 1].body = s.body;
-                res = { id: "body", value: { tag: s.tag, body: s.body } }
+            if (state.body.length > 0) {
+                if (state.opts.entities) {
+                    state.body = replaceEntities(state.body);
+                }
+                state.scope[state.scope.length - 1].body = state.body;
+                res = { type: Type.ELEM_BODY, tag: state.tag, body: state.body };
             }
-            s.state = State.MAYBE_ELEM;
-            s.tag = "";
+            state.state = State.MAYBE_ELEM;
+            state.tag = "";
             return res;
         } else {
-            s.body += x;
+            state.body += ch;
         }
     },
 
-    [State.MAYBE_ATTRIB]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (isTagChar(x)) {
-            s.state = State.ATTRIB_NAME;
-            s.name = x;
-            s.val = "";
-        } else if (x === ">") {
-            const res = { id: "elem", value: { tag: s.tag, attribs: s.attribs } };
-            s.state = State.ELEM_BODY;
-            s.scope.push({ tag: s.tag, attribs: s.attribs, children: [] });
-            s.body = "";
-            return res;
-        } else if (x === "/") {
-            s.state = State.ELEM_SINGLE;
-        } else if (s.tag === "xml" && x === "?") {
-            s.state = State.PROC_END;
-        } else if (!isWS(x)) {
-            return unexpected(s, x);
-        }
-    },
-
-    [State.ATTRIB_NAME]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (isTagChar(x)) {
-            s.name += x;
-        } else if (x === "=") {
-            s.state = State.ATTRIB_VAL_START;
+    [State.MAYBE_ATTRIB]: (state, ch) => {
+        state.pos++;
+        if (isTagChar(ch)) {
+            state.state = State.ATTRIB_NAME;
+            state.name = ch;
+            state.val = "";
         } else {
-            return unexpected(s, x);
-        }
-    },
-
-    [State.ATTRIB_VAL_START]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (x === "\"" || x === "'") {
-            s.state = State.ATTRIB_VALUE;
-            s.quote = x;
-        } else {
-            return unexpected(s, x);
-        }
-    },
-
-    [State.ATTRIB_VALUE]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (x === s.quote) {
-            s.attribs[s.name] = s.val;
-            s.state = State.MAYBE_ATTRIB;
-        } else {
-            s.val += x;
-        }
-    },
-
-    [State.MAYBE_INSTRUCTION]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (x === "-") {
-            s.state = State.COMMENT;
-        } else if (x === "D") {
-            s.state = State.DOCTYPE;
-            s.phase = 1;
-            s.body = "";
-        } else {
-            return unexpected(s, x);
-        }
-    },
-
-    [State.COMMENT]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (x === "-") {
-            s.state = State.COMMENT_BODY;
-            s.body = "";
-        } else {
-            return unexpected(s, x);
-        }
-    },
-
-    [State.COMMENT_BODY]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (x === ">") {
-            const n = s.body.length;
-            if (s.body.substr(n - 2) !== "--") {
-                return unexpected(s, x);
-            }
-            s.state = State.WAIT;
-            return { id: "comment", value: s.body.substr(0, n - 2) };
-        } else {
-            s.body += x;
-        }
-    },
-
-    [State.DOCTYPE]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (s.phase < 8) {
-            if (x === "DOCTYPE "[s.phase]) {
-                s.phase++;
+            if (state.isProc) {
+                if (ch === "?") {
+                    state.state = State.PROC_END;
+                } else if (!isWS(ch)) {
+                    return unexpected(state, ch);
+                }
             } else {
-                return unexpected(s, x);
+                if (ch === ">") {
+                    const res = { type: Type.ELEM_START, tag: state.tag, attribs: state.attribs };
+                    state.state = State.ELEM_BODY;
+                    state.scope.push({ tag: state.tag, attribs: state.attribs, children: [] });
+                    state.body = "";
+                    return res;
+                } else if (ch === "/") {
+                    state.state = State.ELEM_SINGLE;
+                } else if (!isWS(ch)) {
+                    return unexpected(state, ch);
+                }
             }
-        } else if (x === ">") {
-            s.state = State.WAIT;
-            return { id: "doctype", value: s.body.trim() };
-        } else {
-            s.body += x;
         }
     },
 
-    [State.PROC_DECL]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (x === "xml "[s.phase]) {
-            s.phase++;
-            if (s.phase == 4) {
-                s.state = State.MAYBE_ATTRIB;
-                s.tag = "xml";
-                s.attribs = {};
-            }
+    [State.ATTRIB_NAME]: (state, ch) => {
+        state.pos++;
+        if (isTagChar(ch)) {
+            state.name += ch;
+        } else if (ch === "=") {
+            state.state = State.ATTRIB_VAL_START;
         } else {
-            return unexpected(s, x);
+            return unexpected(state, ch);
         }
     },
 
-    [State.PROC_END]: (s: ParseState, x: string) => {
-        s.pos++;
-        if (x === ">") {
-            s.state = State.WAIT;
-            return { id: "proc", value: { tag: s.tag, attribs: s.attribs } };
+    [State.ATTRIB_VAL_START]: (state, ch) => {
+        state.pos++;
+        if (ch === "\"" || ch === "'") {
+            state.state = State.ATTRIB_VALUE;
+            state.quote = ch;
         } else {
-            return unexpected(s, x);
+            return unexpected(state, ch);
+        }
+    },
+
+    [State.ATTRIB_VALUE]: (state, ch) => {
+        state.pos++;
+        if (ch === state.quote) {
+            if (state.opts.entities) {
+                state.val = replaceEntities(state.val);
+            }
+            state.attribs[state.name] = state.val;
+            state.state = State.MAYBE_ATTRIB;
+        } else {
+            state.val += ch;
+        }
+    },
+
+    [State.MAYBE_INSTRUCTION]: (state, ch) => {
+        state.pos++;
+        if (ch === "-") {
+            state.state = State.COMMENT;
+        } else if (ch === "D") {
+            state.state = State.DOCTYPE;
+            state.phase = 1;
+            state.body = "";
+        } else {
+            return unexpected(state, ch);
+        }
+    },
+
+    [State.COMMENT]: (state, ch) => {
+        state.pos++;
+        if (ch === "-") {
+            state.state = State.COMMENT_BODY;
+            state.body = "";
+        } else {
+            return unexpected(state, ch);
+        }
+    },
+
+    [State.COMMENT_BODY]: (state, ch) => {
+        state.pos++;
+        if (ch === ">") {
+            const n = state.body.length;
+            if (state.body.substr(n - 2) !== "--") {
+                return unexpected(state, ch);
+            }
+            state.state = State.WAIT;
+            return { type: Type.COMMENT, body: state.body.substr(0, n - 2) };
+        } else {
+            state.body += ch;
+        }
+    },
+
+    [State.DOCTYPE]: (state, ch) => {
+        state.pos++;
+        if (state.phase < 8) {
+            if (ch === "DOCTYPE "[state.phase]) {
+                state.phase++;
+            } else {
+                return unexpected(state, ch);
+            }
+        } else if (ch === ">") {
+            state.state = State.WAIT;
+            return { type: Type.DOCTYPE, body: state.body.trim() };
+        } else {
+            state.body += ch;
+        }
+    },
+
+    [State.PROC_DECL]: (state, ch) => {
+        state.pos++;
+        if (isTagChar(ch)) {
+            state.tag += ch;
+        } else if (isWS(ch)) {
+            state.state = State.MAYBE_ATTRIB;
+            state.isProc = true;
+            state.attribs = {};
+        } else {
+            return unexpected(state, ch);
+        }
+    },
+
+    [State.PROC_END]: (state, ch) => {
+        state.pos++;
+        if (ch === ">") {
+            state.state = State.WAIT;
+            state.isProc = false;
+            return { type: Type.PROC, tag: state.tag, attribs: state.attribs };
+        } else {
+            return unexpected(state, ch);
         }
     },
 };
