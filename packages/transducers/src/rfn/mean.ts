thi-ng/umbrella

commit 095e6ef332eb4183bce29ff5bc89169aa9ded8a8

refactor(transducers): add reducer() helper, update existing reducers

packages/transducers/src/rfn/mean.ts
@@ -1,10 +1,7 @@
 import { Reducer } from "../api";
+import { reducer } from "../reduce";
 
 export function mean(): Reducer<number, number> {
     let n = 0;
-    return [
-        () => 0,
-        (acc) => acc / n,
-        (acc, x) => (n++ , acc + x),
-    ];
+    return reducer(() => 0, (acc, x) => (n++ , acc + x));
 }

packages/transducers/src/reduce.ts
@@ -1,7 +1,7 @@
 import { illegalArity } from "@thi.ng/errors/illegal-arity";
 
 import { Reducer } from "./api";
-import { isReduced, unreduced } from "./reduced";
+import { isReduced, unreduced, Reduced } from "./reduced";
 
 export function reduce<A, B>(rfn: Reducer<A, B>, xs: Iterable<B>): A;
 export function reduce<A, B>(rfn: Reducer<A, B>, acc: A, xs: Iterable<B>): A;
@@ -28,4 +28,16 @@ export function reduce<A, B>(...args: any[]): A {
         }
     }
     return unreduced(complete(acc));
+}
+
+/**
+ * Convenience helper for building a full `Reducer` using the identity
+ * function (i.e. `(x) => x`) as completion step (true for 90% of all
+ * bundled transducers).
+ *
+ * @param init init step of reducer
+ * @param rfn reduction step of reducer
+ */
+export function reducer<A, B>(init: () => A, rfn: (acc: A, x: B) => A | Reduced<A>) {
+    return <Reducer<A, B>>[init, (acc) => acc, rfn];
 }

packages/transducers/src/rfn/add.ts
@@ -1,9 +1,6 @@
 import { Reducer } from "../api";
+import { reducer } from "../reduce";
 
 export function add(): Reducer<number, number> {
-    return [
-        () => 0,
-        (acc) => acc,
-        (acc, x) => acc + x,
-    ];
+    return reducer(() => 0, (acc, x) => acc + x);
 }

packages/transducers/src/rfn/assoc-map.ts
@@ -1,9 +1,6 @@
 import { Reducer } from "../api";
+import { reducer } from "../reduce";
 
 export function assocMap<A, B>(): Reducer<Map<A, B>, [A, B]> {
-    return [
-        () => new Map(),
-        (acc) => acc,
-        (acc, [k, v]) => acc.set(k, v),
-    ];
+    return reducer(() => new Map(), (acc, [k, v]) => acc.set(k, v));
 }

packages/transducers/src/rfn/assoc-obj.ts
@@ -1,11 +1,8 @@
 import { IObjectOf } from "@thi.ng/api/api";
 
 import { Reducer } from "../api";
+import { reducer } from "../reduce";
 
 export function assocObj<T>(): Reducer<IObjectOf<T>, [PropertyKey, T]> {
-    return [
-        () => <any>new Object(),
-        (acc) => acc,
-        (acc, [k, v]) => (acc[k] = v, acc),
-    ];
+    return reducer(() => <any>new Object(), (acc, [k, v]) => (acc[k] = v, acc));
 }

packages/transducers/src/rfn/conj.ts
@@ -1,9 +1,6 @@
 import { Reducer } from "../api";
+import { reducer } from "../reduce";
 
 export function conj<T>(): Reducer<Set<T>, T> {
-    return [
-        () => new Set(),
-        (acc) => acc,
-        (acc, x) => acc.add(x),
-    ];
+    return reducer(() => new Set(), (acc, x) => acc.add(x));
 }

packages/transducers/src/rfn/count.ts
@@ -1,9 +1,6 @@
 import { Reducer } from "../api";
+import { reducer } from "../reduce";
 
 export function count(offset = 0, step = 1): Reducer<number, any> {
-    return [
-        () => offset,
-        (acc) => acc,
-        (acc, _) => acc + step,
-    ];
+    return reducer(() => offset, (acc, _) => acc + step);
 }

packages/transducers/src/rfn/every.ts
@@ -1,14 +1,14 @@
 import { Predicate } from "@thi.ng/api/api";
 
 import { Reducer } from "../api";
+import { reducer } from "../reduce";
 import { reduced } from "../reduced";
 
 export function every<T>(pred?: Predicate<T>): Reducer<boolean, T> {
-    return [
+    return reducer(
         () => true,
-        (acc) => acc,
         pred ?
             (acc, x) => (pred(x) ? acc : reduced(false)) :
             (acc, x) => (x ? acc : reduced(false))
-    ];
+    );
 }

commit 03543ee96e500189ac8581105f1f0557d1a38ece

fix(transducers): revert mean() from regression introduced in 095e6ef

packages/transducers/src/rfn/mean.ts
@@ -1,7 +1,10 @@
 import { Reducer } from "../api";
-import { reducer } from "../reduce";
 
 export function mean(): Reducer<number, number> {
     let n = 0;
-    return reducer(() => 0, (acc, x) => (n++ , acc + x));
+    return [
+        () => 0,
+        (acc) => acc / n,
+        (acc, x) => (n++ , acc + x),
+    ];
 }