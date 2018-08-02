69d25c8cdeb5d6ccf9d94d47a5662934e3b52369
Karsten Schmidt <k@postspectacular.com>
2018-08-02 21:41:07 +0100

    refactor(examples): update transducers-hdom example

--- examples/transducers-hdom/src/index.ts
+++ examples/transducers-hdom/src/index.ts
@@ -1,29 +1,47 @@
-import * as rs from "@thi.ng/rstream";
-import * as tx from "@thi.ng/transducers";
+import { fromInterval } from "@thi.ng/rstream/from/interval";
+import { Stream } from "@thi.ng/rstream/stream";
+import { sync } from "@thi.ng/rstream/stream-sync";
 import { updateUI } from "@thi.ng/transducers-hdom";
+import { count } from "@thi.ng/transducers/rfn/count";
+import { map } from "@thi.ng/transducers/xform/map";
+import { scan } from "@thi.ng/transducers/xform/scan";
 
 // root component function
+// (using Tachyons CSS classes for styling)
 const app = ({ ticks, clicks }) =>
-    ["div",
-        `${ticks} ticks and `,
-        ["a", { href: "#", onclick: () => clickStream.next(0) }, `${clicks} clicks`]
+    ["div.vh-100.dt.w-100.bg-dark-pink.sans-serif",
+        ["div.dtc.v-mid.tc.white.ph3.ph4-l",
+            ["h1.f6.f2-m.f-subheadline-l.fw1.tc",
+                `${ticks} ticks & `,
+                ["a.link.white.bb.bw1",
+                    { href: "#", onclick: () => clickStream.next(0) },
+                    `${clicks} clicks`]],
+            ["div",
+                ["a.link.white",
+                    { href: "https://github.com/thi-ng/umbrella/tree/master/examples/transducers-hdom/" },
+                    "Source code"]]
+        ]
     ];
 
 // click stream (click counter)
-const clickStream = new rs.Stream().transform(tx.scan(tx.count(-1)));
+const clickStream = new Stream().transform(scan(count(-1)));
 
 // stream combinator
-rs.sync({
+// waits until all inputs have produced at least one value,
+// then updates whenever any input has changed
+sync({
+    // streams to synchronize
     src: {
-        ticks: rs.fromInterval(1000),
+        ticks: fromInterval(1000),
         clicks: clickStream,
     },
+    // only synchronize at first
     reset: false,
 }).transform(
     // transform into hdom component
-    tx.map(app),
-    // apply as DOM
-    updateUI(document.body)
+    map(app),
+    // apply hdom tree to real DOM
+    updateUI("app")
 );
 
 // kick off
