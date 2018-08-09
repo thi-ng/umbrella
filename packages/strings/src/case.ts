653a175a636b5d1d4461fcea65b18766be57d7a2
Karsten Schmidt <k@postspectacular.com>
2018-08-09 17:49:32 +0100

    feat(strings): add case converters
    - camel / kebab / snake

--- packages/strings/src/case.ts
+++ packages/strings/src/case.ts
@@ -23,3 +23,40 @@ export const lower: Stringer<string> =
  */
 export const capitalize: Stringer<string> =
     (x: string) => x[0].toUpperCase() + x.substr(1);
+
+/**
+ * Converts a CamelCase string into kebab case, with optional custom
+ * delimiter (`-` by default).
+ *
+ * ```
+ * kebab("FooBar23Baz");
+ * // "foo-bar23-baz"
+ * ```
+ *
+ * @param x
+ * @param delim
+ */
+export const kebab: Stringer<string> =
+    (x: string, delim = "-") =>
+        lower(x.replace(/(?<=\w)(?=[A-Z])/g, delim));
+
+/**
+ * Short for `kebab` using `_` as delimiter.
+ *
+ * @param x
+ */
+export const snake = (x: string) => kebab(x, "_");
+
+/**
+ * Converts a kebab-case or snake_case string into CamelCase. Uses `-`
+ * as default delimiter.
+ *
+ * @param x
+ * @param delim
+ */
+export const camel: Stringer<string> =
+    (x: string, delim = "-") =>
+        lower(x).replace(
+            new RegExp(`\\${delim}+(\\w)`, "g"),
+            (_, c) => upper(c)
+        );
