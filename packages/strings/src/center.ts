5209c42648cae5a764f9fef027a6c38ea2565d11
Karsten Schmidt <k@postspectacular.com>
2018-08-09 17:48:51 +0100

    fix(strings): buffer length (for null inputs) (`center()`)
    - also truncate oversized inputs

--- packages/strings/src/center.ts
+++ packages/strings/src/center.ts
@@ -2,20 +2,35 @@ import { memoizeJ } from "@thi.ng/memoize/memoizej";
 
 import { Stringer } from "./api";
 import { repeat } from "./repeat";
+import { truncate } from "./truncate";
 
 /**
- * @param n target length
+ * Returns stringer which pads given input with `ch` (default: space) on
+ * both sides and returns fixed width string of given `lineWidth`.
+ * Returns string of only pad characters for any `null` or `undefined`
+ * values. If the string version of an input is > `lineWidth`, no
+ * centering is performed, but the string will be truncated to
+ * `lineWidth`.
+ *
+ * Note: The padding string can contain multiple characters.
+ *
+ * ```
+ * center(20, "<>")(wrap(" ")("thi.ng"))
+ * // "<><><> thi.ng <><><>"
+ * ```
+ *
+ * @param lineWidth target length
  * @param ch pad character(s)
  */
-export const center: (n: number, ch?: string | number) => Stringer<any> =
+export const center: (lineWidth: number, ch?: string | number) => Stringer<any> =
     memoizeJ<number, string, Stringer<any>>((n, ch = " ") => {
-        const buf = repeat(ch, ((n + 1) & ~1) / 2);
+        const buf = repeat(ch, n);
         return (x: any) => {
             if (x == null) return buf;
             x = x.toString();
             const r = (n - x.length) / 2;
             return x.length < n ?
                 buf.substr(0, r) + x + buf.substr(0, r + ((n & 1) === (x.length & 1) ? 0 : 1)) :
-                x;
+                truncate(n)(x);
         }
     });
