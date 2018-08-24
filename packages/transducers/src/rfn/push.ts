832e57f1fa6042330eb857b2d1c263b9dd3442a5
Karsten Schmidt <k@postspectacular.com>
2018-08-24 15:25:07 +0100

    fix(transducers): copy&paste error (push)

--- packages/transducers/src/rfn/push.ts
+++ packages/transducers/src/rfn/push.ts
@@ -1,8 +1,8 @@
 import { Reducer } from "../api";
 import { reducer } from "../reduce";
 
-export function push<T>(): Reducer<Set<T>, T>;
-export function push<T>(xs: Iterable<T>): Set<T>;
+export function push<T>(): Reducer<T[], T>;
+export function push<T>(xs: Iterable<T>): T[];
 export function push<T>(xs?: Iterable<T>): any {
     return xs ?
         [...xs] :
