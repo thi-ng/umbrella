c28591050c117426c1ed648605582702cf86ed1e
Karsten Schmidt <k@postspectacular.com>
2018-09-13 19:37:03 +0100

    perf(checks): update isPlainObject()
    - cache empty object prototype

--- packages/checks/src/is-plain-object.ts
+++ packages/checks/src/is-plain-object.ts
@@ -1,3 +1,5 @@
+const OBJP = Object.getPrototypeOf({});
+
 /**
  * Similar to `isObject()`, but also checks if prototype is that of
  * `Object` (or `null`).
@@ -7,5 +9,5 @@
 export function isPlainObject(x: any): x is object {
     let proto;
     return Object.prototype.toString.call(x) === "[object Object]" &&
-        (proto = Object.getPrototypeOf(x), proto === null || proto === Object.getPrototypeOf({}));
+        (proto = Object.getPrototypeOf(x), proto === null || proto === OBJP);
 }
