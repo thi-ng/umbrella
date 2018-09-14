7a6cf51999bf0c96980ae70911c29140f434d76d
Karsten Schmidt <k@postspectacular.com>
2018-09-14 17:00:16 +0100

    feat(hiccup-svg): re-parse alpha colors in gradients (fix for safari)

--- packages/hiccup-svg/src/gradients.ts
+++ packages/hiccup-svg/src/gradients.ts
@@ -1,14 +1,26 @@
 import { Vec2Like, GradientStop } from "./api";
 import { ff } from "./format";
 
+const RE_ALPHA_COLOR = /(rgb|hsl)a\(([a-z0-9.-]+),([0-9.%]+),([0-9.%]+),([0-9.]+)\)/;
+
 const gradient = (type: string, attribs: any, stops: GradientStop[]): any[] =>
     [
         type,
         attribs,
-        ...stops.map(
-            ([offset, col]) => ["stop", { offset, "stop-color": col }]
-        )
+        ...stops.map(gradientStop)
     ];
+
+const gradientStop = ([offset, col]: GradientStop) => {
+    // use stop-opacity attrib for safari compatibility
+    // https://stackoverflow.com/a/26220870/294515
+    let opacity: string;
+    const parts = RE_ALPHA_COLOR.exec(col);
+    if (parts) {
+        col = `${parts[1]}(${parts[2]},${parts[3]},${parts[4]})`;
+        opacity = parts[5];
+    }
+    return ["stop", { offset, "stop-color": col, "stop-opacity": opacity }];
+};
 
 export const linearGradient = (
     id: string,
