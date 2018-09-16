3b6283269c83041595e042d45194b6795de555e6
Karsten Schmidt <k@postspectacular.com>
2018-09-16 17:35:34 +0100

    refactor(hiccup-svg): update points(), minor fixes, add docs

--- packages/hiccup-svg/src/svg.ts
+++ packages/hiccup-svg/src/svg.ts
@@ -1,5 +1,13 @@
 import { SVG_NS, XLINK_NS } from "@thi.ng/hiccup/api";
 
+/**
+ * Defines an <svg> root element with default XML namespaces. By default
+ * currently still sets SVG version to 1.1 to support Safari and other
+ * legacy tooling.
+ *
+ * @param attribs
+ * @param body
+ */
 export const svg = (attribs: any, ...body: any[]): any[] =>
     ["svg", {
         version: "1.1",
