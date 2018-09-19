e390a54d0b3c06c10a95af564c968d8a1464d368
Karsten Schmidt <k@postspectacular.com>
2018-09-19 21:53:46 +0100

    feat(api): add `IToHiccup` interface

--- packages/api/src/api.ts
+++ packages/api/src/api.ts
@@ -412,6 +412,18 @@ export interface IImmutableStack<V, T> {
     push(x: V): T;
 }
 
+export interface IToHiccup {
+    /**
+     * Returns a thi.ng/hiccup compatible representation. The optional
+     * `ctx` arg is an arbitrary user context object passed to all
+     * hiccup components during serialization (or during DOM creation /
+     * update if used with thi.ng/hdom)
+     *
+     * @param ctx user context object
+     */
+    toHiccup(ctx?: any): any;
+}
+
 /**
  * Interface for types offering observers of internal value changes.
  * Also see `@IWatch` decorator mixin.
