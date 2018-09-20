288b68d960817e441f7b09c11ab8c20fc65f4d88
Karsten Schmidt <k@postspectacular.com>
2018-09-20 19:08:43 +0100

    feat(rstream): add trigger() generics

--- packages/rstream/src/trigger.ts
+++ packages/rstream/src/trigger.ts
@@ -2,9 +2,12 @@ import { fromIterable } from "./from/iterable";
 import { Stream } from "./stream";
 
 /**
- * Utility stream. Returns a new stream which emits a single `true`
- * value and then closes.
+ * Utility stream. Returns a new stream which emits a single value `x`
+ * (if given) and then closes. If no arg is provided, the stream emits a
+ * single `true`.
  */
-export function trigger(): Stream<boolean> {
-    return fromIterable([true]);
+export function trigger(): Stream<boolean>;
+export function trigger<T>(x: T): Stream<T>;
+export function trigger(x: any = true) {
+    return fromIterable([x]);
 }
