0e77d2c631068a2c1383de4e94236990722c4f84
Karsten Schmidt <k@postspectacular.com>
2018-12-01 23:25:00 +0000

    refactor(transducers): implement range() as class w/ IReducible impl

--- packages/transducers/src/iter/range2d.ts
+++ packages/transducers/src/iter/range2d.ts
@@ -21,8 +21,9 @@ export function* range2d(...args: number[]) {
         default:
             illegalArity(args.length);
     }
+    const rx = range(fromX, toX, stepX);
     for (let y of range(fromY, toY, stepY)) {
-        for (let x of range(fromX, toX, stepX)) {
+        for (let x of rx) {
             yield [x, y];
         }
     }
