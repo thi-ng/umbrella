046ab206136f5b60031f711e54fb2e88784a3c9d
Karsten Schmidt <k@postspectacular.com>
2018-12-12 09:23:37 +0000

    minor(hiccup): reformat

--- packages/hiccup/src/css.ts
+++ packages/hiccup/src/css.ts
@@ -1,13 +1,14 @@
 import { isFunction } from "@thi.ng/checks/is-function";
 
-export const css = (rules: any) => {
-    let css = "", v;
-    for (let r in rules) {
-        v = rules[r];
-        if (isFunction(v)) {
-            v = v(rules);
+export const css =
+    (rules: any) => {
+        let css = "", v;
+        for (let r in rules) {
+            v = rules[r];
+            if (isFunction(v)) {
+                v = v(rules);
+            }
+            v != null && (css += `${r}:${v};`);
         }
-        v != null && (css += `${r}:${v};`);
-    }
-    return css;
-};
+        return css;
+    };
