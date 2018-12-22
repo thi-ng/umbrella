8fa05c3d3222ddff6fbd5746e995d6157a172e9a
Karsten Schmidt <k@postspectacular.com>
2018-12-22 21:31:39 +0000

    feat(math): add constants

--- packages/math/src/api.ts
+++ packages/math/src/api.ts
@@ -11,4 +11,8 @@ export const RAD2DEG = 180 / PI;
 export const SQRT2 = Math.SQRT2;
 export const SQRT3 = Math.sqrt(3);
 
+export const THIRD = 1 / 3;
+export const TWO_THIRD = 2 / 3;
+export const SIXTH = 1 / 6;
+
 export let EPS = 1e-6;
