809bf9b945c13338de03a7941ffbff675bab4c7b
Karsten Schmidt <k@postspectacular.com>
2018-12-30 00:32:51 +0000

    refactor(vectors): rename dotValues*() => dotC*()

--- packages/vectors3/src/dotc.ts
+++ packages/vectors3/src/dotc.ts
@@ -1,35 +1,41 @@
 /**
- * Returns pairwise product sum of args (in order).
+ * Returns pairwise product sum of given components.
  *
  * @param a
  * @param b
  * @param c
  * @param d
  */
-export const dotValues4 =
+export const dotC4 =
     (a: number, b: number, c: number, d: number) =>
         a * b + c * d;
 
 /**
- * Returns pairwise product sum of args (in order).
+ * Returns pairwise product sum of given components.
  *
  * @param a
  * @param b
  * @param c
  * @param d
+ * @param e
+ * @param f
  */
-export const dotValues6 =
+export const dotC6 =
     (a: number, b: number, c: number, d: number, e: number, f: number) =>
         a * b + c * d + e * f;
 
 /**
- * Returns pairwise product sum of args (in order).
+ * Returns pairwise product sum of given components.
  *
  * @param a
  * @param b
  * @param c
  * @param d
+ * @param e
+ * @param f
+ * @param g
+ * @param h
  */
-export const dotValues8 =
+export const dotC8 =
     (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) =>
         a * b + c * d + e * f + g * h;
