6857f3475cf62e03480739fe0824deb7087d3ad1
Karsten Schmidt <k@postspectacular.com>
2018-12-30 00:31:43 +0000

    feat(vectors): add clamp01(), clamp11() fns

--- packages/vectors3/src/clamp.ts
+++ packages/vectors3/src/clamp.ts
@@ -1,10 +1,13 @@
-import { clamp as _clamp } from "@thi.ng/math/interval";
+import { clamp as _clamp, clamp01 as _clamp01, clamp11 as _clamp11 } from "@thi.ng/math/interval";
 import { MultiVecOpVVV, VecOpVVV } from "./api";
-import { defHofOp } from "./internal/codegen";
+import { ARGS_VVV, defHofOp } from "./internal/codegen";
+import { HOF_VVV } from "./internal/templates";
 
 export const [clamp, clamp2, clamp3, clamp4] =
-    defHofOp<MultiVecOpVVV, VecOpVVV>(
-        _clamp,
-        ([o, a, b, c]) => `${o}=op(${a},${b},${c});`,
-        "o,a,b,c"
-    );
+    defHofOp<MultiVecOpVVV, VecOpVVV>(_clamp, HOF_VVV, ARGS_VVV);
+
+export const [clamp01, clamp01_2, clamp01_3, clamp01_4] =
+    defHofOp<MultiVecOpVVV, VecOpVVV>(_clamp01, HOF_VVV, ARGS_VVV);
+
+export const [clamp11, clamp11_2, clamp11_3, clamp11_4] =
+    defHofOp<MultiVecOpVVV, VecOpVVV>(_clamp11, HOF_VVV, ARGS_VVV);
