7cf5f7c49acb47456572de489a584122f3718325
Karsten Schmidt <k@postspectacular.com>
2018-12-31 17:31:59 +0000

    refactor(geom): update subdivKernel* fns

--- packages/geom2/src/internal/subdiv-curve.ts
+++ packages/geom2/src/internal/subdiv-curve.ts
@@ -5,29 +5,29 @@ import { transduce } from "@thi.ng/transducers/transduce";
 import { indexed } from "@thi.ng/transducers/xform/indexed";
 import { mapcat } from "@thi.ng/transducers/xform/mapcat";
 import { partition } from "@thi.ng/transducers/xform/partition";
-import { addWeighted2, addWeighted3, addWeighted5 } from "@thi.ng/vectors3/add-weighted";
+import { addW2, addW3, addW5 } from "@thi.ng/vectors3/addw";
 import { ReadonlyVec, Vec } from "@thi.ng/vectors3/api";
 import { SubdivKernel } from "../api";
 
 export const subdivKernel2 =
     ([ua, ub]: number[], [va, vb]: number[]) =>
         ([a, b]: ReadonlyVec[]) => [
-            addWeighted2([], a, b, ua, ub),
-            addWeighted2([], a, b, va, vb),
+            addW2([], a, b, ua, ub),
+            addW2([], a, b, va, vb),
         ];
 
 export const subdivKernel3 =
     ([ua, ub, uc]: number[], [va, vb, vc]: number[]) =>
         ([a, b, c]: ReadonlyVec[]) => [
-            addWeighted3([], a, b, c, ua, ub, uc),
-            addWeighted3([], a, b, c, va, vb, vc),
+            addW3([], a, b, c, ua, ub, uc),
+            addW3([], a, b, c, va, vb, vc),
         ];
 
 export const subdivKernel5 =
     ([ua, ub, uc, ud, ue]: number[], [va, vb, vc, vd, ve]: number[]) =>
         ([a, b, c, d, e]: ReadonlyVec[]) => [
-            addWeighted5([], a, b, c, d, e, ua, ub, uc, ud, ue),
-            addWeighted5([], a, b, c, d, e, va, vb, vc, vd, ve),
+            addW5([], a, b, c, d, e, ua, ub, uc, ud, ue),
+            addW5([], a, b, c, d, e, va, vb, vc, vd, ve),
         ];
 
 /**
