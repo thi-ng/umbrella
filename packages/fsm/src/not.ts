a93360752c2d0a2db7f38ef06e4107792d948e0f
Karsten Schmidt <k@postspectacular.com>
2019-01-04 04:16:49 +0000

    feat(fsm): update not()

--- packages/fsm/src/not.ts
+++ packages/fsm/src/not.ts
@@ -1,17 +1,21 @@
-import { Match, RES_FAIL, RES_PARTIAL, Matcher } from "./api";
+import { Match, RES_FAIL, RES_PARTIAL, Matcher, SeqCallback } from "./api";
 import { success } from "./success";
 
-export const not =
-    <T, C, R>(match: Matcher<T, C, R>, callback?): Matcher<T, C, R> =>
-        () => {
-            let m = match();
-            return (ctx, x) => {
-                const { type } = m(ctx, x);
-                return type === Match.FAIL ?
-                    success(callback && callback(ctx)) :
-                    type !== Match.PARTIAL ?
-                        // TODO Match.FULL_NC handling?
-                        RES_FAIL :
-                        RES_PARTIAL;
-            }
-        };
+export const not = <T, C, R>(
+    match: Matcher<T, C, R>,
+    callback?: SeqCallback<T, C, R>
+): Matcher<T, C, R> =>
+    () => {
+        let m = match();
+        const buf: T[] = [];
+        return (ctx, x) => {
+            buf.push(x);
+            const { type } = m(ctx, x);
+            return type === Match.FAIL ?
+                success(callback && callback(ctx, buf)) :
+                type !== Match.PARTIAL ?
+                    // TODO Match.FULL_NC handling?
+                    RES_FAIL :
+                    RES_PARTIAL;
+        }
+    };
