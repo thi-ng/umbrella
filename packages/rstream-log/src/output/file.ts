14c00b29659ad321fc65b1c679c1ff91e8e42ea1
Karsten Schmidt <k@postspectacular.com>
2019-01-05 06:19:35 +0000

    refactor(rstream-log): use arrow fns

--- packages/rstream-log/src/output/file.ts
+++ packages/rstream-log/src/output/file.ts
@@ -2,18 +2,19 @@ import { isNode } from "@thi.ng/checks/is-node";
 import { unsupported } from "@thi.ng/errors/unsupported";
 import { ISubscriber } from "@thi.ng/rstream/api";
 
-export function writeFile(path: string): ISubscriber<string> {
-    if (isNode()) {
-        const fs = require("fs");
-        return {
-            next(msg) {
-                fs.appendFile(path, msg + "\n", (e) => {
-                    if (e) {
-                        process.stderr.write(e.message);
-                    }
-                });
-            }
-        };
-    }
-    unsupported("only available in NodeJS");
-}
+export const writeFile =
+    (path: string): ISubscriber<string> => {
+        if (isNode()) {
+            const fs = require("fs");
+            return {
+                next(msg) {
+                    fs.appendFile(path, msg + "\n", (e) => {
+                        if (e) {
+                            process.stderr.write(e.message);
+                        }
+                    });
+                }
+            };
+        }
+        unsupported("only available in NodeJS");
+    };
--- packages/rstream-log/src/xform/filter.ts
+++ packages/rstream-log/src/xform/filter.ts
@@ -4,22 +4,22 @@ import { filter } from "@thi.ng/transducers/xform/filter";
 
 import { Level, LogEntry } from "../api";
 
-export function onlyLevel(level: Level): Transducer<LogEntry, LogEntry> {
-    return filter((l) => l[0] === level);
-}
+export const onlyLevel =
+    (level: Level): Transducer<LogEntry, LogEntry> =>
+        filter((l) => l[0] === level);
 
-export function minLevel(level: Level): Transducer<LogEntry, LogEntry> {
-    return filter((l) => l[0] >= level);
-}
+export const minLevel =
+    (level: Level): Transducer<LogEntry, LogEntry> =>
+        filter((l) => l[0] >= level);
 
-export function maxLevel(level: Level): Transducer<LogEntry, LogEntry> {
-    return filter((l) => l[0] <= level);
-}
+export const maxLevel =
+    (level: Level): Transducer<LogEntry, LogEntry> =>
+        filter((l) => l[0] <= level);
 
-export function matchID(id: string | RegExp): Transducer<LogEntry, LogEntry> {
-    return filter(
-        isString(id) ?
-            (l) => l[1] === id :
-            (l) => id.test(l[1])
-    );
-}
+export const matchID =
+    (id: string | RegExp): Transducer<LogEntry, LogEntry> =>
+        filter(
+            isString(id) ?
+                (l) => l[1] === id :
+                (l) => id.test(l[1])
+        );
