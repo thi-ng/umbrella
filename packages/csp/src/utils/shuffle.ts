5d5302866bdcf62fc5b85fd7c5f33cd8bd34e29c
Karsten Schmidt <k@postspectacular.com>
2019-01-05 06:23:37 +0000

    refactor(csp): use arrow fns

--- packages/csp/src/utils/shuffle.ts
+++ packages/csp/src/utils/shuffle.ts
@@ -1,13 +1,14 @@
-export function shuffle<T>(items: T[]) {
-    let n = items.length;
-    if (n > 1) {
-        while (n > 0) {
-            const i = (Math.random() * n) | 0;
-            n--;
-            const t = items[i];
-            items[i] = items[n];
-            items[n] = t;
+export const shuffle =
+    <T>(items: T[]) => {
+        let n = items.length;
+        if (n > 1) {
+            while (n > 0) {
+                const i = (Math.random() * n) | 0;
+                n--;
+                const t = items[i];
+                items[i] = items[n];
+                items[n] = t;
+            }
         }
-    }
-    return items;
-}
+        return items;
+    };
