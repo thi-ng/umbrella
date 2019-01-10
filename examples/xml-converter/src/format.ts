bbe636f486030a4c89124bf1b85a28f6cc52b289
Karsten Schmidt <k@postspectacular.com>
2019-01-10 18:15:40 +0000

    fix(examples): add quote escaping in xml-converter

--- examples/xml-converter/src/format.ts
+++ examples/xml-converter/src/format.ts
@@ -59,12 +59,18 @@ const formatVal = (opts: FormatOpts, x: any, indent = true) =>
         x :
         isPlainObject(x) ?
             format(indent ? indentState(opts) : opts, "", x) :
-            opts.quote + x + opts.quote;
+            opts.quote + escape(opts, x) + opts.quote;
 
 // attrib key-value pair formatter w/ indentation
 const formatPair = (opts: FormatOpts, x: any, k: string) =>
     spaces(opts.indent) + formatAttrib(opts, k) + ":" + opts.ws +
     formatVal(opts, x[k], k !== "style");
+
+const escape =
+    (opts: FormatOpts, x: any) =>
+        opts.quote === "\"" ?
+            String(x).replace(/"/g, "\\\"") :
+            String(x).replace(/'/g, "\\\'");
 
 // multiple-dispatch function to format the transformed tree (hiccup
 // structure) into a more compact & legible format than produced by
