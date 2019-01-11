d5ba06a4d8974d07bfcd386491547ec93eccdeec
Karsten Schmidt <k@postspectacular.com>
2019-01-11 14:27:48 +0000

    build: remove obsolete temp package vectors2

--- packages/vectors2/src/internal/iterator.ts
+++
@@ -1,7 +0,0 @@
-import { Vec } from "../api";
-
-export function* iterator(buf: Vec, n: number, i = 0, s = 1) {
-    for (; n > 0; n-- , i += s) {
-        yield buf[i];
-    }
-}
--- packages/vectors2/test/index.ts
+++
@@ -1,6 +0,0 @@
-// import * as assert from "assert";
-// import * as v from "../src/index";
-
-describe("vectors2", () => {
-    it("tests pending");
-});
