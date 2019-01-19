46bb8730469384c28010770ae0f01d277cc717be
Karsten Schmidt <k@postspectacular.com>
2019-01-19 18:31:09 +0000

    build: remove obsolete geom2 package

--- packages/geom2/src/internal/transform.ts
+++
@@ -1,5 +0,0 @@
-import { Mat, mulV } from "@thi.ng/matrices";
-import { ReadonlyVec } from "@thi.ng/vectors3";
-
-export const transformPoints = (pts: ReadonlyVec[], mat: Mat) =>
-    pts.map((p) => mulV([], mat, p));
--- packages/geom2/src/internal/warp.ts
+++
@@ -1,6 +0,0 @@
-import { ReadonlyVec } from "@thi.ng/vectors3";
-import { IShape, mapPoint, unmapPoint } from "../api";
-
-export const warpPoints =
-    (src: ReadonlyVec[], srcBounds: IShape, dest: IShape) =>
-        src.map((p) => unmapPoint(dest, mapPoint(srcBounds, p)));
