8e2c8b5c835487865200b65cdc5340ccfe7abd5d
Karsten Schmidt <k@postspectacular.com>
2019-01-20 16:01:11 +0000

    refactor(geom): update withAttribs() return type

--- packages/geom3/src/ops/with-attribs.ts
+++ packages/geom3/src/ops/with-attribs.ts
@@ -1,7 +1,10 @@
 import { Attribs, IShape } from "../api";
 
-export const withAttribs =
-    (shape: IShape, attribs: Attribs, replace = true) => {
-        shape.attribs = replace ? attribs : { ...shape.attribs, ...attribs };
-        return shape;
-    };
+export const withAttribs = <T extends IShape>(
+    shape: T,
+    attribs: Attribs,
+    replace = true
+) => {
+    shape.attribs = replace ? attribs : { ...shape.attribs, ...attribs };
+    return shape;
+};
