2aceab97f196de1097eb3f3c12419cf00f39a9fa
Karsten Schmidt <k@postspectacular.com>
2019-01-20 03:54:22 +0000

    feat(matrices): add m22 & m23 matrix converters

---
+++ packages/matrices/src/m22-m23.ts
@@ -0,0 +1,16 @@
+import { set4 } from "@thi.ng/vectors3";
+import { MatOpM } from "./api";
+
+/**
+ * Converts M22 to M23 and writes result to `out`.
+ *
+ * @param out
+ * @param m22
+ */
+export const mat22to23: MatOpM =
+    (out, m22) => (
+        !out && (out = []),
+        set4(out, m22),
+        out[4] = out[5] = 0,
+        out
+    );
---
+++ packages/matrices/src/m23-m22.ts
@@ -0,0 +1,11 @@
+import { set4 } from "@thi.ng/vectors3";
+import { MatOpM } from "./api";
+
+/**
+ * Converts M23 to M22 and writes result to `out`.
+ *
+ * @param out
+ * @param m23
+ */
+export const mat23to22: MatOpM =
+    (out, m23) => set4(out || [], m23);
