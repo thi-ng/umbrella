b9cea8cc981702caf6d69dc46d5f01ab5af2aa5f
Karsten Schmidt <k@postspectacular.com>
2019-01-21 04:08:38 +0000

    refactor(vectors): update angleBetween*() fns, add absInner opt arg

--- packages/vectors/src/angle-between.ts
+++ packages/vectors/src/angle-between.ts
@@ -1,18 +1,23 @@
+import { absInnerAngle } from "@thi.ng/math";
 import { ReadonlyVec } from "./api";
+import { cross2 } from "./cross";
 import { dot } from "./dot";
 import { mag } from "./mag";
-import { cross2 } from "./cross";
 
 export const angleRatio =
     (a: ReadonlyVec, b: ReadonlyVec) =>
         dot(a, b) / (mag(a) * mag(b));
 
 export const angleBetween2 =
-    (a: ReadonlyVec, b: ReadonlyVec) =>
-        Math.atan2(cross2(a, b), dot(a, b));
+    (a: ReadonlyVec, b: ReadonlyVec, absInner = false) => {
+        const t = Math.atan2(cross2(a, b), dot(a, b));
+        return absInner ? absInnerAngle(t) : t;
+    };
 
 export const angleBetween3 =
-    (a: ReadonlyVec, b: ReadonlyVec, normalize = true) =>
-        normalize ?
+    (a: ReadonlyVec, b: ReadonlyVec, normalize = true, absInner = false) => {
+        const t = normalize ?
             Math.acos(angleRatio(a, b)) :
             Math.acos(dot(a, b));
+        return absInner ? absInnerAngle(t) : t;
+    };
