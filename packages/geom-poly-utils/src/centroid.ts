5ef4c56d39152ebde0d669a16c44567b7dbc727c
Karsten Schmidt <k@postspectacular.com>
2019-01-23 17:19:48 +0000

    feat(geom-poly-utils): extract from geom as new package

---
+++ packages/geom-poly-utils/src/centroid.ts
@@ -0,0 +1,19 @@
+import { illegalArgs } from "@thi.ng/errors";
+import {
+    add,
+    divN,
+    empty,
+    ReadonlyVec,
+    Vec
+} from "@thi.ng/vectors";
+
+export const centroid =
+    (pts: ReadonlyVec[], out?: Vec) => {
+        const num = pts.length;
+        !num && illegalArgs("no points");
+        !out && (out = empty(pts[0]));
+        for (let i = num; --i >= 0;) {
+            add(out, out, pts[i]);
+        }
+        return divN(out, out, num);
+    };
---
+++ packages/geom-poly-utils/test/index.ts
@@ -0,0 +1,6 @@
+// import * as assert from "assert";
+// import * as gpu from "../src/index";
+
+describe("geom-poly-utils", () => {
+    it("tests pending");
+});
