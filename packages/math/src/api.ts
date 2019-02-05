57d4488a2596a01f3b2326e53f9709fcbc41f297
Karsten Schmidt <k@postspectacular.com>
2019-02-05 05:06:58 +0000

    feat(math): add PHI const

--- packages/math/src/api.ts
+++ packages/math/src/api.ts
@@ -8,6 +8,8 @@ export const SIXTH_PI = PI / 6;
 export const DEG2RAD = PI / 180;
 export const RAD2DEG = 180 / PI;
 
+export const PHI = (1 + Math.sqrt(5)) / 2;
+
 export const SQRT2 = Math.SQRT2;
 export const SQRT3 = Math.sqrt(3);
 
