f5c7c770e8827fdcd21bf92ef7f388e7f0ce8822
Karsten Schmidt <k@postspectacular.com>
2019-02-10 18:03:59 +0000

    feat(examples): add virtual scroller example

---
+++ examples/hdom-vscroller/README.md
@@ -0,0 +1,13 @@
+# hdom-vscroller
+
+[Live demo](http://demo.thi.ng/umbrella/hdom-vscroller/)
+
+Please refer to the [example build instructions](https://github.com/thi-ng/umbrella/wiki/Example-build-instructions) on the wiki.
+
+## Authors
+
+- Karsten Schmidt
+
+## License
+
+&copy; 2018 Karsten Schmidt // Apache Software License 2.0
---
+++ examples/hdom-vscroller/src/index.ts
@@ -0,0 +1,114 @@
+import { start } from "@thi.ng/hdom";
+import { map, mapIndexed } from "@thi.ng/transducers";
+import _commits from "./commits.json";
+import _logo from "./logo-64.png";
+import _packages from "./packages.json";
+import { virtualScroller } from "./vscroller";
+
+type Commit = [string, string, string];
+type Package = [string, string];
+
+const LOGO = _logo;
+const REPO_BASE = "https://github.com/thi-ng/umbrella/";
+const COMMITS: Commit[] = _commits;
+const PACKAGES: Package[] = _packages;
+
+let query = "";
+let filtered = COMMITS;
+let commitsOffset = 0;
+let commitsTop = 0;
+let pkgOffset = 0;
+let pkgTop = 0;
+
+
+const filterCommit =
+    (q: string) =>
+        (x: Commit) =>
+            x[2].toLowerCase().indexOf(q) >= 0 ||
+            x[1].indexOf(q) >= 0;
+
+const setQuery =
+    (e) => {
+        try {
+            query = e.target.value;
+            filtered = COMMITS.filter(filterCommit(query));
+            commitsOffset = 0;
+            commitsTop = 0;
+        } catch (e) { }
+    };
+
+const queryFilter =
+    (_, query, items) =>
+        ["div.pv2.ph3.bg-black.white",
+            "Filter: ",
+            ["input",
+                { type: "text", oninput: setQuery, value: query }],
+            ` (${items.length})`
+        ];
+
+const repoLink =
+    (_, sha, body) =>
+        ["a.link.blue",
+            { href: `${REPO_BASE}commit/${sha}` },
+            body];
+
+const packageLink =
+    (_, name) =>
+        ["a.link.blue",
+            { href: `${REPO_BASE}tree/master/packages/${name.substr(8)}` },
+            name];
+
+const commit =
+    (i: number, [sha, date, msg]: Commit) =>
+        [`div.f7.pv2.bg-${i & 1 ? "light-gray" : "transparent"}`,
+        ["div.dib.w-30.w-20-m.w-10-l.ph3", date],
+        ["div.dib.w-70.w-80-m.w-90-l.ph3.overflow-x-hidden.nowrap", [repoLink, sha, msg]]
+        ];
+
+const pkgSummary =
+    ([name, desc]: Package) =>
+        ["div.flex.items-center.lh-copy.pa3.ph0-l.bb.b--black-10",
+            { style: { height: "96px" } },
+            ["img.w2.h2", { src: LOGO }],
+            ["div.pl3.flex-auto.f7",
+                ["h3.ma0", [packageLink, name]],
+                desc
+            ]
+        ];
+
+const app = () =>
+    ["div.sans-serif.pa2",
+        [queryFilter, query, filtered],
+        virtualScroller({
+            onscroll: (_top, _offset) => {
+                commitsTop = _top;
+                commitsOffset = _offset;
+            },
+            start: commitsOffset,
+            top: commitsTop,
+            numVisible: 10,
+            numItems: filtered.length,
+            itemHeight: 31,
+            items: mapIndexed(commit, 0, filtered)
+        }),
+        ["h3.ph2", "Packages"],
+        virtualScroller({
+            onscroll: (_top, _offset) => {
+                pkgTop = _top;
+                pkgOffset = _offset;
+            },
+            start: pkgOffset,
+            top: pkgTop,
+            numVisible: 2,
+            numItems: PACKAGES.length,
+            itemHeight: 96,
+            items: map(pkgSummary, PACKAGES)
+        })
+    ];
+
+const cancel = start(app)
+
+if (process.env.NODE_ENV !== "production") {
+    const hot = (<any>module).hot;
+    hot && hot.dispose(cancel);
+}
