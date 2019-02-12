e9d57fcd720a1b44963bd75e4abf066fd9fa76b0
Karsten Schmidt <k@postspectacular.com>
2019-02-12 17:41:53 +0000

    fix(compose): add varargs override for jux(),  add tests

--- packages/compose/src/juxt.ts
+++ packages/compose/src/juxt.ts
@@ -8,7 +8,8 @@ export function juxt<T, A, B, C, D, E>(a: Fn<T, A>, b: Fn<T, B>, c: Fn<T, C>, d:
 export function juxt<T, A, B, C, D, E, F>(a: Fn<T, A>, b: Fn<T, B>, c: Fn<T, C>, d: Fn<T, D>, e: Fn<T, E>, f: Fn<T, F>): Fn<T, [A, B, C, D, E, F]>;
 export function juxt<T, A, B, C, D, E, F, G>(a: Fn<T, A>, b: Fn<T, B>, c: Fn<T, C>, d: Fn<T, D>, e: Fn<T, E>, f: Fn<T, F>, g: Fn<T, G>): Fn<T, [A, B, C, D, E, F, G]>;
 export function juxt<T, A, B, C, D, E, F, G, H>(a: Fn<T, A>, b: Fn<T, B>, c: Fn<T, C>, d: Fn<T, D>, e: Fn<T, E>, f: Fn<T, F>, g: Fn<T, G>, h: Fn<T, H>): Fn<T, [A, B, C, D, E, F, G, H]>;
-export function juxt(...fns: Fn<any, any>[]) {
+export function juxt<T, A, B, C, D, E, F, G, H>(a: Fn<T, A>, b: Fn<T, B>, c: Fn<T, C>, d: Fn<T, D>, e: Fn<T, E>, f: Fn<T, F>, g: Fn<T, G>, h: Fn<T, H>, ...xs: Fn<T, any>[]): Fn<T, any[]>;
+export function juxt<T>(...fns: Fn<any, any>[]) {
     const [a, b, c, d, e, f, g, h] = fns;
     switch (fns.length) {
         case 1:
@@ -28,7 +29,7 @@ export function juxt(...fns: Fn<any, any>[]) {
         case 8:
             return (x) => [a(x), b(x), c(x), d(x), e(x), f(x), g(x), h(x)];
         default:
-            return (x: any) => {
+            return (x: T) => {
                 let res = new Array(fns.length);
                 for (let i = fns.length; --i >= 0;) {
                     res[i] = fns[i](x);
---
+++ packages/compose/test/delay.ts
@@ -0,0 +1,15 @@
+import { delay } from "../src";
+
+import * as assert from "assert";
+
+describe("delay", () => {
+
+    it("only executes once", () => {
+        let num = 0;
+        const a = delay(() => ++num);
+        assert(!a.isRealized());
+        assert.equal(a.deref(), 1);
+        assert.equal(a.deref(), 1);
+        assert(a.isRealized());
+    });
+});
