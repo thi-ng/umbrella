b01abaa5fbe4dff191d3b648902c188dfe7e52e5
Karsten Schmidt <k@postspectacular.com>
2019-02-14 11:40:01 +0000

    refactor(arrays): update arg order, fix shuffle()

--- packages/arrays/src/shuffle.ts
+++ packages/arrays/src/shuffle.ts
@@ -1,8 +1,19 @@
 import { IRandom, SYSTEM } from "@thi.ng/random";
 
+/**
+ * Shuffles the first `n` items of given array, using Fisher-yates and
+ * optional `rnd` PRNG. If `n` is `undefined`, the entire array will be
+ * shuffled.
+ *
+ *
+ * @param buf
+ * @param n
+ * @param rnd
+ */
 export const shuffle =
     (buf: any[], n = buf.length, rnd: IRandom = SYSTEM) => {
-        const l = buf.length;
+        n = Math.min(n, buf.length);
+        const l = n;
         if (l > 1) {
             n = Math.min(n, l);
             while (--n >= 0) {
