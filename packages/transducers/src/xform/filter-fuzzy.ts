f95ab38cfeaad74086a7656f0363a98094169e07
Karsten Schmidt <k@postspectacular.com>
2019-02-14 11:41:23 +0000

    refactor(transducers): update thi.ng/arrays call sites

--- packages/transducers/src/xform/filter-fuzzy.ts
+++ packages/transducers/src/xform/filter-fuzzy.ts
@@ -35,6 +35,6 @@ export function filterFuzzy<A, B>(...args: any[]): any {
     const query: ArrayLike<B> = args[0];
     const { key, equiv } = <FilterFuzzyOpts<A, B>>(args[1] || {});
     return filter(
-        (x: A) => fuzzyMatch(query, key != null ? key(x) : <any>x, equiv)
+        (x: A) => fuzzyMatch(key != null ? key(x) : <any>x, query, equiv)
     );
 }
