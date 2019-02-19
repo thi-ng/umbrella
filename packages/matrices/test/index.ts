6617a6e86aac0386a928c006336abfd4a7af46b8
Karsten Schmidt <k@postspectacular.com>
2019-02-19 10:40:27 +0000

    test(matrices): add identity tests

--- packages/matrices/test/index.ts
+++ packages/matrices/test/index.ts
@@ -1,6 +1,21 @@
-// import * as assert from "assert";
-// import * as m from "../src/index";
+import * as assert from "assert";
+import * as m from "../src/index";
+import { range } from "@thi.ng/transducers";
 
 describe("matrices", () => {
-    it("tests pending");
+
+    it("identity (fixed)", () => {
+        assert.deepEqual(m.identity22([]), m.IDENT22, "m22");
+        assert.deepEqual(m.identity23([]), m.IDENT23, "m23");
+        assert.deepEqual(m.identity33([]), m.IDENT33, "m33");
+        assert.deepEqual(m.identity44([]), m.IDENT44, "m44");
+    });
+
+    it("identity (dyn)", () => {
+        assert.deepEqual(m.identity([...range(4)]), m.IDENT22, "m22");
+        assert.deepEqual(m.identity([...range(6)]), m.IDENT23, "m23");
+        assert.deepEqual(m.identity([...range(9)]), m.IDENT33, "m33");
+        assert.deepEqual(m.identity([...range(16)]), m.IDENT44, "m44");
+    });
+
 });
