da9ff03b328a56ab92a3b5d1962f293a9fcecbe7
Karsten Schmidt <k@postspectacular.com>
2019-02-20 15:05:22 +0000

    feat(fsm): add optional FSM ctx update handler, add iterator support

--- packages/fsm/src/fsm.ts
+++ packages/fsm/src/fsm.ts
@@ -5,76 +5,94 @@ import {
     isReduced,
     Reducer,
     Transducer,
-    unreduced
+    unreduced,
+    iterator
 } from "@thi.ng/transducers";
 import { Match, Matcher } from "./api";
 
 /**
- * Finite-state machine transducer w/ support for single lookahead
- * value. Takes an object of `states` and their matchers, an arbitrary
- * context object and an `initial` state ID.
+ * Finite-state machine transducer / iterator with support for single
+ * lookahead value. Takes an object of `states` and their matchers, an
+ * arbitrary context object and an `initial` state ID.
  *
  * The returned transducer consumes inputs of type `T` and produces
  * results of type `R`. The results are produced by callbacks of the
  * given state matchers. Each can produce any number of values. If a
  * callback returns a result wrapped w/ `reduced()`, the FSM causes
- * early termination of the overall transducer pipeline.
+ * early termination of the overall transducer pipeline. Failed state
+ * callbacks too can produce outputs, but will afterwards terminate the
+ * FSM.
+ *
+ * An `IllegalStateError` will be thrown if a transition to an undefined
+ * state ID occurs.
+ *
+ * The optional `update` function will be invoked for each input prior
+ * to executing the currently active state matcher. It is intended to
+ * update the context object (e.g. to update input location info for
+ * generating error messages).
+ *
+ * If the optional `src` iterable is given, the function returns a
+ * transforming iterator of the FSM results.
  *
  * @param states
  * @param ctx
  * @param initialState
+ * @param update
+ * @param src
  */
-export const fsm = <T, C, R>(
-    states: IObjectOf<Matcher<T, C, R>>,
-    ctx: C,
-    initialState: string | number = "start"
-): Transducer<T, R> =>
-    ([init, complete, reduce]: Reducer<any, R>) => {
-        let currID = initialState;
-        let curr = states[initialState] ?
-            states[initialState]() :
-            illegalArgs(`invalid initial state: ${initialState}`);
-        return [
-            init,
-            complete,
-            (acc, x) => {
-                while (true) {
-                    const { type, body } = curr(ctx, x);
-                    const res = body && body[1];
-                    if (type >= Match.FULL) {
-                        const next = body && states[body[0]];
-                        if (next) {
-                            currID = body[0];
-                            curr = next();
-                        } else {
-                            illegalState(`unknown tx: ${currID} -> ${body && body[0]}`);
-                        }
-                        if (res) {
-                            for (let y of unreduced(res)) {
-                                acc = reduce(acc, y);
-                                if (isReduced(acc)) {
-                                    break;
+export function fsm<T, C, R>(states: IObjectOf<Matcher<T, C, R>>, ctx: C, initial: string | number, update?: (ctx: C, x: T) => void): Transducer<T, R>;
+export function fsm<T, C, R>(states: IObjectOf<Matcher<T, C, R>>, ctx: C, initial: string | number, update?: (ctx: C, x: T) => void, src?: Iterable<T>): IterableIterator<R>;
+export function fsm<T, C, R>(states: IObjectOf<Matcher<T, C, R>>, ctx: C, initial: string | number = "start", update?: (ctx: C, x: T) => void, src?: Iterable<T>) {
+    return src ?
+        iterator(fsm(states, ctx, initial, update), src) :
+        ([init, complete, reduce]: Reducer<any, R>) => {
+            let currID = initial;
+            let curr = states[initial] ?
+                states[initial]() :
+                illegalArgs(`invalid initial state: ${initial}`);
+            return [
+                init,
+                complete,
+                (acc, x) => {
+                    update && update(ctx, x);
+                    while (true) {
+                        const { type, body } = curr(ctx, x);
+                        const res = body && body[1];
+                        if (type >= Match.FULL) {
+                            const next = body && states[body[0]];
+                            if (next) {
+                                currID = body[0];
+                                curr = next();
+                            } else {
+                                illegalState(`unknown tx: ${currID} -> ${body && body[0]}`);
+                            }
+                            if (res) {
+                                for (let y of unreduced(res)) {
+                                    acc = reduce(acc, y);
+                                    if (isReduced(acc)) {
+                                        break;
+                                    }
+                                }
+                                isReduced(res) && (acc = ensureReduced(acc));
+                            }
+                            if (type === Match.FULL_NC && !isReduced(acc)) {
+                                continue;
+                            }
+                        } else if (type === Match.FAIL) {
+                            if (res) {
+                                for (let y of unreduced(res)) {
+                                    acc = reduce(acc, y);
+                                    if (isReduced(acc)) {
+                                        break;
+                                    }
                                 }
                             }
-                            isReduced(res) && (acc = ensureReduced(acc));
+                            return ensureReduced(acc);
                         }
-                        if (type === Match.FULL_NC && !isReduced(acc)) {
-                            continue;
-                        }
-                    } else if (type === Match.FAIL) {
-                        if (res) {
-                            for (let y of unreduced(res)) {
-                                acc = reduce(acc, y);
-                                if (isReduced(acc)) {
-                                    break;
-                                }
-                            }
-                        }
-                        return ensureReduced(acc);
+                        break;
                     }
-                    break;
+                    return acc;
                 }
-                return acc;
-            }
-        ];
-    };
+            ];
+        };
+}
