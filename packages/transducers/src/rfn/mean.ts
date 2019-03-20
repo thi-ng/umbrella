f644ecd26be023baf2a783e54e21a9258f3ffa11
Karsten Schmidt <k@postspectacular.com>
2019-03-20 17:12:31 +0000

    fix(transducers): update mean() completion step to avoid div by zero

--- packages/transducers/src/rfn/mean.ts
+++ packages/transducers/src/rfn/mean.ts
@@ -1,11 +1,19 @@
 import { Reducer } from "../api";
 import { reduce } from "../reduce";
 
+/**
+ * Reducer computing mean of received inputs. Returns 0 if no inputs
+ * were processed.
+ */
 export function mean(): Reducer<number, number>;
 export function mean(xs: Iterable<number>): number;
 export function mean(xs?: Iterable<number>): any {
     let n = 0;
     return xs
         ? reduce(mean(), xs)
-        : [() => 0, (acc) => acc / n, (acc, x) => (n++, acc + x)];
+        : [
+              () => 0,
+              (acc) => (n > 1 ? acc / n : acc),
+              (acc, x) => (n++, acc + x)
+          ];
 }
