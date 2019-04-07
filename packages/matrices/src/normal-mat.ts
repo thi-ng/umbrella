d54f74629ff33a41721360eda925f9fdfcc08df0
Karsten Schmidt <k@postspectacular.com>
2019-04-07 02:29:45 +0100

    feat(matrices): rename normal44 => normal33, add new normal44 (w/ M44 result)

--- packages/matrices/src/normal-mat.ts
+++ packages/matrices/src/normal-mat.ts
@@ -1,14 +1,23 @@
 import { MatOpM } from "./api";
+import { invert33, invert44 } from "./invert";
 import { mat44to33 } from "./m44-m33";
-import { invert33 } from "./invert";
-import { transpose33 } from "./transpose";
+import { transpose33, transpose44 } from "./transpose";
 
 /**
  * Converts given M44 to a M33 normal matrix, i.e. the transposed
- * inverted version of its upper-left 3x3 region.
+ * inverse of its upper-left 3x3 region.
  *
  * @param out
  * @param m
  */
-export const normal44: MatOpM = (out, m) =>
+export const normal33: MatOpM = (out, m) =>
     transpose33(null, invert33(null, mat44to33(out, m)));
+
+/**
+ * Converts given M44 to a M44 normal matrix, i.e. the transposed
+ * inverse.
+ *
+ * @param out
+ * @param m
+ */
+export const normal44: MatOpM = (out, m) => transpose44(null, invert44(out, m));
