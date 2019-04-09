29791fd406c8042f2c32318401208e35fd9d96b7
Karsten Schmidt <k@postspectacular.com>
2019-04-09 01:23:14 +0100

    feat(webgl): add cube modelspec factory

---
+++ packages/webgl/src/geo/cube.ts
@@ -0,0 +1,33 @@
+import { ModelSpec } from "../api";
+
+export interface CubeOpts {
+    size: number;
+    normal: boolean;
+    uv: boolean;
+}
+
+// prettier-ignore
+export const cube = (opts?: Partial<CubeOpts>) => {
+    opts = { size: 1, normal: true, uv: true, ...opts};
+    const s = opts.size;
+    const spec: ModelSpec = {
+        attribs: {
+            position: {
+                data: new Float32Array([s, s, -s, s, s, s, s, -s, s, s, -s, -s, -s, s, s, -s, s, -s, -s, -s, -s, -s, -s, s, -s, s, s, s, s, s, s, s, -s, -s, s, -s, -s, -s, -s, s, -s, -s, s, -s, s, -s, -s, s, s, s, s, -s, s, s, -s, -s, s, s, -s, s, -s, s, -s, s, s, -s, s, -s, -s, -s, -s, -s])
+            },
+        },
+        indices:{
+            data: new Uint16Array([0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23])
+        },
+        shader: null,
+        num: 36
+    };
+    opts.normal && (spec.attribs.normal = {
+        data: new Float32Array([1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1])
+    });
+    opts.uv && (spec.attribs.uv = {
+        data: new Float32Array([1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1]),
+        size: 2
+    });
+    return spec;
+};
--- packages/webgl/src/index.ts
+++ packages/webgl/src/index.ts
@@ -4,6 +4,7 @@ export * from "./canvas";
 export * from "./draw";
 export * from "./error";
 export * from "./fbo";
+export * from "./gpgpu";
 export * from "./material";
 export * from "./normal-mat";
 export * from "./shader";
@@ -13,11 +14,13 @@ export * from "./utils";
 export * from "./shaders/lambert";
 export * from "./shaders/phong";
 
-export * from "./textures/checkerboard";
-export * from "./textures/stripes";
-
 export * from "./glsl/assemble";
 export * from "./glsl/syntax";
 
 export * from "./glsl/lighting";
 export * from "./glsl/vertex";
+
+export * from "./textures/checkerboard";
+export * from "./textures/stripes";
+
+export * from "./geo/cube";
