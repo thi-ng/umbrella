ef9bf8d175a08c754b1272e042c10f1de2decd1e
Karsten Schmidt <k@postspectacular.com>
2019-04-24 15:36:47 +0100

    refactor(rstream-log): update to use base types from @thi.ng/api pkg

--- packages/rstream-log/src/logger.ts
+++ packages/rstream-log/src/logger.ts
@@ -1,21 +1,22 @@
+import { LogLevel } from "@thi.ng/api";
 import { illegalArity } from "@thi.ng/errors";
 import { ISubscribable, nextID, StreamMerge } from "@thi.ng/rstream";
-import { ILogger, Level, LogEntry } from "./api";
+import { ILogger, LogEntry } from "./api";
 
 export class Logger extends StreamMerge<LogEntry, LogEntry> implements ILogger {
-    level: Level;
+    level: LogLevel;
 
     constructor();
     constructor(id: string);
-    constructor(id: string, level: Level);
+    constructor(id: string, level: LogLevel);
     constructor(
         id: string,
         sources: Iterable<ISubscribable<LogEntry>>,
-        level?: Level
+        level?: LogLevel
     );
     constructor(...args: any[]) {
         let id;
-        let level = Level.FINE;
+        let level = LogLevel.FINE;
         let src;
         switch (args.length) {
             case 0:
@@ -39,38 +40,41 @@ export class Logger extends StreamMerge<LogEntry, LogEntry> implements ILogger {
     }
 
     next(x: LogEntry) {
-        if (x[0] >= this.level) {
-            super.next(x);
-        }
+        x[0] >= this.level && super.next(x);
     }
 
     fine(...args: any[]) {
-        if (this.level <= Level.FINE) {
-            this.next(<LogEntry>[Level.FINE, this.id, Date.now(), ...args]);
-        }
+        this.level <= LogLevel.FINE &&
+            super.next(<LogEntry>[LogLevel.FINE, this.id, Date.now(), ...args]);
     }
 
     debug(...args: any[]) {
-        if (this.level <= Level.DEBUG) {
-            this.next(<LogEntry>[Level.DEBUG, this.id, Date.now(), ...args]);
-        }
+        this.level <= LogLevel.DEBUG &&
+            super.next(<LogEntry>[
+                LogLevel.DEBUG,
+                this.id,
+                Date.now(),
+                ...args
+            ]);
     }
 
     info(...args: any[]) {
-        if (this.level <= Level.INFO) {
-            this.next(<LogEntry>[Level.INFO, this.id, Date.now(), ...args]);
-        }
+        this.level <= LogLevel.INFO &&
+            super.next(<LogEntry>[LogLevel.INFO, this.id, Date.now(), ...args]);
     }
 
     warn(...args: any[]) {
-        if (this.level <= Level.WARN) {
-            this.next(<LogEntry>[Level.WARN, this.id, Date.now(), ...args]);
-        }
+        this.level <= LogLevel.WARN &&
+            super.next(<LogEntry>[LogLevel.WARN, this.id, Date.now(), ...args]);
     }
 
     severe(...args: any[]) {
-        if (this.level <= Level.SEVERE) {
-            this.next(<LogEntry>[Level.SEVERE, this.id, Date.now(), ...args]);
-        }
+        this.level <= LogLevel.SEVERE &&
+            super.next(<LogEntry>[
+                LogLevel.SEVERE,
+                this.id,
+                Date.now(),
+                ...args
+            ]);
     }
 }
