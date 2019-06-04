6e3fec894184ec6392d55fb35642c6bc2f4acfda
Karsten Schmidt <k@postspectacular.com>
2019-06-04 23:17:39 +0100

    feat(rstream): add fromDOMEvent()

--- packages/rstream/src/from/event.ts
+++ packages/rstream/src/from/event.ts
@@ -2,7 +2,7 @@ import { Stream } from "../stream";
 import { nextID } from "../utils/idgen";
 
 /**
- * Creates a new stream of DOM events attached to given element / event
+ * Creates a new stream of events attached to given element / event
  * target and using given event listener options (same as supported by
  * `addEventListener()`, default: false).
  *
@@ -20,3 +20,24 @@ export const fromEvent = (
         src.addEventListener(name, listener, opts);
         return () => src.removeEventListener(name, listener, opts);
     }, `event-${name}-${nextID()}`);
+
+/**
+ * Same as `fromEvent`, however only supports well-known DOM event
+ * names. Returned stream instance will use corresponding concrete event
+ * type in its type signature, whereas `fromEvent` will only use the
+ * generic `Event`.
+ *
+ * ```
+ * fromDOMEvent(document.body, "mousemove"); // Stream<MouseEvent>
+ * fromEvent(document.body, "mousemove"); // Stream<Event>
+ * ```
+ *
+ * @param src
+ * @param name
+ * @param opts
+ */
+export const fromDOMEvent = <K extends keyof GlobalEventHandlersEventMap>(
+    src: EventTarget,
+    name: K,
+    opts: boolean | AddEventListenerOptions = false
+): Stream<GlobalEventHandlersEventMap[K]> => fromEvent(src, name, opts);
