fb315fe1af46b55a7f274c87f2d347c2dc142a54
Karsten Schmidt <k@postspectacular.com>
2019-06-07 17:06:33 +0100

    refactor(strings): address TS strictNullChecks flag

--- packages/strings/src/center.ts
+++ packages/strings/src/center.ts
@@ -24,18 +24,20 @@ import { truncate } from "./truncate";
 export const center: (
     lineWidth: number,
     pad?: string | number
-) => Stringer<any> = memoizeJ<number, string | number, Stringer<any>>(
-    (n, pad = " ") => {
-        const buf = repeat(String(pad), n);
-        return (x: any) => {
-            if (x == null) return buf;
-            x = x.toString();
-            const r = (n - x.length) / 2;
-            return x.length < n
-                ? buf.substr(0, r) +
-                      x +
-                      buf.substr(0, r + ((n & 1) === (x.length & 1) ? 0 : 1))
-                : truncate(n)(x);
-        };
-    }
-);
+) => Stringer<any> = memoizeJ<
+    number,
+    string | number | undefined,
+    Stringer<any>
+>((n, pad = " ") => {
+    const buf = repeat(String(pad), n);
+    return (x: any) => {
+        if (x == null) return buf;
+        x = x.toString();
+        const r = (n - x.length) / 2;
+        return x.length < n
+            ? buf.substr(0, r) +
+                  x +
+                  buf.substr(0, r + ((n & 1) === (x.length & 1) ? 0 : 1))
+            : truncate(n)(x);
+    };
+});
--- packages/strings/src/pad-right.ts
+++ packages/strings/src/pad-right.ts
@@ -2,7 +2,6 @@ import { memoizeJ } from "@thi.ng/memoize";
 import { Stringer } from "./api";
 import { repeat } from "./repeat";
 
-
 /**
  * @param n target length
  * @param ch pad character(s)
@@ -10,13 +9,14 @@ import { repeat } from "./repeat";
 export const padRight: (
     n: number,
     ch?: string | number
-) => Stringer<any> = memoizeJ<number, string | number, Stringer<any>>(
-    (n, ch = " ") => {
-        const buf = repeat(String(ch), n);
-        return (x: any) =>
-            x != null
-                ? ((x = x.toString()),
-                  x.length < n ? x + buf.substr(x.length) : x)
-                : buf;
-    }
-);
+) => Stringer<any> = memoizeJ<
+    number,
+    string | number | undefined,
+    Stringer<any>
+>((n, ch = " ") => {
+    const buf = repeat(String(ch), n);
+    return (x: any) =>
+        x != null
+            ? ((x = x.toString()), x.length < n ? x + buf.substr(x.length) : x)
+            : buf;
+});
