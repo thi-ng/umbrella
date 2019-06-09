9f9be1d4c977659ee7fd5df4f4d6d8b76b102aac
Karsten Schmidt <k@postspectacular.com>
2019-06-09 15:19:00 +0100

    feat(iterators): TS strictNullChecks
    - last() now returns <T> or undefined

--- packages/iterators/src/dedupe-with.ts
+++ packages/iterators/src/dedupe-with.ts
@@ -4,7 +4,7 @@ import { iterator } from "./iterator";
 export function* dedupeWith<T>(equiv: Predicate2<T>, input: Iterable<T>) {
     let iter = iterator(input);
     let v: IteratorResult<T>;
-    let prev: T;
+    let prev: T | undefined;
     while (((v = iter.next()), !v.done)) {
         if (prev === undefined || !equiv(prev, v.value)) {
             prev = v.value;
