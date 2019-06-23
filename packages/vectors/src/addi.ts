c8a997ff65a1f928b1d2311a18ad4649c17e02ba
Karsten Schmidt <k@postspectacular.com>
2019-06-23 17:29:14 +0100

    feat(vectors): add integer math ops (signed/unsigned)

---
+++ packages/vectors/src/addi.ts
@@ -0,0 +1,9 @@
+import { defBitOp, defBitOpN } from "./internal/codegen";
+
+export const [addI, addI2, addI3, addI4] = defBitOp("+", true);
+
+export const [addU, addU2, addU3, addU4] = defBitOp("+");
+
+export const [addNI, addNI2, addNI3, addNI4] = defBitOpN("+", true);
+
+export const [addNU, addNU2, addNU3, addNU4] = defBitOpN("+");
