e3498b2915d1c85b49ea53fca4c9c10b129412c0
Karsten Schmidt <k@postspectacular.com>
2019-06-24 18:10:27 +0100

    build(shader-ast): remove obsolete std sources (moved to own pkg)

--- packages/shader-ast/src/index.ts
+++ packages/shader-ast/src/index.ts
@@ -3,11 +3,3 @@ export * from "./ast";
 export * from "./builtins";
 export * from "./optimize";
 export * from "./target";
-
-export * from "./std/fog";
-export * from "./std/lambert";
-export * from "./std/math";
-export * from "./std/matrix";
-export * from "./std/raymarch";
-export * from "./std/sdf";
-export * from "./std/trilight";
