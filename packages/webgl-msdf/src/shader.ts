af6d39fce36c46746f38e2d490c5d8008c636d9d
Karsten Schmidt <k@postspectacular.com>
2019-06-30 01:25:23 +0100

    refactor(webgl-msdf): update shader to use AST

--- packages/webgl-msdf/src/shader.ts
+++ packages/webgl-msdf/src/shader.ts
@@ -1,47 +1,84 @@
-import { ONE4, ZERO4 } from "@thi.ng/vectors";
 import {
-    ALIAS_TEXTURE,
-    DEFAULT_BLEND,
-    defglsl,
-    defglslA,
-    GLVec4,
-    ShaderSpec
-} from "@thi.ng/webgl";
+    $,
+    $x,
+    $y,
+    $z,
+    add,
+    assign,
+    clamp,
+    defMain,
+    defn,
+    discard,
+    div,
+    FLOAT0,
+    FLOAT05,
+    FLOAT1,
+    FloatSym,
+    fwidth,
+    ifThen,
+    lt,
+    max,
+    min,
+    mix,
+    mul,
+    ret,
+    sub,
+    sym,
+    texture,
+    vec2,
+    Vec2Sym,
+    vec4
+} from "@thi.ng/shader-ast";
+import { ONE4, ZERO4 } from "@thi.ng/vectors";
+import { DEFAULT_BLEND, GLVec4, ShaderSpec } from "@thi.ng/webgl";
 
 export interface MSDFShaderOpts {
     color: boolean;
 }
 
-export const median3 = defglsl(`float median3(vec3 x) {
-    return max(min(x.r, x.g), min(max(x.r, x.g), x.b));
-}`);
+export const median3 = defn("float", "median3", [["vec3"]], (v) => [
+    ret(max(min($x(v), $y(v)), min(max($x(v), $y(v)), $z(v))))
+]);
 
-export const msdfSample = defglsl(
-    `vec2 msdfSample(sampler2D tex, vec2 uv) {
-    float sd = median3(texture(tex, uv).rgb) - 0.5;
-    float w = clamp(sd / fwidth(sd) + 0.5, 0., 1.);
-    return vec2(sd, w);
-}`,
-    [median3]
+export const msdfSample = defn(
+    "vec2",
+    "msdfSample",
+    [["sampler2D"], ["vec2"]],
+    (tex, uv) => {
+        let sd: FloatSym;
+        let w: FloatSym;
+        return [
+            (sd = sym(sub(median3($(texture(tex, uv), "xyz")), FLOAT05))),
+            (w = sym(clamp(add(div(sd, fwidth(sd)), FLOAT05), FLOAT0, FLOAT1))),
+            ret(vec2(sd, w))
+        ];
+    }
 );
 
 export const msdfShader = (opts: Partial<MSDFShaderOpts> = {}): ShaderSpec => ({
-    vs: `void main() {
-    vuv = uv;
-    ${opts.color ? "vcolor = color;" : ""}
-    gl_Position = proj * modelview * vec4(position, 1.);
-}`,
-    fs: defglslA(
-        `void main() {
-    vec2 w = msdfSample(tex, vuv);
-    fragColor = mix(bg, ${opts.color ? "vcolor" : "fg"}, w.y);
-    if (w.x < thresh) {
-        discard;
-    }
-}`,
-        [msdfSample]
-    ),
-    pre: ALIAS_TEXTURE,
+    vs: (gl, unis, ins, outs) => [
+        defMain(() => [
+            assign(outs.vuv, ins.uv),
+            opts.color ? assign(outs.vcolor, ins.color) : null,
+            assign(
+                gl.gl_Position,
+                mul(mul(unis.proj, unis.modelview), vec4(ins.position, 1))
+            )
+        ])
+    ],
+    fs: (_, unis, ins, outs) => [
+        defMain(() => {
+            let w: Vec2Sym;
+            return [
+                (w = sym(msdfSample(unis.tex, ins.vuv))),
+                assign(
+                    outs.fragColor,
+                    mix(unis.bg, opts.color ? ins.vcolor : unis.fg, $y(w))
+                ),
+                ifThen(lt($x(w), unis.thresh), [discard])
+            ];
+        })
+    ],
     ext: {
         OES_standard_derivatives: true
     },
