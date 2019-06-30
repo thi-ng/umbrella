341cf046131627126c6c2c9da624c91bd5c49713
Karsten Schmidt <k@postspectacular.com>
2019-06-30 02:38:45 +0100

    refactor(webgl): update FX_SHADER_SPEC to use AST

--- packages/webgl/src/pipeline.ts
+++ packages/webgl/src/pipeline.ts
@@ -1,17 +1,29 @@
-import { ShaderSpec } from "./api";
-import { ALIAS_TEXTURE } from "./glsl/syntax";
+import {
+    assign,
+    defMain,
+    FLOAT0,
+    FLOAT1,
+    texture,
+    vec4
+} from "@thi.ng/shader-ast";
+import { ShaderFn, ShaderSpec } from "./api";
 
-export const PASSTHROUGH_VS =
-    "void main(){v_uv=a_uv;gl_Position=vec4(a_position,0.0,1.0);}";
+export const PASSTHROUGH_VS: ShaderFn = (gl, _, ins, outs) => [
+    defMain(() => [
+        assign(outs.v_uv, ins.uv),
+        assign(gl.gl_Position, vec4(ins.position, FLOAT0, FLOAT1))
+    ])
+];
 
-export const PASSTHROUGH_FS = "void main(){o_fragColor=texture(u_tex,v_uv);}";
+export const PASSTHROUGH_FS: ShaderFn = (_, unis, ins, outs) => [
+    defMain(() => [assign(outs.fragColor, texture(unis.tex, ins.v_uv))])
+];
 
 export const FX_SHADER_SPEC: ShaderSpec = {
     vs: PASSTHROUGH_VS,
     fs: PASSTHROUGH_FS,
-    pre: ALIAS_TEXTURE,
     attribs: { position: "vec2", uv: "vec2" },
-    varying: { uv: "vec2" },
+    varying: { v_uv: "vec2" },
     uniforms: { tex: "sampler2D" },
     state: { depth: false },
     ext: {}
