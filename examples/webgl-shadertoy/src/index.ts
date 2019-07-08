21fba1743c7d20b37aa9e01a84ee7ea41551c989
Karsten Schmidt <k@postspectacular.com>
2019-07-08 02:46:53 +0100

    feat(examples): add minimal webgl-shadertoy example

---
+++ examples/webgl-shadertoy/src/index.ts
@@ -0,0 +1,77 @@
+import {
+    add,
+    bitand,
+    defn,
+    distance,
+    float,
+    FloatSym,
+    fract,
+    int,
+    min,
+    mix,
+    mul,
+    neg,
+    ret,
+    sin,
+    sym,
+    Vec2Sym,
+    Vec2Term,
+    vec3,
+    Vec3Sym,
+    vec4
+} from "@thi.ng/shader-ast";
+import { aspectCorrectedUV, fit1101 } from "@thi.ng/shader-ast-stdlib";
+import { glCanvas } from "@thi.ng/webgl";
+import { shaderToy } from "@thi.ng/webgl-shadertoy";
+
+// main shader function, supplied to `shaderToy` below
+const mainImage = defn(
+    "vec4",
+    "mainImage",
+    [["vec2"], ["vec2"], ["vec2"], ["int"], ["float"]],
+    (fragCoord, res, mouse, buttons, time) => {
+        let uv: Vec2Sym;
+        let mp: Vec2Sym;
+        let d1: FloatSym;
+        let d2: FloatSym;
+        let col: Vec3Sym;
+        const rings = (p: Vec2Term, speed: number, freq: number) =>
+            sin(mul(add(distance(uv, p), fract(mul(time, speed))), freq));
+        return [
+            (uv = sym(aspectCorrectedUV(fragCoord, res))),
+            (mp = sym(aspectCorrectedUV(mouse, res))),
+            (d1 = sym(rings(mp, 0.25, 50))),
+            (d2 = sym(rings(neg(mp), 0.25, 50))),
+            (col = sym(
+                mul(
+                    vec3(fit1101(min(d1, d2))),
+                    mix(
+                        vec3(1),
+                        vec3(d1, 0, d2),
+                        float(bitand(buttons, int(1)))
+                    )
+                )
+            )),
+            ret(vec4(col, 1))
+        ];
+    }
+);
+
+const canvas = glCanvas({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    parent: document.body,
+    version: 2
+});
+
+const toy = shaderToy({
+    canvas: canvas.canvas,
+    gl: canvas.gl,
+    main: mainImage
+});
+
+toy.start();
+
+// toy.stop();
+
+// toy.recompile(/* pass new mainImage fn */);
