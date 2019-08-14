d956954a12423ad4df0b94ebb2a54d94ea121f96
Karsten Schmidt <k@postspectacular.com>
2019-08-14 02:30:26 +0100

    fix(color): add proper rounding to rgbaInt()

--- packages/color/src/rgba-int.ts
+++ packages/color/src/rgba-int.ts
@@ -3,8 +3,8 @@ import { ReadonlyColor } from "./api";
 import { ensureAlpha } from "./internal/ensure-alpha";
 
 export const rgbaInt = (src: ReadonlyColor) =>
-    (((ensureAlpha(src[3]) * 0xff) << 24) |
-        ((clamp01(src[0]) * 0xff) << 16) |
-        ((clamp01(src[1]) * 0xff) << 8) |
-        (clamp01(src[2]) * 0xff)) >>>
+    (((ensureAlpha(src[3]) * 0xff + 0.5) << 24) |
+        ((clamp01(src[0]) * 0xff + 0.5) << 16) |
+        ((clamp01(src[1]) * 0xff + 0.5) << 8) |
+        (clamp01(src[2]) * 0xff + 0.5)) >>>
     0;
