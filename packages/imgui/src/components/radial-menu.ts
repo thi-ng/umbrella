99c2987e21f1f8fbf3e99cb7e7e8dbe5ab3dc394
Karsten Schmidt <k@postspectacular.com>
2019-08-16 00:45:19 +0100

    feat(imgui): add key handling for radialMenu()

--- packages/imgui/src/components/radial-menu.ts
+++ packages/imgui/src/components/radial-menu.ts
@@ -6,8 +6,10 @@ import {
     vertices
 } from "@thi.ng/geom";
 import { triFan } from "@thi.ng/geom-tessellate";
+import { fmod } from "@thi.ng/math";
 import { mapIndexed } from "@thi.ng/transducers";
 import { add2, hash } from "@thi.ng/vectors";
+import { Key } from "../api";
 import { IMGUI } from "../gui";
 import { buttonRaw } from "./button";
 import { textLabelRaw } from "./textlabel";
@@ -41,10 +43,25 @@ export const radialMenu = (
         }, triFan(vertices(circle([x, y], r), n)))
     ]);
     let res: number | undefined;
+    let sel = -1;
     for (let i = 0; i < n; i++) {
         const cell = cells[i];
-        buttonRaw(gui, id + i, cell[0], cell[1], cell[2], cell[3], info[i]) &&
+        const _id = id + i;
+        buttonRaw(gui, _id, cell[0], cell[1], cell[2], cell[3], info[i]) &&
             (res = i);
+        gui.focusID === _id && (sel = i);
+    }
+    if (sel !== -1) {
+        switch (gui.key) {
+            case Key.UP:
+            case Key.RIGHT:
+                gui.focusID = id + fmod(sel + 1, n);
+                break;
+            case Key.DOWN:
+            case Key.LEFT:
+                gui.focusID = id + fmod(sel - 1, n);
+            default:
+        }
     }
     return res;
 };
