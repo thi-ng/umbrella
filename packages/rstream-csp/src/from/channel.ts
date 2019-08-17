9d05f4e5aa35c58b8ef9cd1c264b7a2beb76aa1d
Karsten Schmidt <k@postspectacular.com>
2019-08-17 21:14:28 +0100

    refactor(rstream-csp): update to new rstream api

--- packages/rstream-csp/src/from/channel.ts
+++ packages/rstream-csp/src/from/channel.ts
@@ -1,29 +1,37 @@
 import { Channel } from "@thi.ng/csp";
 import { LOGGER, Stream } from "@thi.ng/rstream";
+import { CommonOpts } from "@thi.ng/rstream/api";
 
 /**
  *
  * @param src
  * @param closeWhenCancelled
  */
-export const fromChannel = <T>(src: Channel<T>, closeWhenCancelled = true) =>
-    new Stream<T>((stream) => {
-        let isActive = true;
-        (async () => {
-            let x;
-            while (((x = null), (x = await src.read())) !== undefined) {
-                if (x === undefined || !isActive) {
-                    break;
+export const fromChannel = <T>(
+    src: Channel<T>,
+    closeWhenCancelled = true,
+    opts?: Partial<CommonOpts>
+) =>
+    new Stream<T>(
+        (stream) => {
+            let isActive = true;
+            (async () => {
+                let x;
+                while (((x = null), (x = await src.read())) !== undefined) {
+                    if (x === undefined || !isActive) {
+                        break;
+                    }
+                    stream.next(x);
                 }
-                stream.next(x);
-            }
-            stream.done();
-        })();
-        return () => {
-            if (closeWhenCancelled) {
-                src.close(true);
-                LOGGER.info("closed channel", src.id);
-            }
-            isActive = false;
-        };
-    }, `channel-${src.id}`);
+                stream.done();
+            })();
+            return () => {
+                if (closeWhenCancelled) {
+                    src.close(true);
+                    LOGGER.info("closed channel", src.id);
+                }
+                isActive = false;
+            };
+        },
+        { id: `channel-${src.id}`, ...opts }
+    );
