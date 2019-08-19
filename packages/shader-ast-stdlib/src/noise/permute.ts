c5a36a1e105d76e29233d5fde9f2c6997cdd2fc6
Karsten Schmidt <k@postspectacular.com>
2019-08-19 16:52:44 +0100

    refactor(shader-ast-stdlib): update permute fns (re-use)

--- packages/shader-ast-stdlib/src/noise/permute.ts
+++ packages/shader-ast-stdlib/src/noise/permute.ts
@@ -5,21 +5,19 @@ import {
     FLOAT1,
     mod,
     mul,
+    Prim,
     ret
 } from "@thi.ng/shader-ast";
 
-export const permute = defn("float", "permute", ["float"], (v) => [
-    ret(mod(mul(v, add(mul(v, float(34)), FLOAT1)), float(289)))
-]);
+const __permute = <T extends Prim>(type: T, suffix = "") =>
+    defn(type, `permute${suffix}`, [type], (v) => [
+        ret(mod(mul(<any>v, add(mul(<any>v, float(34)), FLOAT1)), float(289)))
+    ]);
 
-export const permute2 = defn("vec2", "permute2", ["vec2"], (v) => [
-    ret(mod(mul(v, add(mul(v, float(34)), FLOAT1)), float(289)))
-]);
+export const permute = __permute("float");
 
-export const permute3 = defn("vec3", "permute3", ["vec3"], (v) => [
-    ret(mod(mul(v, add(mul(v, float(34)), FLOAT1)), float(289)))
-]);
+export const permute2 = __permute("vec2", "2");
 
-export const permute4 = defn("vec4", "permute4", ["vec4"], (v) => [
-    ret(mod(mul(v, add(mul(v, float(34)), FLOAT1)), float(289)))
-]);
+export const permute3 = __permute("vec3", "3");
+
+export const permute4 = __permute("vec4", "4");
