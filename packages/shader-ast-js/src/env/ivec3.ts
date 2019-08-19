caa6301d4d3cc48a5a1273927d72ff506d75c5ec
Karsten Schmidt <k@postspectacular.com>
2019-08-19 20:14:39 +0100

    refactor(shader-ast-js): split env.ts into separate files

---
+++ packages/shader-ast-js/src/env/ivec3.ts
@@ -0,0 +1,45 @@
+import {
+    addI3,
+    addNI3,
+    bitAndI3,
+    bitNotI3,
+    bitOrI3,
+    bitXorI3,
+    divI3,
+    divNI3,
+    lshiftI3,
+    mod3,
+    modN3,
+    mulI3,
+    mulNI3,
+    rshiftI3,
+    subI3,
+    subNI3
+} from "@thi.ng/vectors";
+import { JSBuiltinsIntVec } from "../api";
+import { VEC3 } from "./vec3";
+
+export const IVEC3: JSBuiltinsIntVec = {
+    ...VEC3,
+    add: (a, b) => addI3([], a, b),
+    addvn: (a, b) => addNI3([], a, b),
+    addnv: (a, b) => addNI3([], b, a),
+    div: (a, b) => divI3([], a, b),
+    divvn: (a, b) => divNI3([], a, b),
+    divnv: (a, b) => mulNI3([], b, 1 / a),
+    modi: (a, b) => mod3([], a, b),
+    modivn: (a, b) => modN3([], a, b),
+    modinv: (a, b) => mod3([], [a, a, a], b),
+    mul: (a, b) => mulI3([], a, b),
+    mulvn: (a, b) => mulNI3([], a, b),
+    mulnv: (a, b) => mulNI3([], b, a),
+    sub: (a, b) => subI3([], a, b),
+    subvn: (a, b) => subNI3([], a, b),
+    subnv: (a, b) => subI3([], [a, a, a], b),
+    bitand: (a, b) => bitAndI3([], a, b),
+    lshift: (a, b) => lshiftI3([], a, b),
+    bitnot1: (a) => bitNotI3([], a),
+    bitor: (a, b) => bitOrI3([], a, b),
+    rshift: (a, b) => rshiftI3([], a, b),
+    bitxor: (a, b) => bitXorI3([], a, b)
+};
---
+++ packages/shader-ast-js/src/env/ivec4.ts
@@ -0,0 +1,45 @@
+import {
+    addI4,
+    addNI4,
+    bitAndI4,
+    bitNotI4,
+    bitOrI4,
+    bitXorI4,
+    divI4,
+    divNI4,
+    lshiftI4,
+    mod4,
+    modN4,
+    mulI4,
+    mulNI4,
+    rshiftI4,
+    subI4,
+    subNI4
+} from "@thi.ng/vectors";
+import { JSBuiltinsIntVec } from "../api";
+import { VEC4 } from "./vec4";
+
+export const IVEC4: JSBuiltinsIntVec = {
+    ...VEC4,
+    add: (a, b) => addI4([], a, b),
+    addvn: (a, b) => addNI4([], a, b),
+    addnv: (a, b) => addNI4([], b, a),
+    div: (a, b) => divI4([], a, b),
+    divvn: (a, b) => divNI4([], a, b),
+    divnv: (a, b) => mulNI4([], b, 1 / a),
+    modi: (a, b) => mod4([], a, b),
+    modivn: (a, b) => modN4([], a, b),
+    modinv: (a, b) => mod4([], [a, a, a, a], b),
+    mul: (a, b) => mulI4([], a, b),
+    mulvn: (a, b) => mulNI4([], a, b),
+    mulnv: (a, b) => mulNI4([], b, a),
+    sub: (a, b) => subI4([], a, b),
+    subvn: (a, b) => subNI4([], a, b),
+    subnv: (a, b) => subI4([], [a, a, a, a], b),
+    bitand: (a, b) => bitAndI4([], a, b),
+    lshift: (a, b) => lshiftI4([], a, b),
+    bitnot1: (a) => bitNotI4([], a),
+    bitor: (a, b) => bitOrI4([], a, b),
+    rshift: (a, b) => rshiftI4([], a, b),
+    bitxor: (a, b) => bitXorI4([], a, b)
+};
