0c33878cc8f69daedcadd3c84a7aff753af6a1b6
Karsten Schmidt <k@postspectacular.com>
2019-08-21 16:32:02 +0100

    refactor(geom-closest-point): update closestPointRect/AABB (re-use)
    - extract closestBoxEdge()

--- packages/geom-closest-point/src/index.ts
+++ packages/geom-closest-point/src/index.ts
@@ -228,19 +228,7 @@ export const closestPointRect = (
     bmax: ReadonlyVec,
     out: Vec = []
 ) => {
-    let minD = Infinity;
-    let minID: number;
-    let minW: number;
-    for (let i = 0; i < 4; i++) {
-        const j = i >> 1;
-        const w = (i & 1 ? bmax : bmin)[j];
-        const d = Math.abs(p[j] - w);
-        if (d < minD) {
-            minD = d;
-            minID = j;
-            minW = w;
-        }
-    }
+    const [minID, minW] = closestBoxEdge(p, bmin, bmax, 4);
     return minID! === 0
         ? setC2(out, minW!, clamp(p[1], bmin[1], bmax[1]))
         : setC2(out, clamp(p[0], bmin[0], bmax[0]), minW!);
@@ -252,10 +240,39 @@ export const closestPointAABB = (
     bmax: ReadonlyVec,
     out: Vec = []
 ) => {
+    const [minID, minW] = closestBoxEdge(p, bmin, bmax, 6);
+    return minID! === 0
+        ? setC3(
+              out,
+              minW,
+              clamp(p[1], bmin[1], bmax[1]),
+              clamp(p[2], bmin[2], bmax[2])
+          )
+        : minID! === 1
+        ? setC3(
+              out,
+              clamp(p[0], bmin[0], bmax[0]),
+              minW,
+              clamp(p[2], bmin[2], bmax[2])
+          )
+        : setC3(
+              out,
+              clamp(p[0], bmin[0], bmax[0]),
+              clamp(p[1], bmin[1], bmax[1]),
+              minW
+          );
+};
+
+const closestBoxEdge = (
+    p: ReadonlyVec,
+    bmin: ReadonlyVec,
+    bmax: ReadonlyVec,
+    n: number
+) => {
     let minD = Infinity;
     let minID: number;
     let minW: number;
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < n; i++) {
         const j = i >> 1;
         const w = (i & 1 ? bmax : bmin)[j];
         const d = Math.abs(p[j] - w);
@@ -265,24 +282,5 @@ export const closestPointAABB = (
             minW = w;
         }
     }
-    return minID! === 0
-        ? setC3(
-              out,
-              minW!,
-              clamp(p[1], bmin[1], bmax[1]),
-              clamp(p[2], bmin[2], bmax[2])
-          )
-        : minID! === 1
-        ? setC3(
-              out,
-              clamp(p[0], bmin[0], bmax[0]),
-              minW!,
-              clamp(p[2], bmin[2], bmax[2])
-          )
-        : setC3(
-              out,
-              clamp(p[0], bmin[0], bmax[0]),
-              clamp(p[1], bmin[1], bmax[1]),
-              minW!
-          );
+    return [minID!, minW!];
 };
