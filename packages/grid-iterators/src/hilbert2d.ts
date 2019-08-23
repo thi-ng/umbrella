e89687f1387f29c5da1cc078becd265764054391
Karsten Schmidt <k@postspectacular.com>
2019-08-23 21:26:04 +0100

    docs(grid-iterators): update docs

--- packages/grid-iterators/src/hilbert2d.ts
+++ packages/grid-iterators/src/hilbert2d.ts
@@ -22,26 +22,25 @@ export function* hilbert2d(cols: number, rows: number) {
             // s is the hilbert index, shifted to start in the middle
             s = hIndex | (0x55555555 << (2 * hOrder)); // Pad s on left with 01
             sr = (s >>> 1) & 0x55555555; // (no change) groups.
-            cs = ((s & 0x55555555) + sr) ^ 0x55555555; // Compute
-            // complement
-            // & swap info in
-            // two-bit groups.
-            // Parallel prefix xor op to propagate both complement
-            // and swap info together from left to right (there is
-            // no step "cs ^= cs >> 1", so in effect it computes
-            // two independent parallel prefix operations on two
-            // interleaved sets of sixteen bits).
+            cs = ((s & 0x55555555) + sr) ^ 0x55555555;
+            // Compute complement & swap info in two-bit groups.
+            // Parallel prefix xor op to propagate both complement and
+            // swap info together from left to right (there is no step
+            // "cs ^= cs >> 1", so in effect it computes two independent
+            // parallel prefix operations on two interleaved sets of
+            // sixteen bits).
             cs = cs ^ (cs >>> 2);
             cs = cs ^ (cs >>> 4);
             cs = cs ^ (cs >>> 8);
             cs = cs ^ (cs >>> 16);
-            swap = cs & 0x55555555; // Separate the swap and
-            comp = (cs >>> 1) & 0x55555555; // complement bits.
-            t = (s & swap) ^ comp; // Calculate x and y in
-            s = s ^ sr ^ t ^ (t << 1); // the odd & even bit
-            // positions, resp.
-            s = s & ((1 << (2 * hOrder)) - 1); // Clear out any junk
-            // on the left (unpad).
+            // Separate the swap and complement bits.
+            swap = cs & 0x55555555;
+            comp = (cs >>> 1) & 0x55555555;
+            // Calculate x and y in the odd & even bit positions
+            t = (s & swap) ^ comp;
+            s = s ^ sr ^ t ^ (t << 1);
+            // Clear out any junk on the left (unpad).
+            s = s & ((1 << (2 * hOrder)) - 1);
             // Now "unshuffle" to separate the x and y bits.
             t = (s ^ (s >>> 1)) & 0x22222222;
             s = s ^ t ^ (t << 1);
@@ -51,10 +50,12 @@ export function* hilbert2d(cols: number, rows: number) {
             s = s ^ t ^ (t << 4);
             t = (s ^ (s >>> 8)) & 0x0000ff00;
             s = s ^ t ^ (t << 8);
-            hx = s >>> 16; // Assign the two halves
-            hy = s & 0xffff; // of t to x and y.
+            // Assign the two halves to x and y.
+            hx = s >>> 16;
+            hy = s & 0xffff;
             hIndex++;
         } while (
+            // Dont't emit any outside cells
             (hx >= cols || hy >= rows || hx < 0 || hy < 0) &&
             hIndex < numBuckets
         );
