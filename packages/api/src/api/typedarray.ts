ac7fa13fa602947ae4b30c943d8d8fddcd602381
Karsten Schmidt <k@postspectacular.com>
2019-10-10 02:03:46 +0100

    feat(api): add typedArray() factory, update type mappers, docs

--- packages/api/src/api/typedarray.ts
+++ packages/api/src/api/typedarray.ts
@@ -44,11 +44,11 @@ export const enum Type {
 }
 
 /**
- * WebGL numeric type constants. Unless stated otherwise, these can be
- * used as aliases for `Type` enums (see `GL2TYPE` LUT below), but then
- * also used directly when initializing WebGL buffers.
+ * WebGL numeric type constants. Use `GL2TYPE` to convert, if needed.
  *
- * @see thi.ng/vector-pools
+ * @see Type
+ * @see GL2TYPE
+ * @see TYPE2GL
  */
 export const enum GLType {
     I8 = 0x1400,
@@ -61,23 +61,27 @@ export const enum GLType {
 }
 
 /**
- * Conversion from `GLType` to `Type`.
+ * Conversion from `GLType` to `Type` enums.
  */
-export const GL2TYPE: { [id: number]: Type } = {
+export const GL2TYPE: Record<GLType, Type> = {
     [GLType.I8]: Type.I8,
     [GLType.U8]: Type.U8,
     [GLType.I16]: Type.I16,
     [GLType.U16]: Type.U16,
-    [GLType.I32]: Type.I32,
     [GLType.I32]: Type.I32,
     [GLType.U32]: Type.U32,
     [GLType.F32]: Type.F32
 };
 
 /**
- * Conversion from `Type` to `GLType`.
+ * Potentially lossy conversion from `Type` to `GLType` enums.
+ *
+ * Not all enums are mappable:
+ *
+ * - `F64` maps to `undefined`, since unsupported by WebGL
+ * - `U8C` maps to U8
  */
-export const TYPE2GL: { [id: number]: GLType } = {
+export const TYPE2GL: Record<Type, GLType | undefined> = {
     [Type.I8]: GLType.I8,
     [Type.U8]: GLType.U8,
     [Type.U8C]: GLType.U8,
@@ -86,12 +90,13 @@ export const TYPE2GL: { [id: number]: GLType } = {
     [Type.I32]: GLType.I32,
     [Type.I32]: GLType.I32,
     [Type.U32]: GLType.U32,
-    [Type.F32]: GLType.F32
+    [Type.F32]: GLType.F32,
+    [Type.F64]: undefined
 };
 
 /**
  * Size information (in bytes) for `Type` enums. For `GLType`, use this
- * form, e.g. `SIZEOF[GL2TYPE[Type.F32]]`
+ * form, e.g. `SIZEOF[GL2TYPE[GLType.F32]]`
  */
 export const SIZEOF = {
     [Type.U8]: 1,
@@ -105,7 +110,7 @@ export const SIZEOF = {
     [Type.F64]: 8
 };
 
-export const TYPEDARRAY_CTORS: { [id: number]: TypedArrayConstructor } = {
+export const TYPEDARRAY_CTORS: Record<Type | GLType, TypedArrayConstructor> = {
     [Type.U8]: Uint8Array,
     [Type.U8C]: Uint8ClampedArray,
     [Type.I8]: Int8Array,
@@ -114,5 +119,47 @@ export const TYPEDARRAY_CTORS: { [id: number]: TypedArrayConstructor } = {
     [Type.U32]: Uint32Array,
     [Type.I32]: Int32Array,
     [Type.F32]: Float32Array,
-    [Type.F64]: Float64Array
+    [Type.F64]: Float64Array,
+    [GLType.U8]: Uint8Array,
+    [GLType.I8]: Int8Array,
+    [GLType.U16]: Uint16Array,
+    [GLType.I16]: Int16Array,
+    [GLType.U32]: Uint32Array,
+    [GLType.I32]: Int32Array,
+    [GLType.F32]: Float32Array
 };
+
+export interface TypedArrayTypeMap extends Record<Type | GLType, TypedArray> {
+    [Type.U8]: Uint8Array;
+    [Type.U8C]: Uint8ClampedArray;
+    [Type.I8]: Int8Array;
+    [Type.U16]: Uint16Array;
+    [Type.I16]: Int16Array;
+    [Type.U32]: Uint32Array;
+    [Type.I32]: Int32Array;
+    [Type.F32]: Float32Array;
+    [Type.F64]: Float64Array;
+    [GLType.U8]: Uint8Array;
+    [GLType.I8]: Int8Array;
+    [GLType.U16]: Uint16Array;
+    [GLType.I16]: Int16Array;
+    [GLType.U32]: Uint32Array;
+    [GLType.I32]: Int32Array;
+    [GLType.F32]: Float32Array;
+}
+
+/**
+ * Constructs new typed array of given `Type`/`GLType`. Supports all
+ * arities of standard typed array ctors.
+ *
+ * @param type
+ */
+// prettier-ignore
+export function typedArray<T extends Type | GLType>(type: T, length: number): TypedArrayTypeMap[T];
+// prettier-ignore
+export function typedArray<T extends Type | GLType>(type: T, src: ArrayLike<number> | ArrayBufferLike): TypedArrayTypeMap[T];
+// prettier-ignore
+export function typedArray<T extends Type | GLType>(type: T, buf: ArrayBufferLike, byteOffset: number, length?: number): TypedArrayTypeMap[T];
+export function typedArray<T extends Type | GLType>(type: T, ...xs: any[]) {
+    return new (<any>TYPEDARRAY_CTORS[type])(...xs);
+}
