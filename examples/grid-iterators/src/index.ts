0c4d917aae1fa754e9341aa0142532aac8267e22
Karsten Schmidt <k@postspectacular.com>
2019-10-25 18:30:42 +0100

    feat(examples): update grid-iterators demo

--- examples/grid-iterators/src/index.ts
+++ examples/grid-iterators/src/index.ts
@@ -1,13 +1,16 @@
 import { hueRgba, rgbaCss } from "@thi.ng/color";
+import { createElement } from "@thi.ng/hdom";
+import { concat, cycle } from "@thi.ng/transducers";
 import {
     diagonal2d,
     hilbert2d,
+    random2d,
     spiral2d,
+    zcurve2d,
     zigzagColumns2d,
+    // zigzagDiagonal2d,
     zigzagRows2d
 } from "@thi.ng/grid-iterators";
-import { createElement } from "@thi.ng/hdom";
-import { concat, cycle } from "@thi.ng/transducers";
 
 const W = 256;
 const H = 256;
@@ -26,17 +29,20 @@ const BH = Math.ceil(H / NB);
 const buckets = cycle(
     concat(
         diagonal2d(NB, NB),
-        zigzagRows2d(NB, NB),
-        hilbert2d(NB, NB),
-        zigzagColumns2d(NB, NB),
-        spiral2d(NB, NB)
+        zigzagRows2d(NB),
+        hilbert2d(NB),
+        zigzagColumns2d(NB),
+        spiral2d(NB),
+        // zigzagDiagonal2d(NB),
+        zcurve2d(NB),
+        random2d(NB)
     )
 );
 
 let frame = 0;
 setInterval(() => {
     const b = buckets.next();
-    let [x, y] = b.value;
+    let [x, y] = <number[]>b.value;
     x *= BW;
     y *= BH;
     ctx.fillStyle = rgbaCss(hueRgba([], frame++ / (NB * NB)));
