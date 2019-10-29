78c718bbc7356f7272d894089d98cad946d0343e
Karsten Schmidt <k@postspectacular.com>
2019-10-29 12:53:59 +0000

    feat(examples): update soa-ecs to only use strided vec ops

--- examples/soa-ecs/src/index.ts
+++ examples/soa-ecs/src/index.ts
@@ -17,13 +17,14 @@ import {
 } from "@thi.ng/shader-ast";
 import {
     addS2,
-    magSq2,
+    magSqS2,
     mixN2,
-    mulN2,
-    normalize,
+    mixNS2,
+    mulNS2,
+    normalizeS2,
     randNorm,
-    rotateZ,
-    setS2
+    randNormS2,
+    rotateS2
 } from "@thi.ng/vectors";
 import {
     BLEND_ADD,
@@ -45,13 +46,11 @@ const COLOR2 = [0.1, 0.9, 1];
 
 const pos = defComponent("pos", NUM, {
     size: 2
-    // cache: new LRU<Float32Array>(NUM)
 });
 
 const vel = defComponent("vel", NUM, {
     size: 2,
-    default: () => randNorm([0, 0])
-    // cache: new LRU<Float32Array>(NUM)
+    default: () => randNormS2([0, 0])
 });
 
 const group = new Group([pos, vel]);
@@ -61,36 +60,33 @@ for (let i = 0; i < NUM; i++) {
     vel.add(i);
 }
 
-const tmp = [0, 0];
-const tmp2 = [0, 0];
 const dir = [0, 0];
 
 const moveBatch = (
-    { pos, vel }: IObjectOf<ComponentInfo>,
+    info: IObjectOf<ComponentInfo>,
     num: number,
     t: number,
     amp: number
 ) => {
-    const { buffer: pbuf, stride: pstride } = pos;
-    const { buffer: vbuf, stride: vstride } = vel;
+    const { buffer: pos, stride: ps } = info.pos;
+    const { buffer: vel, stride: vs } = info.vel;
     for (let i = 0; i < num; i++) {
-        const ip = i * pstride;
-        const iv = i * vstride;
-        setS2(tmp, pbuf, 0, ip);
-        setS2(tmp2, vbuf, 0, iv);
-        normalize(
-            null,
-            mixN2(null, tmp2, dir, 0.01 + 0.2 * fract((i + t) / num))
+        const ip = i * ps;
+        const iv = i * vs;
+        normalizeS2(
+            vel,
+            mixNS2(vel, vel, dir, 0.01 + 0.2 * fract((i + t) / num), iv, iv, 0),
+            1,
+            iv,
+            iv
         );
-        const m = magSq2(tmp);
-        rotateZ(null, tmp, m * amp);
+        const m = magSqS2(pos, ip);
+        rotateS2(pos, pos, m * amp, ip, ip);
         if (m >= 4e4) {
-            mulN2(null, tmp, 0.98);
-            randNorm(tmp2);
+            mulNS2(pos, pos, 0.98, ip, ip);
+            randNormS2(vel, 1, undefined, iv);
         }
-        setS2(pbuf, tmp, ip);
-        setS2(vbuf, tmp2, iv);
-        addS2(pbuf, pbuf, vbuf, ip, ip, iv);
+        addS2(pos, pos, vel, ip, ip, iv);
     }
 };
 
@@ -157,9 +153,8 @@ const app = () => {
         update: (el, gl, __, time) => {
             if (!model) return;
             time *= 0.001;
-            const amp = Math.sin(time / 8) * 4e-7;
-            mixN2(dir, dir, randNorm([0, 0]), 0.1);
-            moveBatch(group.info, group.n, time, amp);
+            mixN2(dir, dir, randNormS2([0, 0]), 0.1);
+            moveBatch(group.info, group.n, time, Math.sin(time / 8) * 4e-7);
             model.attribs.position.buffer!.set(model.attribs.position.data!);
 
             const alpha = Math.pow(Math.min(time / 5, 1), 3) * ALPHA;
