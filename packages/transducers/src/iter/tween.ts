552358207cdf1dfdcb2ca78deac326ecad895fa9
Karsten Schmidt <k@postspectacular.com>
2019-11-09 01:31:13 +0000

    refactor(transducers): update tween() args
    - add TweenOpts
    - add/update docstrings
    
    BREAKING CHANGE: replace tween() args w/ `TweenOpts` config object

--- packages/transducers/src/iter/tween.ts
+++ packages/transducers/src/iter/tween.ts
@@ -2,44 +2,71 @@ import { Fn2 } from "@thi.ng/api";
 import { normRange } from "./norm-range";
 import { repeat } from "./repeat";
 
+export interface TweenOpts<A, B, C> {
+    /**
+     * Total number (n+1) of tweened values to produce
+     */
+    num: number;
+    /**
+     * Min time boundary. Only values in the closed `[min..max]`
+     * time interval will be computed.
+     */
+    min: number;
+    /**
+     * Max time boundary. Only values in the closed `[min..max]`
+     * time interval will be computed.
+     */
+    max: number;
+    /**
+     * Interval producer (from 2 keyframe values, i.e. `stops`)
+     */
+    init: Fn2<A, A, B>;
+    /**
+     * Interval interpolator
+     */
+    mix: Fn2<B, number, C>;
+    /**
+     * Keyframe definitions, i.e. `[time, value]` tuples
+     */
+    stops: [number, A][];
+}
+
 /**
- * Takes a number of keyframe tuples (`stops`) and yields a sequence of
- * `n+1` equally spaced, tweened values. Keyframes are defined as `[pos,
- * value]`. Only values in the closed `minPos` .. `maxPos` interval will
- * be computed.
+ * Keyframe based interpolator. Yields a sequence of `num+1` equally
+ * spaced, tweened values from given keyframe tuples (`stops`).
+ * Keyframes are defined as `[time, value]` tuples. Only values in the
+ * closed `[min..max]` time interval will be computed.
  *
  * Interpolation happens in two stages: First the given `init` function
  * is called to transform/prepare pairs of consecutive keyframes into a
- * single interval (user defined). Then to produce each tweened value
+ * single interval (user defined). Then, to produce each tweened value
  * calls `mix` with the currently active interval and interpolation time
  * value `t` (re-normalized and relative to current interval). The
  * iterator yields results of these `mix()` function calls.
  *
- * Depending on the overall number of samples requested and the distance
- * between keyframes, some keyframes MIGHT be skipped. E.g. if
+ * Depending on the overall `num`ber of samples requested and the
+ * distance between keyframes, some keyframes MIGHT be skipped. E.g. if
  * requesting 10 samples within [0,1], the interval between two
  * successive keyframes at 0.12 and 0.19 would be skipped entirely,
- * since samples will only be taken at multiples of `1/n` (0.0, 0.1,
- * 0.2... in this example).
+ * since samples will only be taken at multiples of `1/num` (i.e. 0.0,
+ * 0.1, 0.2... in this example).
  *
- * The given keyframe positions can lie outside the `minPos`/`maxPos`
- * range and also don't need to cover the range fully. In the latter
- * case, tweened values before the first or after the last keyframe will
- * yield the value of the 1st/last keyframe. If only a single keyframe
- * is given in total, all `n` yielded samples will be that keyframe's
+ * The given keyframe times can lie outside the `min`/`max` range and
+ * also don't need to cover the range fully. In the latter case, tweened
+ * values before the first or after the last keyframe will yield the
+ * value of the first/last keyframe. If only a single keyframe is given
+ * in total, all `num` yielded samples will be that keyframe's
  * transformed value.
  *
  * ```
- * [...tween(
- *   10,
- *   0,
- *   100,
- *   (a, b) => [a, b],
- *   ([a, b], t) => Math.floor(a + (b - a) * t),
- *   [20, 100],
- *   [50, 200],
- *   [80, 0]
- * )]
+ * [...tween({
+ *   num: 10,
+ *   min: 0,
+ *   max: 100,
+ *   init: (a, b) => [a, b],
+ *   mix: ([a, b], t) => Math.floor(a + (b - a) * t),
+ *   stops: [[20, 100], [50, 200], [80, 0]]
+ * })]
  * // [ 100, 100, 100, 133, 166, 200, 133, 66, 0, 0, 0 ]
  * ```
  *
@@ -49,59 +76,42 @@ import { repeat } from "./repeat";
  * ```
  * import { mix, smoothStep } from "@thi.ng/math"
  *
- * [...tween(
- *   10,
- *   0,
- *   100,
- *   (a, b) => [a, b],
- *   ([a, b], t) => Math.floor(mix(a, b, smoothStep(0.1, 0.9, t))),
- *   [20, 100],
- *   [50, 200],
- *   [80, 0]
- * )]
+ * [...tween({
+ *   num: 10,
+ *   min: 0,
+ *   max: 100,
+ *   init: (a, b) => [a, b],
+ *   mix: ([a, b], t) => Math.floor(mix(a, b, smoothStep(0.1, 0.9, t))),
+ *   stops: [[20, 100], [50, 200], [80, 0]]
+ * })]
  * // [ 100, 100, 100, 120, 179, 200, 158, 41, 0, 0, 0 ]
  * ```
- *
+ * @see TweenOpts
  * @see interpolate
  * @see interpolateHermite
  * @see interpolateLinear
  *
- * @param n
- * @param minPos
- * @param maxPos
- * @param init interval producer (from 2 keyframe values)
- * @param mix interval interpolator
- * @param stops keyframe / stops
+ * @param opts
  */
-export function* tween<A, B, C>(
-    n: number,
-    minPos: number,
-    maxPos: number,
-    init: Fn2<A, A, B>,
-    mix: Fn2<B, number, C>,
-    ...stops: [number, A][]
-): IterableIterator<C> {
+export function* tween<A, B, C>(opts: TweenOpts<A, B, C>): IterableIterator<C> {
+    const { min, max, num, init, mix, stops } = opts;
     let l = stops.length;
     if (l < 1) return;
     if (l === 1) {
-        yield* repeat(mix(init(stops[0][1], stops[0][1]), 0), n);
+        yield* repeat(mix(init(stops[0][1], stops[0][1]), 0), num);
     }
     stops.sort((a, b) => a[0] - b[0]);
-    if (stops[l - 1][0] < maxPos) {
-        stops.push([maxPos, stops[l - 1][1]]);
-    }
-    if (stops[0][0] > minPos) {
-        stops.unshift([minPos, stops[0][1]]);
-    }
-    const range = maxPos - minPos;
+    stops[l - 1][0] < max && stops.push([max, stops[l - 1][1]]);
+    stops[0][0] > min && stops.unshift([min, stops[0][1]]);
+    const range = max - min;
     let start = stops[0][0];
     let end = stops[1][0];
     let delta = end - start;
     let interval = init(stops[0][1], stops[1][1]);
     let i = 1;
     l = stops.length;
-    for (let t of normRange(n)) {
-        t = minPos + range * t;
+    for (let t of normRange(num)) {
+        t = min + range * t;
         if (t > end) {
             while (i < l && t > stops[i][0]) i++;
             start = stops[i - 1][0];
