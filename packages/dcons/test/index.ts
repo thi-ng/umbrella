1cfb02a828db3670a745e7d4e30867614f594881
Karsten Schmidt <k@postspectacular.com>
2019-11-15 22:40:17 +0000

    feat(dcons): add ISeqable impl (seq()) & tests

--- packages/dcons/test/index.ts
+++ packages/dcons/test/index.ts
@@ -1,7 +1,6 @@
 import * as assert from "assert";
 import { DCons } from "../src/index";
 
-
 describe("DCons", () => {
     let a: DCons<any>, src: number[];
     beforeEach(() => {
@@ -23,7 +22,35 @@ describe("DCons", () => {
         assert.deepEqual([...a], src);
     });
 
-    it("works as stack");
+    it("is seqable", () => {
+        assert.equal(a.seq()!.first(), 1);
+        // prettier-ignore
+        assert.equal(a.seq()!.next()!.first(), 2);
+        // prettier-ignore
+        assert.equal(a.seq(3)!.first(), 4);
+        // prettier-ignore
+        assert.equal(a.seq(3)!.next()!.first(), 5);
+        // prettier-ignore
+        assert.equal(a.seq(3)!.next()!.next(), undefined);
+        assert.equal(a.seq(2, 2), undefined);
+        assert.equal(a.seq(2, 3)!.first(), 3);
+        assert.equal(a.seq(2, 3)!.next(), undefined);
+    });
 
-    it("works as queue");
+    it("works as stack", () => {
+        assert.equal(a.push(10).pop(), 10);
+        assert.equal(a.pop(), 5);
+        a = new DCons();
+        assert.equal(a.pop(), undefined);
+    });
+
+    it("works as queue", () => {
+        assert.equal(a.push(10).drop(), 1);
+        assert.equal(a.drop(), 2);
+        assert.equal(a.drop(), 3);
+        assert.equal(a.drop(), 4);
+        assert.equal(a.drop(), 5);
+        assert.equal(a.drop(), 10);
+        assert.equal(a.drop(), undefined);
+    });
 });
