b39f4d023fdb90d5ad095b2e50d76e69c2b50843
Karsten Schmidt <k@postspectacular.com>
2019-11-23 17:05:43 +0000

    refactor(rstream-csp): use options object arg
    BREAKING CHANGE: use options object arg for fromChannel()

--- packages/rstream-csp/src/from/channel.ts
+++ packages/rstream-csp/src/from/channel.ts
@@ -2,36 +2,46 @@ import { Channel } from "@thi.ng/csp";
 import { LOGGER, Stream } from "@thi.ng/rstream";
 import { CommonOpts } from "@thi.ng/rstream/api";
 
+export interface FromChannelOpts extends CommonOpts {
+    /**
+     * If true, the parent CSP channel will be closed when this stream
+     * closes.
+     *
+     * @defaultValue true
+     */
+    closeChannel: boolean;
+}
+
 /**
+ * Returns a stream of values received from given
+ * {@link @thi.ng/csp#Channel}.
  *
  * @param src
- * @param closeWhenCancelled
+ * @param opts
  */
 export const fromChannel = <T>(
     src: Channel<T>,
-    closeWhenCancelled = true,
-    opts?: Partial<CommonOpts>
-) =>
-    new Stream<T>(
-        (stream) => {
-            let isActive = true;
-            (async () => {
-                let x;
-                while (((x = null), (x = await src.read())) !== undefined) {
-                    if (x === undefined || !isActive) {
-                        break;
-                    }
-                    stream.next(x);
+    opts?: Partial<FromChannelOpts>
+) => {
+    opts = { id: `channel-${src.id}`, closeChannel: true, ...opts };
+    return new Stream<T>((stream) => {
+        let isActive = true;
+        (async () => {
+            let x;
+            while (((x = null), (x = await src.read())) !== undefined) {
+                if (x === undefined || !isActive) {
+                    break;
                 }
-                stream.done();
-            })();
-            return () => {
-                if (closeWhenCancelled) {
-                    src.close(true);
-                    LOGGER.info("closed channel", src.id);
-                }
-                isActive = false;
-            };
-        },
-        { id: `channel-${src.id}`, ...opts }
-    );
+                stream.next(x);
+            }
+            stream.done();
+        })();
+        return () => {
+            if (opts!.closeChannel !== false) {
+                src.close(true);
+                LOGGER.info("closed channel", src.id);
+            }
+            isActive = false;
+        };
+    }, opts);
+};
