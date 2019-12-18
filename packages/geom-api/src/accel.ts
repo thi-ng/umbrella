baa05d1908a940115690cb3d1dd403173061d63a
Karsten Schmidt <k@postspectacular.com>
2019-12-18 18:39:24 +0000

    feat(geom-api): replace ISpatialAccel w/ new interfaces
    BREAKING CHANGE: replace ISpatialAccel with new interfaces:
    ISpatialMap, ISpatialSet, IRegionQuery

--- packages/geom-api/src/accel.ts
+++ packages/geom-api/src/accel.ts
@@ -1,13 +1,27 @@
 import { Pair } from "@thi.ng/api";
 
-export interface ISpatialAccel<K, V> {
-    add(p: K, v: V, eps?: number): boolean;
-    addAll(pairs: Iterable<Pair<K, V>>, eps?: number): boolean;
-    addKey(k: K, eps?: number): boolean;
-    addKeys(keys: Iterable<K>, eps?: number): boolean;
-    remove(p: K): boolean;
-    has(k: K, eps?: number): boolean;
-    select(q: K, maxNum: number, maxDist?: number): Pair<K, V>[];
-    selectKeys(q: K, maxNum: number, maxDist?: number): K[];
-    selectVals(q: K, maxNum: number, maxDist?: number): V[];
+export interface ISpatialMap<K, V> {
+    readonly size: number;
+
+    set(key: K, v: V, eps?: number): boolean;
+    into(pairs: Iterable<Pair<K, V>>, eps?: number): boolean;
+    remove(key: K): boolean;
+    has(key: K, eps?: number): boolean;
+    get(key: K, eps?: number): V | undefined;
+}
+
+export interface ISpatialSet<K> {
+    readonly size: number;
+
+    add(key: K, eps?: number): boolean;
+    into(keys: Iterable<K>, eps?: number): boolean;
+    remove(key: K): boolean;
+    has(key: K, eps?: number): boolean;
+    get(key: K, eps?: number): K | undefined;
+}
+
+export interface IRegionQuery<K, V, R> {
+    query(q: K, region: R, limit: number, acc?: Pair<K, V>[]): Pair<K, V>[];
+    queryKeys(q: K, region: number, limit: number, acc?: K[]): K[];
+    queryValues(q: K, region: number, limit: number, acc?: V[]): V[];
 }
