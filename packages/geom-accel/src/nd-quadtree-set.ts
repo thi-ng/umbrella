1df4ab5986c4bfd3b3861e2094a489a8a46e8dd4
Karsten Schmidt <k@postspectacular.com>
2019-12-21 00:55:22 +0000

    refactor(geom-accel): minor updates NdQuadtreeSet

--- packages/geom-accel/src/nd-quadtree-set.ts
+++ packages/geom-accel/src/nd-quadtree-set.ts
@@ -63,13 +63,14 @@ export class NdQuadtreeSet<K extends ReadonlyVec>
     }
 
     add(key: K, eps = EPS) {
-        return this.tree.set(key, key, eps);
+        return this.tree.root.set(key, key, eps);
     }
 
     into(keys: Iterable<K>, eps = EPS) {
         let ok = true;
+        const root = this.tree.root;
         for (let k of keys) {
-            ok = this.tree.set(k, k, eps) && ok;
+            ok = root.set(k, k, eps) && ok;
         }
         return ok;
     }
