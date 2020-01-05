88cfaa45bab2f85d176dc6ad9d3551a9f431d68d
Matei Adriel <rafaeladriel11@gmail.com>
2020-01-05 23:05:04 +0200

    feat(api): added the Reverse type

--- packages/api/src/tuple.ts
+++ packages/api/src/tuple.ts
@@ -23,3 +23,24 @@ export type Tail<T extends unknown[]> = ((...a: T) => void) extends (
 export type Prepend<T, U extends unknown[]> = Parameters<
     (v: T, ...args: U) => void
 >;
+
+/**
+ * Internal version of Reverse accepting 1 extra argument for the accumulated value.
+ */
+type ReverseReducer<T extends unknown[], C extends unknown[] = []> = {
+    // case when we got 0 elements
+    0: C;
+
+    // case when we got 1 element
+    1: Prepend<Head<T>, C>;
+
+    // case when we got more than 1 element
+    2: ReverseReducer<Tail<T>, Prepend<Head<T>, C>>;
+
+    // switch between cases
+}[Tail<T> extends [] ? (Head<T> extends never ? 0 : 1) : 2];
+
+/**
+ * Reverses the order of elements from a tuple.
+ */
+export type Reverse<T extends unknown[]> = ReverseReducer<T>;
