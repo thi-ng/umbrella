0536377e866a5bf0b72303700a0f358de24a43cc
Matei Adriel <rafaeladriel11@gmail.com>
2020-01-05 23:06:54 +0200

    chore(api): moved tuple stuff to the correct file

--- packages/api/src/api/tuple.ts
+++ packages/api/src/api/tuple.ts
@@ -1,3 +1,50 @@
 export type Tuple<T, N extends number> = [T, ...T[]] & { length: N };
 
 export type IterableTuple<T, N extends number> = Tuple<T, N> & Iterable<T>;
+
+/**
+ * Extracts the first element of a tuple.
+ */
+export type Head<T extends unknown[]> = T extends Parameters<
+    (v: infer R, ...args: any[]) => any
+>
+    ? R
+    : never;
+
+/**
+ * Extracts everything except the first element from a tuple.
+ */
+export type Tail<T extends unknown[]> = ((...a: T) => void) extends (
+    v: any,
+    ...args: infer R
+) => void
+    ? R
+    : never;
+
+/**
+ * Add an element at the start of an tuple.
+ */
+export type Prepend<T, U extends unknown[]> = Parameters<
+    (v: T, ...args: U) => void
+>;
+
+/**
+ * Internal version of Reverse accepting 1 extra argument for the accumulated value.
+ */
+type ReverseReducer<T extends unknown[], C extends unknown[] = []> = {
+    // case when we got 0 elements
+    0: C;
+
+    // case when we got 1 element
+    1: Prepend<Head<T>, C>;
+
+    // case when we got more than 1 element
+    2: ReverseReducer<Tail<T>, Prepend<Head<T>, C>>;
+
+    // switch between cases
+}[Tail<T> extends [] ? (Head<T> extends never ? 0 : 1) : 2];
+
+/**
+ * Reverses the order of elements from a tuple.
+ */
+export type Reverse<T extends unknown[]> = ReverseReducer<T>;
