9882196a887c842efda2c835d3b86f491893c6f9
Karsten Schmidt <k@postspectacular.com>
2020-02-10 13:25:12 +0000

    feat(binary): add float & 64bit byte conversions

--- packages/binary/src/bytes.ts
+++ packages/binary/src/bytes.ts
@@ -1,3 +1,5 @@
+import { floatToUintBits, floatToUintBits64 } from "./float";
+
 export const bytes16 = (x: number, le = false) => {
     const b0 = x & 0xff;
     const b1 = (x >> 8) & 0xff;
@@ -18,3 +20,16 @@ export const bytes32 = (x: number, le = false) => {
     const b3 = (x >> 24) & 0xff;
     return le ? [b0, b1, b2, b3] : [b3, b2, b1, b0];
 };
+
+export const bytes64 = (hi: number, lo: number, le = false) => {
+    return le
+        ? bytes32(lo, le).concat(bytes32(hi, le))
+        : bytes32(hi, le).concat(bytes32(lo, le));
+};
+
+export const bytesF32 = (x: number, le = false) =>
+    bytes32(floatToUintBits(x), le);
+
+export const bytesF64 = (x: number, le = false) =>
+    //@ts-ignore
+    bytes64(...floatToUintBits64(x), le);
