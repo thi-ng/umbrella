a6b757dd350e46404bfd2f82e58d8a3bc2c5b133
Karsten Schmidt <k@postspectacular.com>
2020-02-15 23:40:13 +0000

    feat(grid-iterators): add line & circle iterators
    - add line(), hline(), vline()
    - add circle()

---
+++ packages/grid-iterators/src/circle.ts
@@ -0,0 +1,46 @@
+import { hline } from "./hvline";
+
+export function* circle(cx: number, cy: number, r: number, fill = true) {
+    if (r < 1) return;
+    r |= 0;
+    let x = 0;
+    let y = r;
+    let y2 = r * r;
+    let sum = y2 + r;
+    let dx2 = 1;
+    let dy2 = 2 * r - 1;
+
+    while (x <= y) {
+        if (fill) {
+            yield* hline(cx - y, cy + x, y << 1);
+            if (x) yield* hline(cx - y, cy - x, y << 1);
+        } else {
+            yield [cx - y, cy + x];
+            if (y) yield [cx + y, cy + x];
+            if (x) {
+                yield [cx - y, cy - x];
+                if (y) yield [cx + y, cy - x];
+            }
+            if (x !== y) {
+                yield [cx - x, cy - y];
+                if (x) yield [cx + x, cy - y];
+                if (y) {
+                    yield [cx - x, cy + y];
+                    if (x) yield [cx + x, cy + y];
+                }
+            }
+        }
+        sum -= dx2;
+        if (sum <= y2) {
+            if (fill && x !== y) {
+                yield* hline(cx - x, cy - y, x << 1);
+                if (y) yield* hline(cx - x, cy + y, x << 1);
+            }
+            y--;
+            y2 -= dy2;
+            dy2 -= 2;
+        }
+        x++;
+        dx2 += 2;
+    }
+}
