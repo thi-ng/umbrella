e82ea4b6695ef4a93aec0a2e797f24fe5698ab9a
alberto <alberto.massa@ymail.com>
2020-02-19 14:24:02 +0100

    docs(geom): better fitIntoBounds3 docstring

--- packages/geom/src/ops/fit-into-bounds.ts
+++ packages/geom/src/ops/fit-into-bounds.ts
@@ -138,7 +138,8 @@ const translateScale3 = (
     );
 
 /**
- * Fit a 3d point array into a 3d bounding box.
+ * Fit a 3d points into a 3d bounding box.
+ * Instead of the 2d version that takes an IShape as input this function works on raw array of Vec.
  *
  * @param out
  * @param pts
