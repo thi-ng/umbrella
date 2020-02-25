b2d923ecf0b41ce6b8a3e1261957825d6dc1ec93
Karsten Schmidt <k@postspectacular.com>
2020-02-25 13:07:21 +0000

    feat(vectors): add cornerBisector()

--- packages/vectors/src/bisect.ts
+++ packages/vectors/src/bisect.ts
@@ -1,8 +1,33 @@
 import { HALF_PI, PI } from "@thi.ng/math";
-import { VecOpRoVV } from "./api";
+import { addmN } from "./addmn";
+import { VecOpRoVV, VecOpVVV } from "./api";
 import { headingXY } from "./heading";
+import { normalize } from "./normalize";
+import { sub } from "./sub";
 
 export const bisect2: VecOpRoVV<number> = (a, b) => {
     const theta = (headingXY(a) + headingXY(b)) / 2;
     return theta <= HALF_PI ? theta : PI - theta;
 };
+
+/**
+ * Returns normalized bisector vector for point `b` in the triangle `a`
+ * -> `b` -> `c`. If `out` is null, creates a new result vector.
+ *
+ * @param out -
+ * @param a -
+ * @param b -
+ * @param c -
+ */
+export const cornerBisector: VecOpVVV = (out, a, b, c) => (
+    !out && (out = []),
+    normalize(
+        out,
+        addmN(
+            out,
+            normalize(out, sub(out, a, b)),
+            normalize(null, sub([], c, b)),
+            0.5
+        )
+    )
+);
