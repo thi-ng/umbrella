188226216099a33b6251540b497ce8fd946502d8
Karsten Schmidt <k@postspectacular.com>
2020-02-25 13:08:37 +0000

    feat(geom-splines): add openCubicFromBreakPoints(), refactor, #208

--- packages/geom-splines/src/cubic-from-breakpoints.ts
+++ packages/geom-splines/src/cubic-from-breakpoints.ts
@@ -1,16 +1,26 @@
 import {
     corner2,
+    cornerBisector,
+    direction,
     dist,
     maddN,
-    mixN,
     mulN,
-    normalize,
     perpendicularCW,
     ReadonlyVec,
     set,
-    sub,
     Vec
 } from "@thi.ng/vectors";
+
+const buildSegments = (tangents: Vec[][], t: number, uniform: boolean) => {
+    const res: Vec[][] = [];
+    for (let i = 0, num = tangents.length - 1; i < num; i++) {
+        const [a, na] = tangents[i];
+        const [b, nb] = tangents[i + 1];
+        const d = uniform ? t : t * dist(a, b);
+        res.push([a, maddN([], na, d, a), maddN([], nb, -d, b), b]);
+    }
+    return res;
+};
 
 export const closedCubicFromBreakPoints = (
     points: ReadonlyVec[],
@@ -24,29 +34,35 @@ export const closedCubicFromBreakPoints = (
         const c = points[(j + 1) % num];
         const n = mulN(
             null,
-            perpendicularCW(
-                null,
-                normalize(
-                    null,
-                    mixN(
-                        null,
-                        normalize(null, sub([], a, b)),
-                        normalize(null, sub([], c, b)),
-                        0.5
-                    )
-                )
-            ),
+            perpendicularCW(null, cornerBisector([], a, b, c)),
             corner2(a, b, c)
         );
         tangents.push([set([], b), n]);
     }
     tangents.push(tangents[0]);
-    const res: Vec[][] = [];
-    for (let i = 0, num = tangents.length - 1; i < num; i++) {
-        const [a, na] = tangents[i];
-        const [b, nb] = tangents[i + 1];
-        const d = uniform ? t : t * dist(a, b);
-        res.push([a, maddN([], na, d, a), maddN([], nb, -d, b), b]);
+    return buildSegments(tangents, t, uniform);
+};
+
+export const openCubicFromBreakPoints = (
+    points: ReadonlyVec[],
+    t = 1 / 3,
+    uniform = false
+) => {
+    const tangents: Vec[][] = [
+        [points[0], direction([], points[0], points[1])]
+    ];
+    const num = points.length - 1;
+    for (let i = 1; i < num; i++) {
+        const a = points[i - 1];
+        const b = points[i];
+        const c = points[i + 1];
+        const n = mulN(
+            null,
+            perpendicularCW(null, cornerBisector([], a, b, c)),
+            corner2(a, b, c)
+        );
+        tangents.push([set([], b), n]);
     }
-    return res;
+    tangents.push([points[num], direction([], points[num - 1], points[num])]);
+    return buildSegments(tangents, t, uniform);
 };
