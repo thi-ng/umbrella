350a6c6cb010e00f2053fb41eeb0f458ee8fb715
Karsten Schmidt <k@postspectacular.com>
2020-03-21 11:46:03 +0000

    feat(strings): add trim() HOF

---
+++ packages/strings/src/trim.ts
@@ -0,0 +1,26 @@
+import { memoize1 } from "@thi.ng/memoize";
+import { Stringer } from "./api";
+
+/**
+ * Higher order trim function (both sides) with support for user defined
+ * trimmable characters (default: whitespace only).
+ *
+ * @example
+ * ```ts
+ * trim()("  Hello   ")
+ * // "Hello"
+ *
+ * trim(" -+")("-+-+- Hello -+-+-")
+ * // "Hello"
+ * ```
+ *
+ * @param chars -
+ */
+export const trim = memoize1<string, Stringer<string>>((chars = " \t\n\r") => {
+    chars = `(${chars
+        .split("")
+        .map((x) => `\\${x}`)
+        .join("|")})`;
+    const re = new RegExp(`(^${chars}+)|(${chars}+$)`, "g");
+    return (x) => x.replace(re, "");
+});
