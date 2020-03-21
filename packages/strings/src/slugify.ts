1ef805be3f0347751eba6da0122e1277a5b81e21
Karsten Schmidt <k@postspectacular.com>
2020-03-21 11:44:52 +0000

    feat(strings): add slugifyGH(), refactor slugify()

--- packages/strings/src/slugify.ts
+++ packages/strings/src/slugify.ts
@@ -8,6 +8,12 @@ const re = new RegExp(src.split("").join("|"), "g");
  * Based on:
  * {@link https://medium.com/@matthagemann/the-ultimate-way-to-slugify-a-url-string-in-javascript-b8e4a0d849e1}
  *
+ * @example
+ * ```ts
+ * slugify("Me, myself (& ëye)!")
+ * // "me-myself-and-eye"
+ * ```
+ *
  * @param str -
  */
 export const slugify: Stringer<string> = (str: string) => {
@@ -18,6 +24,26 @@ export const slugify: Stringer<string> = (str: string) => {
         .replace(/&+/g, "-and-")
         .replace(/[^\w\-]+/g, "")
         .replace(/\-{2,}/g, "-")
-        .replace(/^-+/, "")
-        .replace(/-+$/, "");
+        .replace(/(^-+)|(-+$)/g, "");
+};
+
+/**
+ * Similar to {@link slugify}, however uses GitHub's anchor naming rules
+ * for headings in markdown files (AFAICT).
+ *
+ * @example
+ * ```ts
+ * slugifyGH("Me, myself (& ëye)!")
+ * // "me--myself--eye"
+ * ```
+ *
+ * @param str
+ */
+export const slugifyGH = (str: string) => {
+    return str
+        .toLowerCase()
+        .replace(/\s/g, "-")
+        .replace(re, (c) => dest[src.indexOf(c)])
+        .replace(/[^\w\-]+/g, "")
+        .replace(/(^-+)|(-+$)/g, "");
 };
