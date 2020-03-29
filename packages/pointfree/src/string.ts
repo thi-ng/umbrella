829f3ab129084619c05b434732b46b6c26d32b5e
Karsten Schmidt <k@postspectacular.com>
2020-03-29 16:09:03 +0100

    feat(pointfree): add tojson()/fromjson() conversion ops

--- packages/pointfree/src/string.ts
+++ packages/pointfree/src/string.ts
@@ -1,9 +1,9 @@
 import { StackContext } from "./api";
-import { $n } from "./safe";
+import { $n, $ } from "./safe";
 
 /**
- * Takes a string and stringified regexp, returns true if regexp matches
- * the string.
+ * Takes a string and stringified regexp (w/o flags), returns true if
+ * regexp matches the string.
  *
  * ( str re -- bool )
  *
@@ -15,5 +15,29 @@ export const ismatch = (ctx: StackContext) => {
     $n(n, 0);
     stack[n] = new RegExp(stack[n + 1]).test(stack[n]);
     stack.length--;
+    return ctx;
+};
+
+/**
+ * ( str -- obj )
+ *
+ * @param ctx
+ */
+export const fromjson = (ctx: StackContext) => {
+    const stack = ctx[0];
+    $(stack, 1);
+    stack.push(JSON.parse(stack.pop()));
+    return ctx;
+};
+
+/**
+ * ( x -- str )
+ *
+ * @param ctx
+ */
+export const tojson = (ctx: StackContext) => {
+    const stack = ctx[0];
+    $(stack, 1);
+    stack.push(JSON.stringify(stack.pop(), null, 4));
     return ctx;
 };
