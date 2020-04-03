64948518a6412cabf53664ac9f89bac2b7ef6892
Karsten Schmidt <k@postspectacular.com>
2020-04-03 11:01:34 +0100

    fix(bench): fallback handlingin now()

--- packages/bench/src/now.ts
+++ packages/bench/src/now.ts
@@ -1,9 +1,7 @@
-let M: bigint;
-
 /**
  * If available, returns wrapper for `process.hrtime.bigint()` else
- * falls back to `Date.now()` (scaled to nanoseconds). In all cases,
- * returns a `bigint` timestamp.
+ * falls back to `Date.now()`. In all cases, returns a nanosec-scale
+ * timestamp, either as `bigint` or `number`.
  */
 export const now =
     typeof BigInt !== "undefined"
@@ -11,5 +9,5 @@ export const now =
           typeof process.hrtime !== "undefined" &&
           typeof process.hrtime.bigint === "function"
             ? () => process.hrtime.bigint()
-            : ((M = BigInt(1_000_000)), () => BigInt(Date.now()) * M)
-        : () => Date.now();
+            : () => BigInt(Date.now() * 1e6)
+        : () => Date.now() * 1e6;
