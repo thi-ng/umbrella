1398e2b06a8eace8b61333c36db6e82d6e1478f3
Karsten Schmidt <k@postspectacular.com>
2020-04-18 01:28:18 +0100

    feat(parse): add more whitespace presets

--- packages/parse/src/presets/whitespace.ts
+++ packages/parse/src/presets/whitespace.ts
@@ -8,6 +8,11 @@ import { oneOf } from "../prims/one-of";
 export const WS = oneOf(" \t\n\r", "ws");
 
 /**
+ * Matches single space or tab char.
+ */
+export const SPACE = oneOf(" \t", "space");
+
+/**
  * Matches single `\n` or `\r` char.
  */
 export const NL = oneOf("\n\r");
@@ -20,9 +25,19 @@ export const DNL = discard(NL);
 /**
  * Zero or more {@link WS}. Result will be discarded.
  */
-export const WS_0 = discard(zeroOrMore(WS, "ws0"));
+export const WS_0 = discard(zeroOrMore(WS));
 
 /**
  * One or more {@link WS}. Result will be discarded.
  */
-export const WS_1 = discard(oneOrMore(WS, "ws1"));
+export const WS_1 = discard(oneOrMore(WS));
+
+/**
+ * Zero or more {@link SPACE}. Result will be discarded.
+ */
+export const SPACE_0 = discard(zeroOrMore(SPACE));
+
+/**
+ * One or more {@link SPACE}. Result will be discarded.
+ */
+export const SPACE_1 = discard(oneOrMore(SPACE));
