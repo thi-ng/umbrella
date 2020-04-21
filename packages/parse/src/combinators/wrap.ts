a3dae6ee1c54b42f789947e6e7fcc39a92e5cbcb
Karsten Schmidt <k@postspectacular.com>
2020-04-21 11:41:51 +0100

    refactor(parse): update wrap() combinator

--- packages/parse/src/combinators/wrap.ts
+++ packages/parse/src/combinators/wrap.ts
@@ -3,9 +3,5 @@ import { litD } from "../prims/lit";
 import { hoist } from "../xform/hoist";
 import { seq } from "./seq";
 
-export const wrap = <T>(
-    parser: Parser<T>,
-    pre: T,
-    post: T = pre,
-    id = "wrap"
-) => hoist(seq([litD(pre), parser, litD(post)], id));
+export const wrap = <T>(parser: Parser<T>, pre: T, post: T = pre) =>
+    hoist(seq([litD(pre), parser, litD(post)]));
