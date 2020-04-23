041f590f6c1c29efd01fccc26cbbb2c0992e1147
Karsten Schmidt <k@postspectacular.com>
2020-04-23 13:02:45 +0100

    fix(vectors): add missing equals2/3/4 exports

--- packages/vectors/src/equals.ts
+++ packages/vectors/src/equals.ts
@@ -4,9 +4,14 @@ import { vop } from "./internal/vop";
 
 export const equals: MultiVecOpRoVV<boolean> = vop(0);
 
-equals.add(2, (a, b) => a[0] === b[0] && a[1] === b[1]);
-equals.add(3, (a, b) => a[0] === b[0] && a[1] === b[1] && a[2] === b[2]);
-equals.add(
+export const equals2 = equals.add(2, (a, b) => a[0] === b[0] && a[1] === b[1]);
+
+export const equals3 = equals.add(
+    3,
+    (a, b) => a[0] === b[0] && a[1] === b[1] && a[2] === b[2]
+);
+
+export const equals4 = equals.add(
     4,
     (a, b) => a[0] === b[0] && a[1] === b[1] && a[2] === b[2] && a[3] === b[3]
 );
