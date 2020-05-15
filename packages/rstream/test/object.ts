f3ca3b6fef4568cb5c8992166e1db931c173ffca
Karsten Schmidt <k@postspectacular.com>
2020-05-15 18:35:30 +0100

    feat(rstream): update fromObject(), add StreamObjOpts, update docs

--- packages/rstream/test/object.ts
+++ packages/rstream/test/object.ts
@@ -1,36 +1,66 @@
 import * as assert from "assert";
-import { fromObject, State, Subscription } from "../src/index";
+import { fromObject, State, stream, Subscription } from "../src/index";
+
+type Foo = { a?: number; b: string };
 
 describe("fromObject", () => {
     it("basic", () => {
-        const streams = fromObject(<{ a?: number; b: string }>{
+        const obj = fromObject(<{ a?: number; b: string }>{
             a: 1,
             b: "foo",
         });
-        assert(streams.a instanceof Subscription);
-        assert(streams.b instanceof Subscription);
-        assert(streams.a.id.startsWith("obj-a-"));
-        assert(streams.b.id.startsWith("obj-b-"));
+        assert(obj.streams.a instanceof Subscription);
+        assert(obj.streams.b instanceof Subscription);
+        assert(obj.streams.a.id.startsWith("obj-a-"));
+        assert(obj.streams.b.id.startsWith("obj-b-"));
 
         const acc: any = { a: [], b: [] };
-        streams.a.subscribe({
+        obj.streams.a.subscribe({
             next(x) {
                 acc.a.push(x);
             },
         });
-        streams.b.subscribe({
+        obj.streams.b.subscribe({
             next(x) {
                 acc.b.push(x);
             },
         });
-        streams.__next({ a: 2, b: "bar" });
-        streams.__next({ b: "baz" });
-        streams.__done();
+        obj.next({ a: 2, b: "bar" });
+        obj.next({ b: "baz" });
+        obj.done();
         assert.deepEqual(acc, {
             a: [1, 2, undefined],
             b: ["foo", "bar", "baz"],
         });
-        assert.equal(streams.a.getState(), State.DONE);
-        assert.equal(streams.b.getState(), State.DONE);
+        assert.equal(obj.streams.a.getState(), State.DONE);
+        assert.equal(obj.streams.b.getState(), State.DONE);
+    });
+
+    it("subscriber", () => {
+        const acc: any = { a: [], b: [] };
+        const obj = fromObject(<Foo>{}, ["a", "b"], { initial: false });
+        obj.streams.a.subscribe({
+            next(x) {
+                acc.a.push(x);
+            },
+        });
+        obj.streams.b.subscribe({
+            next(x) {
+                acc.b.push(x);
+            },
+        });
+
+        const src = stream<Foo>();
+        src.subscribe(obj);
+
+        src.next({ a: 1, b: "foo" });
+        src.next({ b: "bar" });
+        src.done();
+        assert.deepEqual(acc, {
+            a: [1, undefined],
+            b: ["foo", "bar"],
+        });
+        assert.equal(obj.streams.a.getState(), State.DONE);
+        assert.equal(obj.streams.b.getState(), State.DONE);
     });
 });
