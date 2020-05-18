84b2cce7a1e96c1a16dcd7a7639d1fe4e82d2f56
Karsten Schmidt <k@postspectacular.com>
2020-05-18 01:17:05 +0100

    fix(hdom2020): update List, remove @thi.ng/equiv dep
    - actually use provided equiv pred
    - update default equiv to: a === b

--- packages/hdom2020/src/list.ts
+++ packages/hdom2020/src/list.ts
@@ -1,5 +1,4 @@
 import type { Fn, Predicate2 } from "@thi.ng/api";
-import { equiv as _equiv } from "@thi.ng/equiv";
 import type { ISubscribable } from "@thi.ng/rstream";
 import type { IComponent, IMountWithState } from "./api";
 import { $compile } from "./compile";
@@ -10,9 +9,9 @@ export const $list = <T>(
     src: ISubscribable<T[]>,
     tag: string,
     attribs: any,
-    childCtor: Fn<T, any>,
+    ctor: Fn<T, any>,
     equiv?: Predicate2<T>
-) => $sub<T[]>(src, new List<T>(tag, attribs, childCtor, equiv));
+) => $sub<T[]>(src, new List<T>(tag, attribs, ctor, equiv));
 
 export class List<T> implements IMountWithState<T[]> {
     el?: Element;
@@ -23,8 +22,8 @@ export class List<T> implements IMountWithState<T[]> {
     constructor(
         protected tag: string,
         protected attribs: any,
-        protected childCtor: Fn<T, IComponent>,
-        protected equiv: Predicate2<T> = _equiv
+        protected ctor: Fn<T, IComponent>,
+        protected equiv: Predicate2<T> = (a, b) => a === b
     ) {}
 
     async mount(parent: Element, state: T[]) {
@@ -48,14 +47,15 @@ export class List<T> implements IMountWithState<T[]> {
     async update(curr: T[]) {
         if (!curr) return;
         const root = this.root!.el!;
-        const ctor = this.childCtor;
+        const ctor = this.ctor;
+        const equiv = this.equiv;
         const children = this.children!;
         const prev = this.items!;
         const nb = curr.length;
         let na = prev!.length;
         let n = Math.min(na, nb);
         for (let i = 0; i < n; i++) {
-            if (prev[i] !== curr[i]) {
+            if (!equiv(prev[i], curr[i])) {
                 await children[i].unmount();
                 const val = curr[i];
                 const child = $compile(ctor(val));
