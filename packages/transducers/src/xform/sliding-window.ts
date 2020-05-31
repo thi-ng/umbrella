13f4184f755fadb0a585b7e443c1218a7e2df5db
Karsten Schmidt <k@postspectacular.com>
2020-05-31 20:55:56 +0100

    feat(transducers): add IDeref support slidingWindow()
    (cherry picked from commit c75175689544f172acde856b4261ca9dc128d1dd)

--- packages/transducers/src/xform/sliding-window.ts
+++ packages/transducers/src/xform/sliding-window.ts
@@ -1,12 +1,18 @@
+import { deref, MaybeDeref } from "@thi.ng/api";
+import type { Reducer, Transducer } from "../api";
 import { compR } from "../func/compr";
 import { $iter } from "../iterator";
-import type { Reducer, Transducer } from "../api";
 
 /**
  * Sliding window transducer, similar to `partition(size, 1)`, but
  * supports initially partially filled windows, if `partial` is set to
  * true (default). Each emitted window is a shallow copy of the internal
  * accumulation buffer.
+ *
+ * @remarks
+ * If `size` is implements {@link IDeref}, the window size will be
+ * re-evaluated for each new input and therefore can be used as
+ * mechanism to dynamically adjust the window size.
  *
  * @example
  * ```ts
@@ -22,33 +28,32 @@ import type { Reducer, Transducer } from "../api";
  * @param src -
  */
 export function slidingWindow<T>(
-    size: number,
+    size: MaybeDeref<number>,
     partial?: boolean
 ): Transducer<T, T[]>;
 export function slidingWindow<T>(
-    size: number,
+    size: MaybeDeref<number>,
     src: Iterable<T>
 ): IterableIterator<T[]>;
 export function slidingWindow<T>(
-    size: number,
+    size: MaybeDeref<number>,
     partial: boolean,
     src: Iterable<T>
 ): IterableIterator<T[]>;
 export function slidingWindow<T>(...args: any[]): any {
     const iter = $iter(slidingWindow, args);
-    if (iter) {
-        return iter;
-    }
-    const size: number = args[0];
+    if (iter) return iter;
+    const size: MaybeDeref<number> = args[0];
     const partial: boolean = args[1] !== false;
     return (rfn: Reducer<any, T[]>) => {
         const reduce = rfn[2];
         let buf: T[] = [];
         return compR(rfn, (acc, x: T) => {
             buf.push(x);
-            if (partial || buf.length === size) {
+            const _size = deref(size);
+            if (partial || buf.length >= _size) {
                 acc = reduce(acc, buf);
-                buf = buf.slice(buf.length === size ? 1 : 0);
+                buf = buf.slice(buf.length >= _size ? 1 : 0, _size);
             }
             return acc;
         });
