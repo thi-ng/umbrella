41c8a9d696211b13bde358dae431f110ab7b4be5
Karsten Schmidt <k@postspectacular.com>
2020-06-05 13:21:38 +0100

    feat(hdom-canvas): remove obsolete files
    BREAKING CHANGE: tree traversal & rendering parts extracted to new
    package @thi.ng/hiccup-canvas
    
    From now on, this package only contains the canvas component wrapper & hdom related interface implementations, allowing canvas rendering parts to be used separately.

--- packages/hdom-canvas/src/api.ts
+++
@@ -1,8 +0,0 @@
-import type { IObjectOf } from "@thi.ng/api";
-
-export interface DrawState {
-    attribs: IObjectOf<any>;
-    grads?: IObjectOf<CanvasGradient>;
-    edits: string[];
-    restore?: boolean;
-}
