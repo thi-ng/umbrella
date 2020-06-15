fef3eab20bdd18e5ad9e1a9e661577ec53e2df53
Karsten Schmidt <k@postspectacular.com>
2020-06-15 14:17:37 +0100

    feat(examples): add pagination (search-doc)

--- examples/hdom2020-search-docs/src/index.ts
+++ examples/hdom2020-search-docs/src/index.ts
@@ -1,13 +1,17 @@
 import { $compile, $list } from "@thi.ng/hdom2020";
-import { CloseMode, debounce, fromIterable } from "@thi.ng/rstream";
-import { map } from "@thi.ng/transducers";
+import { clamp } from "@thi.ng/math";
+import { CloseMode, debounce, fromIterable, sync } from "@thi.ng/rstream";
+import { map, page } from "@thi.ng/transducers";
 import { indexSize, search } from "./search";
 // IMPORTANT: this file is not checked into git (~350KB) and must be downloaded from:
 // https://gist.github.com/postspectacular/8370adeaf262299065db4aa5849e40d7
 import INDEX from "./search.json";
 
-const BASE_URL = "https://github.com/thi-ng/umbrella/tree/develop/packages/";
-const SIZE = indexSize(INDEX);
+const INDEX_STATS = indexSize(INDEX);
+const REPO_URL = "https://github.com/thi-ng/umbrella/";
+const BASE_URL = `${REPO_URL}tree/develop/packages/`;
+const SRC_URL = `${REPO_URL}/tree/feature/hdom2020/examples/hdom2020-search-docs`;
+const PAGE_SIZE = 25;
 
 // search query stream
 const query = fromIterable(["stream"], { closeIn: CloseMode.NEVER });
@@ -15,34 +19,97 @@ const query = fromIterable(["stream"], { closeIn: CloseMode.NEVER });
 // build results as transformation of query stream
 // first debounce query stream (for fast typers)
 const queryResults = query
-    .subscribe(debounce(250))
+    .subscribe(debounce(100))
     .transform(map((q) => search(INDEX, q, "") || []));
+
+// pagination state
+const pageID = fromIterable([0], { closeIn: CloseMode.NEVER });
+const maxPageID = queryResults.transform(
+    map((res) => ~~(res.length / PAGE_SIZE))
+);
+
+// produce search result page using `page()` transducer
+// the`sync()` construct is a stream combinator which requires all input
+// streams to produce a value first before it emits its own initial
+// result...
+const resultPage = sync<any, string[][]>({
+    src: { queryResults, pageID, maxPageID },
+    xform: map(
+        ({ queryResults, pageID, maxPageID }) =>
+            <string[][]>[
+                ...page(clamp(pageID, 0, maxPageID), PAGE_SIZE, queryResults),
+            ]
+    ),
+});
 
 // event handler for input field
 const updateQuery = (e: InputEvent) => {
     const term = (<HTMLInputElement>e.target).value;
-    term.length > 0 && query.next(term);
+    if (term.length > 0) {
+        query.next(term.toLowerCase());
+        pageID.next(0);
+    }
 };
 
+// event handlers for pager buttons
+const updatePage = (step: number) =>
+    pageID.next(clamp(pageID.deref()! + step, 0, maxPageID.deref() || 0));
+
+const setPage = (id: number) => pageID.next(id);
+
 // compile component tree, including embedded reactive values/streams
+// and controlflow structures (uses tachyons CSS classes for styling)
 $compile([
     "div.ma2.measure-ns.center-ns.f7.f6-ns",
     {},
-    ["h1.mv2", {}, "thi.ng/umbrella doc search"],
+    ["h1.mv0", {}, "thi.ng/umbrella doc search"],
+    ["div.mb2.f7", {}, ["a.link.blue", { href: SRC_URL }, "Source code"]],
     [
         "input.w-100.mv2.pa2",
         { type: "text", autofocus: true, oninput: updateQuery, value: query },
     ],
+    // query result & search index stats
     [
-        "div",
+        "div.mv2",
         {},
         // derived view of result stream to compute number of results
         queryResults.transform(map((results) => results.length)),
-        ` results (total keys: ${SIZE.keys} in ${SIZE.files} files)`,
+        ` results (total keys: ${INDEX_STATS.keys} in ${INDEX_STATS.files} files)`,
     ],
-    // reactive list component
+    // pagination
+    [
+        "div.mv2.w-100",
+        {
+            style: {
+                // only show if there's a need for it...
+                display: maxPageID.transform(
+                    map((x) => (x > 0 ? "flex" : "none"))
+                ),
+            },
+        },
+        [
+            "div.w-33.tl",
+            {},
+            ["button", { onclick: () => setPage(0) }, "<<"],
+            ["button", { onclick: () => updatePage(-1) }, "<"],
+        ],
+        [
+            "div.w-34.tc",
+            {},
+            pageID.transform(map((x) => x + 1)),
+            " / ",
+            maxPageID.transform(map((x) => x + 1)),
+        ],
+        [
+            "div.w-33.tr",
+            {},
+            ["button", { onclick: () => updatePage(1) }, ">"],
+            ["button", { onclick: () => setPage(maxPageID.deref()!) }, ">>"],
+        ],
+    ],
+    // reactive list component (for search results)
     // the function arg is used to create new list items if needed
-    $list(queryResults, "ul.list.pl0", {}, ([suffix, file]) => [
+    $list(resultPage, "ul.list.pl0", {}, ([suffix, file]) => [
         "li",
         {},
         [
@@ -52,6 +119,6 @@ $compile([
             ["span.b.bg-washed-green", {}, query.deref()],
             `${suffix}: `,
         ],
-        ["a.link.blue", { href: BASE_URL + file }, file],
+        ["a.link.blue", { href: BASE_URL + file, target: "_new" }, file],
     ]),
 ]).mount(document.getElementById("app")!);
