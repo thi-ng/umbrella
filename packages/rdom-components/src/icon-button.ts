d8d478fd977076d1220f823b1168da4b01970ad0
Karsten Schmidt <k@postspectacular.com>
2020-07-12 03:26:11 +0100

    refactor(rdom-components): update attribs (tabs, iconbt)

--- packages/rdom-components/src/icon-button.ts
+++ packages/rdom-components/src/icon-button.ts
@@ -4,25 +4,22 @@ import { button, i, InputAttribs } from "@thi.ng/hiccup-html";
 export interface IconButtonOpts {
     attribs: Partial<InputAttribs>;
     icon: any;
-    iconPos?: "L" | "R";
+    iconPos?: "left" | "right";
     label?: MaybeDeref<string>;
 }
 
-export const iconButton = (opts: IconButtonOpts) =>
-    opts.iconPos !== "R"
-        ? button(
-              opts.attribs,
-              i(
-                  { style: { fill: "currentColor", "margin-right": "0.5rem" } },
-                  opts.icon
-              ),
-              opts.label
-          )
-        : button(
-              opts.attribs,
-              opts.label,
-              i(
-                  { style: { fill: "currentColor", "margin-left": "0.5rem" } },
-                  opts.icon
-              )
-          );
+export const iconButton = (opts: IconButtonOpts) => {
+    const isLeft = opts.iconPos !== "right";
+    const icon = i(
+        {
+            style: {
+                fill: "currentColor",
+                [`margin-${isLeft ? "right" : "left"}`]: "0.5rem",
+            },
+        },
+        opts.icon
+    );
+    return isLeft
+        ? button(opts.attribs, icon, opts.label)
+        : button(opts.attribs, opts.label, icon);
+};
