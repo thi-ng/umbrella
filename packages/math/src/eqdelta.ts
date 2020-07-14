5404a5699a44d7ef6c2ccb5804f2b099a4358eb1
Karsten Schmidt <k@postspectacular.com>
2020-07-14 12:25:09 +0100

    refactor(math): swap `eqDelta()` impls, rename
    BREAKING CHANGE: Revert/rename `eqDeltaFixed()` => `eqDelta()`. Rename curr `eqDelta()` => `eqDeltaScaled()`
    
    - this is essentially a revert to 5018009
    - also keep, but deprecate `eqDeltaFixed()` (synonym for `eqDelta` now)

--- packages/math/src/eqdelta.ts
+++ packages/math/src/eqdelta.ts
@@ -4,6 +4,20 @@ const abs = Math.abs;
 const max = Math.max;
 
 /**
+ * Similar to {@link eqDelta}, but used given `eps` as is.
+ *
+ * @param a - left value
+ * @param b - right value
+ * @param eps - epsilon / tolerance, default `1e-6`
+ */
+export const eqDelta = (a: number, b: number, eps = EPS) => abs(a - b) <= eps;
+
+/**
+ * @deprecated
+ */
+export const eqDeltaFixed = eqDelta;
+
+/**
  * Checks if `|a - b| <= ε` and adapts given epsilon value to the given
  * arguments:
  *
@@ -16,15 +30,5 @@ const max = Math.max;
  * @param b - right value
  * @param eps - epsilon / tolerance, default `1e-6`
  */
-export const eqDelta = (a: number, b: number, eps = EPS) =>
+export const eqDeltaScaled = (a: number, b: number, eps = EPS) =>
     abs(a - b) <= eps * max(1, abs(a), abs(b));
-
-/**
- * Similar to {@link eqDelta}, but used given `eps` as is.
- *
- * @param a - left value
- * @param b - right value
- * @param eps - epsilon / tolerance, default `1e-6`
- */
-export const eqDeltaFixed = (a: number, b: number, eps = EPS) =>
-    abs(a - b) <= eps;
