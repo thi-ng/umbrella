253ed3f79ff4d073f18adaacb4001ec6651341e2
Karsten Schmidt <k@postspectacular.com>
2020-07-18 12:11:40 +0100

    minor(parse): update indent helper

--- packages/parse/src/utils.ts
+++ packages/parse/src/utils.ts
@@ -7,5 +7,4 @@ const cache: string[] = [];
  *
  * @internal
  */
-export const indent = (x: number) =>
-    x > 0 ? cache[x] || (cache[x] = "  ".repeat(x)) : "";
+export const indent = (x: number) => (cache[x] = "  ".repeat(x));
