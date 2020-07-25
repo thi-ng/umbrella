32340f0e2a5a817daf49f1d72e78cbb415704d4b
Karsten Schmidt <k@postspectacular.com>
2020-07-25 23:03:30 +0100

    refactor(rstream): add opts arg for trigger()

--- packages/rstream/src/trigger.ts
+++ packages/rstream/src/trigger.ts
@@ -1,14 +1,21 @@
-import { fromIterable } from "./from/iterable";
+import { CommonOpts } from "./api";
+import { fromIterableSync } from "./from/iterable";
 import { Stream } from "./stream";
 import { optsWithID } from "./utils/idgen";
 
 /**
- * Utility stream. Returns a new stream which emits a single value `x`
- * (if given) and then closes. If no arg is provided, the stream emits a
- * single `true`.
+ * Utility stream. Returns a new stream which emits a single value `x` (if
+ * given) and then closes. If no arg is provided, the stream emits a single
+ * `true`.
+ *
+ * @remarks
+ * Syntax sugar for {@link fromIterableSync}.
+ *
+ * @param x -
+ * @param opts -
  */
 export function trigger(): Stream<boolean>;
-export function trigger<T>(x: T): Stream<T>;
-export function trigger(x: any = true) {
-    return fromIterable([x], optsWithID("trigger"));
+export function trigger<T>(x: T, opts?: Partial<CommonOpts>): Stream<T>;
+export function trigger(x: any = true, opts?: Partial<CommonOpts>) {
+    return fromIterableSync([x], optsWithID("trigger", opts));
 }
