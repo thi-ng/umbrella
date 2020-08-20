e699f545d5ed771f402b6054734b34bf8e39287c
Karsten Schmidt <k@postspectacular.com>
2020-08-20 11:30:05 +0100

    feat(examples): add dynamic node addition (rdom-svg-nodes)

--- examples/rdom-svg-nodes/src/index.ts
+++ examples/rdom-svg-nodes/src/index.ts
@@ -1,8 +1,9 @@
 import { defAtom } from "@thi.ng/atom";
-import { $compile } from "@thi.ng/rdom";
-import { fromView } from "@thi.ng/rstream";
-import { map, range, repeatedly } from "@thi.ng/transducers";
-import { random2, Vec } from "@thi.ng/vectors";
+import { equivArrayLike } from "@thi.ng/equiv";
+import { $compile, $list } from "@thi.ng/rdom";
+import { fromAtom } from "@thi.ng/rstream";
+import { indexed, partition, repeatedly } from "@thi.ng/transducers";
+import { random2 } from "@thi.ng/vectors";
 
 const WIDTH = 600;
 const NUM = 10;
@@ -11,15 +12,8 @@ const R = 20;
 // define atom of NUM random points
 const db = defAtom([...repeatedly(() => random2([], R, WIDTH - R), NUM)]);
 
-// attach reactive stream for each point
-const points = [...map((i) => fromView(db, { path: [i] }), range(NUM))];
-
 // ID of currently dragged point
 let clicked = -1;
-
-// destructuring helpers
-const $x = (p: Vec) => p[0];
-const $y = (p: Vec) => p[1];
 
 $compile([
     "svg",
@@ -32,45 +26,43 @@ $compile([
         onmousemove: (e: MouseEvent) =>
             clicked !== -1 && db.resetIn([clicked], [e.clientX, e.clientY]),
         onmouseup: () => (clicked = -1),
+        // add new point on double click
+        ondblclick: (e: MouseEvent) =>
+            db.swap((pts) => [...pts, [e.clientX, e.clientY]]),
     },
-    [
+    // reactive list of lines
+    $list(
+        // transform atom view into consecutive pairs:
+        // e.g. [a,b,c,d] => [[a,b],[b,c],[c,d]]
+        fromAtom(db).map((pts) => [...partition(2, 1, pts)]),
         "g",
         { stroke: "#00f" },
-        ...map((i) => {
-            const a = points[i];
-            const b = points[i + 1];
-            // define SVG line element with reactive end points
-            // line will only update if end points change
-            return [
-                "line",
-                {
-                    x1: a.map($x),
-                    y1: a.map($y),
-                    x2: b.map($x),
-                    y2: b.map($y),
-                },
-            ];
-        }, range(NUM - 1)),
-    ],
-    [
+        ([a, b]) => ["line", { x1: a[0], y1: a[1], x2: b[0], y2: b[1] }],
+        // value based equivalence predicate
+        equivArrayLike
+    ),
+    // reactive list of circles
+    $list(
+        // transform atom view to label each point with its array index
+        // (needed to determine point selection in mouse event handler)
+        fromAtom(db).map((pts) => [...indexed(0, pts)]),
         "g",
         { fill: "#00f" },
-        // likewise, define SVG circle with reactive center point
+        // define SVG circle with reactive center point
         // update point position in atom on mouse click
-        ...map(
-            (i) => [
-                "circle",
-                {
-                    cx: points[i].map($x),
-                    cy: points[i].map($y),
-                    r: R,
-                    onmousedown: (e: MouseEvent) => {
-                        clicked = i;
-                        db.resetIn([i], [e.clientX, e.clientY]);
-                    },
+        ([i, p]) => [
+            "circle",
+            {
+                cx: p[0],
+                cy: p[1],
+                r: R,
+                onmousedown: (e: MouseEvent) => {
+                    clicked = i;
+                    db.resetIn([i], [e.clientX, e.clientY]);
                 },
-            ],
-            range(NUM)
-        ),
-    ],
+            },
+        ],
+        // value based equivalence predicate
+        equivArrayLike
+    ),
 ]).mount(document.getElementById("app")!);
