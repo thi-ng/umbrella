d40c4a7b33e1a88f1ffb928086f3f10d7744cce0
Karsten Schmidt <k@postspectacular.com>
2020-08-28 15:17:32 +0100

    refactor(transducers): update/fix normRange2d/3d()

--- packages/transducers/src/iter/norm-range.ts
+++ packages/transducers/src/iter/norm-range.ts
@@ -1,3 +1,5 @@
+import { map } from "../xform/map";
+
 /**
  * Yields sequence of `n+1` monotonically increasing numbers in the
  * closed interval (0.0 .. 1.0). If `n <= 0`, yields nothing.
@@ -9,49 +11,58 @@
  * ```
  *
  * @param n - number of steps
- * @param inclLast - include last value (i.e. `1.0`)
+ * @param includeLast - include last value (i.e. `1.0`)
  */
 export function* normRange(
     n: number,
-    inclLast = true
+    includeLast = true
 ): IterableIterator<number> {
     if (n > 0) {
-        for (let i = 0, m = inclLast ? n + 1 : n; i < m; i++) {
+        for (let i = 0, m = includeLast ? n + 1 : n; i < m; i++) {
             yield i / n;
         }
     }
 }
 
+/**
+ * 2D version of {@link normRange} in Y-major order (i.e. X is inner loop).
+ *
+ * @param nx
+ * @param ny
+ * @param includeLastX
+ * @param includeLastY
+ */
 export function* normRange2d(
     nx: number,
     ny: number,
-    includeLastX: boolean = true,
-    includeLastY: boolean = true,
-) { 
-    const rx = normRange(nx, includeLastX);
+    includeLastX = true,
+    includeLastY = true
+) {
+    const rx = [...normRange(nx, includeLastX)];
     for (let y of normRange(ny, includeLastY)) {
-        for (let x of rx) {
-            yield [x, y];
-        }
+        yield* map((x) => [x, y], rx);
     }
 }
 
+/**
+ * 3D version of {@link normRange} in Z-major order (i.e. X being innermost
+ * loop).
+ *
+ * @param nx
+ * @param ny
+ * @param includeLastX
+ * @param includeLastY
+ */
 export function* normRange3d(
     nx: number,
     ny: number,
     nz: number,
-    includeLastX: boolean = true,
-    includeLastY: boolean = true,
-    includeLastZ: boolean = true,
-) { 
-    const rx = normRange(nx, includeLastX);
-    const ry = normRange(ny, includeLastY);
+    includeLastX = true,
+    includeLastY = true,
+    includeLastZ = true
+) {
+    const sliceXY = [...normRange2d(nx, ny, includeLastX, includeLastY)];
     for (let z of normRange(nz, includeLastZ)) {
-        for (let y of ry) {
-            for (let x of rx) {
-                yield [x, y, z];
-            }
-        }
+        yield* map((xy) => [...xy, z], sliceXY);
     }
 }
-
