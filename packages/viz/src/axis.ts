b423600bbf208e8630ecb2205eec45895e6b8ea8
Karsten Schmidt <k@postspectacular.com>
2020-09-05 21:58:09 +0100

    feat(viz): add lensAxis(), lensScale(), InitialAxisSpec

--- packages/viz/src/axis.ts
+++ packages/viz/src/axis.ts
@@ -2,8 +2,8 @@ import { mergeDeepObj } from "@thi.ng/associative";
 import { inRange, roundTo } from "@thi.ng/math";
 import { float } from "@thi.ng/strings";
 import { filter, range } from "@thi.ng/transducers";
-import { AxisSpec, Domain } from "./api";
-import { linearScale } from "./scale";
+import type { AxisSpec, Domain } from "./api";
+import { lensScale, linearScale } from "./scale";
 
 const NO_TICKS = () => [];
 
@@ -21,17 +21,32 @@ const axisDefaults = (): Partial<AxisSpec> => ({
     minor: { ticks: NO_TICKS, size: 5 },
 });
 
-export const linTickMarks = (step = 1) => ([d1, d2]: Domain) => [
+export const linTickMarks = (step: number) => ([d1, d2]: Domain) => [
     ...filter(
         (x) => inRange(x, d1, d2),
         range(roundTo(d1, step), d2 + step, step)
     ),
 ];
 
-export const linearAxis = (
-    src: Partial<AxisSpec> & Pick<AxisSpec, "domain" | "range" | "pos">
-) => {
+type InitialAxisSpec = Partial<AxisSpec> &
+    Pick<AxisSpec, "domain" | "range" | "pos">;
+
+export const linearAxis = (src: InitialAxisSpec) => {
     const spec = <AxisSpec>mergeDeepObj(axisDefaults(), src);
     !spec.scale && (spec.scale = linearScale(spec.domain, spec.range));
+    return spec;
+};
+
+export const lensAxis = (
+    src: InitialAxisSpec & { focus: number; strength: number }
+): AxisSpec => {
+    const spec = mergeDeepObj(axisDefaults(), src);
+    !spec.scale &&
+        (spec.scale = lensScale(
+            spec.domain,
+            spec.range,
+            spec.focus,
+            spec.strength
+        ));
     return spec;
 };
