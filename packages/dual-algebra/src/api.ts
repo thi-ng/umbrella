24380544167d70bd166c6fc8471d5bb67e04b468
Karsten Schmidt <k@postspectacular.com>
2020-09-05 18:27:16 +0100

    refactor(dual-algebra): update deps, imports, use new Fn types

--- packages/dual-algebra/src/api.ts
+++ packages/dual-algebra/src/api.ts
@@ -1,9 +1,26 @@
-import type { Fn, Fn2, Fn3, Fn4, NumericArray } from "@thi.ng/api";
+import type {
+    Fn,
+    Fn2,
+    FnU2,
+    FnU3,
+    FnU4,
+    FnU5,
+    FnU6,
+    NumericArray,
+} from "@thi.ng/api";
 
 export type Dual = NumericArray;
 
 export type Op1 = Fn<Dual, Dual>;
+
 export type Op1N = Fn2<Dual, number, Dual>;
-export type Op2 = Fn2<Dual, Dual, Dual>;
-export type Op3 = Fn3<Dual, Dual, Dual, Dual>;
-export type Op4 = Fn4<Dual, Dual, Dual, Dual, Dual>;
+
+export type Op2 = FnU2<Dual>;
+
+export type Op3 = FnU3<Dual>;
+
+export type Op4 = FnU4<Dual>;
+
+export type Op5 = FnU5<Dual>;
+
+export type Op6 = FnU6<Dual>;
--- packages/dual-algebra/src/poly.ts
+++ packages/dual-algebra/src/poly.ts
@@ -1,8 +1,9 @@
-import { Dual, Op4 } from "./api";
+import type { FnU4, FnU5, FnU6 } from "@thi.ng/api";
+import type { Dual, Op4, Op5 } from "./api";
 import { add, mul } from "./ops";
 
 /**
- * Computes: `ax^2 + bx + c`
+ * Computes: `ax^2 + bx + c`. All args must have same size/arity.
  *
  * @param x
  * @param a
@@ -21,11 +22,11 @@ export const quadratic: Op4 = (x, a, b, c) =>
  * @param b
  * @param c
  */
-export const quadraticS = (x: number, a: number, b: number, c: number) =>
+export const quadraticS: FnU4<number, Dual> = (x, a, b, c) =>
     quadratic([x, 1], [a, 0], [b, 0], [c, 0]);
 
 /**
- * Computes: `ax^3 + bx^2 + cx + d`
+ * Computes: `ax^3 + bx^2 + cx + d`. All args must have same size/arity.
  *
  * @param x
  * @param a
@@ -33,7 +34,7 @@ export const quadraticS = (x: number, a: number, b: number, c: number) =>
  * @param c
  * @param d
  */
-export const cubic = (x: Dual, a: Dual, b: Dual, c: Dual, d: Dual) => {
+export const cubic: Op5 = (x, a, b, c, d) => {
     const x2 = mul(x, x);
     return add(add(add(mul(a, mul(x2, x)), mul(b, x2)), mul(c, x)), d);
 };
@@ -48,11 +49,11 @@ export const cubic = (x: Dual, a: Dual, b: Dual, c: Dual, d: Dual) => {
  * @param c
  * @param d
  */
-export const cubicS = (x: number, a: number, b: number, c: number, d: number) =>
+export const cubicS: FnU5<number, Dual> = (x, a, b, c, d) =>
     cubic([x, 1], [a, 0], [b, 0], [c, 0], [d, 0]);
 
 /**
- * Computes: `ax^4 + bx^3 + cx^2 + dx + e`
+ * Computes: `ax^4 + bx^3 + cx^2 + dx + e`. All args must have same size/arity.
  *
  * @param x
  * @param a
@@ -88,11 +89,5 @@ export const quartic = (
  * @param d
  * @param e
  */
-export const quarticS = (
-    x: number,
-    a: number,
-    b: number,
-    c: number,
-    d: number,
-    e: number
-) => quartic([x, 1], [a, 0], [b, 0], [c, 0], [d, 0], [e, 0]);
+export const quarticS: FnU6<number, Dual> = (x, a, b, c, d, e) =>
+    quartic([x, 1], [a, 0], [b, 0], [c, 0], [d, 0], [e, 0]);
