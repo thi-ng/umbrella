22cf5c6cd10d43d239855664e37b15392e5f1395
Karsten Schmidt <k@postspectacular.com>
2020-09-06 01:45:38 +0100

    refactor(router): update imports

--- packages/router/src/basic.ts
+++ packages/router/src/basic.ts
@@ -1,15 +1,21 @@
-import { assert, INotifyMixin } from "@thi.ng/api";
+import {
+    assert,
+    Event,
+    INotify,
+    INotifyMixin,
+    IObjectOf,
+    Listener,
+} from "@thi.ng/api";
 import { isString } from "@thi.ng/checks";
 import { equiv } from "@thi.ng/equiv";
 import { illegalArgs, illegalArity } from "@thi.ng/errors";
-import { EVENT_ROUTE_CHANGED } from "./constants";
-import type { Event, INotify, IObjectOf, Listener } from "@thi.ng/api";
 import type {
     Route,
     RouteMatch,
     RouteParamValidator,
     RouterConfig,
 } from "./api";
+import { EVENT_ROUTE_CHANGED } from "./constants";
 
 @INotifyMixin
 export class BasicRouter implements INotify {
