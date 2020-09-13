95557f6716071a92433868ce8536ca1c38a54073
Karsten Schmidt <k@postspectacular.com>
2020-09-13 16:42:21 +0100

    feat(geom-closest-point): add support for custom dist fn

--- packages/geom-closest-point/src/points.ts
+++ packages/geom-closest-point/src/points.ts
@@ -1,14 +1,25 @@
+import type { FnU2 } from "@thi.ng/api";
 import { distSq, ReadonlyVec, set, Vec } from "@thi.ng/vectors";
 
+/**
+ * Returns closest point to `p` in given point array, optionally using custom
+ * distance function `dist` (default: {@link @thi.ng/vectors#distSq}).
+ *
+ * @param p
+ * @param pts
+ * @param out
+ * @param dist
+ */
 export const closestPointArray = (
     p: ReadonlyVec,
     pts: Vec[],
-    out: Vec = []
+    out: Vec = [],
+    dist: FnU2<ReadonlyVec, number> = distSq
 ) => {
     let minD = Infinity;
     let closest: Vec | undefined;
     for (let i = pts.length; --i >= 0; ) {
-        const d = distSq(pts[i], p);
+        const d = dist(pts[i], p);
         if (d < minD) {
             minD = d;
             closest = pts[i];
