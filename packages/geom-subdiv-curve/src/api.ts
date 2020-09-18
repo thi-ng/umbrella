933920d0d4ed125a9ea687e0a44509a5312bd008
Karsten Schmidt <k@postspectacular.com>
2020-09-18 21:09:26 +0100

    refactor(geom-subdiv-curve): update MID_OPEN, THIRDS_OPEN

--- packages/geom-subdiv-curve/src/api.ts
+++ packages/geom-subdiv-curve/src/api.ts
@@ -1,14 +1,10 @@
-import { wrapSides } from "@thi.ng/transducers";
-import { mixN, ReadonlyVec } from "@thi.ng/vectors";
-import { kernel3 } from "./kernels";
+import type { FnU } from "@thi.ng/api";
 import type { SubdivKernel } from "@thi.ng/geom-api";
+import { wrapSides } from "@thi.ng/transducers";
+import { addmN, mixN, ReadonlyVec } from "@thi.ng/vectors";
+import { kernel3 } from "./kernels";
 
-const CHAIKIN_FIRST = kernel3([1 / 2, 1 / 2, 0], [0, 3 / 4, 1 / 4]);
-const CHAIKIN_MAIN = kernel3([1 / 4, 3 / 4, 0], [0, 3 / 4, 1 / 4]);
-const CHAIKIN_LAST = kernel3([1 / 4, 3 / 4, 0], [0, 1 / 2, 1 / 2]);
-const CUBIC_MAIN = kernel3([1 / 8, 3 / 4, 1 / 8], [0, 1 / 2, 1 / 2]);
-
-const MIDP = ([a, b]: ReadonlyVec[]) => [a, mixN([], a, b, 0.5)];
+const MIDP = ([a, b]: ReadonlyVec[]) => [a, addmN([], a, b, 0.5)];
 const THIRDS = ([a, b]: ReadonlyVec[]) => [
     a,
     mixN([], a, b, 1 / 3),
@@ -18,12 +14,18 @@ const THIRDS = ([a, b]: ReadonlyVec[]) => [
 const wrap2 = (pts: ReadonlyVec[]) => wrapSides(pts, 0, 1);
 const wrap3 = (pts: ReadonlyVec[]) => wrapSides(pts, 1, 1);
 
+const subdivWith = (fn: FnU<ReadonlyVec[]>): SubdivKernel["fn"] => (
+    pts,
+    i,
+    n
+) => (i < n - 2 ? fn(pts) : [...fn(pts), pts[1]]);
+
 /**
  * Splits each curve / line segment into halves at midpoint. Version for
  * open curves.
  */
 export const SUBDIV_MID_OPEN: SubdivKernel = {
-    fn: (pts, i, n) => (i < n - 2 ? MIDP(pts) : [...MIDP(pts), pts[1]]),
+    fn: subdivWith(MIDP),
     size: 2,
 };
 
@@ -42,7 +44,7 @@ export const SUBDIV_MID_CLOSED: SubdivKernel = {
  * open curves.
  */
 export const SUBDIV_THIRDS_OPEN: SubdivKernel = {
-    fn: (pts, i, n) => (i < n - 2 ? THIRDS(pts) : [...THIRDS(pts), pts[1]]),
+    fn: subdivWith(THIRDS),
     size: 2,
 };
 
@@ -55,6 +57,10 @@ export const SUBDIV_THIRDS_CLOSED: SubdivKernel = {
     pre: wrap2,
     size: 2,
 };
+
+const CHAIKIN_FIRST = kernel3([1 / 2, 1 / 2, 0], [0, 3 / 4, 1 / 4]);
+const CHAIKIN_MAIN = kernel3([1 / 4, 3 / 4, 0], [0, 3 / 4, 1 / 4]);
+const CHAIKIN_LAST = kernel3([1 / 4, 3 / 4, 0], [0, 1 / 2, 1 / 2]);
 
 /**
  * Chaikin subdivision scheme for open curves.
@@ -77,6 +83,8 @@ export const SUBDIV_CHAIKIN_CLOSED: SubdivKernel = {
     pre: wrap3,
     size: 3,
 };
+
+const CUBIC_MAIN = kernel3([1 / 8, 3 / 4, 1 / 8], [0, 1 / 2, 1 / 2]);
 
 /**
  * Cubic bezier subdivision scheme for closed curves.
