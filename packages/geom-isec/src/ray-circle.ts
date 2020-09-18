b42071ed3af7a0148cc08d97f9a866e53a3970a7
Karsten Schmidt <k@postspectacular.com>
2020-09-18 17:59:41 +0100

    minor(geom-isec): update intersectRayCircle()

--- packages/geom-isec/src/ray-circle.ts
+++ packages/geom-isec/src/ray-circle.ts
@@ -11,21 +11,19 @@ export const intersectRayCircle = (
     const delta = sub([], spos, rpos);
     const w = dot(delta, dir);
     let d = r * r + w * w - magSq(delta);
-    if (d >= 0) {
-        d = Math.sqrt(d);
-        const a = w + d;
-        const b = w - d;
-        const isec =
-            a >= 0
-                ? b >= 0
-                    ? a > b
-                        ? [maddN(delta, dir, b, rpos), maddN([], dir, a, rpos)]
-                        : [maddN(delta, dir, a, rpos), maddN([], dir, b, rpos)]
-                    : [maddN(delta, dir, a, rpos)]
-                : b >= 0
-                ? [maddN(delta, dir, b, rpos)]
-                : undefined;
-        return isec ? { type: IntersectionType.INTERSECT, isec } : NONE;
-    }
-    return NONE;
+    if (d < 0) return NONE;
+    d = Math.sqrt(d);
+    const a = w + d;
+    const b = w - d;
+    const isec =
+        a >= 0
+            ? b >= 0
+                ? a > b
+                    ? [maddN(delta, dir, b, rpos), maddN([], dir, a, rpos)]
+                    : [maddN(delta, dir, a, rpos), maddN([], dir, b, rpos)]
+                : [maddN(delta, dir, a, rpos)]
+            : b >= 0
+            ? [maddN(delta, dir, b, rpos)]
+            : undefined;
+    return isec ? { type: IntersectionType.INTERSECT, isec } : NONE;
 };
