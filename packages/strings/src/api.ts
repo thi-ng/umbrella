b6751fc506a28a075ea9fee1a5f6d3520449f5af
Karsten Schmidt <k@postspectacular.com>
2020-09-18 12:34:50 +0100

    feat(strings): add BOM const, update pkg meta

--- packages/strings/src/api.ts
+++ packages/strings/src/api.ts
@@ -1,5 +1,11 @@
 import type { FnU } from "@thi.ng/api";
 
+/**
+ * UTF-8 Byte Order Mark character
+ * https://en.wikipedia.org/wiki/Byte_order_mark
+ */
+export const BOM = "\ufeff";
+
 export type Stringer<T> = (x: T, ...xs: any[]) => string;
 
 export type FnS = FnU<string>;
