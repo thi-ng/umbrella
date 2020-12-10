0ffc9d01f9bd40ba616d1f59e3ced74fa7c0dc7f
Karsten Schmidt <k@postspectacular.com>
2020-12-10 13:58:53 +0000

    feat(fuzzy): add evaluate()

--- packages/fuzzy/src/var.ts
+++ packages/fuzzy/src/var.ts
@@ -1,3 +1,4 @@
+import type { IObjectOf } from "@thi.ng/api";
 import type { LVar } from "./api";
 
 /**
@@ -28,8 +29,22 @@ export const variable = (
 });
 
 /**
- * Takes an LVar and a domain value. Returns the ID of the var's term which
+ * Takes an LVar and a domain value `x`. Returns the ID of the var's term which
  * produces the largest value for given `x`.
+ *
+ * @example
+ * ```ts
+ * // temperature sets (in celsius)
+ * const temp = variable([-20, 40], {
+ *   freezing: negate(sigmoid(0, 2)),
+ *   cold: trapezoid(0, 4, 16, 20),
+ *   warm: trapezoid(15, 20, 25, 30),
+ *   hot: sigmoid(30, 2)
+ * });
+ *
+ * classify(temp, 28)
+ * // "warm"
+ * ```
  *
  * @param var
  * @param x
@@ -45,4 +60,33 @@ export const classify = ({ terms }: LVar, x: number) => {
         }
     }
     return maxID;
+};
+
+/**
+ * Takes an LVar and a domain value `x`, evaluates all of var's terms for given
+ * `x` and returns object of results.
+ *
+ * @example
+ * ```ts
+ * // temperature sets (in celsius)
+ * const temp = variable([-20, 40], {
+ *   freezing: negate(sigmoid(0, 2)),
+ *   cold: trapezoid(0, 4, 16, 20),
+ *   warm: trapezoid(15, 20, 25, 30),
+ *   hot: sigmoid(30, 2)
+ * });
+ *
+ * evaluate(temp, 28)
+ * // { freezing: 0, cold: 0, warm: 0.4, hot: 0.01798620996209156 }
+ * ```
+ *
+ * @param var
+ * @param x
+ */
+export const evaluate = ({ terms }: LVar, x: number) => {
+    const res: IObjectOf<number> = {};
+    for (let id in terms) {
+        res[id] = terms[id](x);
+    }
+    return res;
 };
