4e7d8d301a4f7315803b8e1bf5c6211065738dec
Karsten Schmidt <k@postspectacular.com>
2020-12-18 18:37:35 +0000

    test(dsp): add/update tests (WIP)

--- packages/dsp/test/fft.ts
+++ packages/dsp/test/fft.ts
@@ -1,13 +1,25 @@
 import type { NumericArray } from "@thi.ng/api";
+import { eqDelta } from "@thi.ng/math";
 import * as assert from "assert";
 import {
+    add,
     ComplexArray,
-    denormalizeFFT,
+    copyComplex,
+    cos,
     fft,
+    freqBin,
     ifft,
     normalizeFFT,
+    osc,
+    powerMeanSquared,
+    powerSumSquared,
+    powerTimeIntegral,
+    spectrumMag,
     spectrumPhase,
     spectrumPow,
+    thresholdFFT,
+    window,
+    windowRect,
 } from "../src";
 
 const pulse8 = [-1, -1, -1, -1, 1, 1, 1, 1];
@@ -16,7 +28,11 @@ const deltaEq = (a: NumericArray, b: NumericArray, eps = 1e-3) => {
     if (a.length != b.length) return false;
     eps **= 2;
     for (let i = a.length; --i >= 0; ) {
-        if ((a[i] - b[i]) ** 2 > eps) return false;
+        const diff = (a[i] - b[i]) ** 2;
+        if (diff > eps) {
+            console.log("deltaEq: ", i, diff);
+            return false;
+        }
     }
     return true;
 };
@@ -25,50 +41,106 @@ const deltaEqComplex = (a: ComplexArray, b: ComplexArray, eps?: number) =>
     deltaEq(a[0], b[0], eps) && deltaEq(a[1], b[1], eps);
 
 describe("fft", () => {
-    it("pulse(8)", () => {
-        const res = fft([...pulse8]);
+    it("roundtrip", () => {
+        const src = osc(cos, 64 / 512, 1).take(512);
+        const rev = ifft(fft([...src]));
+        assert(deltaEq(rev[0], src));
+    });
 
-        assert.ok(
-            deltaEqComplex(res, [
-                [0, -2, 0, -2, 0, -2, 0, -2],
-                [0, 4.828, 0, 0.828, 0, -0.828, 0, -4.828],
-            ])
+    it("parseval", () => {
+        const FC = 64;
+        const FS = 512;
+        const A = 0.5;
+        const N = 2 * FS;
+        const src = osc(cos, add(FC / FS, 1 / 12), A).take(N);
+        const win = window(windowRect, N);
+        const fwd = fft([...src], win);
+        // parseval's theorem: sum(src[i]^2) = sum(|fft[i]|^2) / N
+        const sumT = src.reduce((acc, x) => acc + x * x, 0);
+        const sumF =
+            (<number[]>fwd[0]).reduce(
+                (acc, x, i) => acc + x ** 2 + fwd[1][i] ** 2,
+                0
+            ) / N;
+        console.log(
+            sumT,
+            sumF,
+            powerMeanSquared(fwd),
+            powerTimeIntegral(src, FS),
+            powerTimeIntegral(fwd, FS)
+        );
+        assert(eqDelta(powerSumSquared(src), sumT), "sumT1");
+        assert(eqDelta(powerSumSquared(fwd), sumF), "sumF1");
+        assert(eqDelta(powerMeanSquared(src), sumT / N), "sumT2");
+        assert(eqDelta(powerMeanSquared(fwd), sumF / N), "sumF2");
+
+        const normF = normalizeFFT(copyComplex(fwd), win);
+        const sumF2 = powerSumSquared(normF);
+
+        const thresh = thresholdFFT(copyComplex(fwd), 1 / 10000);
+        const phase = spectrumPhase(thresh);
+
+        const I = freqBin(FC, FS, N);
+        console.log(
+            "fwd",
+            spectrumMag(fwd)[I],
+            spectrumPow(fwd, false, N / 2, win)[I],
+            spectrumPow(fwd, true, N / 2, win)[I],
+            phase[I]
+        );
+        console.log(
+            "norm",
+            spectrumMag(normF)[I],
+            spectrumPow(normF)[I],
+            spectrumPow(normF, true)[I]
+        );
+        console.log(sumF2);
+    });
+
+    it("issue", () => {
+        const FC = 64;
+        const FS = 512;
+        const A = 0.5;
+        const N = 2 * FS;
+        const I = freqBin(FC, FS, N);
+
+        // cosine osc w/ 30 degree phase shift (= 2π/12)
+        const src = osc(cos, add(FC / FS, 1 / 12), A).take(N);
+
+        // compute window LUT
+        const win = window(windowRect, N);
+
+        const fwd = fft(src.slice(), win);
+
+        console.log(
+            "powSumSq: src =",
+            powerSumSquared(src),
+            "fft =",
+            powerSumSquared(fwd)
+        );
+        console.log(
+            "powMeanSq: src =",
+            powerMeanSquared(src),
+            "fft =",
+            powerMeanSquared(fwd)
         );
 
-        assert.ok(
-            deltaEqComplex(ifft(fft([...pulse8])), [
-                pulse8,
-                [0, 0, 0, 0, 0, 0, 0, 0],
-            ])
-        );
+        const spMag = spectrumMag(fwd);
+        const spPow = spectrumPow(fwd, false, N / 2, win);
+        const spDb = spectrumPow(fwd, true, N / 2, win);
 
-        assert.ok(
-            deltaEqComplex(
-                ifft(denormalizeFFT(normalizeFFT(fft([...pulse8])))),
-                [pulse8, [0, 0, 0, 0, 0, 0, 0, 0]]
-            )
-        );
+        const spPhase = spectrumPhase(thresholdFFT(copyComplex(fwd)));
 
-        const norm = normalizeFFT(fft([...pulse8]));
+        console.log("mag[I] =", spMag[I]);
+        console.log("pow[I] =", spPow[I]);
+        console.log("dB[I] =", spDb[I]);
+        console.log("phase[I] =", (spPhase[I] * 180) / Math.PI, "deg");
 
-        assert.ok(
-            deltaEqComplex(norm, [
-                [0, -0.707, 0, -0.707, 0, -0.707, 0, -0.707],
-                [0, 1.707, 0, 0.293, 0, -0.293, 0, -1.707],
-            ])
-        );
-
-        assert.ok(deltaEq(spectrumPow(res, false), [0, 3.414, 0, 0.586]));
-
-        assert.ok(
-            deltaEq(spectrumPow(res, true), [
-                -Infinity,
-                -3.698,
-                -Infinity,
-                -11.354,
-            ])
-        );
-
-        assert.ok(deltaEq(spectrumPhase(res), [0, 1.963, 0, 2.749]));
+        const norm = normalizeFFT(copyComplex(fwd), win);
+        const spMagNorm = spectrumMag(norm);
+        // since `norm` already includes the window weight adjustments
+        // we use a scale factor of 1.0 here
+        const spPowNorm = spectrumPow(norm, false, N / 2, 1);
+        console.log("norm mag[I] =", spMagNorm[I], "pow[I] =", spPowNorm[I]);
     });
 });
