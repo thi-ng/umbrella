22f37a526acd6911720100e77ad41029d8799004
Karsten Schmidt <k@postspectacular.com>
2020-12-21 23:56:56 +0000

    fix(fuzzy-viz): update imports

--- packages/fuzzy-viz/test/index.ts
+++ packages/fuzzy-viz/test/index.ts
@@ -1,14 +1,15 @@
-import { cogStrategy, gaussian } from "@thi.ng/fuzzy";
+import { centroidStrategy, gaussian } from "@thi.ng/fuzzy";
+import { eqDelta } from "@thi.ng/math";
 import * as assert from "assert";
 import { fuzzySetToAscii, instrumentStrategy } from "../src";
 
 describe("fuzzy-viz", () => {
     it("strategy (ascii)", () => {
         const strategy = instrumentStrategy(
-            cogStrategy({ samples: 1000 }),
+            centroidStrategy({ samples: 1000 }),
             fuzzySetToAscii({ width: 40, height: 8 })
         );
-        assert.strictEqual(strategy(gaussian(5, 2), [0, 10]), 4.995);
+        assert(eqDelta(strategy(gaussian(5, 2), [0, 10]), 5));
         assert.deepStrictEqual(strategy.deref(), [
             `.................▄▆█|█▆▄.................
 ...............▅████|████▅...............
