34037ad4ec87027779c72b683c3a4194f1995e79
Karsten Schmidt <k@postspectacular.com>
2020-12-23 16:24:02 +0000

    feat(text-canvas): add imageRaw(), update image()
    - add imageRaw() for direct use of pixels as format data (e.g. for FMT_HTML_565)
    - update ImageOpts.format to allow functions
    - update Canvas ctor, initial clear value to include format

--- packages/text-canvas/src/image.ts
+++ packages/text-canvas/src/image.ts
@@ -1,4 +1,5 @@
 import { peek } from "@thi.ng/arrays";
+import { isNumber } from "@thi.ng/checks";
 import { ImageOpts, SHADES_BLOCK } from "./api";
 import { Canvas } from "./canvas";
 import { charCode, intersectRect } from "./utils";
@@ -115,6 +116,7 @@ export const image = (
         bits: 8,
         ...opts,
     };
+    const fmt = isNumber(format) ? () => format : format;
     const max = (1 << bits) - 1;
     const mask = invert ? max : 0;
     const norm = 1 / max;
@@ -123,14 +125,51 @@ export const image = (
         let sidx = sx + yy * w;
         let didx = x1 + dy * width;
         for (let xx = sx, dx = x1; dx < x2; xx++, dx++) {
-            buf[didx++] = charCode(
-                chars[
-                    (Math.pow((pixels[sidx++] ^ mask) * norm, gamma) * num +
-                        0.5) |
-                        0
-                ],
-                format
-            );
+            const col = Math.pow((pixels[sidx++] ^ mask) * norm, gamma);
+            buf[didx++] = charCode(chars[(col * num + 0.5) | 0], fmt(col));
+        }
+    }
+};
+
+/**
+ * Optimized version of {@link image}, which only uses a single char for all
+ * pixels and applies pixel values directly as formatting data (for each pixel).
+ *
+ * @param canvas
+ * @param x
+ * @param y
+ * @param w
+ * @param h
+ * @param pixels
+ * @param char
+ */
+export const imageRaw = (
+    canvas: Canvas,
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+    pixels: ArrayLike<number>,
+    char = "█"
+) => {
+    x |= 0;
+    y |= 0;
+    w |= 0;
+    h |= 0;
+    const { buf, width } = canvas;
+    const { x1, y1, x2, y2, w: iw, h: ih } = intersectRect(
+        { x1: x, y1: y, x2: x + w, y2: y + h, w, h },
+        peek(canvas.clipRects)
+    );
+    if (!iw || !ih) return;
+    const sx = Math.max(0, x1 - x);
+    const sy = Math.max(0, y1 - y);
+    const code = char.charCodeAt(0);
+    for (let yy = sy, dy = y1; dy < y2; yy++, dy++) {
+        let sidx = sx + yy * w;
+        let didx = x1 + dy * width;
+        for (let xx = sx, dx = x1; dx < x2; xx++, dx++) {
+            buf[didx++] = code | ((pixels[sidx++] & 0xffff) << 16);
         }
     }
 };
