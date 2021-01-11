268ec3f47470184068fd66b5cc147d8c2e0e0ccb
Karsten Schmidt <k@postspectacular.com>
2021-01-11 15:13:40 +0000

    fix(random): add opt start index arg for uuid()

--- packages/random/src/uuid.ts
+++ packages/random/src/uuid.ts
@@ -19,23 +19,14 @@ export const uuidv4Bytes = (buf?: Uint8Array, rnd?: IRandom) => {
 };
 
 /**
- * Returns a UUID string, either from given byte array, of if omitted, a new
- * UUID v4 produced by {@link uuidv4Bytes}.
+ * Returns a UUID string, either from given byte array, or if omitted, using a
+ * new UUID v4 produced by {@link uuidv4Bytes}.
  *
  * @param id - byte array
  * @param i - start index
  */
-export const uuid = (id?: ArrayLike<number>) => {
+export const uuid = (id?: ArrayLike<number>, i = 0) => {
     id = id || uuidv4Bytes();
-    return (
-        U32BE(id, 0) +
-        "-" +
-        U16BE(id, 4) +
-        "-" +
-        U16BE(id, 6) +
-        "-" +
-        U16BE(id, 8) +
-        "-" +
-        U48BE(id, 10)
-    );
+    // prettier-ignore
+    return `${U32BE(id, i)}-${U16BE(id, i + 4)}-${U16BE(id, i + 6)}-${U16BE(id, i + 8)}-${U48BE(id, i + 10)}`;
 };
