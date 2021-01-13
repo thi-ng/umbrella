aace41ce8ec0864d38a27d9b0461b705e9e122dc
Karsten Schmidt <k@postspectacular.com>
2021-01-13 01:58:28 +0000

    perf(ksuid): add benchmarks

---
+++ packages/ksuid/bench/index.ts
@@ -0,0 +1,19 @@
+// import { benchmark } from "@thi.ng/bench";
+import { assert } from "@thi.ng/api";
+import { benchmark } from "@thi.ng/bench";
+import { defKSUID, KSUID } from "../src";
+
+const opts = { iter: 100, warmup: 10 };
+
+const bench = (id: KSUID, n = 10000) => {
+    const acc = new Set<string>();
+    for (let i = 0; i < n; i++) acc.add(id.next());
+    assert(acc.size === n, `collision`);
+};
+
+// prettier-ignore
+benchmark(() => bench(defKSUID({ bytes:16 })), { title: "b62, 128bit, n=10000", ...opts });
+// prettier-ignore
+benchmark(() => bench(defKSUID({ bytes: 8 })), { title: "b62, 64bit, n=10000", ...opts });
+// prettier-ignore
+benchmark(() => bench(defKSUID({ bytes: 4 })), { title: "b62, 32bit, n=10000", ...opts });
