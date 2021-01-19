eb334fa764dc3d7093b1c64afb1fbdb1b1053831
Karsten Schmidt <k@postspectacular.com>
2021-01-19 16:57:19 +0000

    feat(vectors): add dist2/3

--- packages/vectors/src/dist.ts
+++ packages/vectors/src/dist.ts
@@ -1,6 +1,12 @@
 import type { FnU2 } from "@thi.ng/api";
 import type { ReadonlyVec } from "./api";
-import { distSq } from "./distsq";
+import { distSq, distSq2, distSq3 } from "./distsq";
 
 export const dist: FnU2<ReadonlyVec, number> = (a, b) =>
     Math.sqrt(distSq(a, b));
+
+export const dist2: FnU2<ReadonlyVec, number> = (a, b) =>
+    Math.sqrt(distSq2(a, b));
+
+export const dist3: FnU2<ReadonlyVec, number> = (a, b) =>
+    Math.sqrt(distSq3(a, b));
