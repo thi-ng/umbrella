e5b1b9bbb22424786987b2a9c685432daf398c18
Karsten Schmidt <k@postspectacular.com>
2021-01-24 17:06:21 +0000

    refactor(color): update args for swatch fns

--- packages/color/src/swatches.ts
+++ packages/color/src/swatches.ts
@@ -2,20 +2,20 @@ import type { Fn2, IObjectOf } from "@thi.ng/api";
 import type { ReadonlyColor } from "./api";
 
 export const swatches = (
-    cols: ReadonlyColor[],
-    fn: Fn2<ReadonlyColor, number, any[]>,
+    cols: (ReadonlyColor | string)[],
+    shapeFn: Fn2<ReadonlyColor | string, number, any[]>,
     attribs: IObjectOf<any> = {}
-) => ["g", attribs, ...cols.map(fn)];
+) => ["g", attribs, ...cols.map(shapeFn)];
 
 export const swatchesH = (
-    cols: ReadonlyColor[],
+    cols: (ReadonlyColor | string)[],
     w = 5,
     h = 50,
     attribs?: IObjectOf<any>
 ) => swatches(cols, (fill, i) => ["rect", { fill }, [i * w, 0], w, h], attribs);
 
 export const swatchesV = (
-    cols: ReadonlyColor[],
+    cols: (ReadonlyColor | string)[],
     w = 50,
     h = 5,
     attribs: IObjectOf<any> = {}
