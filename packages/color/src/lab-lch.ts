c0b9e9d90172487c7b0dce7d252d30bba92e425e
Karsten Schmidt <k@postspectacular.com>
2021-01-25 00:48:34 +0000

    fix(color): normalize LCH hue channel

--- packages/color/src/lab-lch.ts
+++ packages/color/src/lab-lch.ts
@@ -1,5 +1,5 @@
 import { setC4 } from "@thi.ng/vectors";
-import { atan2Abs, cossin } from "@thi.ng/math";
+import { atan2Abs, cossin, INV_TAU, TAU } from "@thi.ng/math";
 import type { ColorOp } from "./api";
 import { ensureAlpha } from "./internal/ensure-alpha";
 
@@ -8,7 +8,7 @@ export const labLch: ColorOp = (out, src) =>
         out || src,
         src[0],
         Math.hypot(src[1], src[2]),
-        atan2Abs(src[2], src[1]),
+        atan2Abs(src[2], src[1]) * INV_TAU,
         ensureAlpha(src[3])
     );
 
@@ -16,6 +16,8 @@ export const lchLab: ColorOp = (out, src) =>
     setC4(
         out || src,
         src[0],
-        ...(<[number, number]>(src[1] > 0 ? cossin(src[2], src[1]) : [0, 0])),
+        ...(<[number, number]>(
+            (src[1] > 0 ? cossin(src[2] * TAU, src[1]) : [0, 0])
+        )),
         ensureAlpha(src[3])
     );
