6bd6fd1413b5b13cbc48a3f0e4fe9d5beb98baba
Jamie Slome <jamie@418sec.com>
2021-01-26 21:32:38 +0000

    Update is-prototype-polluted.ts

--- packages/checks/src/is-prototype-polluted.ts
+++ packages/checks/src/is-prototype-polluted.ts
@@ -1,2 +1,2 @@
 const ILLEGAL_KEYS = new Set(["__proto__", "prototype", "constructor"]);
-export const isPrototypePolluted = (key: string): Boolean => ILLEGAL_KEYS.includes(key);
+export const isPrototypePolluted = (key: string): Boolean => ILLEGAL_KEYS.has(key);
