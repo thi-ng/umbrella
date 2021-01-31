9a548eccf3553d03afaff7817ce47f4bfeb98691
Karsten Schmidt <k@postspectacular.com>
2021-01-31 17:25:36 +0000

    feat(color): add sortMapped() for mapped memory cols

--- packages/color/src/ops/sort.ts
+++ packages/color/src/ops/sort.ts
@@ -1,7 +1,7 @@
 import type { Fn } from "@thi.ng/api";
-import { sortByCachedKey } from "@thi.ng/arrays";
+import { quickSort, sortByCachedKey, swap } from "@thi.ng/arrays";
 import { compareNumAsc, compareNumDesc } from "@thi.ng/compare";
-import type { ReadonlyColor } from "../api";
+import type { ReadonlyColor, TypedColor } from "../api";
 import { distHsv, distRgb } from "./distance";
 
 export const selectChannel = (id: number) => (col: ReadonlyColor) => col[id];
@@ -12,8 +12,62 @@ export const proximityHSV = (target: ReadonlyColor) => (col: ReadonlyColor) =>
 export const proximityRGB = (target: ReadonlyColor) => (col: ReadonlyColor) =>
     distRgb(target, col);
 
-export const sortColors = (
-    cols: ReadonlyColor[],
+export const sort = (
+    colors: ReadonlyColor[],
     key: Fn<ReadonlyColor, number>,
     isReverse = false
-) => sortByCachedKey(cols, key, isReverse ? compareNumDesc : compareNumAsc);
+) => sortByCachedKey(colors, key, isReverse ? compareNumDesc : compareNumAsc);
+
+/**
+ * Similar to {@link sort}, but only for memory mapped colors (e.g. mapping a
+ * pixel buffer). Does NOT change the order of elements in given `colors` array,
+ * BUT sorts the **apparent** order by swapping the contents of the backing
+ * memory.
+ *
+ * ```ts
+ * // memory buffer of 4 sRGB colors
+ * const buf = new Float32Array([0,1,0,1, 0,0.5,0,1, 0,0.25,0,1, 0,0.75,0,1]);
+ *
+ * // map buffer (creates 4 SRGB instances linked to the buffer)
+ * const pix = srgb.mapBuffer(buf);
+ *
+ * // display original order
+ * pix.map(css);
+ * // [ '#00ff00', '#008000', '#004000', '#00bf00' ]
+ *
+ * // sort colors (buffer!) by luminance
+ * sortMapped(pix, luminanceSrgb);
+ *
+ * // new order
+ * pix.map(css);
+ * // [ '#004000', '#008000', '#00bf00', '#00ff00' ]
+ *
+ * // buffer contents have been re-ordered
+ * buf
+ * // Float32Array(16) [
+ * //     0, 0.25, 0, 1,    0,
+ * //   0.5,    0, 1, 0, 0.75,
+ * //     0,    1, 0, 1,    0,
+ * //     1
+ * // ]
+ * ```
+ *
+ * @param colors
+ * @param key
+ * @param isReverse
+ */
+export const sortMapped = <T extends TypedColor<any>>(
+    colors: T[],
+    key: Fn<ReadonlyColor, number>,
+    isReverse = false
+) => {
+    const keys = colors.map(key);
+    const tmp = new Float32Array(colors[0].length);
+    quickSort(keys, isReverse ? compareNumDesc : compareNumAsc, (_, x, y) => {
+        swap(keys, x, y);
+        tmp.set(colors[x]);
+        colors[x].set(colors[y]);
+        colors[y].set(tmp);
+    });
+    return colors;
+};
