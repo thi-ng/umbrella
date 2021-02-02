d4b397b99f5d6c0daef76c86011b165ecda31b4d
Karsten Schmidt <k@postspectacular.com>
2021-02-02 01:01:57 +0000

    feat(vector-pools): export asNativeType/asGLType()

--- packages/vector-pools/src/index.ts
+++ packages/vector-pools/src/index.ts
@@ -2,6 +2,7 @@ export * from "./api";
 export * from "./alist";
 export * from "./array-list";
 export * from "./attrib-pool";
+export * from "./convert";
 export * from "./linked-list";
 export * from "./vec-pool";
 export * from "./wrap";
