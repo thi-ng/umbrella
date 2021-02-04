c9ade117bb92759b7d9355216b2d7cb17e1d7af6
Karsten Schmidt <k@postspectacular.com>
2021-02-04 14:10:29 +0000

    refactor(pixel): split defFloatFormat(), extract helpers

--- packages/pixel/src/format/float-format.ts
+++ packages/pixel/src/format/float-format.ts
@@ -1,4 +1,4 @@
-import type { FnN2, IObjectOf, NumericArray } from "@thi.ng/api";
+import type { Fn2, FnN2, IObjectOf, NumericArray } from "@thi.ng/api";
 import { FloatFormat, FloatFormatSpec, Lane } from "../api";
 import { luminanceABGR } from "../utils";
 
@@ -23,85 +23,116 @@ export const defFloatFormat = (fmt: FloatFormatSpec) => {
     const from: FnN2 = (col, i) => ((col >>> chanShift[chan[i]]) & 0xff) / 0xff;
     switch (chan.length) {
         case 1:
-            if (fmt.gray) {
-                res.toABGR = (col) =>
-                    ((((col[0] * 0xff + 0.5) | 0) * 0x010101) | 0xff000000) >>>
-                    0;
-                res.fromABGR = (col, out = []) => (
-                    (out[0] = luminanceABGR(col) / 0xff), out
-                );
-            } else {
-                res.toABGR = (col) => {
-                    let out = fmt.alpha ? 0 : 0xff000000;
-                    out |= to(col, 0);
-                    return out >>> 0;
-                };
-                res.fromABGR = (col, out = []) => {
-                    out[0] = from(col, 0);
-                    return out;
-                };
-            }
+            fmt.gray ? defConvert1Gray(res) : defConvert1(res, from, to);
             break;
         case 2:
-            if (fmt.gray) {
-                const gray = ~~(chan[0] === Lane.ALPHA);
-                const alpha = gray ^ 1;
-                res.toABGR = (col) => {
-                    let out = ((col[gray] * 0xff + 0.5) | 0) * 0x010101;
-                    out |= ((col[alpha] * 0xff + 0.5) | 0) << 24;
-                    return out >>> 0;
-                };
-                res.fromABGR = (col, out = []) => {
-                    out[gray] = luminanceABGR(col) / 0xff;
-                    out[alpha] = from(col, alpha);
-                    return out;
-                };
-            } else {
-                res.toABGR = (col) => {
-                    let out = fmt.alpha ? 0 : 0xff000000;
-                    out |= to(col, 0);
-                    out |= to(col, 1);
-                    return out >>> 0;
-                };
-                res.fromABGR = (col, out = []) => {
-                    out[0] = from(col, 0);
-                    out[1] = from(col, 1);
-                    return out;
-                };
-            }
+            fmt.gray ? defConvert2Gray(res, from) : defConvert2(res, from, to);
             break;
         case 3:
-            res.toABGR = (col) => {
-                let out = fmt.alpha ? 0 : 0xff000000;
-                out |= to(col, 0);
-                out |= to(col, 1);
-                out |= to(col, 2);
-                return out >>> 0;
-            };
-            res.fromABGR = (col, out = []) => {
-                out[0] = from(col, 0);
-                out[1] = from(col, 1);
-                out[2] = from(col, 2);
-                return out;
-            };
+            defConvert3(res, from, to);
             break;
         case 4:
-            res.toABGR = (col) => {
-                let out = fmt.alpha ? 0 : 0xff000000;
-                out |= to(col, 0);
-                out |= to(col, 1);
-                out |= to(col, 2);
-                out |= to(col, 3);
-                return out >>> 0;
-            };
-            res.fromABGR = (col, out = []) => {
-                out[0] = from(col, 0);
-                out[1] = from(col, 1);
-                out[2] = from(col, 2);
-                out[3] = from(col, 3);
-                return out;
-            };
+            defConvert4(res, from, to);
             break;
     }
     return res;
+};
+
+const defConvert1 = (
+    res: FloatFormat,
+    from: FnN2,
+    to: Fn2<NumericArray, number, number>
+) => {
+    res.toABGR = (col) => {
+        let out = res.alpha ? 0 : 0xff000000;
+        out |= to(col, 0);
+        return out >>> 0;
+    };
+    res.fromABGR = (col, out = []) => {
+        out[0] = from(col, 0);
+        return out;
+    };
+};
+
+const defConvert1Gray = (res: FloatFormat) => {
+    res.toABGR = (col) =>
+        ((((col[0] * 0xff + 0.5) | 0) * 0x010101) | 0xff000000) >>> 0;
+    res.fromABGR = (col, out = []) => (
+        (out[0] = luminanceABGR(col) / 0xff), out
+    );
+};
+
+const defConvert2 = (
+    res: FloatFormat,
+    from: FnN2,
+    to: Fn2<NumericArray, number, number>
+) => {
+    res.toABGR = (col) => {
+        let out = res.alpha ? 0 : 0xff000000;
+        out |= to(col, 0);
+        out |= to(col, 1);
+        return out >>> 0;
+    };
+    res.fromABGR = (col, out = []) => {
+        out[0] = from(col, 0);
+        out[1] = from(col, 1);
+        return out;
+    };
+};
+
+const defConvert2Gray = (res: FloatFormat, from: FnN2) => {
+    const gray = ~~(res.channels[0] === Lane.ALPHA);
+    const alpha = gray ^ 1;
+    res.toABGR = (col) => {
+        let out = ((col[gray] * 0xff + 0.5) | 0) * 0x010101;
+        out |= ((col[alpha] * 0xff + 0.5) | 0) << 24;
+        return out >>> 0;
+    };
+    res.fromABGR = (col, out = []) => {
+        out[gray] = luminanceABGR(col) / 0xff;
+        out[alpha] = from(col, alpha);
+        return out;
+    };
+};
+
+const defConvert3 = (
+    res: FloatFormat,
+    from: FnN2,
+    to: Fn2<NumericArray, number, number>
+) => {
+    res.toABGR = (col) => {
+        let out = res.alpha ? 0 : 0xff000000;
+        out |= to(col, 0);
+        out |= to(col, 1);
+        out |= to(col, 2);
+        return out >>> 0;
+    };
+    res.fromABGR = (col, out = []) => {
+        out[0] = from(col, 0);
+        out[1] = from(col, 1);
+        out[2] = from(col, 2);
+        return out;
+    };
+};
+
+const defConvert4 = (
+    res: FloatFormat,
+    from: FnN2,
+    to: Fn2<NumericArray, number, number>
+) => {
+    res.toABGR = (col) => {
+        let out = res.alpha ? 0 : 0xff000000;
+        out |= to(col, 0);
+        out |= to(col, 1);
+        out |= to(col, 2);
+        out |= to(col, 3);
+        return out >>> 0;
+    };
+    res.fromABGR = (col, out = []) => {
+        out[0] = from(col, 0);
+        out[1] = from(col, 1);
+        out[2] = from(col, 2);
+        out[3] = from(col, 3);
+        return out;
+    };
 };
