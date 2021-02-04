7fe966a6533daf0b36820a631cd005f9f73592f2
Karsten Schmidt <k@postspectacular.com>
2021-02-04 14:37:12 +0000

    refactor(strings): update uuid()
    - re-use fn from thi.ng/hex

--- packages/strings/src/uuid.ts
+++ packages/strings/src/uuid.ts
@@ -1,8 +1,9 @@
-import { U16BE, U32BE, U48BE } from "@thi.ng/hex";
+import { uuid as $uuid } from "@thi.ng/hex";
 
 /**
- * Returns UUID formatted string of given byte array from optional start index
- * `i` (default: 0). Array must have min. length 16.
+ * Same as {@link @thi.ng/hex#uuid}. Returns UUID formatted string of given byte
+ * array from optional start index `i` (default: 0). Array must have min. length
+ * 16.
  *
  * @remarks
  * Use {@link @thi.ng/random#uuid} to also generate an UUID from scratch (rather
@@ -11,6 +12,4 @@ import { U16BE, U32BE, U48BE } from "@thi.ng/hex";
  * @param id -
  * @param i -
  */
-export const uuid = (id: ArrayLike<number>, i = 0) =>
-    // prettier-ignore
-    `${U32BE(id, i)}-${U16BE(id, i + 4)}-${U16BE(id, i + 6)}-${U16BE(id, i + 8)}-${U48BE(id, i + 10)}`;
+export const uuid = $uuid;
