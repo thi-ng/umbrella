117a5bcdcdd3c84f7c9eb8d68197963db4515b07
Karsten Schmidt <k@postspectacular.com>
2021-02-06 15:01:09 +0000

    feat(color): add generic analog() (for all color types)

--- packages/color/src/ops/analog.ts
+++ packages/color/src/ops/analog.ts
@@ -1,24 +1,75 @@
-import type { FnN } from "@thi.ng/api";
+import type { Fn3, Fn4, FnN, FnU3 } from "@thi.ng/api";
+import { DEFAULT, defmulti } from "@thi.ng/defmulti";
 import { clamp01, fract } from "@thi.ng/math";
 import { IRandom, SYSTEM } from "@thi.ng/random";
 import { setC4 } from "@thi.ng/vectors";
-import type { Color, ReadonlyColor } from "../api";
+import type { Color, ReadonlyColor, TypedColor } from "../api";
 import { ensureAlpha } from "../internal/ensure-alpha";
 
-const $analog = (x: number, delta: number, rnd: IRandom, post: FnN = clamp01) =>
+/** @internal */
+const analogN = (x: number, delta: number, rnd: IRandom, post: FnN = clamp01) =>
     delta !== 0 ? post(x + rnd.norm(delta)) : x;
 
-const $alpha = (a: number, delta: number, rnd: IRandom) =>
+/** @internal */
+const analogH = (x: number, delta: number, rnd: IRandom) =>
+    analogN(x, delta, rnd, fract);
+
+/** @internal */
+const analogA = (a: number, delta: number, rnd: IRandom) =>
     delta !== 0
         ? clamp01((a !== undefined ? a : 1) + rnd.norm(delta))
         : ensureAlpha(a);
 
+export const defAnalog: FnU3<
+    Fn3<number, number, IRandom, number>,
+    Fn4<Color | null, TypedColor<any>, number, IRandom | undefined, Color>
+> = (x, y, z) => (out, src, delta, rnd = SYSTEM) =>
+    setC4(
+        out || src,
+        x(src[0], delta, rnd),
+        y(src[1], delta, rnd),
+        z(src[2], delta, rnd),
+        ensureAlpha(src[3])
+    );
+
+/** @internal */
+const analogHNN = defAnalog(analogH, analogN, analogN);
+
+/** @internal */
+const analogNNN = defAnalog(analogN, analogN, analogN);
+
+/**
+ * Returns a random analog color based on given `src` color and variance
+ * `delta`. Each channel will be randomized by +/- `delta`, optionally using
+ * provided {@link @thi.ng/random#IRandom} PRNG.
+ */
+export const analog = defmulti<
+    Color | null,
+    TypedColor<any>,
+    number,
+    IRandom | undefined,
+    Color
+>((_: Color | null, src: TypedColor<any>) => src.mode);
+
+analog.add(DEFAULT, analogNNN);
+
+analog.addAll({
+    hcy: analogHNN,
+    hsi: analogHNN,
+    hsl: analogHNN,
+    hsv: analogHNN,
+    lch: defAnalog(analogN, analogN, analogH),
+});
+
 /**
  * Similar to {@link analogRGB}. Returns an analog color based on given HSVA
- * color,with each channel randomly varied by given delta amounts (and
- * optionally given {@link @thi.ng/random#IRandom} PRNG).
+ * color, with each channel randomly varied by given channel-specific delta
+ * amounts (and optionally given {@link @thi.ng/random#IRandom} PRNG).
  *
  * @remarks
+ * This function provides more user control than {@link analog}, but the latter
+ * is recommended for most use cases.
+ *
  * By default (unless `deltaS`, `deltaV`, `deltaA` are provided) only the hue of
  * the color will be modulated.
  *
@@ -41,10 +92,10 @@ export const analogHsv = (
 ) =>
     setC4(
         out || src,
-        $analog(src[0], deltaH, rnd, fract),
-        $analog(src[1], deltaS, rnd),
-        $analog(src[2], deltaV, rnd),
-        $alpha(src[3], deltaA, rnd)
+        analogN(src[0], deltaH, rnd, fract),
+        analogN(src[1], deltaS, rnd),
+        analogN(src[2], deltaV, rnd),
+        analogA(src[3], deltaA, rnd)
     );
 
 /**
@@ -74,8 +125,8 @@ export const analogRgb = (
 ) =>
     setC4(
         out || src,
-        $analog(src[0], deltaR, rnd),
-        $analog(src[1], deltaG, rnd),
-        $analog(src[2], deltaB, rnd),
-        $alpha(src[3], deltaA, rnd)
+        analogN(src[0], deltaR, rnd),
+        analogN(src[1], deltaG, rnd),
+        analogN(src[2], deltaB, rnd),
+        analogA(src[3], deltaA, rnd)
     );
