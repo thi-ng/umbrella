e0eb47bf4293832347b99a6706d257b391fd31b9
Karsten Schmidt <k@postspectacular.com>
2021-02-07 17:52:16 +0000

    feat(binary): add MSB_BITS8/16/32 LUTs

--- packages/binary/src/constants.ts
+++ packages/binary/src/constants.ts
@@ -1,1 +1,18 @@
+const defBits = (n: number) =>
+    new Array(n).fill(0).map((_, i) => 1 << (n - 1 - i));
+/**
+ * 8bit values in MSB order (i.e. MSB_BITS[0] = 0x80)
+ */
+export const MSB_BITS8 = defBits(8);
+
+/**
+ * 16bit values in MSB order (i.e. MSB_BITS[0] = 0x8000)
+ */
+export const MSB_BITS16 = defBits(16);
+
+/**
+ * 32bit values in MSB order (i.e. MSB_BITS[0] = 0x80000000)
+ */
+export const MSB_BITS32 = defBits(32);
+
 export const MASKS = new Array(33).fill(0).map((_, i) => Math.pow(2, i) - 1);
