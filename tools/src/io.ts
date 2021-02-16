0ab3dd402aec48efb620b5c5728d3691cf7da558
Karsten Schmidt <k@postspectacular.com>
2021-02-16 17:30:04 +0000

    feat(tools): update files(), add max depth

--- tools/src/io.ts
+++ tools/src/io.ts
@@ -8,16 +8,24 @@ export const readText = (path: string) => readFileSync(path).toString();
  * Recursively reads given directory and returns file names matching
  * given extension.
  *
- * @param {*} dir
- * @param {*} ext
+ * @param dir
+ * @param ext
+ * @param maxDepth
+ * @param depth
  */
-export function* files(dir: string, ext: string): IterableIterator<string> {
+export function* files(
+    dir: string,
+    ext: string,
+    maxDepth = Infinity,
+    depth = 0
+): IterableIterator<string> {
+    if (depth >= maxDepth) return;
     for (let f of readdirSync(dir)) {
         const curr = dir + "/" + f;
         if (f.endsWith(ext)) {
             yield curr;
         } else if (statSync(curr).isDirectory()) {
-            yield* files(curr, ext);
+            yield* files(curr, ext, maxDepth, depth + 1);
         }
     }
 }
