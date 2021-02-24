b1cf4d8feccac4b3468a2fb0fdee268306406d78
Karsten Schmidt <k@postspectacular.com>
2021-02-24 16:28:12 +0000

    fix(random): update weightedRandom()
    - don't throw error if total weights <= 0 (only print warning)

--- packages/random/src/weighted-random.ts
+++ packages/random/src/weighted-random.ts
@@ -3,15 +3,21 @@ import type { IRandom } from "./api";
 import { SYSTEM } from "./system";
 
 /**
- * Returns a no-arg function which produces a random choice of given
- * weighted `choices` and using given {@link IRandom} instance (default
- * {@link SYSTEM}. If `weights` are given, it must be the same size as
- * `choices`. If omitted, each choice will have same probability.
+ * Returns a no-arg function which produces a random choice of given weighted
+ * `choices` and using given {@link IRandom} instance (default {@link SYSTEM}.
+ * If `weights` are given, it must be the same size as `choices`. If omitted,
+ * each choice will have same probability.
  *
+ * @remarks
+ * Throws an error if the `choices` array is empty (requires at least 1 item).
+ * If the total sum of `weights` is <= 0 a warning is printed to the console and
+ * the resulting function will only ever return the first `choice`.
+ *
+ * Based on:
  * {@link https://www.electricmonk.nl/log/2009/12/23/weighted-random-distribution/}
  *
  * @param choices -
- * @param weights -
+ * @param weights - optional weights
  */
 export const weightedRandom = <T>(
     choices: Array<T>,
@@ -26,7 +32,7 @@ export const weightedRandom = <T>(
               .sort((a, b) => b[1] - a[1])
         : choices.map((x) => <[T, number]>[x, 1]);
     const total = opts.reduce((acc, o) => acc + o[1], 0);
-    assert(total > 0, "no choices given");
+    total <= 0 && console.warn("total weights <= 0");
     return () => {
         const r = rnd.float(total);
         let sum = total;
