d525a48e5cd19932c8c947d38d90a3570d9881ec
Karsten Schmidt <k@postspectacular.com>
2021-02-28 15:11:59 +0000

    perf(examples): update adaptive-threshold demo
    - use new (much faster) convolution feature in thi.ng/pixel

--- examples/adaptive-threshold/src/threshold.ts
+++ examples/adaptive-threshold/src/threshold.ts
@@ -1,21 +1,15 @@
-import { GRAY8, PackedBuffer } from "@thi.ng/pixel";
 import {
-    comp,
-    convolve2d,
-    fillN,
-    map,
-    mean,
-    multiplex,
-    range2d,
-    repeat,
-    transduce,
-} from "@thi.ng/transducers";
+    convolveChannel,
+    FloatBuffer,
+    FLOAT_GRAY,
+    GRAY8,
+    PackedBuffer,
+} from "@thi.ng/pixel";
 
 /**
- * Adaptive threshold computation: uses `convolve2d` transducer to
- * compute mean brightness for each pixel's neighborhood, then applies
- * offset value and checks if pixel itself is above/below localized
- * threshold.
+ * Adaptive threshold computation: uses `convolveChannel` with a custom pooling
+ * kernel to compute mean brightness for each pixel's neighborhood, then applies
+ * offset value and checks if pixel itself is above/below localized threshold.
  *
  * @param src
  * @param windowSize
@@ -26,32 +20,12 @@ export const adaptiveThreshold = (
     windowSize: number,
     offset = 0
 ) =>
-    new PackedBuffer(
-        src.width,
-        src.height,
-        GRAY8,
-        <Uint8Array>transduce(
-            comp(
-                multiplex(
-                    map((xy) => xy),
-                    convolve2d({
-                        src: src.pixels,
-                        width: src.width,
-                        height: src.height,
-                        weights: repeat(1),
-                        kwidth: windowSize,
-                        kheight: windowSize,
-                        wrap: false,
-                        border: 0,
-                        reduce: mean,
-                    })
-                ),
-                map(([[x, y], mean]) =>
-                    src.getAt(x, y) - mean + offset < 0 ? 0 : 255
-                )
-            ),
-            fillN(),
-            new Uint8Array(src.width * src.height),
-            range2d(src.width, src.height)
-        )
-    );
+    convolveChannel(FloatBuffer.fromPacked(src, FLOAT_GRAY), {
+        kernel: {
+            pool: (body, w, h) =>
+                `(${body[(h >> 1) * w + (w >> 1)]} - (${body.join("+")})/${
+                    w * h
+                } + ${offset / 255}) < 0 ? 0 : 1`,
+            size: windowSize,
+        },
+    }).as(GRAY8);
