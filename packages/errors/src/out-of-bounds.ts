ab007d6b502c3d1650c7e9cf50da05f0ac042ef3
Karsten Schmidt <k@postspectacular.com>
2021-03-13 11:22:38 +0000

    feat(errors): add ensureIndex2(), update outOfBounds() arg type

--- packages/errors/src/out-of-bounds.ts
+++ packages/errors/src/out-of-bounds.ts
@@ -2,7 +2,7 @@ import { defError } from "./deferror";
 
 export const OutOfBoundsError = defError<any>(() => "index out of bounds");
 
-export const outOfBounds = (index: number): never => {
+export const outOfBounds = (index: any): never => {
     throw new OutOfBoundsError(index);
 };
 
@@ -15,3 +15,20 @@ export const outOfBounds = (index: number): never => {
  */
 export const ensureIndex = (index: number, min: number, max: number) =>
     (index < min || index >= max) && outOfBounds(index);
+
+/**
+ * Throws an {@link OutOfBoundsError} if either `x` or `y` is outside their
+ * respective `[0..max)` range.
+ *
+ * @param x
+ * @param y
+ * @param maxX
+ * @param maxY
+ * @returns
+ */
+export const ensureIndex2 = (
+    x: number,
+    y: number,
+    maxX: number,
+    maxY: number
+) => (x < 0 || x >= maxX || y < 0 || y >= maxY) && outOfBounds([x, y]);
