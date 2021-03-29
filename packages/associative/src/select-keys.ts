e0977db6708abdaaa2ef9dc78d472d77467e30bb
Karsten Schmidt <k@postspectacular.com>
2021-03-29 20:01:33 +0100

    feat(associative): add selectDefinedKeys*() fns

--- packages/associative/src/select-keys.ts
+++ packages/associative/src/select-keys.ts
@@ -19,19 +19,57 @@ export const selectKeysMap = <K, V>(
 };
 
 /**
+ * Similar to {@link selectKeysMap}, but only selects keys if their value is
+ * defined (i.e. non-nullish).
+ *
+ * @param src
+ * @param ks
+ */
+export const selectDefinedKeysMap = <K, V>(
+    src: Map<K, V>,
+    ks: Iterable<K>
+): Map<K, V> => {
+    const dest = empty(src, Map);
+    for (let k of ks) {
+        const val = src.get(k);
+        if (val != null) dest.set(k, val);
+    }
+    return dest;
+};
+
+/**
  * Returns a new object only containing given keys (and only if they
  * existed in the original).
  *
  * @param src - source object
  * @param ks - selected keys
  */
-export const selectKeysObj = <T extends object>(
+export const selectKeysObj = <T extends object, K extends keyof T>(
     src: T,
-    ks: Iterable<PropertyKey>
+    ks: Iterable<K>
 ): Partial<T> => {
     const dest: any = {};
     for (let k of ks) {
         src.hasOwnProperty(k) && (dest[k] = (<any>src)[<any>k]);
     }
     return dest;
+};
+
+/**
+ * Similar to {@link selectKeysObj}, but only selects keys if their value is
+ * defined (i.e. non-nullish).
+ *
+ * @param src
+ * @param ks
+ */
+export const selectDefinedKeysObj = <T extends object, K extends keyof T>(
+    src: T,
+    ks: Iterable<K>
+) => {
+    const res: Partial<T> = {};
+    for (let k of ks) {
+        const val = src[k];
+        if (val != null) res[k] = val;
+    }
+    return res;
 };
