63d78cbac7528bf98e368ef473f3fa6f8d9bc43f
Karsten Schmidt <k@postspectacular.com>
2021-04-01 10:25:25 +0100

    refactor(checks): add implementsFunction() generics

--- packages/checks/src/implements-function.ts
+++ packages/checks/src/implements-function.ts
@@ -1,2 +1,4 @@
-export const implementsFunction = (x: any, fn: string | symbol) =>
-    x != null && typeof x[fn] === "function";
+export const implementsFunction = <T = any, K extends keyof T = any>(
+    x: any,
+    fn: K
+): x is Pick<T, K> => x != null && typeof x[fn] === "function";
