a0e5a11fd88256624689cc78c99c7b491d6f88ac
Karsten Schmidt <k@thi.ng>
2021-04-24 13:52:57 +0100

    test(math): add modulo tests

--- packages/math/test/index.ts
+++ packages/math/test/index.ts
@@ -1,6 +1,19 @@
-// import * as assert from "assert";
-// import * as m from "../src";
+import * as assert from "assert";
+import { fmod, mod, remainder } from "../src";
 
 describe("math", () => {
-    it("tests pending");
+    it("fmod", () => {
+        assert.strictEqual(fmod(3.75, 2), 1.75);
+        assert.strictEqual(fmod(-3.75, 2), -1.75);
+        assert.strictEqual(3.75 % 2, 1.75);
+        assert.strictEqual(-3.75 % 2, -1.75);
+    });
+    it("mod", () => {
+        assert.strictEqual(mod(3.75, 2), 1.75);
+        assert.strictEqual(mod(-3.75, 2), 0.25);
+    });
+    it("remainder", () => {
+        assert.strictEqual(remainder(3.75, 2), -0.25);
+        assert.strictEqual(remainder(-3.75, 2), 0.25);
+    });
 });
