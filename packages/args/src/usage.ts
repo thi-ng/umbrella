fa56620e7491b75c859a2914c02c73bebbaa6bd3
Karsten Schmidt <k@thi.ng>
2021-08-04 15:15:15 +0200

    reafactor(args): split/simplify usage()

--- packages/args/src/usage.ts
+++ packages/args/src/usage.ts
@@ -29,48 +29,7 @@ export const usage = <T extends IObjectOf<any>>(
             : <ColorTheme>{};
     const indent = repeat(" ", opts.paramWidth!);
     const format = (ids: string[]) =>
-        ids.map((id) => {
-            const spec: ArgSpecExt = specs[id];
-            const hint = spec.hint ? ansi(" " + spec.hint, theme.hint!) : "";
-            const name = ansi(`--${kebab(id)}`, theme.param!);
-            const alias = spec.alias
-                ? `${ansi("-" + spec.alias, theme.param!)}${hint}, `
-                : "";
-            const params = `${alias}${name}${hint}`;
-            const isRequired =
-                spec.optional === false && spec.default === undefined;
-            const prefixes: string[] = [];
-            isRequired && prefixes.push("required");
-            spec.multi && prefixes.push("multiple");
-            const prefix = prefixes.length
-                ? ansi(
-                      `[${prefixes.join(", ")}] `,
-                      isRequired ? theme.required! : theme.multi!
-                  )
-                : "";
-            const defaults =
-                opts.showDefaults &&
-                spec.default != null &&
-                spec.default !== false
-                    ? ansi(
-                          ` (default: ${stringify(true)(
-                              spec.defaultHint != undefined
-                                  ? spec.defaultHint
-                                  : spec.default
-                          )})`,
-                          theme.default
-                      )
-                    : "";
-            return (
-                padRight(opts.paramWidth!)(params, lengthAnsi(params)) +
-                wrap(
-                    prefix + (spec.desc || "") + defaults,
-                    opts.lineWidth! - opts.paramWidth!
-                )
-                    .map((l, i) => (i > 0 ? indent + l : l))
-                    .join("\n")
-            );
-        });
+        ids.map((id) => argUsage(id, specs[id], opts, theme, indent));
     const sortedIDs = Object.keys(specs).sort();
     const groups: Pair<string, string[]>[] = opts.groups
         ? opts.groups
@@ -94,6 +53,67 @@ export const usage = <T extends IObjectOf<any>>(
         ...wrap(opts.suffix, opts.lineWidth!),
     ].join("\n");
 };
+
+const argUsage = (
+    id: string,
+    spec: ArgSpecExt,
+    opts: Partial<UsageOpts>,
+    theme: ColorTheme,
+    indent: string
+) => {
+    const hint = argHint(spec, theme);
+    const alias = argAlias(spec, theme, hint);
+    const name = ansi(`--${kebab(id)}`, theme.param!);
+    const params = `${alias}${name}${hint}`;
+    const isRequired = spec.optional === false && spec.default === undefined;
+    const prefixes: string[] = [];
+    isRequired && prefixes.push("required");
+    spec.multi && prefixes.push("multiple");
+    const body =
+        argPrefix(prefixes, theme, isRequired) +
+        (spec.desc || "") +
+        argDefault(spec, opts, theme);
+    return (
+        padRight(opts.paramWidth!)(params, lengthAnsi(params)) +
+        wrap(body, opts.lineWidth! - opts.paramWidth!)
+            .map((l, i) => (i > 0 ? indent + l : l))
+            .join("\n")
+    );
+};
+
+const argHint = (spec: ArgSpecExt, theme: ColorTheme) =>
+    spec.hint ? ansi(" " + spec.hint, theme.hint!) : "";
+
+const argAlias = (spec: ArgSpecExt, theme: ColorTheme, hint: string) =>
+    spec.alias ? `${ansi("-" + spec.alias, theme.param!)}${hint}, ` : "";
+
+const argPrefix = (
+    prefixes: string[],
+    theme: ColorTheme,
+    isRequired: boolean
+) =>
+    prefixes.length
+        ? ansi(
+              `[${prefixes.join(", ")}] `,
+              isRequired ? theme.required! : theme.multi!
+          )
+        : "";
+
+const argDefault = (
+    spec: ArgSpecExt,
+    opts: Partial<UsageOpts>,
+    theme: ColorTheme
+) =>
+    opts.showDefaults && spec.default != null && spec.default !== false
+        ? ansi(
+              ` (default: ${stringify(true)(
+                  spec.defaultHint != undefined
+                      ? spec.defaultHint
+                      : spec.default
+              )})`,
+              theme.default
+          )
+        : "";
 
 const ansi = (x: string, col: number) =>
     col != null ? `\x1b[${col}m${x}\x1b[0m` : x;
