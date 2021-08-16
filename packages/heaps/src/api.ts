c33027bfe8cc1cb5aa0241767d7bc788ff6b63f6
Karsten Schmidt <k@thi.ng>
2021-08-16 15:38:48 +0200

    feat(heaps): add PriorityQueue impl

--- packages/heaps/src/api.ts
+++ packages/heaps/src/api.ts
@@ -8,3 +8,14 @@ export interface HeapOpts<T> {
 export interface DHeapOpts<T> extends HeapOpts<T> {
     d: number;
 }
+
+export interface PriorityQueueOpts<T> {
+    /**
+     * Comparator for priorities. Default: ascending order
+     */
+    compare: Comparator<number>;
+    /**
+     * Equality predicate for queued values. Default: thi.ng/equiv
+     */
+    equiv: Predicate2<T>;
+}
---
+++ packages/heaps/src/priority-queue.ts
@@ -0,0 +1,122 @@
+import type {
+    IClear,
+    ICopy,
+    IEmpty,
+    IInto,
+    ILength,
+    Pair,
+    Predicate2,
+} from "@thi.ng/api";
+import { compareNumDesc } from "@thi.ng/compare";
+import { equiv } from "@thi.ng/equiv";
+import type { PriorityQueueOpts } from "./api";
+import { Heap } from "./heap";
+
+/**
+ * Returns a new {@link PriorityQueue} instance, a thin veneer wrapper for a
+ * backing {@link Heap} and exposing a PQ-like API for arbitrary values, with
+ * configurable value equality handling and priority ordering.
+ *
+ * @remarks
+ * - By default higher priority values mean higher priority.
+ * - Already queued items can be reprioritized or removed.
+ * - The queue head can be inspected via `peek()` and/or `peekPriority()`
+ *   without removing it from the queue.
+ * - Multiple items can be added at once via `into()`
+ * - The queue is iterable (in priority order, according to given comparator)
+ */
+export const defPriorityQueue = <T>(
+    values?: Iterable<Pair<number, T>> | null,
+    opts?: Partial<PriorityQueueOpts<T>>
+) => new PriorityQueue(values, opts);
+
+export class PriorityQueue<T>
+    implements
+        Iterable<Pair<number, T>>,
+        IClear,
+        ICopy<PriorityQueue<T>>,
+        IEmpty<PriorityQueue<T>>,
+        IInto<Pair<number, T>, PriorityQueue<T>>,
+        ILength
+{
+    heap: Heap<Pair<number, T>>;
+    equiv: Predicate2<T>;
+
+    constructor(
+        values?: Iterable<Pair<number, T>> | null,
+        protected opts: Partial<PriorityQueueOpts<T>> = {}
+    ) {
+        const compare = opts.compare || compareNumDesc;
+        this.heap = new Heap<Pair<number, T>>(null, {
+            compare: (a, b) => compare(a[0], b[0]),
+        });
+        this.equiv = opts.equiv || equiv;
+        values && this.into(values);
+    }
+
+    get length() {
+        return this.heap.length;
+    }
+
+    *[Symbol.iterator]() {
+        yield* this.heap;
+    }
+
+    clear() {
+        this.heap.clear();
+    }
+
+    copy() {
+        return new PriorityQueue<T>(this.heap, this.opts);
+    }
+
+    empty() {
+        return new PriorityQueue<T>(null, this.opts);
+    }
+
+    push(val: T, priority: number) {
+        this.heap.push([priority, val]);
+        return this;
+    }
+
+    pushPop(val: T, priority: number) {
+        return this.heap.pushPop([priority, val]);
+    }
+
+    pop() {
+        const res = this.heap.pop();
+        return res ? res[1] : undefined;
+    }
+
+    peek() {
+        const res = this.heap.peek();
+        return res ? res[1] : undefined;
+    }
+
+    peekPriority() {
+        const res = this.heap.peek();
+        return res ? res[0] : undefined;
+    }
+
+    remove(val: T) {
+        const { heap, equiv } = this;
+        const item = heap.findWith((x) => equiv(x[1], val));
+        return item ? heap.remove(item) : false;
+    }
+
+    into(values: Iterable<Pair<number, T>>) {
+        this.heap.into(values);
+        return this;
+    }
+
+    reprioritize(val: T, priority: number) {
+        const { heap, equiv } = this;
+        const item = heap.findWith((x) => equiv(x[1], val));
+        if (item) {
+            item[0] = priority;
+            heap.heapify();
+            return true;
+        }
+        return false;
+    }
+}
