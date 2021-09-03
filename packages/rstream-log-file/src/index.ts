bfb004c4a42c04c7e040e2491abd2d4466d57b5c
Karsten Schmidt <k@thi.ng>
2021-09-03 09:57:47 +0200

    refactor(rstream-log-file): fix up TS4.4 changes

--- packages/rstream-log-file/src/index.ts
+++ packages/rstream-log-file/src/index.ts
@@ -5,7 +5,7 @@ export const writeFile = (path: string): ISubscriber<string> => ({
     next(msg) {
         appendFile(path, msg + "\n", (e) => {
             if (e) {
-                process.stderr.write(e.message);
+                process.stderr.write((<Error>e).message);
             }
         });
     },
