a71ccba887fbd5df9340b8bf3e841e1339364fb3
Karsten Schmidt <k@thi.ng>
2021-09-10 23:41:21 +0200

    refactor(geom-io-obj): update imports

--- packages/geom-io-obj/src/parser.ts
+++ packages/geom-io-obj/src/parser.ts
@@ -1,4 +1,4 @@
-import { assert } from "@thi.ng/api";
+import { assert } from "@thi.ng/api/assert";
 import type { Vec } from "@thi.ng/vectors";
 import type { OBJFace, OBJGroup, OBJModel, ParseOpts } from "./api";
 
