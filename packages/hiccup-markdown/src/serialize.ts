0188d235ab1bf5a51d0b65e159952d9c1dcae012
Karsten Schmidt <k@thi.ng>
2021-09-29 21:20:54 +0200

    refactor(hiccup-markdown): update defmulti impls

--- packages/hiccup-markdown/src/serialize.ts
+++ packages/hiccup-markdown/src/serialize.ts
@@ -2,8 +2,7 @@ import { implementsFunction } from "@thi.ng/checks/implements-function";
 import { isFunction } from "@thi.ng/checks/is-function";
 import { isNotStringAndIterable } from "@thi.ng/checks/is-not-string-iterable";
 import { isString } from "@thi.ng/checks/is-string";
-import type { MultiFn3 } from "@thi.ng/defmulti";
-import { defmulti } from "@thi.ng/defmulti/defmulti";
+import { DEFAULT, defmulti } from "@thi.ng/defmulti/defmulti";
 import { illegalArgs } from "@thi.ng/errors/illegal-arguments";
 import { normalize } from "@thi.ng/hiccup/normalize";
 import { repeat } from "@thi.ng/strings/repeat";
@@ -92,117 +91,122 @@ const header =
 const body = (el: any[], ctx: any, state: SerializeState) =>
     serializeIter(el[2], ctx, state);
 
-export const serializeElement: MultiFn3<any, any, SerializeState, string> =
-    defmulti((el) => el[0]);
-serializeElement.setDefault(body);
-
-serializeElement.addAll({
-    h1: header(1),
-    h2: header(2),
-    h3: header(3),
-    h4: header(4),
-    h5: header(5),
-    h6: header(6),
-
-    p: (el, ctx, state) => `\n${body(el, ctx, state)}\n`,
-
-    img: (el) => `![${el[1].alt || ""}](${el[1].src})`,
-
-    a: (el, ctx, state) => `[${body(el, ctx, state)}](${el[1].href})`,
-
-    em: (el, ctx, state) => `_${body(el, ctx, state)}_`,
-
-    strong: (el, ctx, state) => `**${body(el, ctx, state)}**`,
-
-    pre: (el, ctx, state) =>
-        `\n\`\`\`${el[1].lang || ""}\n${body(el, ctx, {
-            ...state,
-            pre: true,
-            sep: "\n",
-        })}\n\`\`\`\n`,
-
-    code: (el, ctx, state) =>
-        state.pre ? el[2][0] : `\`${body(el, ctx, state)}\``,
-
-    ul: (el, ctx, state) => {
-        const cstate: SerializeState = {
-            ...state,
-            indent: state.indent + 4,
-            sep: "\n",
-        };
-        return wrap(state.indent === 0 ? "\n" : "")(body(el, ctx, cstate));
+export const serializeElement = defmulti<any, any, SerializeState, string>(
+    (el) => el[0],
+    {
+        th: "strong",
     },
+    {
+        [DEFAULT]: body,
+        h1: header(1),
+        h2: header(2),
+        h3: header(3),
+        h4: header(4),
+        h5: header(5),
+        h6: header(6),
 
-    ol: (el, ctx, state) => {
-        const cstate: SerializeState = {
-            ...state,
-            indent: state.indent + 4,
-            id: 0,
-            sep: "\n",
-        };
-        return wrap(state.indent === 0 ? "\n" : "")(body(el, ctx, cstate));
-    },
+        p: (el, ctx, state) => `\n${body(el, ctx, state)}\n`,
 
-    li: (el, ctx, state) =>
-        repeat(" ", state.indent - 4) +
-        (state.id != null ? ++state.id + "." : "-") +
-        " " +
-        body(el, ctx, { ...state, sep: "" }),
+        img: (el) => `![${el[1].alt || ""}](${el[1].src})`,
 
-    blockquote: (el, ctx, state) => `\n> ${body(el, ctx, state)}\n`,
+        a: (el, ctx, state) => `[${body(el, ctx, state)}](${el[1].href})`,
 
-    br: () => "\\\n",
+        em: (el, ctx, state) => `_${body(el, ctx, state)}_`,
 
-    hr: () => "\n---\n",
+        strong: (el, ctx, state) => `**${body(el, ctx, state)}**`,
 
-    table: (el, ctx, state) => {
-        let caption = "";
-        let thead: any[] = [];
-        let tbody: any[] = [];
-        let colWidths: number[] = [];
+        pre: (el, ctx, state) =>
+            `\n\`\`\`${el[1].lang || ""}\n${body(el, ctx, {
+                ...state,
+                pre: true,
+                sep: "\n",
+            })}\n\`\`\`\n`,
 
-        const rows = (rows: any[]) =>
-            rows.map((x: any) =>
-                normalize(x)[2].map((td: any, i: number) => {
-                    const cell = serialize(td, ctx);
-                    colWidths[i] = Math.max(colWidths[i] || 3, cell.length);
-                    return cell;
-                })
-            );
+        code: (el, ctx, state) =>
+            state.pre ? el[2][0] : `\`${body(el, ctx, state)}\``,
 
-        for (let child of el[2]) {
-            child = normalize(child);
-            switch (child[0]) {
-                case "thead":
-                    thead = rows(child[2]);
-                    break;
-                case "tbody":
-                    tbody = rows(child[2]);
-                    break;
-                case "caption":
-                    caption = body(child, ctx, state);
-                    break;
-                default:
-                // TODO output warning?
+        ul: (el, ctx, state) => {
+            const cstate: SerializeState = {
+                ...state,
+                indent: state.indent + 4,
+                sep: "\n",
+            };
+            return wrap(state.indent === 0 ? "\n" : "")(body(el, ctx, cstate));
+        },
+
+        ol: (el, ctx, state) => {
+            const cstate: SerializeState = {
+                ...state,
+                indent: state.indent + 4,
+                id: 0,
+                sep: "\n",
+            };
+            return wrap(state.indent === 0 ? "\n" : "")(body(el, ctx, cstate));
+        },
+
+        li: (el, ctx, state) =>
+            repeat(" ", state.indent - 4) +
+            (state.id != null ? ++state.id + "." : "-") +
+            " " +
+            body(el, ctx, { ...state, sep: "" }),
+
+        blockquote: (el, ctx, state) => `\n> ${body(el, ctx, state)}\n`,
+
+        br: () => "\\\n",
+
+        hr: () => "\n---\n",
+
+        table: (el, ctx, state) => {
+            let caption = "";
+            let thead: any[] = [];
+            let tbody: any[] = [];
+            let colWidths: number[] = [];
+
+            const rows = (rows: any[]) =>
+                rows.map((x: any) =>
+                    normalize(x)[2].map((td: any, i: number) => {
+                        const cell = serialize(td, ctx);
+                        colWidths[i] = Math.max(colWidths[i] || 3, cell.length);
+                        return cell;
+                    })
+                );
+
+            for (let child of el[2]) {
+                child = normalize(child);
+                switch (child[0]) {
+                    case "thead":
+                        thead = rows(child[2]);
+                        break;
+                    case "tbody":
+                        tbody = rows(child[2]);
+                        break;
+                    case "caption":
+                        caption = body(child, ctx, state);
+                        break;
+                    default:
+                    // TODO output warning?
+                }
             }
-        }
-        return (
-            "\n" +
-            formatCanvas(
-                tableCanvas(
-                    {
-                        cols: colWidths.map((width) => ({ width })),
-                        padding: [1, 0],
-                        border: Border.V,
-                    },
-                    [...thead, colWidths.map((w) => repeat("-", w)), ...tbody]
-                )
-            ) +
-            "\n" +
-            caption +
-            "\n"
-        );
-    },
-});
-
-serializeElement.isa("th", "strong");
+            return (
+                "\n" +
+                formatCanvas(
+                    tableCanvas(
+                        {
+                            cols: colWidths.map((width) => ({ width })),
+                            padding: [1, 0],
+                            border: Border.V,
+                        },
+                        [
+                            ...thead,
+                            colWidths.map((w) => repeat("-", w)),
+                            ...tbody,
+                        ]
+                    )
+                ) +
+                "\n" +
+                caption +
+                "\n"
+            );
+        },
+    }
+);
