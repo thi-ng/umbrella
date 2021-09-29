761de32b5366577dc1ee624bdd798dd3448212bf
Karsten Schmidt <k@thi.ng>
2021-09-29 19:06:34 +0200

    refactor(rstream-log): minor pkg restructure

--- packages/rstream-log/src/filter.ts
+++ packages/rstream-log/src/filter.ts
@@ -2,7 +2,7 @@ import type { LogLevel } from "@thi.ng/logger";
 import { isString } from "@thi.ng/checks/is-string";
 import type { Transducer } from "@thi.ng/transducers";
 import { filter } from "@thi.ng/transducers/filter";
-import type { LogEntry } from "../api";
+import type { LogEntry } from "./api";
 
 export const onlyLevel = (level: LogLevel): Transducer<LogEntry, LogEntry> =>
     filter((l) => l[0] === level);
