532e59fbe762452ef9309f212c0e7108a696bf57
Karsten Schmidt <k@thi.ng>
2021-10-05 15:00:54 +0200

    feat(examples): add pixel-dither example

---
+++ examples/pixel-dither/README.md
@@ -0,0 +1,15 @@
+# pixel-dither
+
+![screenshot](https://raw.githubusercontent.com/thi-ng/umbrella/develop/assets/examples/pixel-dither.jpg)
+
+[Live demo](http://demo.thi.ng/umbrella/pixel-dither/)
+
+Please refer to the [example build instructions](https://github.com/thi-ng/umbrella/wiki/Example-build-instructions) on the wiki.
+
+## Authors
+
+- Karsten Schmidt
+
+## License
+
+&copy; 2021 Karsten Schmidt // Apache Software License 2.0
---
+++ examples/pixel-dither/src/index.ts
@@ -0,0 +1,59 @@
+import type { IObjectOf } from "@thi.ng/api";
+import { GRAY8 } from "@thi.ng/pixel";
+import {
+    ATKINSON,
+    BURKES,
+    DIFFUSION_2D,
+    DIFFUSION_COLUMN,
+    DIFFUSION_ROW,
+    ditherWith,
+    FLOYD_STEINBERG,
+    JARVIS_JUDICE_NINKE,
+    SIERRA2,
+    STUCKI,
+    THRESHOLD,
+} from "@thi.ng/pixel-dither";
+import type { DitherKernel } from "@thi.ng/pixel-dither/api";
+import { canvas2d, imagePromise } from "@thi.ng/pixel/canvas";
+import { PackedBuffer, packedBufferFromImage } from "@thi.ng/pixel/packed";
+
+(async () => {
+    const img = await imagePromise("assets/michelangelo.png");
+
+    const root = document.getElementById("app")!;
+    root.appendChild(img);
+
+    const processImage = (
+        buf: PackedBuffer,
+        id: string,
+        kernel: DitherKernel
+    ) => {
+        const { canvas, ctx } = canvas2d(buf.width, buf.height, root);
+        ditherWith(kernel, buf.copy()).blitCanvas(canvas);
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, buf.height - 12, ctx.measureText(id).width + 8, 12);
+        ctx.fillStyle = "red";
+        ctx.fillText(id, 4, buf.height - 2);
+    };
+
+    const buf = packedBufferFromImage(img, GRAY8);
+
+    Object.entries(<IObjectOf<DitherKernel>>{
+        ATKINSON: ATKINSON,
+        BURKES: BURKES,
+        DIFFUSION_ROW: DIFFUSION_ROW,
+        DIFFUSION_COLUMN: DIFFUSION_COLUMN,
+        DIFFUSION_2D: DIFFUSION_2D,
+        FLOYD_STEINBERG: FLOYD_STEINBERG,
+        JARVIS_JUDICE_NINKE: JARVIS_JUDICE_NINKE,
+        SIERRA2: SIERRA2,
+        STUCKI: STUCKI,
+        THRESHOLD: THRESHOLD,
+        CUSTOM: {
+            ox: [1],
+            oy: [1],
+            weights: [1],
+            shift: 1,
+        },
+    }).forEach(([id, k]) => processImage(buf, id, k));
+})();
