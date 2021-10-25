32d77cf0954986d2acc4487c81780cce41c2eeb0
Karsten Schmidt <k@thi.ng>
2021-10-25 14:17:08 +0200

    fix(hdiff): migrate CLI to TS, update wrapper

--- packages/hdiff/src/cli.ts
+++ packages/hdiff/src/cli.ts
@@ -1,8 +1,8 @@
-#!/usr/bin/env node
-
-const fs = require("fs");
-const cproc = require("child_process");
-const hdiff = require("@thi.ng/hdiff");
+// thing:no-export
+import { readFileSync } from "fs";
+import { execSync } from "child_process";
+import { computeDiff } from "./diff.js";
+import { generateHtml } from "./html.js";
 
 let src1;
 let src2;
@@ -11,17 +11,17 @@ let headerBody;
 const args = process.argv.slice(2);
 if (args.length === 3) {
     const [path, rev1, rev2] = args;
-    src1 = cproc.execSync(`git show ${rev1}:${path}`).toString();
-    src2 = cproc.execSync(`git show ${rev2}:${path}`).toString();
+    src1 = execSync(`git show ${rev1}:${path}`).toString();
+    src2 = execSync(`git show ${rev2}:${path}`).toString();
     headerBody = ["header", ["h1", path], ["code", `${rev1} ⇌ ${rev2}`]];
 } else if (args.length === 2) {
     const [rev1, rev2] = args;
-    src1 = fs.readFileSync(rev1).toString();
-    src2 = fs.readFileSync(rev2).toString();
+    src1 = readFileSync(rev1).toString();
+    src2 = readFileSync(rev2).toString();
     headerBody = ["header", ["h1", "File diff"], ["code", `${rev1} ⇌ ${rev2}`]];
 } else {
     console.log("Usage:\n\thdiff file1 file2\n\thdiff relpath gitrev1 gitrev2");
     process.exit(1);
 }
 
-console.log(hdiff.generateHtml(hdiff.computeDiff(src1, src2), headerBody));
+console.log(generateHtml(computeDiff(src1, src2), headerBody));
