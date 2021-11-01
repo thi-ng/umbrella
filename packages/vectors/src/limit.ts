d9e8404ace40c6b3047167de5f5f1db2e302152c
Karsten Schmidt <k@thi.ng>
2021-11-01 13:03:22 +0100

    fix(vectors): arg types `limit()`, add docs

--- packages/vectors/src/limit.ts
+++ packages/vectors/src/limit.ts
@@ -1,9 +1,17 @@
-import type { ReadonlyVec, Vec } from "./api.js";
+import type { VecOpVN } from "./api.js";
 import { mag } from "./mag.js";
 import { mulN } from "./muln.js";
 import { set } from "./set.js";
 
-export const limit = (out: Vec, v: ReadonlyVec, n: number) => {
+/**
+ * Limits `v` to max length `n` and writes result to `out` (or to itself if
+ * `out` is null).
+ *
+ * @param out
+ * @param v
+ * @param n
+ */
+export const limit: VecOpVN = (out, v, n) => {
     !out && (out = v);
     const m = mag(v);
     return m > n ? mulN(out, v, n / m) : out !== v ? set(out, v) : out;
