fde4d2b6aab02887e12f14c75fccf37b686844ae
Karsten Schmidt <k@thi.ng>
2021-11-06 18:01:44 +0100

    fix(checks): add boolean to isPrimitive()

--- packages/checks/src/is-primitive.ts
+++ packages/checks/src/is-primitive.ts
@@ -1,4 +1,9 @@
-export const isPrimitive = (x: any): x is string | number => {
+/**
+ * Returns true if `x` is a string, number or boolean.
+ *
+ * @param x
+ */
+export const isPrimitive = (x: any): x is string | number | boolean => {
     const t = typeof x;
-    return t === "string" || t === "number";
+    return t === "string" || t === "number" || t === "boolean";
 };
