b42e315092262088054f37774cdeebd128beffeb
Karsten Schmidt <k@thi.ng>
2021-11-22 09:43:29 +0100

    feat(vectors): update variance(), sd(), standardize()
    - add support for pre-centered inputs to avoid extraneous computation
    - add vector length check to avoid NaN for 1D vectors

--- packages/vectors/src/standardize.ts
+++ packages/vectors/src/standardize.ts
@@ -1,11 +1,13 @@
 import type { ReadonlyVec, Vec } from "./api.js";
 import { center } from "./center.js";
-import { magSq } from "./magsq.js";
 import { mulN } from "./muln.js";
+import { set } from "./set.js";
+import { sd } from "./variance.js";
 
 /**
  * Returns standardized version of `a`, i.e. `a' / sd(a')`, where `a'` is the
- * {@link center}'ed version of `a`.
+ * {@link center}'ed version of `a`. If `isCentered` is false (default), the
+ * vector will first be centered in order to compute the result.
  *
  * @remarks
  * If `mag(a')` is zero, the returned vector will have all components
@@ -13,9 +15,14 @@ import { mulN } from "./muln.js";
  *
  * @param out
  * @param a
+ * @param isCentered
  */
-export const standardize = (out: Vec | null, a: ReadonlyVec) => {
-    out = center(out, a);
-    const m = magSq(out);
-    return m > 0 ? mulN(null, out, 1 / Math.sqrt(m / (out.length - 1))) : out;
+export const standardize = (
+    out: Vec | null,
+    a: ReadonlyVec,
+    isCentered = false
+) => {
+    out = isCentered ? center(out, a) : !out ? a : set(out, a);
+    const d = sd(out, true);
+    return d > 0 ? mulN(null, out, 1 / d) : out;
 };
--- packages/vectors/src/variance.ts
+++ packages/vectors/src/variance.ts
@@ -2,7 +2,30 @@ import type { ReadonlyVec } from "./api.js";
 import { center } from "./center.js";
 import { magSq } from "./magsq.js";
 
-export const variance = (a: ReadonlyVec) =>
-    magSq(center([], a)) / (a.length - 1);
+/**
+ * Computes variance of vector components in `a`. If `isCentered` is
+ * false (default), the vector will first be {@link center}ed (copy) in order to
+ * compute the result.
+ *
+ * @remarks
+ * var(a) = magSq(a) / (len(a)-1)). Returns 0 if len(a) < 2
+ *
+ * @param a
+ * @param isCentered
+ */
+export const variance = (a: ReadonlyVec, isCentered = false) =>
+    a.length > 1 ? magSq(isCentered ? a : center([], a)) / (a.length - 1) : 0;
 
-export const sd = (a: ReadonlyVec) => Math.sqrt(variance(a));
+/**
+ * Computes standard deviation of vector components in `a`. If `isCentered` is
+ * false (default), the vector will first be {@link center}ed (copy) in order to
+ * compute the result.
+ *
+ * @remarks
+ * sd = sqrt(var(a)). Returns 0 if len(a) < 2
+ *
+ * @param a
+ * @param isCentered
+ */
+export const sd = (a: ReadonlyVec, isCentered = false) =>
+    Math.sqrt(variance(a, isCentered));
