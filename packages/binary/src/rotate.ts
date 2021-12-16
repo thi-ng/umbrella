804565e6156079e5e70b5aa53800e6e51dc1a1cb
Karsten Schmidt <k@thi.ng>
2021-12-16 13:20:05 +0100

    feat(binary): add shiftRL()

--- packages/binary/src/rotate.ts
+++ packages/binary/src/rotate.ts
@@ -17,3 +17,12 @@ export const rotateLeft = (x: number, n: Bit) =>
  */
 export const rotateRight = (x: number, n: Bit) =>
     ((x >>> n) | (x << (32 - n))) >>> 0;
+
+/**
+ * Shifts `x` by `n` bits left or right. If `n` >= 0, the value will be `>>>`
+ * shifted to right, if `n` < 0 the value will be shifted left.
+ *
+ * @param x
+ * @param n
+ */
+export const shiftRL = (x: number, n: number) => (n < 0 ? x << -n : x >>> n);
