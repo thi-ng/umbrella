c37c27e2c3a40d999af5fc972a242e5007ef4451
Karsten Schmidt <k@thi.ng>
2022-03-11 11:45:22 +0100

    fix(geom-poly-utils): fix equilateralTriangle2()

--- packages/geom-poly-utils/src/equilateral.ts
+++ packages/geom-poly-utils/src/equilateral.ts
@@ -1,6 +1,7 @@
 import type { FnU2 } from "@thi.ng/api";
-import { THIRD_PI } from "@thi.ng/math/api";
+import { SQRT3_2 } from "@thi.ng/math/api";
 import type { Vec } from "@thi.ng/vectors";
+import { add2 } from "@thi.ng/vectors/add";
 import { maddN2 } from "@thi.ng/vectors/maddn";
 import { mag } from "@thi.ng/vectors/mag";
 import { normalize } from "@thi.ng/vectors/normalize";
@@ -9,10 +10,13 @@ import { sub2 } from "@thi.ng/vectors/sub";
 
 export const equilateralTriangle2: FnU2<Vec, Vec[]> = (a, b) => {
     const dir = sub2([], b, a);
-    const c = normalize(
-        null,
-        perpendicularCCW([], dir),
-        mag(dir) * Math.sin(THIRD_PI)
-    );
-    return [a, b, maddN2(null, dir, 0.5, c)];
+    return [
+        a,
+        b,
+        add2(
+            null,
+            normalize(null, perpendicularCCW([], dir), mag(dir) * SQRT3_2),
+            maddN2(dir, dir, 0.5, a)
+        ),
+    ];
 };
