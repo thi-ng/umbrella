10bc32ebc4f09f15eba29460ec2a8c84f7ac5937
Karsten Schmidt <k@thi.ng>
2022-05-03 12:11:14 +0200

    fix(vectors): fix clamp01/11 signatures

--- packages/vectors/src/clamp.ts
+++ packages/vectors/src/clamp.ts
@@ -3,9 +3,9 @@ import {
     clamp01 as _clamp01,
     clamp11 as _clamp11,
 } from "@thi.ng/math/interval";
-import type { MultiVecOpVVV, VecOpVVV } from "./api.js";
+import type { MultiVecOpV, MultiVecOpVVV, VecOpV, VecOpVVV } from "./api.js";
 import { defHofOp } from "./compile/emit.js";
-import { ARGS_VVV, FN3 } from "./compile/templates.js";
+import { ARGS_V, ARGS_VVV, FN, FN3 } from "./compile/templates.js";
 
 export const [clamp, clamp2, clamp3, clamp4] = defHofOp<
     MultiVecOpVVV,
@@ -13,11 +13,11 @@ export const [clamp, clamp2, clamp3, clamp4] = defHofOp<
 >(_clamp, FN3(), ARGS_VVV);
 
 export const [clamp01, clamp01_2, clamp01_3, clamp01_4] = defHofOp<
-    MultiVecOpVVV,
-    VecOpVVV
->(_clamp01, FN3(), ARGS_VVV);
+    MultiVecOpV,
+    VecOpV
+>(_clamp01, FN(), ARGS_V);
 
 export const [clamp11, clamp11_2, clamp11_3, clamp11_4] = defHofOp<
-    MultiVecOpVVV,
-    VecOpVVV
->(_clamp11, FN3(), ARGS_VVV);
+    MultiVecOpV,
+    VecOpV
+>(_clamp11, FN(), ARGS_V);
