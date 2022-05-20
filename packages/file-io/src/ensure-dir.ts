4ae95c24f37c0cb8d8815fa6532f13a78177bf2e
Karsten Schmidt <k@thi.ng>
2022-05-20 09:53:37 +0200

    fix(file-io): fix ensureDir() for local file paths

--- packages/file-io/src/ensure-dir.ts
+++ packages/file-io/src/ensure-dir.ts
@@ -2,14 +2,18 @@ import { existsSync, mkdirSync } from "fs";
 import { sep } from "path";
 
 /**
- * Checks if the directory for given file path already exists, and if not the
+ * Checks if the directory for given file `path` already exists, and if not the
  * case, creates it. Returns true if the latter case.
+ *
+ * @remarks
+ * If `path` only contains a filename (without any directory structure), the
+ * function does nothing.
  *
  * @param path
  */
 export const ensureDirForFile = (path: string) => {
     const dir = path.substring(0, path.lastIndexOf(sep));
-    return !existsSync(dir)
+    return dir.length > 0 && !existsSync(dir)
         ? (mkdirSync(dir, { recursive: true }), true)
         : false;
 };
