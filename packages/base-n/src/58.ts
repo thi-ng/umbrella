755a2a7201a3faf8eceaa8b4864495c016554f51
Karsten Schmidt <k@thi.ng>
2022-05-22 17:34:58 +0200

    feat(base-n): add BASE58_LC alt version

--- packages/base-n/src/58.ts
+++ packages/base-n/src/58.ts
@@ -1,9 +1,21 @@
 import { defBase } from "./base.js";
 
-export const B58_CHARS =
-    "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+const D = "123456789";
+const U = "ABCDEFGHJKLMNPQRSTUVWXYZ";
+const L = "abcdefghijkmnopqrstuvwxyz";
+
+export const B58_CHARS_UC = D + U + L;
+
+export const B58_CHARS_LC = D + L + U;
+
+export const B58_CHARS = B58_CHARS_UC;
 
 /**
  * Reference: https://en.wikipedia.org/wiki/Binary-to-text_encoding
  */
 export const BASE58 = defBase(B58_CHARS);
+
+/**
+ * Alt version of Base58 which uses {@link B58_CHARS_LC}.
+ */
+export const BASE58_LC = defBase(B58_CHARS_LC);
