35799dbd340b1ffdf544298df904b1fbd9073fb0
Karsten Schmidt <k@thi.ng>
2022-06-24 16:20:50 +0200

    feat(geom-api): add IAttributed, update IShape

--- packages/geom-api/src/shape.ts
+++ packages/geom-api/src/shape.ts
@@ -3,12 +3,19 @@ import type { Vec } from "@thi.ng/vectors";
 
 export type Attribs = IObjectOf<any>;
 
-export interface IShape extends ICopy<IShape> {
-    readonly type: number | string;
+export interface IAttributed<T> {
     attribs?: Attribs;
+
+    withAttribs(attribs: Attribs): T;
 }
 
-export interface AABBLike extends IShape {
+export interface IShape<T extends IShape = IShape<any>>
+    extends IAttributed<T>,
+        ICopy<T> {
+    readonly type: number | string;
+}
+
+export interface AABBLike extends IShape<AABBLike> {
     pos: Vec;
     size: Vec;
 
@@ -16,17 +23,17 @@ export interface AABBLike extends IShape {
     offset(x: number): this;
 }
 
-export interface SphereLike extends IShape {
+export interface SphereLike extends IShape<SphereLike> {
     pos: Vec;
     r: number;
 }
 
-export interface IHiccupShape extends IShape, IToHiccup {}
-
-export interface PCLike extends IShape {
+export interface PCLike extends IShape<PCLike> {
     points: Vec[];
 }
 
 export interface PCLikeConstructor {
     new (pts: Vec[], attribs: Attribs): PCLike;
 }
+
+export interface IHiccupShape extends IShape, IToHiccup {}
