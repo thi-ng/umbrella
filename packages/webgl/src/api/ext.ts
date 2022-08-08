dd2e2959413485570bb200ddaad6e34a1edb14fa
Karsten Schmidt <k@thi.ng>
2022-08-08 13:26:56 +0200

    feat(webgl): add more extensions to WebGLExtensionMap

--- packages/webgl/src/api/ext.ts
+++ packages/webgl/src/api/ext.ts
@@ -21,11 +21,15 @@ export interface WebGLExtensionMap {
 	ANGLE_instanced_arrays: ANGLE_instanced_arrays;
 	EXT_blend_minmax: EXT_blend_minmax;
 	EXT_color_buffer_float: WEBGL_color_buffer_float;
+	EXT_color_buffer_half_float: EXT_color_buffer_half_float;
+	EXT_float_blend: EXT_float_blend;
 	EXT_frag_depth: EXT_frag_depth;
 	EXT_shader_texture_lod: EXT_shader_texture_lod;
 	EXT_sRGB: EXT_sRGB;
+	EXT_texture_compression_rgtc: EXT_texture_compression_rgtc;
 	EXT_texture_filter_anisotropic: EXT_texture_filter_anisotropic;
 	OES_element_index_uint: OES_element_index_uint;
+	OES_fbo_render_mipmap: OES_fbo_render_mipmap;
 	OES_standard_derivatives: OES_standard_derivatives;
 	OES_texture_float_linear: OES_texture_float_linear;
 	OES_texture_float: OES_texture_float;
@@ -34,6 +38,8 @@ export interface WebGLExtensionMap {
 	OES_vertex_array_object: OES_vertex_array_object;
 	WEBGL_color_buffer_float: WEBGL_color_buffer_float;
 	WEBGL_compressed_texture_astc: WEBGL_compressed_texture_astc;
+	WEBGL_compressed_texture_etc: WEBGL_compressed_texture_etc;
+	WEBGL_compressed_texture_etc1: WEBGL_compressed_texture_etc1;
 	WEBGL_compressed_texture_s3tc_srgb: WEBGL_compressed_texture_s3tc_srgb;
 	WEBGL_compressed_texture_s3tc: WEBGL_compressed_texture_s3tc;
 	WEBGL_debug_renderer_info: WEBGL_debug_renderer_info;
@@ -41,6 +47,7 @@ export interface WebGLExtensionMap {
 	WEBGL_depth_texture: WEBGL_depth_texture;
 	WEBGL_draw_buffers: WEBGL_draw_buffers;
 	WEBGL_lose_context: WEBGL_lose_context;
+	WEBGL_multi_draw: WEBGL_multi_draw;
 }
 
 export type ExtensionName = keyof WebGLExtensionMap;
