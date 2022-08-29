03616b5d23c0e356417ad6d31350034ba89969ef
Karsten Schmidt <k@thi.ng>
2022-08-30 01:17:49 +0200

    feat(logger): update MemoryLogger
    - add .clear() & .messages() methods
    - update pkg meta

--- packages/logger/src/memory.ts
+++ packages/logger/src/memory.ts
@@ -12,6 +12,25 @@ export class MemoryLogger extends ALogger {
 		super(id, level);
 	}
 
+	/**
+	 * Clears the {@link MemoryLogger.journal} of all recorded log entries.
+	 */
+	clear() {
+		this.journal.length = 0;
+	}
+
+	/**
+	 * Returns an array of stringified payloads of all current journal log
+	 * entries.
+	 *
+	 * @remarks
+	 * Only the user provided args of each {@link LogEntry} (excluding log
+	 * level, id, epoch) are included and concatenated as strings.
+	 */
+	messages() {
+		return this.journal.map((x) => x.slice(3).join(" "));
+	}
+
 	protected log(level: LogLevel, args: any[]) {
 		if (this.journal.length >= this.limit) this.journal.shift();
 		this.journal.push([level, this.id, Date.now(), ...args]);
