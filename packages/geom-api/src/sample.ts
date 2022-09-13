9c1e40e05ec4e46ff43fadf979eed5e5e81d0ba0
Karsten Schmidt <k@thi.ng>
2022-09-13 15:18:13 +0200

    docs(geom-api): update SamplingOpts docstrings

--- packages/geom-api/src/sample.ts
+++ packages/geom-api/src/sample.ts
@@ -15,13 +15,16 @@ export interface SamplingOpts {
 	 */
 	num: number;
 	/**
-	 * Defines the target angle between sampled points. If greater than
-	 * the actual range of the arc, only the two end points will be
-	 * returned at most. This option is used to derive a `num` value and
-	 * takes priority if `num` is given as well.
+	 * Defines the target angle between sampled points. If greater than the
+	 * actual range of the arc, only the two end points will be returned at
+	 * most. This option is used to derive a `num` value and takes priority if
+	 * `num` is given as well.
 	 *
-	 * This option is useful to adapt the sampling based on angular
-	 * resolution, rather than a fixed number of samples.
+	 * This option is useful to adapt the sampling based on angular resolution,
+	 * rather than a fixed number of samples.
+	 *
+	 * Note: Currently, in thi.ng/geom this option is only used for resampling
+	 * arcs, circles or ellipses.
 	 */
 	theta: number;
 	/**
