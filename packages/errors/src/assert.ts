781470d92b0a9aa6fe4a93935bd346a7fd4fc4fc
Karsten Schmidt <k@thi.ng>
2022-10-04 19:21:59 +0200

    fix(errors): update assertion switch logic
    - remove support for obsolete (& broken) snowpack setup
    - add support for Vite's env var handling

--- packages/errors/src/assert.ts
+++ packages/errors/src/assert.ts
@@ -1,30 +1,24 @@
 import { defError } from "./deferror.js";
 
-// FIXME https://github.com/snowpackjs/snowpack/issues/3621#issuecomment-907731004
-// FIXME https://github.com/snowpackjs/snowpack/issues/3768
-/* imports meta.hot method */
-
-declare const __SNOWPACK_ENV__: any;
-
 export const AssertionError = defError<any>(() => "Assertion failed");
 
 /**
- * Takes a `test` result or predicate function without args and throws
- * error with given `msg` if test failed (i.e. is falsy).
+ * Takes a `test` result or predicate function without args and throws error
+ * with given `msg` if test failed (i.e. is falsy).
  *
  * @remarks
- * The function is only enabled if `process.env.NODE_ENV != "production"`
- * or if the `UMBRELLA_ASSERTS` env var is set to 1.
+ * The function is only enabled if `process.env.NODE_ENV != "production"` or if
+ * the `UMBRELLA_ASSERTS` or `VITE_UMBRELLA_ASSERTS` env var is set to 1.
  */
-export const assert = (() =>
-	typeof process !== "undefined" && typeof process.env !== "undefined"
+export const assert = (
+	typeof process !== "undefined"
 		? process.env.NODE_ENV !== "production" ||
 		  !!process.env.UMBRELLA_ASSERTS
-		: typeof __SNOWPACK_ENV__ !== "undefined"
-		? __SNOWPACK_ENV__.MODE !== "production" ||
-		  !!__SNOWPACK_ENV__.UMBRELLA_ASSERTS ||
-		  !!__SNOWPACK_ENV__.SNOWPACK_PUBLIC_UMBRELLA_ASSERTS
-		: true)()
+		: (<any>import.meta).env
+		? (<any>import.meta).env.MODE !== "production" ||
+		  !!(<any>import.meta).env.VITE_UMBRELLA_ASSERTS
+		: true
+)
 	? (test: boolean | (() => boolean), msg?: string | (() => string)) => {
 			if ((typeof test === "function" && !test()) || !test) {
 				throw new AssertionError(
