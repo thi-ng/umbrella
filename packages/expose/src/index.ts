9a98c3e3b4ce93b765d1ca18bc16b70bc53eaa20
Karsten Schmidt <k@thi.ng>
2022-10-04 19:22:37 +0200

    fix(expose): update expose switch logic
    - remove support for obsolete (& broken) snowpack setup
    - add support for Vite's env var handling

--- packages/expose/src/index.ts
+++ packages/expose/src/index.ts
@@ -1,13 +1,7 @@
-// FIXME https://github.com/snowpackjs/snowpack/issues/3621#issuecomment-907731004
-// FIXME https://github.com/snowpackjs/snowpack/issues/3768
-/* imports meta.hot method */
-
-declare const __SNOWPACK_ENV__: any;
-
 /**
- * Exposes given `value` as `id` in global scope, iff `always = true`
- * (default: false) or if `process.env.NODE_ENV != "production"` or if the
- * `UMBRELLA_GLOBALS` env var is set to 1.
+ * Exposes given `value` as `id` in global scope, iff `always = true` (default:
+ * false) or if `process.env.NODE_ENV != "production"` or if the
+ * `UMBRELLA_GLOBALS`  or `VITE_UMBRELLA_GLOBALS` env var is set to 1.
  *
  * @param id -
  * @param value -
@@ -23,16 +17,13 @@ export const exposeGlobal = (id: string, value: any, always = false) => {
 	if (
 		glob &&
 		(always ||
-			(() =>
-				typeof process !== "undefined" &&
-				typeof process.env !== "undefined"
-					? process.env.NODE_ENV !== "production" ||
-					  !!process.env.UMBRELLA_GLOBALS
-					: typeof __SNOWPACK_ENV__ !== "undefined"
-					? __SNOWPACK_ENV__.MODE !== "production" ||
-					  !!__SNOWPACK_ENV__.UMBRELLA_GLOBALS ||
-					  !!__SNOWPACK_ENV__.SNOWPACK_PUBLIC_UMBRELLA_GLOBALS
-					: true)())
+			(typeof process !== "undefined"
+				? process.env.NODE_ENV !== "production" ||
+				  !!process.env.UMBRELLA_GLOBALS
+				: (<any>import.meta).env
+				? (<any>import.meta).env.MODE !== "production" ||
+				  !!(<any>import.meta).env.VITE_UMBRELLA_GLOBALS
+				: true))
 	) {
 		glob[id] = value;
 	}
