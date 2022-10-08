72c604a1e1cbc55ec82dd2963516ddc299dc0365
Karsten Schmidt <k@thi.ng>
2022-10-08 11:38:23 +0200

    feat(examples): add pixel-normal-map demo

---
+++ examples/pixel-normal-map/README.md
@@ -0,0 +1,15 @@
+# pixel-normal-map
+
+![screenshot](https://raw.githubusercontent.com/thi-ng/umbrella/develop/assets/examples/pixel-normal-map.png)
+
+[Live demo](http://demo.thi.ng/umbrella/pixel-normal-map/)
+
+Please refer to the [example build instructions](https://github.com/thi-ng/umbrella/wiki/Example-build-instructions) on the wiki.
+
+## Authors
+
+- Karsten Schmidt
+
+## License
+
+&copy; 2022 Karsten Schmidt // Apache Software License 2.0
---
+++ examples/pixel-normal-map/src/index.ts
@@ -0,0 +1,27 @@
+import {
+	canvas2d,
+	floatBufferFromImage,
+	FLOAT_GRAY,
+	imagePromise,
+	normalMap,
+} from "@thi.ng/pixel";
+import TEX from "./tex.png";
+
+(async () => {
+	// load texture image
+	const img = await imagePromise(TEX);
+	document.body.appendChild(img);
+	// convert image to grayscale float buffer
+	const buf = floatBufferFromImage(img, FLOAT_GRAY);
+	// compute normalmap w/ given options
+	const nmap = normalMap(buf, { scale: 8, step: 2 });
+	// create canvas and convert/copy normal map into it
+	nmap.blitCanvas(canvas2d(img.width, img.height, document.body).canvas);
+	// another normal map with sign of `scale` flipped to "invert" normal direction
+	const nmap2 = normalMap(buf, { scale: -8, step: 2 });
+	nmap2.blitCanvas(canvas2d(img.width, img.height, document.body).canvas);
+
+	// if a result canvas isn't needed, converting to JS native ImageData
+	// might be sufficient (e.g. for WebGL textures):
+	// const idata = nmap.toImageData()
+})();
