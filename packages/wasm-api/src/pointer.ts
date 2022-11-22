f1eef25caf325a07b14637b8f98934de7f2a4675
Karsten Schmidt <k@thi.ng>
2022-11-22 23:54:25 +0100

    feat(wasm-api): add Pointer64 (still unused)

--- packages/wasm-api/src/pointer.ts
+++ packages/wasm-api/src/pointer.ts
@@ -31,3 +31,34 @@ export class Pointer<T> implements IDeref<T> {
 		return this.fn(this.addr);
 	}
 }
+
+/**
+ * Generic pointer facility for {@link WASM64} target, which on
+ * {@link Pointer64.deref()} calls wrapper function (provided as ctor arg) to
+ * realise the pointer's target value. The pointer's target address can be
+ * accessed via {@link Pointer.addr} (read/write).
+ *
+ * @remarks
+ * The pointer always behaves like `volatile`, i.e. memoization of target values
+ * is purposfully avoided and the wrapper function is executed anew _each_ time
+ * the pointer is deref'd.
+ */
+export class Pointer64<T> implements IDeref<T> {
+	constructor(
+		public readonly mem: IWasmMemoryAccess,
+		public readonly base: bigint,
+		public readonly fn: Fn<bigint, T>
+	) {}
+
+	get addr() {
+		return this.mem.u64[Number(this.base >> BigInt(3))];
+	}
+
+	set addr(addr: bigint) {
+		this.mem.u64[Number(this.base >> BigInt(3))] = addr;
+	}
+
+	deref() {
+		return this.fn(this.addr);
+	}
+}
