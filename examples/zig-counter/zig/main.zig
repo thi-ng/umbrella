f9f906ef34a42e75266c79d53c792c2d5872fc46
Karsten Schmidt <k@thi.ng>
2022-11-24 18:16:18 +0100

    docs(examples): update ref links

--- examples/zig-counter/zig/main.zig
+++ examples/zig-counter/zig/main.zig
@@ -9,7 +9,7 @@ pub usingnamespace wasm;
 // allocator, also exposed & used by JS-side WasmBridge & DOM module
 // see further comments in:
 // https://github.com/thi-ng/umbrella/blob/develop/packages/wasm-api/zig/lib.zig
-// https://github.com/thi-ng/umbrella/blob/develop/packages/wasm-api-dom/zig/events.zig
+// https://github.com/thi-ng/umbrella/blob/develop/packages/wasm-api-dom/zig/lib.zig
 // https://github.com/thi-ng/umbrella/blob/develop/packages/wasm-api-schedule/zig/lib.zig
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 pub const WASM_ALLOCATOR = gpa.allocator();
