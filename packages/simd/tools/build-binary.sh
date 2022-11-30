398248fd1081dc361f5b99f461be4197f02ce2f9
Karsten Schmidt <k@thi.ng>
2022-11-30 19:59:45 +0100

    build(simd): update build-binary script

--- packages/simd/tools/build-binary.sh
+++ packages/simd/tools/build-binary.sh
@@ -1,10 +1,10 @@
-yarn asc assembly/index.ts -b simd.wasm -t simd.wat --optimize --enable simd --runtime stub --importMemory --memoryBase 0
+yarn asc assembly/index.ts -b simd.wasm -t simd.wast --optimize --enable simd --runtime stub --importMemory --memoryBase 0
 
 # apply binaryen optimizer
 wasm-opt simd.wasm -o opt.wasm -Oz --enable-simd
 
 # display as .wat text format
-wasm-dis opt.wasm -o opt.wat
+wasm-dis opt.wasm -o opt.wast
 
 # encode as base64
 cat << EOF > src/binary.ts
