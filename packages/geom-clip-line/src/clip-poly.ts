445ccd651b7bdfd515db7b527cefa5365243794f
Karsten Schmidt <k@thi.ng>
2022-12-08 16:34:34 +0100

    feat(geom-clip-line): add clipPolylinePoly()

--- packages/geom-clip-line/src/clip-poly.ts
+++ packages/geom-clip-line/src/clip-poly.ts
@@ -11,17 +11,17 @@ import { direction2 } from "@thi.ng/vectors/direction";
  *
  * @param a -
  * @param b -
- * @param pts -
+ * @param poly -
  */
 export const clipLinePoly = (
 	a: ReadonlyVec,
 	b: ReadonlyVec,
-	pts: ReadonlyVec[]
+	poly: ReadonlyVec[]
 ) => {
 	const isecs = intersectRayPolylineAll(
 		a,
 		direction2([], a, b),
-		pts,
+		poly,
 		true
 	).isec;
 	return isecs ? collectSegments(<Vec[]>isecs) : undefined;
@@ -30,17 +30,55 @@ export const clipLinePoly = (
 export const clipLineSegmentPoly = (
 	a: ReadonlyVec,
 	b: ReadonlyVec,
-	pts: ReadonlyVec[]
+	poly: ReadonlyVec[]
 ) => {
-	const isecs = intersectLinePolylineAll(a, b, pts, true).isec;
-	const isAInside = pointInPolygon2(a, pts);
-	const isBInside = pointInPolygon2(b, pts);
+	const isecs = intersectLinePolylineAll(a, b, poly, true).isec;
+	const isAInside = pointInPolygon2(a, poly);
+	const isBInside = pointInPolygon2(b, poly);
 	if (!isecs) {
 		return isAInside && isBInside ? [[a, b]] : undefined;
 	}
 	isAInside && (<Vec[]>isecs).unshift(a);
 	isBInside && (<Vec[]>isecs).push(b);
 	return collectSegments(<Vec[]>isecs);
+};
+
+/**
+ * Similar to {@link clipLineSegmentPoly}, but for polylines. Returns array of
+ * new vertex chunks where the given polyline is inside the given bounding poly.
+ *
+ * @param pts
+ * @param poly
+ */
+export const clipPolylinePoly = (pts: ReadonlyVec[], poly: ReadonlyVec[]) => {
+	let res: ReadonlyVec[][] = [];
+	if (pts.length < 2) return res;
+	let isAInside = pointInPolygon2(pts[0], poly);
+	for (let i = 0, n = pts.length - 1; i < n; i++) {
+		const a = pts[i];
+		const b = pts[i + 1];
+		const isBInside = pointInPolygon2(b, poly);
+		const isecs = intersectLinePolylineAll(a, b, poly, true).isec;
+		if (!isecs) {
+			if (isAInside && isBInside) {
+				if (res.length) res[res.length - 1].push(b);
+				else res.push([a, b]);
+			}
+		} else {
+			isAInside && (<Vec[]>isecs).unshift(a);
+			isBInside && (<Vec[]>isecs).push(b);
+			const [first, ...segments] = collectSegments(<Vec[]>isecs);
+			if (isAInside) {
+				if (res.length) res[res.length - 1].push(first[1]);
+				else res.push(first);
+			} else {
+				res.push(first);
+			}
+			res.push(...segments);
+		}
+		isAInside = isBInside;
+	}
+	return res;
 };
 
 const collectSegments = (isecs: Vec[]) => {
