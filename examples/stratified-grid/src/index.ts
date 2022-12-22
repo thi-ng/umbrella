9a12ae31a5fc0e4d2a8bddebca83264511b07b95
Karsten Schmidt <k@thi.ng>
2022-12-22 21:01:20 +0100

    refactor(examples): update stratified-grid example

--- examples/stratified-grid/src/index.ts
+++ examples/stratified-grid/src/index.ts
@@ -1,28 +1,30 @@
-import { KdTreeSet } from "@thi.ng/geom-accel/kd-tree-set";
-import { asSvg, svgDoc } from "@thi.ng/geom/as-svg";
-import { circle } from "@thi.ng/geom/circle";
-import { stratifiedGrid } from "@thi.ng/poisson/stratified";
-import { map } from "@thi.ng/transducers/map";
-import { dist } from "@thi.ng/vectors/dist";
+import { asSvg, group, line, points, svgDoc } from "@thi.ng/geom";
+import { stratifiedGrid2 } from "@thi.ng/poisson";
+import { map, range } from "@thi.ng/transducers";
 
-const index = new KdTreeSet(2);
-index.into(stratifiedGrid({ dim: [50, 50], samples: 1 }));
+const W = 50;
 
 document.body.innerHTML = asSvg(
 	svgDoc(
 		{
 			width: 600,
 			height: 600,
-			stroke: "blue",
-			"stroke-width": 0.1,
+			fill: "blue",
+			stroke: "none",
+			weight: 0.1,
 		},
-		...map(
-			(p) =>
-				circle(
-					p,
-					dist(p, index.queryKeys(p, 2 * Math.SQRT2, 2)[1]) / 2
-				),
-			index.keys()
+		group({ stroke: "#fcc" }, [
+			...map((x) => line([x, 0], [x, W]), range(1, W)),
+			...map((y) => line([0, y], [W, y]), range(1, W)),
+		]),
+		points(
+			[
+				...stratifiedGrid2({
+					dim: [W, W],
+					separation: 0.5,
+				}),
+			],
+			{ shape: "circle", size: 0.25 }
 		)
 	)
 );
