3cb07a6aaef41c671df0523466dd0cd3d5be85e4
Karsten Schmidt <k@thi.ng>
2022-12-22 11:59:40 +0100

    feat(geom): add SVG default attribs & setter
    - set defaults to no fill & black stroke
    - add setSvgDefaultAttribs()

--- packages/geom/src/as-svg.ts
+++ packages/geom/src/as-svg.ts
@@ -1,5 +1,5 @@
-import type { Attribs, IShape } from "@thi.ng/geom-api";
 import { withoutKeysObj } from "@thi.ng/associative/without-keys";
+import type { Attribs, IShape } from "@thi.ng/geom-api";
 import { convertTree } from "@thi.ng/hiccup-svg/convert";
 import { ff } from "@thi.ng/hiccup-svg/format";
 import { svg } from "@thi.ng/hiccup-svg/svg";
@@ -7,14 +7,34 @@ import { serialize } from "@thi.ng/hiccup/serialize";
 import { bounds } from "./bounds.js";
 import { __collBounds } from "./internal/bounds.js";
 
+/**
+ * Can be overridden via {@link setSvgDefaultAttribs}.
+ */
+let DEFAULT_ATTRIBS: Attribs = { fill: "none", stroke: "#000" };
+
+/**
+ * Sets the SVG root element attribs used by default by {@link svgDoc}.
+ *
+ * @param attribs
+ */
+export const setSvgDefaultAttribs = (attribs: Attribs) => {
+	DEFAULT_ATTRIBS = attribs;
+};
+
+/**
+ * Serializes given hiccup tree to an actual SVG source string.
+ *
+ * @param args
+ */
 export const asSvg = (...args: any[]) =>
 	args.map((x) => serialize(convertTree(x))).join("");
 
 /**
- * Creates a hiccup SVG doc element for given {@link IShape}s and attribs. If
- * the attribs do not include a `viewBox`, it will be computed automatically.
- * Furthermore (and only for the case a viewbox needs to be computed), a `bleed`
- * attrib can be provided to include a bleed/margin for the viewbox.
+ * Creates a hiccup SVG doc element for given {@link IShape}s and attribs
+ * (merged with default attribs). If the attribs do not include a `viewBox`, it
+ * will be computed automatically. Furthermore (and only for the case a viewbox
+ * needs to be computed), a `bleed` attrib can be provided to include a
+ * bleed/margin for the viewbox.
  *
  * @remarks
  * Use {@link asSvg} to serialize the resulting doc to an SVG string.
@@ -23,6 +43,7 @@ export const asSvg = (...args: any[]) =>
  * @param xs
  */
 export const svgDoc = (attribs: Attribs, ...xs: IShape[]) => {
+	attribs = { ...DEFAULT_ATTRIBS, ...attribs };
 	if (xs.length > 0) {
 		if (!attribs.viewBox) {
 			const cbounds = __collBounds(xs, bounds);
