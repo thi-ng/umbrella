04946086ec1e8e203d54811d292a42342a3030ab
Karsten Schmidt <k@thi.ng>
2022-12-28 16:41:27 +0100

    test(axidraw): add tests

--- packages/axidraw/test/index.ts
+++ packages/axidraw/test/index.ts
@@ -1,7 +1,63 @@
 import { group } from "@thi.ng/testament";
-// import * as assert from "assert";
-// import { } from "../src";
+import { eqDelta } from "@thi.ng/math";
+import * as assert from "assert";
+import { AxiDraw, MockSerial, MOCK_SERIAL } from "../src/index.js";
 
-group("axidraw", {
-
-});
+group(
+	"axidraw",
+	{
+		"square + diagonals": async (ctx) => {
+			const axi = new AxiDraw({
+				serial: MOCK_SERIAL,
+			});
+			await axi.connect("/foo");
+			const metrics = await axi.draw([
+				["m", [0, 0]],
+				["d"],
+				["m", [100, 0]],
+				["m", [100, 100]],
+				["m", [0, 100]],
+				["m", [0, 0]],
+				["u"],
+				["m", [0, 0]],
+				["d"],
+				["m", [100, 100]],
+				["u"],
+				["m", [100, 0]],
+				["d"],
+				["m", [0, 100]],
+				["u"],
+			]);
+			assert.ok(eqDelta(metrics.drawDist, 682.843, 1e3));
+			assert.ok(eqDelta(metrics.totalDist, 782.843, 1e3));
+			assert.strictEqual(metrics.commands, 17);
+			assert.deepStrictEqual((<MockSerial>axi.serial).sent, [
+				"EM,1,1\r",
+				"SC,5,12750\r",
+				"SC,4,18000\r",
+				"SC,10,65535\r",
+				"SP,1,150\r",
+				"XM,0,0,0\r",
+				"SP,0,150\r",
+				"XM,2000,8000,0\r",
+				"XM,2000,0,8000\r",
+				"XM,2000,-8000,0\r",
+				"XM,2000,0,-8000\r",
+				"SP,1,150\r",
+				"XM,0,0,0\r",
+				"SP,0,150\r",
+				"XM,2000,8000,8000\r",
+				"SP,1,150\r",
+				"XM,2000,0,-8000\r",
+				"SP,0,150\r",
+				"XM,2000,-8000,8000\r",
+				"SP,1,150\r",
+				"SP,1,150\r",
+				"XM,2000,0,-8000\r",
+				"EM,0,0\r",
+			]);
+			ctx.done();
+		},
+	},
+	{ timeOut: 20000 }
+);
