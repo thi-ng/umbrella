a8c5bbe5ef82a16839ea467b70d08bc63b0f8df2
Karsten Schmidt <k@thi.ng>
2023-02-05 15:41:13 +0100

    feat(tools): add latest-changes table generator

---
+++ tools/src/latest-changes.ts
@@ -0,0 +1,75 @@
+import type { IObjectOf } from "@thi.ng/api";
+import { FMT_yyyyMMdd } from "@thi.ng/date";
+import { table } from "@thi.ng/markdown-table";
+import {
+	comp,
+	groupByObj,
+	keep,
+	map,
+	push,
+	transduce,
+} from "@thi.ng/transducers";
+import { execFileSync } from "child_process";
+
+const sha = process.argv[2];
+
+const IGNORE_PACKAGES = new Set(["examples", "tools"]);
+
+const CHANGE_TYPES: IObjectOf<number> = {
+	perf: 0,
+	refactor: 0,
+	fix: 1,
+	feat: 2,
+};
+
+const CHANGE_COLORS: IObjectOf<string> = {
+	perf: "cyan",
+	refactor: "cyan",
+	fix: "orange",
+	feat: "green",
+};
+
+const changes = transduce(
+	comp(
+		map((x) => {
+			const match = /^([a-z]+)\(([a-z0-9_-]+)\):/i.exec(x);
+			if (
+				!match ||
+				!CHANGE_TYPES[match[1]] ||
+				IGNORE_PACKAGES.has(match[2])
+			)
+				return;
+			return [match[2], match[1]];
+		}),
+		keep((x) => x && x[0])
+	),
+	groupByObj<string[], any>({
+		key: (x) => x[0],
+		group: [
+			() => "refactor",
+			(acc) => acc,
+			(acc, x) => (CHANGE_TYPES[x[1]] > CHANGE_TYPES[acc] ? x[1] : acc),
+		],
+	}),
+	execFileSync("git", ["log", "--pretty=format:%s", `${sha}..`], {
+		encoding: "utf-8",
+	}).split("\n")
+);
+
+const res = table(
+	["Status", "Package", "Version", "Changelog"],
+	transduce(
+		map((k) => [
+			`![](https://img.shields.io/badge/-${changes[k]}-${
+				CHANGE_COLORS[changes[k]]
+			})`,
+			`[\`@thi.ng/${k}\`](./packages/${k})`,
+			`[![version](https://img.shields.io/npm/v/@thi.ng/${k}.svg)](https://www.npmjs.com/package/@thi.ng/${k})`,
+			`[changelog](./packages/${k}/CHANGELOG.md)`,
+		]),
+		push(),
+		Object.keys(changes).sort()
+	)
+);
+
+console.log(`### Latest updates\n\nAs of: ${FMT_yyyyMMdd()}\n\n${res}`);
