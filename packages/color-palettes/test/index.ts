359c83f7ade22b288f6854e4807f872eec4b3284
Karsten Schmidt <k@thi.ng>
2023-02-10 23:39:05 +0100

    test(color-palettes): add tests

--- packages/color-palettes/test/index.ts
+++ packages/color-palettes/test/index.ts
@@ -1,5 +1,13 @@
 import { group } from "@thi.ng/testament";
-// import * as assert from "assert";
-// import { } from "../src/index.js"
+import * as assert from "assert";
+import { chroma, compFilter, cssThemes, luma } from "../src/index.js";
 
-group("palettes", {});
+group("palettes", {
+	compFilter: () => {
+		const f1 = chroma(0, 0.25);
+		const f2 = luma(0.5, 1, 3);
+		const expected = [...cssThemes(54, 112)];
+		assert.deepStrictEqual([...cssThemes(f1, f2)], expected);
+		assert.deepStrictEqual([...cssThemes(compFilter(f1, f2))], expected);
+	},
+});
