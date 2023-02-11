201d960447473fe8dd5bbd396806f444abfa3938
Karsten Schmidt <k@thi.ng>
2023-02-11 22:08:22 +0100

    refactor(tools): update NPM stats collector

--- tools/src/npm-stats.ts
+++ tools/src/npm-stats.ts
@@ -1,38 +1,36 @@
+import type { IObjectOf } from "@thi.ng/api";
 import { delayed } from "@thi.ng/compose";
 import { defFormat } from "@thi.ng/date";
-import { writeJSON } from "@thi.ng/file-io";
-import { add, range } from "@thi.ng/transducers";
-import { readdirSync } from "fs";
+import { dirs, writeJSON } from "@thi.ng/file-io";
+import { add, map, range, vals } from "@thi.ng/transducers";
 import { request } from "https";
+import { basename } from "path";
 import { LOGGER } from "./api.js";
 
 const currDate = defFormat(["yyyy", "-", "MM", "-", "dd"])();
 const currYear = new Date().getFullYear();
 
 (async () => {
-	const stats: any = {};
-	for (let f of readdirSync("packages")) {
-		console.log(f);
+	const stats: IObjectOf<number> = {};
+	for (let f of map(basename, dirs("packages", "", 1))) {
+		LOGGER.info(f);
 		for (let year of range(2018, currYear + 1)) {
 			const endDate = year < currYear ? `${year + 1}-01-01` : currDate;
 			const url = `https://api.npmjs.org/downloads/point/${year}-01-01:${endDate}/@thi.ng/${f}`;
-			// console.log(url);
 			const req = request(url, (res) => {
-				// console.log("status:", res.statusCode);
 				res.setEncoding("utf8");
 				res.on("data", (d) => {
 					const num = JSON.parse(d.toString()).downloads || 0;
 					stats[f] = stats[f] ? stats[f] + num : num;
 				});
 			});
-
 			req.on("error", (e) => {
-				console.error(e);
+				LOGGER.warn(e);
 			});
 			req.end();
 			await delayed(null, 2000);
 		}
-		console.log(`${f},${stats[f]}`);
+		LOGGER.info(`${f}: ${stats[f]}`);
 
 		await delayed(null, 5000);
 	}
@@ -40,7 +38,5 @@ const currYear = new Date().getFullYear();
 	await delayed(null, 5000);
 	writeJSON("npm-stats.json", stats, null, "\t", LOGGER);
 
-	console.log("total:", add(Object.values<number>(stats)));
-
-	console.log("done");
+	LOGGER.info("total:", add(vals(stats)));
 })();
