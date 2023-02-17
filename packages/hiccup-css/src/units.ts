e590d3f9fcedeaee89c46d077c78a840f276fc9d
Karsten Schmidt <k@thi.ng>
2023-02-17 20:35:20 +0100

    fix(hiccup-css): update px() & ms() units #383
    - remove pixel rounding
    - use signed flooring for ms()

--- packages/hiccup-css/src/units.ts
+++ packages/hiccup-css/src/units.ts
@@ -2,11 +2,11 @@ export const em = (x: number) => `${x}em`;
 export const ex = (x: number) => `${x}ex`;
 export const rem = (x: number) => `${x}rem`;
 export const percent = (x: number) => `${x}%`;
-export const px = (x: number) => `${x | 0}px`;
+export const px = (x: number) => `${x}px`;
 export const vh = (x: number) => `${x}vh`;
 export const vw = (x: number) => `${x}vw`;
 
-export const ms = (x: number) => `${x >>> 0}ms`;
+export const ms = (x: number) => `${x | 0}ms`;
 export const sec = (x: number) => `${x}s`;
 
 export const deg = (x: number) => `${x}deg`;
