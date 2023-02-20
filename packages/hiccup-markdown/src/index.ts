dcb7b193f09028bc0b69472701d7e1e0f26ce9d7
Karsten Schmidt <k@thi.ng>
2023-02-20 12:22:57 +0100

    feat(hiccup-markdown): add new parser (incomplete WIP)

--- packages/hiccup-markdown/src/index.ts
+++ packages/hiccup-markdown/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./api.js";
 export * from "./parse.js";
+export * from "./parser.js";
 export * from "./serialize.js";
---
+++ packages/hiccup-markdown/src/parser.ts
@@ -0,0 +1,297 @@
+// thing:no-export
+
+// Downloaded @ 2023-02-18T13:59:10.044Z
+// Source: https://demo.thi.ng/umbrella/parse-playground/#ldoKU0ROTDE6IDxETkw-KyA9PiBkaXNjYXJkIDsKRE5MMjogPE5MPnsyLH0gOwppbmxpbmVkZWxpbTogKCAiIVsiIHwgJ1snIHwgIioqIiB8ICdfJyB8ICJ-fiIgfCAnYCcgfCAiIDoiICkgOwpkZWxpbTogKCA8aW5saW5lZGVsaW0-IHwgPEROTDI-ICkgOwpkZWxpbTE6ICggPGlubGluZWRlbGltPiB8IDxOTD4gKSA7CmJvZHk6IC4oPy08ZGVsaW0-ISkgPT4gam9pbiA7CmJvZHkxOiAuKD8tPGRlbGltMT4hKSA9PiBqb2luIDsKCnJlZjogIltbIiEgLig_KyJdXSIhKSA9PiBqb2luIDsKZm5yZWY6ICJbXiIhIDx1aW50PiAiXSIhIDsKZm5vdGU6IDxMU1RBUlQ-ICJbXiIhIDx1aW50PiAiXToiISA8V1MxPiA8cGFyYT4gOwpsYWJlbDogLig_KyddJyEpID0-IGpvaW4gOwp0YXJnZXQ6IC4oPysnKSchKSA9PiBqb2luIDsKbGluazogJ1snISA8bGFiZWw-ICcoJyEgPHRhcmdldD4gOwpsaW5rcmVmOiAnWychIDxsYWJlbD4gJ1snISA8bGFiZWw-IDsKbGlua2RlZjogPExTVEFSVD4gJ1snISA8bGFiZWw-ICc6JyEgPFdTMT4gPGxkdGFyZ2V0PiA7CmxkdGFyZ2V0OiAuKD8rPEROTDE-KSA9PiBqb2luIDsKaW1nOiAiIVsiISA8bGFiZWw-ICcoJyEgPHRhcmdldD4gOwpib2xkOiAiKioiISAuKD8rIioqIiEpID0-IGpvaW4gOwppdGFsaWM6ICJfIiEgLig_KyJfIiEpID0-IGpvaW4gOwpjb2RlOiAnYCchIC4oPysnYCchKSA9PiBqb2luIDsKc3RyaWtlOiAifn4iISAuKD8rIn5-IiEpID0-IGpvaW4gOwplbW9qaTogIiA6IiEgPEFMUEhBX05VTT4oPysnOichKSA9PiBqb2luIDsKcGFyYTogKDxyZWY-IHwgPGltZz4gfCA8Zm5yZWY-IHwgPGxpbmtyZWY-IHwgPGxpbms-IHwgPGJvbGQ-IHwgPGl0YWxpYz4gfCA8c3RyaWtlPiB8IDxjb2RlPiB8IDxlbW9qaT4gfCA8Ym9keT4pKiA8RE5MMj4hIDsKCmhkbGV2ZWw6ICcjJysgPT4gY291bnQgOwpoZDogPExTVEFSVD4gPGhkbGV2ZWw-IDxXUzA-CiAgICAoPHJlZj4gfCA8aW1nPiB8IDxmbnJlZj4gfCA8bGluaz4gfCA8Ym9sZD4gfCA8aXRhbGljPiB8IDxzdHJpa2U-IHwgPGNvZGU-IHwgPGVtb2ppPiB8IDxib2R5MT4gKSogPEROTDE-IDsKCmxpbGV2ZWw6ICcgJyogPT4gY291bnQgOwp1aW50OiA8RElHSVQ-KyA9PiBpbnQgOwp1bGlkOiA8RE5MPiA8V1MwPiAnLSchIDsKb2xpZDogPEROTD4gPFdTMD4gPERJR0lUPishICcuJyEgOwpsaWRlbGltOiAoIDxkZWxpbT4gfCA8dWxpZD4gfCA8b2xpZD4gKSA7CmxpYm9keTogLig_LTxsaWRlbGltPiEpID0-IGpvaW4gOwp0b2RvOiAnWychIFsgeFhdICddJyEgPFdTMT4gPT4gaG9pc3RSIDsKdWxpdGVtOiA8TFNUQVJUPiA8bGlsZXZlbD4gIi0gIiEgPHRvZG8-PwogICAgICAgICg8cmVmPiB8IDxpbWc-IHwgPGZucmVmPiB8IDxsaW5rPiB8IDxib2xkPiB8IDxpdGFsaWM-IHwgPHN0cmlrZT4gfCA8Y29kZT4gfCA8ZW1vamk-IHwgPGxpYm9keT4gKSogPEROTD4gOwpvbGl0ZW06IDxMU1RBUlQ-IDxsaWxldmVsPiA8dWludD4gIi4gIiEgPHRvZG8-PwogICAgICAgICg8cmVmPiB8IDxpbWc-IHwgPGZucmVmPiB8IDxsaW5rPiB8IDxib2xkPiB8IDxpdGFsaWM-IHwgPHN0cmlrZT4gfCA8Y29kZT4gfCA8ZW1vamk-IHwgPGxpYm9keT4gKSogPEROTD4gOwpsaXN0OiAoPHVsaXRlbT4gfCA8b2xpdGVtPikrIDxETkwxPiA7CgpjYmRlbGltOiA8TFNUQVJUPiAiYGBgIiEgOwpjb2RlYmxvY2s6IDxjYmRlbGltPiEgPGNvZGVtZXRhPiA8Y29kZWJvZHk-IDxETkwxPiA7CmNvZGVtZXRhOiAuKD8rPE5MPiEpID0-IGpvaW4gOwpjb2RlYm9keTogLig_KzxjYmRlbGltPikgPT4gam9pbiA7CgpjdXN0b21kZWxpbTogPExTVEFSVD4gIjo6OiIhIDsKY3VzdG9tYmxvY2s6IDxjdXN0b21kZWxpbT4hIDxjdXN0b21tZXRhPiA8Y3VzdG9tYm9keT4gPEROTDE-IDsKY3VzdG9tbWV0YTogLig_KzxOTD4hKSA9PiBqb2luIDsKY3VzdG9tYm9keTogLig_KzxjdXN0b21kZWxpbT4pID0-IGpvaW4gOwoKbWV0YWJsb2NrOiA8TFNUQVJUPiAie3t7IiEgPG1ldGE-IDxETkwxPiA9PiBob2lzdCA7Cm1ldGFlbmQ6ICJ9fX0iIDxMRU5EPiA7Cm1ldGE6IC4oPys8bWV0YWVuZD4hKSA9PiBqc29uIDsKCmJxbGluZTogPExTVEFSVD4gIj4gIiEKICAgICAgICAoPHJlZj4gfCA8aW1nPiB8IDxmbnJlZj4gfCA8bGluaz4gfCA8Ym9sZD4gfCA8aXRhbGljPiB8IDxzdHJpa2U-IHwgPGNvZGU-IHwgPGVtb2ppPiB8IDxib2R5MT4pKiA8RE5MPiA7CmJxdW90ZTogPGJxbGluZT4rIDxETkwxPiA7Cgp0ZGVsaW06ICg8aW5saW5lZGVsaW0-IHwgJ3wnICkgOwp0Ym9keTogLig_LTx0ZGVsaW0-ISkgPT4gam9pbiA7CnRjZWxsOiA8V1MwPiAoPHJlZj4gfCA8aW1nPiB8IDxmbnJlZj4gfCA8bGluaz4gfCA8Ym9sZD4gfCA8aXRhbGljPiB8IDxzdHJpa2U-IHwgPGNvZGU-IHwgPGVtb2ppPiB8IDx0Ym9keT4gKSogJ3wnISA7CnRyb3c6IDxMU1RBUlQ-ICd8JyEgPHRjZWxsPig_KzxETkw-KSA7CnRhYmxlOiA8dHJvdz4rIDxETkwxPiA7CgpocjogIi0tIiEgJy0nISg_LTxOTD4hKSA8RE5MMT4gOwoKbWFpbjogPFdTMD4gKDxoZD4gfCA8bGlzdD4gfCA8YnF1b3RlPiB8IDxjb2RlYmxvY2s-IHwgPGN1c3RvbWJsb2NrPiB8IDxtZXRhYmxvY2s-IHwgPHRhYmxlPiB8IDxocj4gfCA8Zm5vdGU-IHwgPGxpbmtkZWY-IHwgPHBhcmE-KSogO6RtYWlu2gHNIyBIZWxsbyA6d29ybGQ6W14xXQoKOjo6YWxlcnQKVGVzdCBbbGFiZWxdW3JlZnJlZl0KOjo6Cgp7e3sgeyJpZCI6IDEyMywgInByb3RlY3RlZCI6IHRydWUgfSB9fX0KU28gW1tzbWlsZV9mb28xMl1dIPCfmI0gd2hhdCdzIFt0aGlzXSguL3RoaXMubWQpW14xXSB0aGVuOgoKW14xXTogRm9vIGJhciBiYXoKCjpzbWlsZToKCmBgYHRzIHRhbmdsZTpubyBsbjp5ZXMKY29uc3QgZm9vID0KCmBgYAoKMS4gZm9vCiAgMjMuIGJhcgogICAgIGJheiBhbmQgc28gb24KICAgICBtdWx0aWxpbmUKMi4gYmFycnJyCgo-IGJsb2NrcXVvdGUKPiBtdWx0aWxpbmUgdG9vCj4gLS0gc291cmNlCgp8IGZvbyB8IGJhciB8Cnw6LXwtOnwKfCBbW3JlZiMyMl1dIHwgYG9uYCAqKm9mZioqIHwKClteMV06IEZvbyBfYmFyXyBiYXoKClteMl06IFRPRE8KCltyZWZyZWZdOiBodHRwOi8vdGhpLm5nLwpbZm9vXTogLi9mb28ubWQjYmFyCgrZIjExLiBhYmMKICAtIHh5egoyMS4geHh4CiAgMS4gZm9vCgq6aW50ZW50aW9uYWxseSBsZWZ0IGJsYW5rCgo
+
+import type { Fn3, IObjectOf } from "@thi.ng/api";
+import { peek } from "@thi.ng/arrays/peek";
+import { DEFAULT, defmulti } from "@thi.ng/defmulti";
+import type { ContextOpts, ParseScope } from "@thi.ng/parse";
+import { defContext } from "@thi.ng/parse/context";
+import { defGrammar } from "@thi.ng/parse/grammar";
+
+export const GRAMMAR = defGrammar(`
+DNL1: <DNL>+ => discard ;
+DNL2: <NL>{2,} ;
+inlinedelim: ( "![" | '[' | "**" | '_' | "~~" | '\`' | " :" ) ;
+delim: ( <inlinedelim> | <DNL2> ) ;
+delim1: ( <inlinedelim> | <NL> ) ;
+body: .(?-<delim>!) => join ;
+body1: .(?-<delim1>!) => join ;
+
+ref: "[["! .(?+"]]"!) => join ;
+fnref: "[^"! <uint> "]"! ;
+fnote: <LSTART> "[^"! <uint> "]:"! <WS1> <para> ;
+label: .(?+']'!) => join ;
+target: .(?+')'!) => join ;
+link: '['! <label> '('! <target> ;
+linkref: '['! <label> '['! <label> ;
+linkdef: <LSTART> '['! <label> ':'! <WS1> <ldtarget> ;
+ldtarget: .(?+<DNL1>) => join ;
+img: "!["! <label> '('! <target> ;
+bold: "**"! .(?+"**"!) => join ;
+italic: "_"! .(?+"_"!) => join ;
+code: '\`'! .(?+'\`'!) => join ;
+strike: "~~"! .(?+"~~"!) => join ;
+emoji: " :"! <ALPHA_NUM>(?+':'!) => join ;
+para: (<ref> | <img> | <fnref> | <linkref> | <link> | <bold> | <italic> | <strike> | <code> | <emoji> | <body>)* <DNL2>! ;
+
+hdlevel: '#'+ => count ;
+hd: <LSTART> <hdlevel> <WS0>
+    (<ref> | <img> | <fnref> | <link> | <bold> | <italic> | <strike> | <code> | <emoji> | <body1> )* <DNL1> ;
+
+lilevel: ' '* => count ;
+uint: <DIGIT>+ => int ;
+ulid: <DNL> <WS0> '-'! ;
+olid: <DNL> <WS0> <DIGIT>+! '.'! ;
+lidelim: ( <delim> | <ulid> | <olid> ) ;
+libody: .(?-<lidelim>!) => join ;
+todo: '['! [ xX] ']'! <WS1> => hoistR ;
+ulitem: <LSTART> <lilevel> "- "! <todo>?
+        (<ref> | <img> | <fnref> | <link> | <bold> | <italic> | <strike> | <code> | <emoji> | <libody> )* <DNL> ;
+olitem: <LSTART> <lilevel> <uint> ". "! <todo>?
+        (<ref> | <img> | <fnref> | <link> | <bold> | <italic> | <strike> | <code> | <emoji> | <libody> )* <DNL> ;
+list: (<ulitem> | <olitem>)+ <DNL1> ;
+
+cbdelim: <LSTART> "\`\`\`"! ;
+codeblock: <cbdelim>! <codemeta> <codebody> <DNL1> ;
+codemeta: .(?+<NL>!) => join ;
+codebody: .(?+<cbdelim>) => join ;
+
+customdelim: <LSTART> ":::"! ;
+customblock: <customdelim>! <custommeta> <custombody> <DNL1> ;
+custommeta: .(?+<NL>!) => join ;
+custombody: .(?+<customdelim>) => join ;
+
+metablock: <LSTART> "{{{"! <meta> <DNL1> => hoist ;
+metaend: "}}}" <LEND> ;
+meta: .(?+<metaend>!) => json ;
+
+bqline: <LSTART> "> "!
+        (<ref> | <img> | <fnref> | <link> | <bold> | <italic> | <strike> | <code> | <emoji> | <body1>)* <DNL> ;
+bquote: <bqline>+ <DNL1> ;
+
+tdelim: (<inlinedelim> | '|' ) ;
+tbody: .(?-<tdelim>!) => join ;
+tcell: <WS0> (<ref> | <img> | <fnref> | <link> | <bold> | <italic> | <strike> | <code> | <emoji> | <tbody> )* '|'! ;
+trow: <LSTART> '|'! <tcell>(?+<DNL>) ;
+table: <trow>+ <DNL1> ;
+
+hr: "--"! '-'!(?-<NL>!) <DNL1> ;
+
+main: <WS0> (<hd> | <list> | <bquote> | <codeblock> | <customblock> | <metablock> | <table> | <hr> | <fnote> | <linkdef> | <para>)* ;
+`);
+
+export const parseRaw = (src: string, opts?: Partial<ContextOpts>) => {
+	const ctx = defContext(src, opts);
+	return { result: GRAMMAR!.rules.main(ctx), ctx };
+};
+
+export interface TagTransforms {
+	bold(body: string): any;
+	code(body: string): any;
+	custom(kind: string, body: string, meta?: any): any;
+	hd(level: number, body: any[], meta?: any): any;
+	italic(body: string): any;
+	link(target: string, body: any[]): any;
+	ol(items: any[], meta?: any): any;
+	olitem(attribs: TodoAttribs, index: number, ...body: any[]): any;
+	para(body: any[], meta?: any): any;
+	strike(body: string): any;
+	ul(items: any[], meta?: any): any;
+	ulitem(attribs: TodoAttribs, ...body: any[]): any;
+}
+
+export interface MDParseContext {
+	tags: TagTransforms;
+	linkRefs: IObjectOf<string>;
+	fnRefs: IObjectOf<string>;
+	meta?: any;
+}
+
+export type TodoAttribs = Partial<{ __todo: true; __done: boolean }>;
+
+export const defMDContext = (tags: Partial<TagTransforms>): MDParseContext => ({
+	linkRefs: {},
+	fnRefs: {},
+	meta: null,
+	tags: {
+		bold: (body) => ["strong", {}, body],
+		code: (body) => ["code", {}, body],
+		custom: (kind, body, meta) => [kind, withMeta({}, meta), body],
+		hd: (level, body, meta) => [`h${level}`, withMeta({}, meta), ...body],
+		italic: (body) => ["em", {}, body],
+		link: (href, body) => ["a", { href }, ...body],
+		olitem: (attribs, index, body) => [
+			"li",
+			{ ...attribs, __index: index },
+			...body,
+		],
+		ol: (items, meta) => ["ol", withMeta({}, meta), ...items],
+		para: (body, meta) => ["p", withMeta({}, meta), ...body],
+		strike: (body) => ["s", {}, body],
+		ul: (items, meta) => ["ul", withMeta({}, meta), ...items],
+		ulitem: (attribs, body) => ["li", attribs, ...body],
+		...tags,
+	},
+});
+
+const withMeta = (target: any, meta?: any) => {
+	if (meta != null) {
+		target.__meta = meta;
+	}
+	return target;
+};
+
+export const walk: Fn3<
+	ParseScope<string>,
+	MDParseContext,
+	any[],
+	void
+> = defmulti<ParseScope<string>, MDParseContext, any[], void>(
+	(x) => {
+		console.log(x);
+		return x.id;
+	},
+	{
+		main: "root",
+		body1: "body",
+		libody: "body",
+		repeat1: "repeat0",
+	},
+	{
+		[DEFAULT]: (scope: ParseScope<string>, ctx: MDParseContext) => {
+			throw new Error(
+				`unknown ID: ${scope.id}, ctx: ${JSON.stringify(ctx)}`
+			);
+		},
+
+		root: (scope, ctx, acc) => walk(scope.children![0], ctx, acc),
+
+		repeat0: (scope, ctx, acc) => {
+			for (let c of scope.children!) walk(c, ctx, acc);
+		},
+
+		hd: ({ children }, ctx, acc) => {
+			const body: any[] = [];
+			walk(children![1], ctx, body);
+			acc.push(ctx.tags.hd(children![0].result, body, ctx.meta));
+			ctx.meta = null;
+		},
+
+		body: (scope, _, acc) => acc.push(scope.result),
+
+		bold: (scope, ctx, acc) => acc.push(ctx.tags.bold(scope.result)),
+
+		code: (scope, ctx, acc) => acc.push(ctx.tags.code(scope.result)),
+
+		customblock: ({ children }, ctx, acc) => {
+			acc.push(
+				ctx.tags.custom(
+					children![0].result,
+					children![1].result.trim(),
+					ctx.meta
+				)
+			);
+			ctx.meta = null;
+		},
+
+		italic: (scope, ctx, acc) => acc.push(ctx.tags.italic(scope.result)),
+
+		link: (scope, ctx, acc) => {
+			const body: any[] = [];
+			walk(scope.children![0], ctx, body);
+			acc.push(ctx.tags.link(scope.children![1].result, body));
+		},
+
+		list: (scope, ctx, acc) => {
+			const children = scope.children![0].children!;
+			const stack: any[][] = [
+				[children[0].id === "ulitem" ? "ul" : "ol"],
+			];
+			const levels = [0];
+			for (let item of children) {
+				const currLevel = item.children![0].result;
+				if (currLevel > peek(levels)) {
+					const sublist = [item.id === "ulitem" ? "ul" : "ol"];
+					const parent = peek(stack);
+					parent.length > 1
+						? peek(parent).push(sublist)
+						: parent.push([
+								parent[0] === "ul" ? "ulitem" : "olitem",
+								{},
+								sublist,
+						  ]);
+					stack.push(sublist);
+					levels.push(currLevel);
+				} else if (currLevel < peek(levels)) {
+					while (currLevel < peek(levels)) {
+						stack.pop();
+						levels.pop();
+					}
+				}
+				walk(item, ctx, peek(stack));
+			}
+
+			const $list = (root: any[], isTop = false) =>
+				ctx.tags[<"ul" | "ol">root[0]](
+					root.slice(1).map($item),
+					isTop ? ctx.meta : null
+				);
+
+			const $item = (item: any[]) => {
+				let last = item[item.length - 1];
+				if (last[0] === "ul" || last[0] === "ol")
+					item[item.length - 1] = $list(last);
+				return item[0] === "ulitem"
+					? ctx.tags.ulitem(item[1], item.slice(2))
+					: ctx.tags.olitem(item[1], item[2], item.slice(3));
+			};
+
+			acc.push($list(stack[0], true));
+			ctx.meta = null;
+		},
+
+		meta: (scope, ctx) => {
+			ctx.meta = scope.result;
+		},
+
+		olitem: (scope, ctx, acc) => {
+			const body: any[] = [];
+			walk(scope.children![3], ctx, body);
+			acc.push([
+				"olitem",
+				__listItemAttribs(scope.children![2]),
+				scope.children![1].result,
+				...body,
+			]);
+		},
+
+		para: (scope, ctx, acc) => {
+			const body: any[] = [];
+			for (let c of scope.children!) walk(c, ctx, body);
+			acc.push(ctx.tags.para(body, ctx.meta));
+			ctx.meta = null;
+		},
+
+		strike: (scope, ctx, acc) => acc.push(ctx.tags.strike(scope.result)),
+
+		ulitem: (scope, ctx, acc) => {
+			const body: any[] = [];
+			walk(scope.children![2], ctx, body);
+			acc.push([
+				"ulitem",
+				__listItemAttribs(scope.children![1]),
+				...body,
+			]);
+		},
+	}
+);
+
+const __listItemAttribs = (scope?: ParseScope<string>): TodoAttribs =>
+	scope?.id === "todo"
+		? {
+				__todo: true,
+				__done: scope.result === "x",
+		  }
+		: {};
