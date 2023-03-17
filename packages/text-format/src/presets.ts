28e4db47c92e05088e0020d02692ad79ee2364e7
Karsten Schmidt <k@thi.ng>
2023-03-17 09:21:32 +0100

    feat(text-format): add PRESET_NONE

--- packages/text-format/src/presets.ts
+++ packages/text-format/src/presets.ts
@@ -1,4 +1,7 @@
 import { FMT_ANSI16 } from "./ansi.js";
 import { defFormatPresets } from "./format.js";
+import { FMT_NONE } from "./none.js";
 
 export const PRESET_ANSI16 = defFormatPresets(FMT_ANSI16);
+
+export const PRESET_NONE = defFormatPresets(FMT_NONE);
