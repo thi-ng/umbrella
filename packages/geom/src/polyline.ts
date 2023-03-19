572e0ef91a3d6230a6ecb8edbd39a4937a26de1d
Karsten Schmidt <k@thi.ng>
2023-03-19 23:33:51 +0100

    feat(geom): add spiral() polyline factory fn

--- packages/geom/src/polyline.ts
+++ packages/geom/src/polyline.ts
@@ -1,6 +1,52 @@
 import type { Attribs } from "@thi.ng/geom-api";
-import type { Vec } from "@thi.ng/vectors";
+import { mix } from "@thi.ng/math/mix";
+import { map } from "@thi.ng/transducers/map";
+import { normRange } from "@thi.ng/transducers/norm-range";
+import type { ReadonlyVec, Vec } from "@thi.ng/vectors";
+import { cartesian2 } from "@thi.ng/vectors/cartesian";
 import { Polyline } from "./api/polyline.js";
 
 export const polyline = (pts: Vec[], attribs?: Attribs) =>
 	new Polyline(pts, attribs);
+
+/**
+ * Creates a polyline spiral from given params. The number of twists is defined
+ * by the angle range. Resolution depends on angle range and number of steps.
+ *
+ * @example
+ * ```ts
+ * // 4 full turns over 80 steps
+ * spiral([0, 0], 5, 100, 0, Math.PI * 2 * 4, 80);
+ * ```
+ *
+ * @param origin
+ * @param r1
+ * @param r2
+ * @param startTheta
+ * @param endTheta
+ * @param steps
+ * @param attribs
+ */
+export const spiral = (
+	origin: ReadonlyVec,
+	r1: number,
+	r2: number,
+	startTheta: number,
+	endTheta: number,
+	steps: number,
+	attribs?: Attribs
+) =>
+	new Polyline(
+		[
+			...map(
+				(t) =>
+					cartesian2(
+						null,
+						[mix(r1, r2, t), mix(startTheta, endTheta, t)],
+						origin
+					),
+				normRange(steps - 1)
+			),
+		],
+		attribs
+	);
