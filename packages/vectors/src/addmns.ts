00470d136a072f3e9be26a7ec5e3559a0ce67552
Karsten Schmidt <k@thi.ng>
2023-04-15 11:28:12 +0200

    fix(vectors): update addmNS/submNS() signatures

--- packages/vectors/src/addmns.ts
+++ packages/vectors/src/addmns.ts
@@ -1,8 +1,8 @@
-import type { VecOpSGVNV, VecOpSVNV } from "./api.js";
+import type { VecOpSGVVN, VecOpSVVN } from "./api.js";
 import { defOpS } from "./compile/emit.js";
 import { ARGS_VVN, MATH2_N, SARGS_VV } from "./compile/templates.js";
 
 export const [addmNS, addmNS2, addmNS3, addmNS4] = defOpS<
-	VecOpSGVNV,
-	VecOpSVNV
+	VecOpSGVVN,
+	VecOpSVVN
 >(MATH2_N("+", "*"), ARGS_VVN, SARGS_VV);
--- packages/vectors/src/submns.ts
+++ packages/vectors/src/submns.ts
@@ -1,8 +1,8 @@
-import type { VecOpSGVNV, VecOpSVNV } from "./api.js";
+import type { VecOpSGVVN, VecOpSVVN } from "./api.js";
 import { defOpS } from "./compile/emit.js";
 import { ARGS_VVN, MATH2_N, SARGS_VV } from "./compile/templates.js";
 
 export const [submNS, submNS2, submNS3, submNS4] = defOpS<
-	VecOpSGVNV,
-	VecOpSVNV
+	VecOpSGVVN,
+	VecOpSVVN
 >(MATH2_N("-", "*"), ARGS_VVN, SARGS_VV);
