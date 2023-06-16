46bbf239885ddeb7e8aeee1933c40ac09d910915
Karsten Schmidt <k@thi.ng>
2023-06-17 00:02:49 +0200

    feat(vectors): add limit2/3/4()

--- packages/vectors/src/limit.ts
+++ packages/vectors/src/limit.ts
@@ -1,7 +1,7 @@
 import type { VecOpVN } from "./api.js";
-import { magSq } from "./magsq.js";
-import { mulN } from "./muln.js";
-import { set } from "./set.js";
+import { magSq, magSq2, magSq3, magSq4 } from "./magsq.js";
+import { mulN, mulN2, mulN3, mulN4 } from "./muln.js";
+import { set, set2, set3, set4 } from "./set.js";
 
 /**
  * Limits `v` to max length `n` and writes result to `out` (or to itself if
@@ -18,5 +18,35 @@ export const limit: VecOpVN = (out, v, n) => {
 		? mulN(out, v, n / Math.sqrt(m))
 		: out !== v
 		? set(out, v)
+		: out;
+};
+
+export const limit2: VecOpVN = (out, v, n) => {
+	!out && (out = v);
+	const m = magSq2(v);
+	return m > n * n
+		? mulN2(out, v, n / Math.sqrt(m))
+		: out !== v
+		? set2(out, v)
+		: out;
+};
+
+export const limit3: VecOpVN = (out, v, n) => {
+	!out && (out = v);
+	const m = magSq3(v);
+	return m > n * n
+		? mulN3(out, v, n / Math.sqrt(m))
+		: out !== v
+		? set3(out, v)
+		: out;
+};
+
+export const limit4: VecOpVN = (out, v, n) => {
+	!out && (out = v);
+	const m = magSq4(v);
+	return m > n * n
+		? mulN4(out, v, n / Math.sqrt(m))
+		: out !== v
+		? set4(out, v)
 		: out;
 };
