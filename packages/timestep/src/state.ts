65ace1e4ae874c642831ee28a498f1569aa67f03
Karsten Schmidt <k@thi.ng>
2023-06-19 11:16:02 +0200

    feat(timestep): update Numeric/VectorState, add 2/3/4d versions
    - remove abstract AState class
    - update NumericState
    - add new VectorState impl
    - add defVector2/3/4()

--- packages/timestep/src/state.ts
+++ packages/timestep/src/state.ts
@@ -1,35 +1,30 @@
 import type { IDeref } from "@thi.ng/api";
 import { mix } from "@thi.ng/math/mix";
-import type { ReadonlyVec } from "@thi.ng/vectors";
-import { mixN } from "@thi.ng/vectors/mixn";
-import { set } from "@thi.ng/vectors/set";
-import type {
-	ITimeStep,
-	ReadonlyTimeStep,
-	StateInterpolation,
-	StateUpdate,
-} from "./api.js";
+import type { ReadonlyVec, Vec, VecOpV, VecOpVVN } from "@thi.ng/vectors";
+import { mixN, mixN2, mixN3, mixN4 } from "@thi.ng/vectors/mixn";
+import { set, set2, set3, set4 } from "@thi.ng/vectors/set";
+import type { ITimeStep, ReadonlyTimeStep, StateUpdate } from "./api.js";
 
-/**
- * Abstract base class for simulation state wrappers.
- *
- * @remarks
- * See {@link defNumeric} and {@link defVector}.
- */
-export abstract class AState<T> implements IDeref<T>, ITimeStep {
-	curr: T;
-	prev: T;
+export class NumericState implements IDeref<number>, ITimeStep {
+	curr!: number;
+	prev!: number;
 
-	constructor(
-		public value: T,
-		public update: StateUpdate<T>,
-		public mix: StateInterpolation<T>
-	) {
-		this.prev = this.curr = value;
+	constructor(public value: number, public update: StateUpdate<number>) {
+		this.reset(value);
 	}
 
 	deref() {
 		return this.value;
+	}
+
+	/**
+	 * Sets {@link NumericState.prev}, {@link NumericState.curr} and
+	 * {@link NumericState.value} to given new value.
+	 *
+	 * @param value
+	 */
+	reset(value: number) {
+		this.prev = this.curr = this.value = value;
 	}
 
 	integrate(dt: number, ctx: ReadonlyTimeStep) {
@@ -37,32 +32,49 @@ export abstract class AState<T> implements IDeref<T>, ITimeStep {
 		this.curr = this.update(this.curr, dt, ctx);
 	}
 
-	interpolate(alpha: number, ctx: ReadonlyTimeStep): void {
-		this.value = this.mix(this.prev, this.curr, alpha, ctx);
-	}
-
-	abstract reset(value: T): void;
-}
-
-export class NumericState extends AState<number> {
-	constructor(x: number, update: StateUpdate<number>) {
-		super(x, update, mix);
-	}
-
-	reset(value: number) {
-		this.prev = this.curr = this.value = value;
+	interpolate(alpha: number): void {
+		this.value = mix(this.prev, this.curr, alpha);
 	}
 }
 
-export class VectorState extends AState<ReadonlyVec> {
-	constructor(x: ReadonlyVec, update: StateUpdate<ReadonlyVec>) {
-		super(x, update, (a, b, t) => mixN([], a, b, t));
+export class VectorState implements IDeref<ReadonlyVec>, ITimeStep {
+	prev: Vec;
+	curr: Vec;
+
+	constructor(
+		public value: Vec,
+		public setFn: VecOpV,
+		public mixFn: VecOpVVN,
+		public update: StateUpdate<ReadonlyVec>
+	) {
+		this.prev = [...value];
+		this.curr = [...value];
 	}
 
+	deref(): ReadonlyVec {
+		return this.value;
+	}
+
+	/**
+	 * Copies given vector to {@link VectorState.prev}, {@link VectorState.curr}
+	 * and {@link VectorState.value}.
+	 *
+	 * @param value
+	 */
 	reset(value: ReadonlyVec) {
+		const set = this.setFn;
 		set(this.prev, value);
 		set(this.curr, value);
 		set(this.value, value);
+	}
+
+	integrate(dt: number, ctx: ReadonlyTimeStep) {
+		this.setFn(this.prev, this.curr);
+		this.update(this.curr, dt, ctx);
+	}
+
+	interpolate(alpha: number) {
+		this.mixFn(this.value, this.prev, this.curr, alpha);
 	}
 }
 
@@ -77,15 +89,44 @@ export const defNumeric = (x: number, update: StateUpdate<number>) =>
 	new NumericState(x, update);
 
 /**
- * Returns a new {@link VectorState} wrapper for given vector `v` and its update
- * function for use with {@link TimeStep.update}.
+ * Returns a new {@link VectorState} wrapper for given vector `v` (arbitrary
+ * length) and its update function for use with {@link TimeStep.update}.
  *
  * @remarks
- * **IMPORTANT:** The `update` function MUST return a new vector and not mutate
- * the existing one!
+ * **IMPORTANT:** The `update` function MUST manipulate the vector received as
+ * 1st arg (which is {@link VectorState.curr}).
+ *
+ * Also see {@link defVector2}, {@link defVector3}, {@link defVector4}.
  *
  * @param x
  * @param update
  */
 export const defVector = (v: ReadonlyVec, update: StateUpdate<ReadonlyVec>) =>
-	new VectorState(v, update);
+	new VectorState(v, set, mixN, update);
+
+/**
+ * 2D optimized version of {@link defVector}.
+ *
+ * @param v
+ * @param update
+ */
+export const defVector2 = (v: ReadonlyVec, update: StateUpdate<ReadonlyVec>) =>
+	new VectorState(v, set2, mixN2, update);
+
+/**
+ * 3D optimized version of {@link defVector}.
+ *
+ * @param v
+ * @param update
+ */
+export const defVector3 = (v: ReadonlyVec, update: StateUpdate<ReadonlyVec>) =>
+	new VectorState(v, set3, mixN3, update);
+
+/**
+ * 4D optimized version of {@link defVector}.
+ *
+ * @param v
+ * @param update
+ */
+export const defVector4 = (v: ReadonlyVec, update: StateUpdate<ReadonlyVec>) =>
+	new VectorState(v, set4, mixN4, update);
