4a4359dedb2aa726898082d779def7c447c43dde
Karsten Schmidt <k@thi.ng>
2023-06-27 12:27:26 +0200

    refactor(tools): simplify depgraph tool

--- tools/src/depgraph.ts
+++ tools/src/depgraph.ts
@@ -9,58 +9,65 @@ import {
 	transduce,
 	zip,
 } from "@thi.ng/transducers";
-import { execSync } from "child_process";
+import { execFileSync } from "child_process";
 
-// load package.json of all packages and extract key info...
-const packages = [
-	...map((f: string) => {
-		const pkg = readJSON(f);
-		return {
-			id: pkg.name,
-			v: pkg.version,
-			deps: pkg.dependencies ? Object.keys(pkg.dependencies) : [],
-		};
-	}, files("./packages/", "package.json", 2)),
-];
+// load package.json of all packages in monorepo and extract key details...
+const packages = [...map(readJSON, files("packages", "package.json", 2))];
 
-// serialize to Graphviz DOT format
+// transform & serialize to Graphviz DOT format
 const dot = serializeGraph({
 	attribs: {
 		rankdir: "LR",
+		overlap: "scale",
 		node: {
-			fontname: "Menlo",
-			fontsize: 9,
-			fontcolor: "white",
-			color: "#555555",
 			style: "filled",
+			color: "black",
+			fontcolor: "white",
+			fontsize: 9,
 		},
-		edge: { arrowsize: 0.66, color: "#aaaaaa" },
 	},
 	nodes: transduce(
-		map((p) => <[string, Node]>[p.id, <Node>{ label: `${p.id}\\n${p.v}` }]),
+		map(
+			({ name, version }) =>
+				<[string, Node]>[name, { label: `${name}\\n${version}` }]
+		),
 		assocObj(),
 		packages
 	),
 	edges: [
-		...mapcat((p) => p.deps.map((d) => ({ src: p.id, dest: d })), packages),
+		...mapcat(
+			(p) =>
+				Object.keys(p.dependencies || {}).map((d) => ({
+					src: p.name,
+					dest: d,
+				})),
+			packages
+		),
 	],
 });
 
 // output and execute
 writeText("assets/deps.dot", dot);
-execSync("dot -Tsvg -o assets/deps.svg assets/deps.dot");
+execFileSync("neato", ["-Tsvg", "-o", "assets/deps.svg", "assets/deps.dot"]);
 
 // alternative textual output from here onwards...
 
 const g = defDGraph<string>(
-	mapcat((p) => zip(repeat(p.id), p.deps.length ? p.deps : [null]), packages)
+	mapcat(
+		(p) =>
+			zip(
+				repeat(p.name),
+				p.dependies ? Object.keys(p.dependencies) : [null]
+			),
+		packages
+	)
 );
 
 const sorted = g.sort();
 console.log("topo order:", sorted.map((x) => x.substring(8)).join(" "), "\n\n");
 
-const noprefix = (ids: Iterable<string>) =>
-	[...ids].sort().map((x) => x.substring(8));
+const noprefix = (names: Iterable<string>) =>
+	[...names].sort().map((x) => x.substring(8));
 
 sorted.forEach((x) => {
 	console.log(`${x}:`);
@@ -70,5 +77,5 @@ sorted.forEach((x) => {
 
 console.log(
 	"leaves",
-	packages.filter((p) => !p.deps.length).map((p) => p.id)
+	packages.filter((p) => !p.dependencies).map((p) => p.name)
 );
