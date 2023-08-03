dd0a6ed09495ffd9335baa9564119eb78dfbb9c0
Karsten Schmidt <k@thi.ng>
2023-08-03 17:30:26 +0200

    feat(api): add generics for INotify, Event, Listener types

--- packages/api/src/event.ts
+++ packages/api/src/event.ts
@@ -2,14 +2,14 @@ import type { Fn } from "./fn.js";
 import type { IID } from "./id.js";
 
 /**
- * Event listener.
+ * Event listener for {@link Event}.
  */
-export type Listener = Fn<Event, void>;
+export type Listener<T extends string = string> = Fn<Event<T>, void>;
 
 /**
  * Event type used in combination with {@link INotify}.
  */
-export interface Event<T extends PropertyKey = PropertyKey> extends IID<T> {
+export interface Event<T extends string = string> extends IID<T> {
 	target?: any;
 	canceled?: boolean;
 	value?: any;
@@ -22,8 +22,8 @@ export interface Event<T extends PropertyKey = PropertyKey> extends IID<T> {
  * The type param `T` can be used to constrain the event type/id.
  */
 export interface INotify<T extends string = string> {
-	addListener(id: T, fn: Listener, scope?: any): boolean;
-	removeListener(id: T, fn: Listener, scope?: any): boolean;
+	addListener(id: T, fn: Listener<T>, scope?: any): boolean;
+	removeListener(id: T, fn: Listener<T>, scope?: any): boolean;
 	/**
 	 * Broadcasts all registered listeners for given event type (in order
 	 * registration) and returns true if any of them have been successfully
