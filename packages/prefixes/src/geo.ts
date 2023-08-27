13dd8d38e4b10dab8aa6f4a522b7f592624d4082
Karsten Schmidt <k@thi.ng>
2023-08-27 15:56:24 +0200

    feat(prefixes): add GPX & KML namespace URIs

--- packages/prefixes/src/geo.ts
+++ packages/prefixes/src/geo.ts
@@ -2,5 +2,7 @@ export const geo = "http://www.w3.org/2003/01/geo/wgs84_pos#";
 export const geom = "http://data.ign.fr/def/geometrie";
 export const geonames = "http://www.geonames.org/ontology#";
 export const geosparql = "http://www.opengis.net/ont/geosparql#";
+export const gpx = "http://www.topografix.com/GPX/1/1";
+export const kml = "http://www.opengis.net/kml/2.2";
 export const locn = "http://www.w3.org/ns/locn#";
 export const omg = "https://w3id.org/omg#";
