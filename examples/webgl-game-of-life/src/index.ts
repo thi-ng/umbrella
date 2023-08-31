35317bedd1c4c21fe4da37af83efa1cb4b67d2ef
Karsten Schmidt <k@thi.ng>
2023-08-31 11:58:19 +0200

    feat(examples): add webgl-game-of-life demo

---
+++ examples/webgl-game-of-life/src/index.ts
@@ -0,0 +1,121 @@
+import { ConsoleLogger } from "@thi.ng/logger";
+import { SYSTEM } from "@thi.ng/random";
+import { repeatedly } from "@thi.ng/transducers";
+import {
+	TextureFormat,
+	defMultiPass,
+	glCanvas,
+	passCopy,
+	setLogger,
+} from "@thi.ng/webgl";
+
+// configure a logger to display generated shaders in console
+setLogger(new ConsoleLogger("webgl"));
+
+// simulation size
+const W = 512;
+
+// create WebGL2 canvas
+const canvas = glCanvas({
+	version: 2,
+	width: W,
+	height: W,
+	autoScale: false,
+	parent: document.getElementById("app")!,
+});
+
+// create multipass render pipeline
+const gameOfLife = defMultiPass({
+	gl: canvas.gl,
+	width: W,
+	height: W,
+	// define interim buffers to store sim state
+	textures: {
+		// current CA generation
+		// use R8 format since we don't need more to store cell states
+		curr: {
+			format: TextureFormat.R8,
+			// pre-seed simulation with noise texture
+			image: new Uint8Array(
+				repeatedly(() => (SYSTEM.probability(0.4) ? -1 : 0), W * W)
+			),
+		},
+		// interim buffer for next CA generation
+		// (shaders cannot read & write to the same texture)
+		next: { format: TextureFormat.R8 },
+	},
+	// configure shader passes (will be executed in given order)
+	// we don't (but could) specify geometry or vertex shader here
+	// if omitted a default "fullscreen quad" with a simple
+	// passthrough vertex shader will be used...
+	passes: [
+		// first pass: Game Of Life (GoL) simulation
+		{
+			fs: `
+// helper function to read cell state (with wrap-around)
+float read(ivec2 p) {
+    ivec2 ires = ivec2(resolution);
+    // return texelFetch(input0, (p + ires) % ires, 0).r;
+    return texelFetch(input0, (p + resolution) % resolution, 0).r;
+}
+
+void main() {
+    ivec2 p = ivec2(gl_FragCoord.xy);
+    ivec3 o = ivec3(-1, 0, 1);
+    // read cell states & count neighbors
+    float state = read(p);
+    float num = read(p + o.yz);
+    num += read(p + o.yx);
+    num += read(p + o.zy);
+    num += read(p + o.xy);
+    num += read(p + o.zz);
+    num += read(p + o.xz);
+    num += read(p + o.zx);
+    num += read(p + o.xx);
+    // apply sim rules from lookup tables (see below)
+    // ("output0" refers to first output texture of potentially multiple)
+    output0 = vec4(state > 0.5 ? alive[int(num)] : birth[int(num)]);
+}`,
+			// define texture inputs & outputs (IDs from above `textures` config)
+			inputs: ["curr"],
+			outputs: ["next"],
+			// each shader pass can define its own uniforms
+			// (`time` & `resolution` are populated automatically if defined here)
+			uniforms: {
+				resolution: "ivec2",
+				// CA simulation rules as look up tables for all possible neighbor counts
+				// enables easy experimentation with new rules...
+				// Game of Life rules: stay alive if 2 or 3, birth if 3 neighbors
+				alive: ["int[]", 8, [0, 0, 1, 1, 0, 0, 0, 0, 0]],
+				birth: ["int[]", 8, [0, 0, 0, 1, 0, 0, 0, 0, 0]],
+			},
+		},
+
+		// 2nd shader pass is a simple ping-pong copy pass
+		// this will copy the "next" texture back into the "curr" texture
+		passCopy(["next"], ["curr"]),
+
+		// final shader pass: render the CA to the main color buffer
+		{
+			fs: `
+void main() {
+    // read cell state and choose color
+    fragColor = vec4(mix(dead, alive, texelFetch(input0, ivec2(gl_FragCoord.xy), 0).r), 1);
+}`,
+			inputs: ["curr"],
+			// final shader pass NEVER has any other outputs
+			outputs: [],
+			// custom uniforms for cell state colors
+			uniforms: {
+				alive: ["vec3", [1, 1, 1]],
+				dead: ["vec3", [0, 0, 1]],
+			},
+		},
+	],
+});
+
+// the returned multipass object allows access to all generated
+// passes, textures, FBOs, uniforms...
+// also includes a simple shadertoy-like animation control
+// see: https://docs.thi.ng/umbrella/webgl/interfaces/Multipass.html
+gameOfLife.start();
