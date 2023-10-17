507fb28b2c85d7f979df1c6ce850cf904b684891
Karsten Schmidt <k@thi.ng>
2023-10-17 15:19:11 +0200

    test(bitfield): add tests

--- packages/bitfield/test/index.ts
+++ packages/bitfield/test/index.ts
@@ -1,5 +1,37 @@
 import { group } from "@thi.ng/testament";
-// import * as assert from "assert";
-// import * as b from "../src/index.js"
+import * as assert from "assert";
+import { defBitField } from "../src/index.js";
 
-group("bitfield", {});
+group("bitfield", {
+	positions: () => {
+		assert.deepStrictEqual(
+			[
+				// prettier-ignore
+				...defBitField([
+					0, 0, 1, 0, 1, 0, 1, 1,
+					0, 0, 0, 0, 0, 0, 0, 0,
+					0, 0, 0, 0, 0, 1, 0, 0,
+					1, 1,
+				]).positions(),
+			],
+			[2, 4, 6, 7, 21, 24, 25]
+		);
+	},
+
+	similarity: () => {
+		const bits = [0, 1, 1, 0, 1, 0];
+		const a = defBitField(bits);
+		assert.strictEqual(a.similarity(a), 1);
+		assert.strictEqual(a.similarity(bits), 1);
+		assert.strictEqual(
+			a.similarity([0, 1, 0, 0, 1, 0]).toFixed(3),
+			(2 / 3).toFixed(3)
+		);
+		assert.strictEqual(
+			a.similarity([0, 0, 1, 0, 0, 0]).toFixed(3),
+			(1 / 3).toFixed(3)
+		);
+		assert.strictEqual(a.similarity([0, 0, 0, 0, 0, 0]), 0);
+		assert.throws(() => a.similarity([]));
+	},
+});
