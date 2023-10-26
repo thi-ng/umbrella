3a4063a959fbc53a262fe73029547aee564f0ef8
Karsten Schmidt <k@thi.ng>
2023-10-26 20:39:26 +0200

    feat(vectors): add mag2/3/4()

--- packages/vectors/src/mag.ts
+++ packages/vectors/src/mag.ts
@@ -1,4 +1,10 @@
-import type { ReadonlyVec } from "./api.js";
-import { magSq } from "./magsq.js";
+import type { VecOpRoV } from "./api.js";
+import { magSq, magSq2, magSq3, magSq4 } from "./magsq.js";
 
-export const mag = (v: ReadonlyVec) => Math.sqrt(magSq(v));
+export const mag: VecOpRoV<number> = (v) => Math.sqrt(magSq(v));
+
+export const mag2: VecOpRoV<number> = (v) => Math.sqrt(magSq2(v));
+
+export const mag3: VecOpRoV<number> = (v) => Math.sqrt(magSq3(v));
+
+export const mag4: VecOpRoV<number> = (v) => Math.sqrt(magSq4(v));
