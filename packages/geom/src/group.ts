7909ee4775deb611ce461ede8ef2013ea8846dfd
Karsten Schmidt <k@thi.ng>
2023-12-26 14:39:24 +0100

    docs(geom): add group docstring

--- packages/geom/src/group.ts
+++ packages/geom/src/group.ts
@@ -1,6 +1,18 @@
 import type { Attribs, IHiccupShape } from "@thi.ng/geom-api";
 import { Group } from "./api/group.js";
 
+/**
+ * Creates a group with given attribs and child shapes (nested groups are
+ * supported).
+ *
+ * @remarks
+ * Use the special `__inkscapeLayer` control attrib to declare the group as an
+ * Inkscape layer and the given value as layer name (only relevant and used when
+ * later converted to SVG).
+ *
+ * @param attribs
+ * @param children
+ */
 export const group = (
 	attribs: Attribs = {},
 	children?: Iterable<IHiccupShape>
