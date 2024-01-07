647d7e11c7ab47c3d30fad956d33f0b3a45bcc59
Karsten Schmidt <k@thi.ng>
2024-01-07 23:09:22 +0100

    feat(boids): update dynamicTarget()
    - add radius param

--- packages/boids/src/behaviors/dynamic.ts
+++ packages/boids/src/behaviors/dynamic.ts
@@ -9,24 +9,29 @@ import { __ensureFn } from "../internal/ensure.js";
  * `target` fn. That `target` function will be called successively for each boid
  * update. If it returns a point, it will be used as steer target until a new
  * one is returned. If the function returns null/undefined the current target
- * will be kept. The behavior is a no-op until the target function returns its
- * first point. no-op.
+ * will be kept. The behavior is a no-op for boids outside the configured
+ * `radius` (around the target) and also has no effect until the target function
+ * returns its first point.
  *
  * @param target
+ * @param radius
  * @param weight
  */
 export const dynamicTarget = (
 	target: Fn<Boid, Nullable<ReadonlyVec>>,
+	radius: ScalarOrField = Infinity,
 	weight: ScalarOrField = 1
 ): IBoidBehavior => {
-	let currTarget: ReadonlyVec;
+	let currTarget: ReadonlyVec | undefined;
+	const $radius = __ensureFn(radius);
 	const force: Vec = [];
 	return {
 		weight: __ensureFn(weight),
 		update: (boid) => {
-			const t = target(boid);
-			if (t) currTarget = t;
-			return currTarget
+			currTarget = target(boid) || currTarget;
+			if (!currTarget) return boid.api.ZERO;
+			const r = $radius(boid);
+			return boid.api.distSq(currTarget, boid.pos.curr) < r * r
 				? boid.steerTowards(currTarget, force)
 				: boid.api.ZERO;
 		},
