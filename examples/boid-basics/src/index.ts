dc2b2bc9e95eb96d1a8c2be293b23986bcf2a88e
Karsten Schmidt <k@thi.ng>
2024-01-23 14:58:29 +0100

    feat(examples): update boid-basics to new API

--- examples/boid-basics/src/index.ts
+++ examples/boid-basics/src/index.ts
@@ -1,43 +1,52 @@
 import type { Nullable } from "@thi.ng/api";
-import { defBoid2, type Boid, type BoidOpts } from "@thi.ng/boids";
+import {
+	alignment,
+	cohesion,
+	defBoid2,
+	defFlock,
+	separation,
+	wrap2,
+	type Boid,
+	type BoidOpts,
+} from "@thi.ng/boids";
+import { isMobile } from "@thi.ng/checks";
 import { multiCosineGradient } from "@thi.ng/color";
 import { circle, group } from "@thi.ng/geom";
 import { HashGrid2 } from "@thi.ng/geom-accel/hash-grid";
-import { fitClamped } from "@thi.ng/math";
+import { weightedRandom } from "@thi.ng/random";
 import { $canvas } from "@thi.ng/rdom-canvas";
 import { fromRAF } from "@thi.ng/rstream";
 import { defTimeStep } from "@thi.ng/timestep";
 import { repeatedly } from "@thi.ng/transducers";
-import { clamp2, distSq2, randMinMax2, randNorm2 } from "@thi.ng/vectors";
+import { distSq2, randMinMax2, randNorm2 } from "@thi.ng/vectors";
 
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
-const PAD = 10;
+const PAD = -40;
 // screen bounds min/max
 const BMIN = [PAD, PAD];
 const BMAX = [WIDTH - PAD, HEIGHT - PAD];
 
+// derive number of boids from screen resolution
+const NUM = isMobile() ? 500 : 800;
+
+// setup spatial indexing structure. this should be used for the boid sim (and
+// here also for our rendering) to efficiently find neighbors.
+const ACCEL = new HashGrid2<Boid>((x) => x.pos.prev, 64, NUM);
+
+// alternatively we can use a dummy acceleration impl
+// const ACCEL = noAccel();
+
 // boid behavior options
-const BEHAVIOR: Partial<BoidOpts> = {
-	// min distance to enforce to neighbors
-	minDist: 30,
-	// max distance to consider for neighbors
-	maxDist: 80,
-	// force multiplier
-	maxForce: 2,
+const OPTS: BoidOpts = {
+	accel: ACCEL,
+	behaviors: [separation(40, 1.2), alignment(80, 0.5), cohesion(80, 0.8)],
 	// max speed (per second)
 	maxSpeed: 50,
-	// sub-behavior weights/importance
-	// change to prioritize/de-emphasize
-	alignment: 1,
-	cohesion: 0.1,
-	separation: 2,
 	// optional constraint (here to keep boids within screen bounds)
-	constrain: (p) => clamp2(p, p, BMIN, BMAX),
+	constrain: wrap2(BMIN, BMAX),
 };
 
-// derive number of boids from screen resolution
-const NUM = fitClamped(WIDTH * HEIGHT, 640 * 480, 1440 * 810, 500, 1000) | 0;
 // max radius for neighbor queries
 const MAX_RADIUS = 50;
 
@@ -51,51 +60,51 @@ const gradient = multiCosineGradient({
 		[1, [0.6, 0, 0.6]],
 	],
 });
-// setup spatial indexing structure. this is needed by the boid sim (and here
-// also for our rendering) to efficiently find neighbors.
-const grid = new HashGrid2<Boid>((x) => x.pos.curr, 32, NUM);
+
 // setup time-based simulation (with defaults)
 const sim = defTimeStep();
-// initialize the array of boids (also available for 3D)
-const boids = [
+
+// initialize the flock of boids (also available for 3D)
+// see: https://docs.thi.ng/umbrella/boids/classes/Flock.html
+const flock = defFlock(ACCEL, [
 	...repeatedly(
 		// spawn each boid at a random pos & direction inside the screen bounds
 		// in this example all boids share the same behavior (but don't have to...)
+		// also pick random max speed for each boid
 		() =>
 			defBoid2(
-				grid,
 				randMinMax2([], BMIN, BMAX),
-				randNorm2([]),
-				BEHAVIOR
+				randNorm2([], OPTS.maxSpeed),
+				{
+					...OPTS,
+					maxSpeed: weightedRandom([20, 50, 100], [1, 4, 2])(),
+				}
 			),
 		NUM
 	),
-];
+]);
 
 // requestAnimationFrame based animation/simulation
 const scene = fromRAF({ timestamp: true }).map((t) => {
-	// rebuild the spatial index (using current boid positions)
-	grid.build(boids);
-	// update all boids for the required number of timesteps
+	// update all boids for the required number of time steps
 	// (see https://thi.ng/timestep for details of the sim approach)
-	sim.update(t, boids);
+	sim.update(t, [flock]);
 	// return a shape representations of the boids as a group of circles, with
 	// each circle's color based on the above multi-color gradient and the
 	// distance to the nearest neighbor.
-	return group(
-		{ __background: "#112" },
-		// transform the boids into shapes...
-		boids.map((x) => {
-			const pos = x.pos.value;
+	return group({ __background: "#112", stroke: "none", fill: "none" }, [
+		...flock.boids.map((boid) => {
+			const pos = boid.pos.value;
+			// let radius = 4;
 			let radius = MAX_RADIUS;
 			// find neighbors with the query radius around current pos
-			const neighbors = x.neighbors(radius, pos);
+			const neighbors = boid.neighbors(radius, pos);
 			if (neighbors.length > 1) {
 				// find distance to closest neighbor
 				let closest: Nullable<Boid>;
-				let minD = Infinity;
+				let minD = MAX_RADIUS ** 2;
 				for (let n of neighbors) {
-					if (n === x) continue;
+					if (n === boid) continue;
 					const d = distSq2(pos, n.pos.value);
 					if (d < minD) {
 						closest = n;
@@ -106,12 +115,14 @@ const scene = fromRAF({ timestamp: true }).map((t) => {
 			}
 			// create a circle with its color based on current radius
 			// (which itself is constantly changing based on neighbor distances)
-			return circle(x.pos.value, radius / 2, {
+			return circle(boid.pos.value, radius / 2, {
 				fill: gradient[Math.min(radius | 0, MAX_RADIUS)],
 			});
-		})
-	);
+		}),
+	]);
 });
 
 // create reactive canvas component, subscribed to above scene stream
-$canvas(scene, [WIDTH, HEIGHT]).mount(document.getElementById("app")!);
+$canvas(scene, [WIDTH, HEIGHT], { id: "main" }).mount(
+	document.getElementById("app")!
+);
