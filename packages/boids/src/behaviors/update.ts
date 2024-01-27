03c5fb896c29ae099807c3ee4335900ac7f5dd9c
Karsten Schmidt <k@thi.ng>
2024-01-27 10:11:02 +0100

    docs(boids): add docstring

--- packages/boids/src/behaviors/update.ts
+++ packages/boids/src/behaviors/update.ts
@@ -1,6 +1,16 @@
 import type { Vec } from "@thi.ng/vectors";
 import type { Boid } from "../boid.js";
 
+/**
+ * Behavior update function (the default implementation fro
+ * {@link BoidOpts.update}). Takes a single {@link Boid} and evaluates all of
+ * its assigned behaviors (in the order given). If a behavior's weight evaluates
+ * to zero, the behavior will be skipped, otherwise the returned force vector
+ * will be added (multiplied with the behavior's current weight). Returns summed
+ * force vector of all behaviors.
+ *
+ * @param boid
+ */
 export const blendedBehaviorUpdate = (boid: Boid) => {
 	const { maddN, zeroes } = boid.api;
 	const force: Vec = zeroes();
