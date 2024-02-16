6d3381381a81e4971f2ea23bc7eff9d1b9b60854
Karsten Schmidt <k@thi.ng>
2024-02-16 18:36:39 +0100

    refactor(ecs): update LOGGER handling

--- packages/ecs/src/logger.ts
+++ packages/ecs/src/logger.ts
@@ -1,6 +1,6 @@
-import type { ILogger } from "@thi.ng/logger";
-import { NULL_LOGGER } from "@thi.ng/logger/null";
+import { ROOT } from "@thi.ng/logger/root";
 
-export let LOGGER = NULL_LOGGER;
-
-export const setLogger = (logger: ILogger) => (LOGGER = logger);
+/**
+ * See [thi.ng/logger](https://docs.thi.ng/umbrella/logger/) for usage.
+ */
+export const LOGGER = ROOT.childLogger("ecs");
