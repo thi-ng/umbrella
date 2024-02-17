1c138ef17e9623789bf663b3940d9c4b96ab93d0
Karsten Schmidt <k@thi.ng>
2024-02-17 13:29:47 +0100

    test(mime): add tests

--- packages/mime/test/main.test.ts
+++ packages/mime/test/main.test.ts
@@ -1,4 +1,13 @@
 import { expect, test } from "bun:test";
-// import { } from "../src/index.js"
+import { comp } from "@thi.ng/compose";
+import { preferredType, preferredExtension } from "../src/index.js";
 
-test.todo("mime", () => {});
+test("roundtrip", () => {
+	const extensions = "aac,avif,css,jpg,js,md,mp4,obj,png,stl,txt,webp".split(
+		","
+	);
+	const fn = comp(preferredExtension, preferredType);
+	for (let ext of extensions) {
+		expect(fn(ext)).toBe(ext);
+	}
+});
