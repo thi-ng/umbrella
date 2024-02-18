8ca308527e3287cdcc205cebd9fabaa47f23c49e
Karsten Schmidt <k@thi.ng>
2024-02-18 10:52:41 +0100

    feat(file-io): add ensureDir()

--- packages/file-io/src/dir.ts
+++ packages/file-io/src/dir.ts
@@ -1,5 +1,16 @@
 import { existsSync, mkdirSync, statSync } from "fs";
-import { sep } from "path";
+import { dirname } from "path";
+
+/**
+ * Assumes given path is to a directory. Checks if the dir exists and if not
+ * attempts to create it and then returns true. Otherwise returns false.
+ *
+ * @param dir
+ */
+export const ensureDir = (dir: string) =>
+	dir.length > 0 && !existsSync(dir)
+		? (mkdirSync(dir, { recursive: true }), true)
+		: false;
 
 /**
  * Checks if the directory for given file `path` already exists, and if not the
@@ -11,15 +22,11 @@ import { sep } from "path";
  *
  * @param path
  */
-export const ensureDirForFile = (path: string) => {
-	const dir = path.substring(0, path.lastIndexOf(sep));
-	return dir.length > 0 && !existsSync(dir)
-		? (mkdirSync(dir, { recursive: true }), true)
-		: false;
-};
+export const ensureDirForFile = (path: string) => ensureDir(dirname(path));
 
 /**
- * Returns true if `path` is a directory (assumes path exists).
+ * Returns true if `path` is a directory (assumes path exists, if not throws an
+ * error).
  *
  * @param path
  */
