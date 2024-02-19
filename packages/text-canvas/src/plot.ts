cb275ae402dcf3086ba0a188a8f1d7b41f327b2f
Karsten Schmidt <k@thi.ng>
2024-02-19 14:42:38 +0100

    refactor(text-canvas): unify plotting function naming
    - plotBarsV() => plotBarChartV()
    - lineChart() => plotLineChart()
    - migrate line chart fns to plot.ts

--- packages/text-canvas/src/plot.ts
+++ packages/text-canvas/src/plot.ts
@@ -1,7 +1,14 @@
 import type { NumericArray } from "@thi.ng/api";
+import { ensureArray } from "@thi.ng/arrays/ensure-array";
+import { peek } from "@thi.ng/arrays/peek";
+import { fitClamped } from "@thi.ng/math/fit";
+import { minMax } from "@thi.ng/math/interval";
+import { max as $max } from "@thi.ng/transducers/max";
+import { min as $min } from "@thi.ng/transducers/min";
 import type { BlendFn } from "./api.js";
 import { barChartVLines } from "./bars.js";
-import type { Canvas } from "./canvas.js";
+import { Canvas } from "./canvas.js";
+import { formatCanvas } from "./format.js";
 import { blendBarsVAdd, blitBarsV } from "./image.js";
 import { textLines } from "./text.js";
 
@@ -16,7 +23,7 @@ interface PlotSpec {
 	color: number;
 }
 
-export const plotBarsV = (
+export const plotBarChartV = (
 	canv: Canvas,
 	opts: PlotBarsVOpts,
 	...plots: PlotSpec[]
@@ -35,4 +42,52 @@ export const plotBarsV = (
 		blitBarsV(canv, channel, 0, 0, blend);
 	}
 	return canv;
+};
+
+export const plotLineChart = (
+	canvas: Canvas,
+	x: number,
+	y: number,
+	height: number,
+	vals: Iterable<number>,
+	min?: number,
+	max?: number,
+	format = canvas.format
+) => {
+	const $vals = ensureArray(vals);
+	min = min !== undefined ? min : $min($vals);
+	max = max !== undefined ? max : $max($vals);
+	height--;
+	format <<= 16;
+	const { x1, x2 } = peek(canvas.clipRects);
+	for (let i = 0, n = $vals.length - 1; i < n; i++) {
+		const xx = x + i;
+		if (xx < x1) continue;
+		if (xx > x2) break;
+		const ya = Math.round(fitClamped($vals[i], min, max, height, 0)) + y;
+		const yb =
+			Math.round(fitClamped($vals[i + 1], min, max, height, 0)) + y;
+		if (ya === yb) {
+			canvas.setAt(xx, ya, 0x2500 | format); // ─
+		} else {
+			// vline to new Y
+			let [y1, y2] = minMax(ya, yb);
+			while (++y1 < y2) canvas.setAt(xx, y1, 0x2502 | format); // │
+			// end points
+			canvas.setAt(xx, ya, (ya < yb ? 0x256e : 0x256f) | format); // ╮ ╯
+			canvas.setAt(xx, yb, (ya < yb ? 0x2570 : 0x256d) | format); // ╰ ╭
+		}
+	}
+};
+
+export const lineChartStr = (
+	height: number,
+	vals: Iterable<number>,
+	min?: number,
+	max?: number
+) => {
+	const $vals = ensureArray(vals);
+	const surf = new Canvas($vals.length, height);
+	plotLineChart(surf, 0, 0, height, $vals, min, max);
+	return formatCanvas(surf);
 };
