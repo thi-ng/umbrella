ffe0876936c1aae03f2ad2921adb16558d29e577
Karsten Schmidt <k@thi.ng>
2024-02-22 12:28:44 +0100

    refactor(tools): update module-stats

--- tools/src/module-stats.ts
+++ tools/src/module-stats.ts
@@ -1,14 +1,16 @@
 import { readJSON, writeJSON } from "@thi.ng/file-io";
 import { execFileSync } from "node:child_process";
-import { existsSync, mkdirSync } from "node:fs";
 import { LOGGER } from "./api.js";
 
 const pkg = readJSON("package.json", LOGGER);
 const deps = [
 	"tslib",
-	"fs",
-	"child_process",
-	"path",
+	"node:crypto",
+	"node:fs",
+	"node:child_process",
+	"node:os",
+	"node:path",
+	"node:process",
 	...Object.keys(pkg.dependencies || {}),
 ];
 const opts = [
@@ -18,17 +20,17 @@ const opts = [
 	...deps.map((x) => `--external:${x}`),
 	"index.js",
 ];
-if (pkg.keywords.includes("nodejs") || pkg.keywords.includes("no-browser")) {
+if (
+	pkg.keywords.includes("nodejs") ||
+	pkg.keywords.includes("bun") ||
+	pkg.keywords.includes("no-browser")
+) {
 	opts.unshift("--platform=node");
 }
-const raw = execFileSync(
-	"../../node_modules/esbuild/bin/esbuild",
-	opts
-).toString();
-const min = execFileSync("../../node_modules/esbuild/bin/esbuild", [
-	"--minify",
-	...opts,
-]).toString();
+
+const ESBUILD = "../../node_modules/esbuild/bin/esbuild";
+const raw = execFileSync(ESBUILD, opts).toString();
+const min = execFileSync(ESBUILD, ["--minify", ...opts]).toString();
 const brotli = execFileSync("brotli", { input: min });
 
 const stats = {
@@ -37,7 +39,7 @@ const stats = {
 	brotli: brotli.byteLength,
 };
 
-!existsSync(".meta") && mkdirSync(".meta");
+// !existsSync(".meta") && mkdirSync(".meta");
 // writeFileSync(".meta/raw.js", raw);
 // writeFileSync(".meta/min.js", min);
 writeJSON(".meta/size.json", { esm: stats }, null, 0, LOGGER);
