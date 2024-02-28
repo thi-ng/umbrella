f024045e8342056f780b5990bec7659f9189b54a
Karsten Schmidt <k@thi.ng>
2024-02-28 12:10:55 +0100

    docs(strings): add docstring

--- packages/strings/src/stringify.ts
+++ packages/strings/src/stringify.ts
@@ -1,10 +1,25 @@
 import type { Stringer } from "./api.js";
 
 /**
- * Higher order version of `JSON.stringify()` with the option to treat strings
- * and numbers differently. If `all` is `false` (default), strings and numbers
- * are simply converted using `String(x)`. If `indent` is given, it will be used
- * for `JSON.stringify(x, null, indent)`
+ * Higher order version of `JSON.stringify()` with the option to treat top-level
+ * strings and numbers differently (i.e. NOT if they're nested values!). If
+ * `all` is `false` (default), strings and numbers are simply converted using
+ * `String(x)`. If `indent` is given, it will be used for `JSON.stringify(x,
+ * null, indent)`
+ *
+ * @example
+ * ```ts
+ * import { stringify } from "@thi.ng/strings";
+ *
+ * stringify()("hello");
+ * // hello
+ *
+ * stringify(true)("hello");
+ * // "hello"
+ *
+ * stringify()({ a: "hello" })
+ * // { "a": "hello" }
+ * ```
  *
  * @param all -
  * @param indent -
