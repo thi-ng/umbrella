6b13b0d3916ef6093f02b0d0f87ab0d94dfea3e5
Karsten Schmidt <k@thi.ng>
2024-03-01 15:38:45 +0100

    feat(imago): update resize, add support for proportional resize
    - update resizeProc() to handle scalar `size` to scale proportionally
      with automatic aspect detection

--- packages/imago/src/ops/resize.ts
+++ packages/imago/src/ops/resize.ts
@@ -1,9 +1,16 @@
+import { isNumber } from "@thi.ng/checks";
 import { GRAVITY_POSITION, type Processor, type ResizeSpec } from "../api.js";
 import { coerceColor, computeSize } from "../units.js";
 
 export const resizeProc: Processor = async (spec, input, ctx) => {
 	const { bg, filter, fit, gravity, ref, size, unit } = <ResizeSpec>spec;
-	const [width, height] = computeSize(size, ctx.size, ref, unit);
+	const aspect = ctx.size[0] / ctx.size[1];
+	let $size = size;
+	let width: number, height: number;
+	if (isNumber($size) && unit !== "%") {
+		$size = aspect > 1 ? [$size, $size / aspect] : [$size * aspect, $size];
+	}
+	[width, height] = computeSize($size, ctx.size, ref, unit);
 	return [
 		input.resize({
 			width,
