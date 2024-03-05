e3e8979e4ea51c8a25b4ff6b12d683cc3b48d03a
Karsten Schmidt <k@thi.ng>
2024-03-05 20:01:05 +0100

    feat(shader-ast-stdlib): update cossin(), sincos(), add opt. scale factor arg

--- packages/shader-ast-stdlib/src/math/sincos.ts
+++ packages/shader-ast-stdlib/src/math/sincos.ts
@@ -1,17 +1,26 @@
 import type { FloatTerm } from "@thi.ng/shader-ast";
 import { vec2 } from "@thi.ng/shader-ast/ast/lit";
+import { mul } from "@thi.ng/shader-ast/ast/ops";
 import { cos, sin } from "@thi.ng/shader-ast/builtin/math";
 
 /**
- * Inline function. Returns vec2(sin(x), cos(x)).
+ * Inline function. Returns vec2(sin(x), cos(x)), optionally scaled by `r`
  *
  * @param x -
+ * @param r -
  */
-export const sincos = (x: FloatTerm) => vec2(sin(x), cos(x));
+export const sincos = (x: FloatTerm, r?: FloatTerm | number) => {
+	const res = vec2(sin(x), cos(x));
+	return r != undefined ? mul(res, r) : res;
+};
 
 /**
- * Inline function. Returns vec2(cos(x), sin(x)).
+ * Inline function. Returns vec2(cos(x), sin(x)), optionally scaled by `r`.
  *
  * @param x -
+ * @param r -
  */
-export const cossin = (x: FloatTerm) => vec2(cos(x), sin(x));
+export const cossin = (x: FloatTerm, r?: FloatTerm | number) => {
+	const res = vec2(cos(x), sin(x));
+	return r != undefined ? mul(res, r) : res;
+};
