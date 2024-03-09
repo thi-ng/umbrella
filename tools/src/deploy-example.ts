bcaff8f3aa9a790ed0f0cf6fa79874980bdb1e8b
Karsten Schmidt <k@thi.ng>
2024-03-09 10:40:09 +0100

    feat(tools): update deploy-example script

--- tools/src/deploy-example.ts
+++ tools/src/deploy-example.ts
@@ -4,12 +4,13 @@ import { preferredType } from "@thi.ng/mime";
 import { execFileSync } from "node:child_process";
 import { existsSync } from "node:fs";
 import { exit } from "node:process";
+import { LOGGER } from "./api.js";
 import {
 	AWS_PROFILE,
 	CF_DISTRO_EXAMPLES,
 	S3_BUCKET_EXAMPLES,
 	S3_OPTS,
-} from "./aws-config";
+} from "./aws-config.js";
 
 interface UploadOpts {
 	ext: string;
@@ -20,13 +21,13 @@ interface UploadOpts {
 
 const EXAMPLE = process.argv[2];
 if (!EXAMPLE) {
-	console.warn("\nUsage: deploy-example.ts <example-name>");
+	LOGGER.warn("\nUsage: deploy-example.ts <example-name>");
 	exit(1);
 }
 
 const PKG = readJSON(`examples/${EXAMPLE}/package.json`);
 if (PKG["thi.ng"]?.online === false) {
-	console.log("example marked as offline-only, skipping...");
+	LOGGER.warn("example marked as offline-only, skipping...");
 	process.exit(0);
 }
 
@@ -37,19 +38,25 @@ const COMPRESS_OPTS = `${S3_OPTS} --content-encoding br`;
 const NEVER_COMPRESS = new Set(["mp4"]);
 
 const args = new Set(process.argv.slice(3).map((x) => x.substring(2)));
-console.log(args);
+LOGGER.debug(args);
 
 execFileSync(
 	"find",
 	`examples/${EXAMPLE} -type f -name '*.DS_Store' -ls -delete`.split(" ")
 );
 
+const execAWS = (args: string) => {
+	const $args = args.split(" ");
+	LOGGER.info("aws", args);
+	execFileSync("aws", $args);
+};
+
 const uploadAssets = (dir: string, opts?: Partial<UploadOpts>) => {
 	const root = `${BUILD}${dir}`;
 	if (!existsSync(root)) return;
 	opts = { ext: "", compress: true, depth: Infinity, ...opts };
 	for (let f of files(root, opts.ext!, opts.depth)) {
-		const fd = `${S3_BUCKET_EXAMPLES}/${DEST_DIR}/${f
+		const fd = `${S3_BUCKET_EXAMPLES}${DEST_DIR}/${f
 			.replace(BUILD, "")
 			.substring(dir === "" ? 1 : 0)}`;
 		const ext = f.substring(f.lastIndexOf(".") + 1);
@@ -58,17 +65,11 @@ const uploadAssets = (dir: string, opts?: Partial<UploadOpts>) => {
 		opts.process && opts.process(f);
 		if (opts.compress && !NEVER_COMPRESS.has(ext)) {
 			execFileSync("brotli", ["-9", f]);
-			execFileSync(
-				"aws",
-				`s3 cp ${f}.br ${fd} ${COMPRESS_OPTS} --content-type ${type}`.split(
-					" "
-				)
+			execAWS(
+				`s3 cp ${f}.br ${fd} ${COMPRESS_OPTS} --content-type ${type}`
 			);
 		} else {
-			execFileSync(
-				"aws",
-				`s3 cp ${f} ${fd} ${S3_OPTS} --content-type ${type}`.split(" ")
-			);
+			execAWS(`s3 cp ${f} ${fd} ${S3_OPTS} --content-type ${type}`);
 		}
 	}
 };
@@ -88,13 +89,10 @@ uploadAssets("", { ext: ".js", depth: 1 });
 uploadAssets("", { ext: ".html" });
 
 if (!args.has("no-invalidate")) {
-	console.log("invaliding", DEST_DIR);
-	execFileSync(
-		"aws",
-		`cloudfront create-invalidation --distribution-id ${CF_DISTRO_EXAMPLES} --paths ${DEST_DIR}/* ${AWS_PROFILE}`.split(
-			" "
-		)
+	LOGGER.info("invaliding", DEST_DIR);
+	execAWS(
+		`cloudfront create-invalidation --distribution-id ${CF_DISTRO_EXAMPLES} --paths ${DEST_DIR}/* ${AWS_PROFILE}`
 	);
 }
 
-console.log("done");
+LOGGER.debug("done");
