13b31871760aabe85417c29087f556cdec7cbf80
Karsten Schmidt <k@thi.ng>
2024-03-19 17:27:49 +0100

    feat(tools): update deploy-example script
    - refactor using thi.ng/args cliApp()
    - add support for batch uploading all/multiple examples
    - improve CDN invalidation logic

--- tools/src/deploy-example.ts
+++ tools/src/deploy-example.ts
@@ -1,10 +1,12 @@
-import type { Fn } from "@thi.ng/api";
-import { files, readJSON } from "@thi.ng/file-io";
+import { type Fn } from "@thi.ng/api";
+import { cliApp, flag, type CommandCtx } from "@thi.ng/args";
+import { dirs, files, readJSON } from "@thi.ng/file-io";
+import type { ILogger } from "@thi.ng/logger";
 import { preferredType } from "@thi.ng/mime";
+import { map } from "@thi.ng/transducers";
 import { execFileSync } from "node:child_process";
 import { existsSync } from "node:fs";
-import { exit } from "node:process";
-import { LOGGER } from "./api.js";
+import { basename, resolve } from "node:path";
 import {
 	AWS_PROFILE,
 	CF_DISTRO_EXAMPLES,
@@ -18,81 +20,125 @@ interface UploadOpts {
 	depth: number;
 	process: Fn<string, void>;
 }
-
-const EXAMPLE = process.argv[2];
-if (!EXAMPLE) {
-	LOGGER.warn("\nUsage: deploy-example.ts <example-name>");
-	exit(1);
+interface CLIOpts {
+	noInvalidate: boolean;
 }
 
-const PKG = readJSON(`examples/${EXAMPLE}/package.json`);
-if (PKG["thi.ng"]?.online === false) {
-	LOGGER.warn("example marked as offline-only, skipping...");
-	process.exit(0);
-}
+interface CLICtx extends CommandCtx<CLIOpts, any> {}
 
-const BUILD = `examples/${EXAMPLE}/dist/`;
-const DEST_DIR = `/umbrella/${EXAMPLE}`;
 const COMPRESS_OPTS = `${S3_OPTS} --content-encoding br`;
-
+const DEST_BASE = "/umbrella";
 const NEVER_COMPRESS = new Set(["mp4"]);
 
-const args = new Set(process.argv.slice(3).map((x) => x.substring(2)));
-LOGGER.debug(args);
-
-execFileSync(
-	"find",
-	`examples/${EXAMPLE} -type f -name '*.DS_Store' -ls -delete`.split(" ")
-);
-
-const execAWS = (args: string) => {
+const execAWS = (args: string, logger: ILogger) => {
 	const $args = args.split(" ");
-	LOGGER.info("aws", args);
+	logger.info("aws", args);
 	execFileSync("aws", $args);
 };
 
-const uploadAssets = (dir: string, opts?: Partial<UploadOpts>) => {
-	const root = `${BUILD}${dir}`;
-	if (!existsSync(root)) return;
-	opts = { ext: "", compress: true, depth: Infinity, ...opts };
-	for (let f of files(root, opts.ext!, opts.depth)) {
-		const fd = `${S3_BUCKET_EXAMPLES}${DEST_DIR}/${f
-			.replace(BUILD, "")
-			.substring(dir === "" ? 1 : 0)}`;
-		const ext = f.substring(f.lastIndexOf(".") + 1);
-		const type = preferredType(ext);
-		console.log(f, "->", fd, type);
-		opts.process && opts.process(f);
-		if (opts.compress && !NEVER_COMPRESS.has(ext)) {
-			execFileSync("brotli", ["-9", f]);
-			execAWS(
-				`s3 cp ${f}.br ${fd} ${COMPRESS_OPTS} --content-type ${type}`
-			);
-		} else {
-			execAWS(`s3 cp ${f} ${fd} ${S3_OPTS} --content-type ${type}`);
+const deploy = async ({ opts, logger }: CLICtx, name: string) => {
+	const PKG = readJSON(resolve(`examples/${name}/package.json`), logger);
+	if (PKG["thi.ng"]?.online === false) {
+		logger.warn("example marked as offline-only, skipping...");
+		return;
+	}
+
+	const BUILD = `examples/${name}/dist/`;
+	const DEST_DIR = `${DEST_BASE}/${name}`;
+
+	execFileSync(
+		"find",
+		`examples/${name} -type f -name '*.DS_Store' -ls -delete`.split(" ")
+	);
+
+	const uploadAssets = (dir: string, opts?: Partial<UploadOpts>) => {
+		const root = `${BUILD}${dir}`;
+		if (!existsSync(root)) return;
+		opts = { ext: "", compress: true, depth: Infinity, ...opts };
+		for (let f of files(root, opts.ext!, opts.depth)) {
+			const fd = `${S3_BUCKET_EXAMPLES}${DEST_DIR}/${f
+				.replace(BUILD, "")
+				.substring(dir === "" ? 1 : 0)}`;
+			const ext = f.substring(f.lastIndexOf(".") + 1);
+			const type = preferredType(ext);
+			logger.debug(f, "->", fd, type);
+			opts.process && opts.process(f);
+			if (opts.compress && !NEVER_COMPRESS.has(ext)) {
+				execFileSync("brotli", ["-9", f]);
+				execAWS(
+					`s3 cp ${f}.br ${fd} ${COMPRESS_OPTS} --content-type ${type}`,
+					logger
+				);
+			} else {
+				execAWS(
+					`s3 cp ${f} ${fd} ${S3_OPTS} --content-type ${type}`,
+					logger
+				);
+			}
 		}
+	};
+
+	uploadAssets("assets");
+
+	uploadAssets("js", { ext: ".js", depth: 2 });
+	uploadAssets("", { ext: ".js", depth: 1 });
+	uploadAssets("", { ext: ".html" });
+
+	if (!opts.noInvalidate) {
+		logger.info("invaliding", DEST_DIR);
+		execAWS(
+			`cloudfront create-invalidation --distribution-id ${CF_DISTRO_EXAMPLES} --paths ${DEST_DIR}/* ${AWS_PROFILE}`,
+			logger
+		);
 	}
 };
 
-// const interpolateFile = (tpl: any) => (src: string) => {
-//     let body = fs.readFileSync(src, "utf-8");
-//     body = body.replace(/\{\{(\w+)\}\}/g, (_, id) => tpl[id]);
-//     fs.writeFileSync(src, body);
-// };
-
-// const include = (id: string) => args.has(id) || args.has("all");
-
-uploadAssets("assets");
-
-uploadAssets("js", { ext: ".js", depth: 2 });
-uploadAssets("", { ext: ".js", depth: 1 });
-uploadAssets("", { ext: ".html" });
-
-if (!args.has("no-invalidate")) {
-	LOGGER.info("invaliding", DEST_DIR);
-	execAWS(
-		`cloudfront create-invalidation --distribution-id ${CF_DISTRO_EXAMPLES} --paths ${DEST_DIR}/* ${AWS_PROFILE}`
-	);
-}
-
-LOGGER.debug("done");
+cliApp<CLIOpts, CLICtx>({
+	opts: {
+		noInvalidate: flag({
+			desc: "Don't create a CDN invalidation for the example(s)",
+		}),
+	},
+	commands: {
+		default: {
+			desc: "Deploy example(s) to CDN",
+			fn: async (ctx) => {
+				let inputs = ctx.inputs;
+				let isAll = !inputs.length;
+				if (isAll) {
+					inputs = [...map(basename, dirs("examples", "", 1))];
+					ctx.opts.noInvalidate = true;
+				}
+				for (let name of inputs) {
+					ctx.logger.info("--------------------------------");
+					ctx.logger.info(name);
+					try {
+						await deploy(ctx, name);
+					} catch (e) {
+						ctx.logger.severe(
+							"error deploying:",
+							name,
+							(<Error>e).message
+						);
+					}
+				}
+				if (isAll) {
+					ctx.logger.info("invaliding all");
+					execAWS(
+						`cloudfront create-invalidation --distribution-id ${CF_DISTRO_EXAMPLES} --paths ${DEST_BASE}/* ${AWS_PROFILE}`,
+						ctx.logger
+					);
+				}
+				ctx.logger.info("done");
+			},
+			opts: {},
+		},
+	},
+	single: true,
+	name: "deploy-example",
+	ctx: async (ctx) => ctx,
+	usage: {
+		prefix: "Usage: deploy-example [OPTS] [NAME...]",
+		paramWidth: 20,
+	},
+});
