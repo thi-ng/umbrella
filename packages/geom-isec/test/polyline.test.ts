48647be0a373f39bc2883d3c48f448780d1cfeba
Karsten Schmidt <k@thi.ng>
2024-03-20 21:30:57 +0100

    test(geom-isec): update tests (add alpha/beta)

--- packages/geom-isec/test/polyline.test.ts
+++ packages/geom-isec/test/polyline.test.ts
@@ -16,6 +16,8 @@ test("ray (x)", () => {
 	expect(intersectRayPolylineAll([-50, 25], [1, 0], pts, false)).toEqual({
 		type: IntersectionType.INTERSECT,
 		isec: [[100, 25]],
+		alpha: 150,
+		beta: 150,
 	});
 	expect(intersectRayPolylineAll([-50, 25], [1, 0], pts, true)).toEqual({
 		type: IntersectionType.INTERSECT,
@@ -23,6 +25,8 @@ test("ray (x)", () => {
 			[0, 25],
 			[100, 25],
 		],
+		alpha: 50,
+		beta: 150,
 	});
 });
 
@@ -33,6 +37,8 @@ test("ray (y)", () => {
 			[50, 0],
 			[50, 75],
 		],
+		alpha: 50,
+		beta: 125,
 	});
 	expect(intersectRayPolylineAll([50, -50], [0, 1], pts, true)).toEqual({
 		type: IntersectionType.INTERSECT,
@@ -40,6 +46,8 @@ test("ray (y)", () => {
 			[50, 0],
 			[50, 75],
 		],
+		alpha: 50,
+		beta: 125,
 	});
 });
 
@@ -50,10 +58,14 @@ test("line (x)", () => {
 	expect(intersectLinePolylineAll([-50, 25], [110, 25], pts, false)).toEqual({
 		type: IntersectionType.INTERSECT,
 		isec: [[100, 25]],
+		alpha: 150,
+		beta: 150,
 	});
 	expect(intersectLinePolylineAll([-50, 25], [50, 25], pts, true)).toEqual({
 		type: IntersectionType.INTERSECT,
 		isec: [[0, 25]],
+		alpha: 50,
+		beta: 50,
 	});
 	expect(intersectLinePolylineAll([-50, 25], [110, 25], pts, true)).toEqual({
 		type: IntersectionType.INTERSECT,
@@ -61,6 +73,8 @@ test("line (x)", () => {
 			[0, 25],
 			[100, 25],
 		],
+		alpha: 50,
+		beta: 150,
 	});
 });
 
@@ -71,6 +85,8 @@ test("line (y)", () => {
 	expect(intersectLinePolylineAll([50, -25], [50, 50], pts, false)).toEqual({
 		type: IntersectionType.INTERSECT,
 		isec: [[50, 0]],
+		alpha: 25,
+		beta: 25,
 	});
 	expect(intersectLinePolylineAll([50, -25], [50, 100], pts, false)).toEqual({
 		type: IntersectionType.INTERSECT,
@@ -78,6 +94,8 @@ test("line (y)", () => {
 			[50, 0],
 			[50, 75],
 		],
+		alpha: 25,
+		beta: 100,
 	});
 });
 
@@ -90,11 +108,15 @@ test("ray minD/maxD", () => {
 				[0, 25],
 				[100, 25],
 			],
+			alpha: -50,
+			beta: 50,
 		}
 	);
 	expect(intersectRayPolylineAll([-50, 25], [1, 0], pts, true, 60)).toEqual({
 		type: IntersectionType.INTERSECT,
 		isec: [[100, 25]],
+		alpha: 150,
+		beta: 150,
 	});
 	expect(intersectRayPolylineAll([50, 25], [1, 0], pts, true, 0, 10)).toEqual(
 		{
