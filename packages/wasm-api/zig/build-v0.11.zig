99a1916e9d30f605d006abe9b91aab0b97be0a97
Karsten Schmidt <k@thi.ng>
2024-04-21 23:28:30 +0200

    feat(wasm-api): update build script for zig v0.12.0 (incomplete)
    - store existing script as build-v0.11.zig

--- packages/wasm-api/zig/build-v0.11.zig
+++ packages/wasm-api/zig/build-v0.11.zig
@@ -50,58 +50,55 @@ const wasmbind = "wasm-api-bindgen";
 /// Creates and returns a build step to build a dynamic WASM library, configured
 /// with given options and package declarations. The given package specs will be inserted
 pub fn wasmLib(b: *Build, opts: WasmLibOpts) *Build.Step.Compile {
-    const optimize = if (opts.optimize) |m| m else b.standardOptimizeOption(.{});
-    const lib = b.addExecutable(.{
+    const lib = b.addSharedLibrary(.{
         .name = "main",
         .root_source_file = .{ .path = opts.root },
-        .target = b.resolveTargetQuery(
-            .{
-                .cpu_arch = .wasm32,
-                .os_tag = .freestanding,
-                .cpu_features_add = if (opts.features) |features| std.Target.wasm.featureSet(features) else std.Target.Cpu.Feature.Set.empty,
-            },
-        ),
-        .optimize = optimize,
-        .strip = (optimize == .ReleaseSmall or optimize == .ReleaseFast),
+        .target = .{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
+            .cpu_features_add = if (opts.features) |features| std.Target.wasm.featureSet(features) else std.Target.Cpu.Feature.Set.empty,
+        },
+        .optimize = if (opts.optimize) |m| m else b.standardOptimizeOption(.{}),
     });
+    if (lib.optimize == .ReleaseSmall or lib.optimize == .ReleaseFast) lib.strip = true;
 
     // build flags
-    // https://github.com/ziglang/zig/pull/17815
-    lib.entry = .disabled;
+    lib.rdynamic = true;
     lib.import_symbols = true;
     lib.initial_memory = opts.initialMemory;
     lib.max_memory = opts.maxMemory;
 
     // add default dependencies
-    // https://ziggit.dev/t/what-happened-to-addmodule/2908/3
-    _ = lib.root_module.addImport(
+    lib.addModule(
         wasmapi,
         b.createModule(.{
-            .root_source_file = modulePath(b.allocator, opts.base, "@thi.ng/wasm-api/zig/lib.zig"),
+            .source_file = modulePath(b.allocator, opts.base, "@thi.ng/wasm-api/zig/lib.zig"),
         }),
     );
-    _ = lib.root_module.addImport(
+    lib.addModule(
         wasmbind,
         b.createModule(.{
-            .root_source_file = modulePath(b.allocator, opts.base, "@thi.ng/wasm-api-bindgen/zig/lib.zig"),
+            .source_file = modulePath(b.allocator, opts.base, "@thi.ng/wasm-api-bindgen/zig/lib.zig"),
         }),
     );
     if (opts.modules) |modules| {
-        for (modules) |mod| register(lib, mod, opts);
+        for (modules) |mod| {
+            register(lib, mod, opts);
+        }
     }
     return lib;
 }
 
 /// Registers a single package and its deps (also injects core wasm-api deps)
-pub fn register(lib: *Build.Step.Compile, mod: ModuleSpec, opts: WasmLibOpts) void {
+pub fn register(step: *Build.Step.Compile, mod: ModuleSpec, opts: WasmLibOpts) void {
     const num = if (mod.dependencies) |ids| ids.len else 0;
-    var dpkgs = lib.step.owner.allocator.alloc(Build.Module.Import, num + 2) catch unreachable;
-    dpkgs[0] = if (lib.root_module.import_table.get(wasmapi)) |m| .{ .name = wasmapi, .module = m } else unreachable;
-    dpkgs[1] = if (lib.root_module.import_table.get(wasmbind)) |m| .{ .name = wasmbind, .module = m } else unreachable;
+    var dpkgs = step.step.owner.allocator.alloc(Build.ModuleDependency, num + 2) catch unreachable;
+    dpkgs[0] = if (step.modules.get(wasmapi)) |m| .{ .name = wasmapi, .module = m } else unreachable;
+    dpkgs[1] = if (step.modules.get(wasmbind)) |m| .{ .name = wasmbind, .module = m } else unreachable;
     var i: usize = 2;
     if (mod.dependencies) |ids| {
         for (ids) |id| {
-            if (lib.root_module.import_table.get(id)) |m| {
+            if (step.modules.get(id)) |m| {
                 if (isDuplicateId(dpkgs[0..i], id)) {
                     std.log.info("ignoring duplicate dependency: {s}", .{id});
                     continue;
@@ -111,19 +108,19 @@ pub fn register(lib: *Build.Step.Compile, mod: ModuleSpec, opts: WasmLibOpts) vo
             } else @panic("unknown dependency");
         }
     }
-    _ = lib.root_module.addImport(mod.name, lib.step.owner.createModule(.{
-        .root_source_file = modulePath(lib.step.owner.allocator, opts.base, mod.path),
-        .imports = dpkgs[0..i],
+    step.addModule(mod.name, step.step.owner.createModule(.{
+        .source_file = modulePath(step.step.owner.allocator, opts.base, mod.path),
+        .dependencies = dpkgs[0..i],
     }));
 }
 
-fn isDuplicateId(deps: []Build.Module.Import, name: []const u8) bool {
+fn isDuplicateId(deps: []Build.ModuleDependency, name: []const u8) bool {
     for (deps) |d| {
         if (std.mem.eql(u8, d.name, name)) return true;
     }
     return false;
 }
 
-fn modulePath(allocator: std.mem.Allocator, base: []const u8, path: []const u8) Build.LazyPath {
+fn modulePath(allocator: std.mem.Allocator, base: []const u8, path: []const u8) Build.FileSource {
     return .{ .path = std.fs.path.join(allocator, &.{ base, path }) catch unreachable };
 }
