06b4ffc668fa3df80ee46b9ffa37784baf853006
Karsten Schmidt <k@thi.ng>
2024-04-22 15:43:22 +0200

    feat(file-io): add deleteDir()

--- packages/file-io/src/delete.ts
+++ packages/file-io/src/delete.ts
@@ -1,5 +1,5 @@
 import type { ILogger } from "@thi.ng/logger";
-import { unlinkSync } from "node:fs";
+import { rmSync, unlinkSync } from "node:fs";
 
 /**
  * Deletes file at given path. If `dryRun` is true (default: false), the file
@@ -14,4 +14,18 @@ export const deleteFile = (path: string, logger?: ILogger, dryRun = false) => {
 	logger && logger.info(`${dryRun ? "[dryrun] " : ""}deleting file: ${path}`);
 	if (dryRun) return;
 	unlinkSync(path);
+};
+
+/**
+ * Like {@link deleteFile}, but attempts to recursively remove an entire
+ * directory at given path.
+ *
+ * @param path
+ * @param logger
+ * @param dryRun
+ */
+export const deleteDir = (path: string, logger?: ILogger, dryRun = false) => {
+	logger && logger.info(`${dryRun ? "[dryrun] " : ""}deleting file: ${path}`);
+	if (dryRun) return;
+	rmSync(path, { recursive: true, force: true });
 };
