46d809394050c55b71282ba8ae587ee16b927096
Karsten Schmidt <k@thi.ng>
2024-05-08 16:10:35 +0200

    feat(examples): update convex hull example

--- examples/geom-convex-hull/src/index.ts
+++ examples/geom-convex-hull/src/index.ts
@@ -1,17 +1,21 @@
+import { canvas2d } from "@thi.ng/canvas";
 import {
 	area,
+	asPath,
 	centroid,
 	clipConvex,
 	convexHull,
+	group,
 	points,
 	polygon,
-	rect,
+	rectFromMinMaxWithMargin,
 	scatter,
+	text,
+	vertices,
 	withAttribs,
 } from "@thi.ng/geom";
-import type { IShape } from "@thi.ng/geom-api";
-import { renderOnce } from "@thi.ng/hdom";
-import { canvas } from "@thi.ng/hdom-canvas";
+import type { IHiccupShape, IShape } from "@thi.ng/geom-api";
+import { draw } from "@thi.ng/hiccup-canvas";
 
 // refactored version of an example by Pete Cory
 // http://www.petecorey.com/blog/2019/07/29/clipping-convex-hulls-with-thing/
@@ -19,44 +23,47 @@ import { canvas } from "@thi.ng/hdom-canvas";
 const width = 600;
 const height = 600;
 
-const bounds = rect([width, height]);
+const bounds = rectFromMinMaxWithMargin([0, 0], [width, height], -50);
 
-// generate random points within given shape
-const pts1 = scatter(bounds, 5)!;
-const pts2 = scatter(bounds, 5)!;
+// generate random points, compute convex hull and convert to path
+const hull1 = asPath(convexHull(polygon(scatter(bounds, 5)!)), {
+	breakPoints: true,
+});
+// this hull will be used as clipping polygon and MUST be convex
+// (blue shape in the visualization)
+const hull2 = convexHull(polygon(scatter(bounds, 5)!));
 
-// complex hull for shape
-const hull1 = convexHull(polygon(pts1));
-const hull2 = convexHull(polygon(pts2));
+// sample boundaries at uniform distance
+const pts1 = vertices(hull1, { dist: 50 });
+const pts2 = vertices(hull2, { dist: 50 });
 
-// Sutherland-Hodgman
+// apply Sutherland-Hodgman clipping
 const clip = clipConvex(hull1, hull2)!;
 
 const COL1 = (a = 1) => `rgba(245,93,62,${a})`;
 const COL2 = (a = 1) => `rgba(118,190,208,${a})`;
 const COL3 = (a = 1) => `rgba(102,102,102,${a})`;
 
-// render shapes with thi.ng/hdom & thi.ng/hdom-canvas
-renderOnce([
-	canvas,
-	{ width, height },
-	points(pts1, { shape: "circle", fill: COL1(), size: 5 }),
-	points(pts2, { shape: "circle", fill: COL2(), size: 5 }),
-	withAttribs(hull1, { fill: COL1(0.5) }),
-	withAttribs(hull2, { fill: COL2(0.5) }),
-	withAttribs(clip, {
-		fill: COL3(0.5),
-		stroke: COL3(),
-		weight: 3,
-	}),
-	...(<[IShape, string][]>[
-		[hull1, COL1()],
-		[hull2, COL2()],
-		[clip, COL3()],
-	]).map(([shape, col]) => [
-		"text",
-		{ fill: col },
-		centroid(shape),
-		area(shape).toFixed(2),
-	]),
-]);
+// render shapes with thi.ng/hiccup-canvas
+const { ctx } = canvas2d(width, height, document.getElementById("app")!);
+draw(
+	ctx,
+	group({ __background: "#f0f0f0" }, <IHiccupShape[]>[
+		points(pts1, { shape: "circle", fill: COL1(), size: 4 }),
+		points(pts2, { shape: "circle", fill: COL2(), size: 4 }),
+		withAttribs(hull1, { fill: COL1(0.5) }),
+		withAttribs(hull2, { fill: COL2(0.5) }),
+		withAttribs(clip, {
+			fill: COL3(0.5),
+			stroke: COL3(),
+			weight: 3,
+		}),
+		// text labels (shape areas)
+		...(<[IShape, string][]>[
+			[hull2, COL2()],
+			[clip, COL3()],
+		]).map(([shape, col]) =>
+			text(centroid(shape)!, area(shape) | 0, { fill: col })
+		),
+	])
+);
