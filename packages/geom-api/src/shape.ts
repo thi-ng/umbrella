2325ee8050c2b8f99a5d73da1ab1689e296a0b21
Karsten Schmidt <k@thi.ng>
2024-05-10 12:34:09 +0200

    feat(geom-api): add/update/rename types, more specific 2d/3d versions
    - add IShape2/3
    - add IHiccupShape2/3
    - add PathSegment2/3, HiccupPathSegment
    - add SegmentType2/3
    - update PCLikeConstructor, add generics

--- packages/geom-api/src/shape.ts
+++ packages/geom-api/src/shape.ts
@@ -15,6 +15,14 @@ export interface IShape<T extends IShape = IShape<any>>
 	readonly type: number | string;
 }
 
+export type IShape2<T extends IShape2 = IShape2<any>> = IShape<T> & {
+	readonly dim: 2;
+};
+
+export type IShape3<T extends IShape3 = IShape3<any>> = IShape<T> & {
+	readonly dim: 3;
+};
+
 export interface AABBLike extends IShape<AABBLike> {
 	pos: Vec;
 	size: Vec;
@@ -32,8 +40,14 @@ export interface PCLike extends IShape<PCLike> {
 	points: Vec[];
 }
 
-export interface PCLikeConstructor {
-	new (pts: Vec[], attribs?: Attribs): PCLike;
+export interface PCLikeConstructor<T extends PCLike = PCLike> {
+	new (pts: Vec[], attribs?: Attribs): T;
 }
 
 export interface IHiccupShape extends IShape, IToHiccup {}
+
+export type IHiccupShape2<T extends IHiccupShape2 = IHiccupShape2<any>> =
+	IHiccupShape & IShape2<T>;
+
+export type IHiccupShape3<T extends IHiccupShape3 = IHiccupShape3<any>> =
+	IHiccupShape & IShape3<T>;
