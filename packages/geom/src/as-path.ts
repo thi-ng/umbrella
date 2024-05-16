9b4df2e0c70f04f052fda999b43c90bd09b23d6b
Karsten Schmidt <k@thi.ng>
2024-05-16 21:46:08 +0200

    feat(geom): add/update asCubic()/asPath() impls/types/signatures
    BREAKING CHANGE: update asCubic/asPath() to use new CubicOpts
    
    - add support for more shape types, incl. 3D

--- packages/geom/src/as-path.ts
+++ packages/geom/src/as-path.ts
@@ -1,22 +1,39 @@
 import type { Maybe } from "@thi.ng/api";
-import type { MultiFn2O } from "@thi.ng/defmulti";
+import type { MultiFn1O } from "@thi.ng/defmulti";
 import { DEFAULT, defmulti } from "@thi.ng/defmulti/defmulti";
-import type { Attribs, CubicOpts, IShape, PathSegment } from "@thi.ng/geom-api";
+import type {
+	CubicOpts,
+	IShape,
+	IShape2,
+	IShape3,
+	PCLikeConstructor,
+	PathSegment,
+	PathSegment2,
+	PathSegment3,
+} from "@thi.ng/geom-api";
 import type { ReadonlyVec } from "@thi.ng/vectors";
 import { copy } from "@thi.ng/vectors/copy";
 import type { APC } from "./api/apc.js";
 import type { Arc } from "./api/arc.js";
 import type { ComplexPolygon } from "./api/complex-polygon.js";
 import { Line } from "./api/line.js";
+import { Line3 } from "./api/line3.js";
 import { Path } from "./api/path.js";
+import { Path3 } from "./api/path3.js";
 import type { Polygon } from "./api/polygon.js";
-import type { Polyline } from "./api/polyline.js";
+import { Polyline } from "./api/polyline.js";
 import { asCubic } from "./as-cubic.js";
 import { asPolygon } from "./as-polygon.js";
 import { asPolyline } from "./as-polyline.js";
-import { __copyAttribs } from "./internal/copy.js";
+import { __copyAttribs, __copyAttribsRaw } from "./internal/copy.js";
 import { __dispatch } from "./internal/dispatch.js";
 import { pathFromCubics } from "./path.js";
+import { pathFromCubics3 } from "./path3.js";
+
+export type AsPathFn = {
+	(shape: IShape2, opts?: Partial<AsPathOpts>): Path;
+	(shape: IShape3, opts?: Partial<AsPathOpts>): Path3;
+} & MultiFn1O<IShape, Partial<AsPathOpts>, Path | Path3>;
 
 export interface AsPathOpts extends CubicOpts {
 	/**
@@ -37,98 +54,128 @@ export interface AsPathOpts extends CubicOpts {
  * {@link asPolyline} with default opts.
  *
  * @param src
- * @param attribs
+ * @param opts
  */
-export const asPath: ((
-	shape: IShape,
-	opts?: Partial<AsPathOpts>,
-	attribs?: Attribs
-) => Path) &
-	MultiFn2O<IShape, Maybe<Partial<AsPathOpts>>, Attribs, Path> = defmulti<
-	any,
-	Maybe<Partial<AsPathOpts>>,
-	Maybe<Attribs>,
-	Path
->(
-	__dispatch,
-	{
-		line: "polyline",
-		quad: "poly",
-		tri: "poly",
-	},
-	{
-		[DEFAULT]: (
-			src: IShape,
-			opts?: Partial<AsPathOpts>,
-			attribs?: Attribs
-		) =>
-			opts?.linear
-				? asPath(asPolygon(src)[0], opts, attribs || __copyAttribs(src))
-				: __defaultImpl(src, opts, attribs),
-
-		arc: ($: Arc, opts, attribs) =>
-			opts?.linear
-				? asPath(asPolyline($)[0], opts, attribs || __copyAttribs($))
-				: __defaultImpl($, opts, attribs),
-
-		complexpoly: ($: ComplexPolygon, opts, attribs) => {
-			attribs = attribs || __copyAttribs($);
-			if (opts?.linear) {
-				return __linearPath(
-					$.boundary,
-					$.children.map((c) => __lineSegments(c.points, true)),
-					attribs,
-					true
-				);
-			}
-			const res = pathFromCubics(asCubic($.boundary, opts), attribs);
-			for (let child of $.children) {
-				res.addSubPaths(pathFromCubics(asCubic(child, opts)).segments);
-			}
-			return res;
+export const asPath = <AsPathFn>(
+	defmulti<any, Maybe<Partial<AsPathOpts>>, Path | Path3>(
+		__dispatch,
+		{
+			line: "polyline",
+			quad: "poly",
+			tri: "poly",
 		},
+		{
+			[DEFAULT]: (src: IShape2, opts?: Partial<AsPathOpts>) =>
+				opts?.linear
+					? asPath(asPolygon(src)[0], opts)
+					: __defaultImpl(src, opts),
 
-		poly: ($: Polygon, opts, attribs) =>
-			opts?.linear
-				? __linearPath($, [], attribs, true)
-				: __defaultImpl($, opts, attribs),
+			arc: ($: Arc, opts) =>
+				opts?.linear
+					? asPath(asPolyline($)[0], opts)
+					: __defaultImpl($, opts),
 
-		polyline: ($: Polyline, opts, attribs) =>
-			opts?.linear
-				? __linearPath($, [], attribs, false)
-				: __defaultImpl($, opts, attribs),
-	}
+			complexpoly: (
+				{ boundary, children, attribs }: ComplexPolygon,
+				opts
+			) => {
+				attribs = __copyAttribsRaw(attribs);
+				if (opts?.linear) {
+					return __linearPath(
+						boundary,
+						children.map((c) =>
+							__lineSegments(Line, c.points, true)
+						),
+						true
+					);
+				}
+				const res = pathFromCubics(asCubic(boundary, opts), attribs);
+				for (let child of children) {
+					res.addSubPaths(
+						pathFromCubics(asCubic(child, opts)).segments
+					);
+				}
+				return res;
+			},
+
+			poly: ($: Polygon, opts) =>
+				opts?.linear
+					? __linearPath($, [], true)
+					: __defaultImpl($, opts),
+
+			polyline: ($: Polyline, opts) =>
+				opts?.linear
+					? __linearPath($, [], false)
+					: __defaultImpl($, opts),
+		}
+	)
 );
 
-/** @internal */
-const __defaultImpl = (
-	src: IShape,
-	opts?: Partial<CubicOpts>,
-	attribs?: Attribs
-) => pathFromCubics(asCubic(src, opts), attribs || __copyAttribs(src));
+/**
+ * TODO update to support Path3
+ *
+ * @internal
+ */
+const __defaultImpl = (src: IShape, opts?: Partial<CubicOpts>) =>
+	(src.dim === 2 ? pathFromCubics : pathFromCubics3)(
+		<any>asCubic(src, { attribs: false, ...opts }),
+		__copyAttribs(src)
+	);
 
-/** @internal */
-const __lineSegments = (points: ReadonlyVec[], closed: boolean) => {
-	if (!points.length) return [];
+/**
+ * TODO update to support Path3
+ *
+ * @internal
+ */
+function __lineSegments(
+	ctor: PCLikeConstructor<Line>,
+	points: ReadonlyVec[],
+	closed: boolean
+): PathSegment2[];
+function __lineSegments(
+	ctor: PCLikeConstructor<Line3>,
+	points: ReadonlyVec[],
+	closed: boolean
+): PathSegment3[];
+function __lineSegments(
+	ctor: PCLikeConstructor<any>,
+	points: ReadonlyVec[],
+	closed: boolean
+): PathSegment[] {
+	const n = points.length;
+	if (!n) return [];
 	const segments: PathSegment[] = [{ type: "m", point: copy(points[0]) }];
-	for (let i = 1, n = points.length; i < n; i++)
+	for (let i = 1; i < n; i++)
 		segments.push({
 			type: "l",
-			geo: new Line([copy(points[i - 1]), copy(points[i])]),
+			geo: new ctor([copy(points[i - 1]), copy(points[i])]),
 		});
-	if (closed) segments.push({ type: "z" });
+	if (closed) {
+		segments.push(
+			{
+				type: "l",
+				geo: new ctor([copy(points[n - 1]), copy(points[0])]),
+			},
+			{ type: "z" }
+		);
+	}
 	return segments;
-};
+}
 
-/** @internal */
-const __linearPath = (
-	shape: APC,
-	subPaths: PathSegment[][],
-	attribs?: Attribs,
-	closed = false
-) =>
-	new Path(
-		__lineSegments(shape.points, closed),
-		subPaths,
-		attribs || __copyAttribs(shape)
-	);
+/**
+ * TODO update to support Path3
+ *
+ * @internal
+ */
+const __linearPath = (shape: APC, subPaths: PathSegment[][], closed = false) =>
+	shape.dim === 2
+		? new Path(
+				__lineSegments(Line, shape.points, closed),
+				<PathSegment2[][]>subPaths,
+				__copyAttribs(shape)
+		  )
+		: new Path3(
+				__lineSegments(Line3, shape.points, closed),
+				<PathSegment3[][]>subPaths,
+				__copyAttribs(shape)
+		  );
