5fe2f78620fcee5ed491a6a94eb0d85493f0eb29
Karsten Schmidt <k@thi.ng>
2024-05-23 13:05:37 +0200

    feat(units): add DPI constants/presets

--- packages/units/src/constants/paper-sizes.ts
+++ packages/units/src/constants/paper-sizes.ts
@@ -72,3 +72,10 @@ export const US_ARCH_E_LANDSCAPE = $(US_ARCH_E);
 export const US_ARCH_E1_LANDSCAPE = $(US_ARCH_E1);
 export const US_ARCH_E2_LANDSCAPE = $(US_ARCH_E2);
 export const US_ARCH_E3_LANDSCAPE = $(US_ARCH_E3);
+
+// screen/print densities
+
+export const DPI_72 = quantity(72, "dpi");
+export const DPI_150 = quantity(150, "dpi");
+export const DPI_300 = quantity(300, "dpi");
+export const DPI_600 = quantity(600, "dpi");
