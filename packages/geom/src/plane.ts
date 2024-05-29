c44d8da167179727444e8d963bc8549d4353e0fc
Karsten Schmidt <k@thi.ng>
2024-05-29 13:39:59 +0200

    feat(geom): add planeFromRay()

--- packages/geom/src/plane.ts
+++ packages/geom/src/plane.ts
@@ -3,7 +3,9 @@ import type { ReadonlyVec, Vec } from "@thi.ng/vectors";
 import { dot3 } from "@thi.ng/vectors/dot";
 import { normalize3 } from "@thi.ng/vectors/normalize";
 import { orthoNormal3 } from "@thi.ng/vectors/ortho-normal";
+import { set3 } from "@thi.ng/vectors/set";
 import { Plane } from "./api/plane.js";
+import type { Ray3 } from "./api/ray3.js";
 
 export const plane = (normal: Vec, w: number, attribs?: Attribs) =>
 	new Plane(normalize3(null, normal), w, attribs);
@@ -16,6 +18,17 @@ export const planeWithPoint = (
 	normal = normalize3(null, normal);
 	return new Plane(normal, dot3(normal, p), attribs);
 };
+
+/**
+ * Creates a new plane from the given ray, using the ray's position as point on
+ * the plane and the ray's direction as plane normal. If `attribs` are given,
+ * they take precedence over the ray's attribs.
+ *
+ * @param ray
+ * @param attribs
+ */
+export const planeFromRay = ({ pos, dir, attribs }: Ray3, $attribs?: Attribs) =>
+	new Plane(set3([], dir), dot3(dir, pos), $attribs || attribs);
 
 export const planeFrom3Points = (
 	a: ReadonlyVec,
