03c5e54a8f700b8ad04d1b2869363b5d42a3edf7
Karsten Schmidt <k@thi.ng>
2024-05-30 23:51:15 +0200

    feat(geom-subdiv-curve): add support for passing multiple kernels to subdivide()
    - update args to support multiple kernels as array

--- packages/geom-subdiv-curve/src/subdivide.ts
+++ packages/geom-subdiv-curve/src/subdivide.ts
@@ -1,24 +1,36 @@
 import type { SubdivKernel } from "@thi.ng/geom-api";
+import { isArray } from "@thi.ng/checks/is-array";
 import { comp } from "@thi.ng/transducers/comp";
 import { mapcatIndexed } from "@thi.ng/transducers/mapcat-indexed";
 import { partition } from "@thi.ng/transducers/partition";
 import { push } from "@thi.ng/transducers/push";
 import { transduce } from "@thi.ng/transducers/transduce";
 import type { ReadonlyVec, Vec } from "@thi.ng/vectors";
+import { repeat } from "@thi.ng/transducers/repeat";
 
 /**
  * http://algorithmicbotany.org/papers/subgpu.sig2003.pdf
  *
- * @param kernel - subdivision scheme
+ * @param kernel - subdivision scheme(s)
  * @param pts - source points
  * @param iter - number of iterations
  */
-export const subdivide = (
+export function subdivide(
 	pts: ReadonlyVec[],
-	{ fn, pre, size }: SubdivKernel,
+	kernel: SubdivKernel,
+	iter?: number
+): ReadonlyVec[];
+export function subdivide(
+	pts: ReadonlyVec[],
+	kernels: SubdivKernel[]
+): ReadonlyVec[];
+export function subdivide(
+	pts: ReadonlyVec[],
+	kernels: SubdivKernel | SubdivKernel[],
 	iter = 1
-) => {
-	while (iter-- > 0) {
+) {
+	kernels = isArray(kernels) ? kernels : [...repeat(kernels, iter)];
+	for (let { fn, pre, size } of kernels) {
 		const nump = pts.length;
 		pts = transduce<ReadonlyVec, ReadonlyVec, Vec[]>(
 			comp(
@@ -30,4 +42,4 @@ export const subdivide = (
 		);
 	}
 	return pts;
-};
+}
