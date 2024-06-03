f76c5bdbc41213ca9186c7b5219f39bfab43e492
Karsten Schmidt <k@thi.ng>
2024-06-03 22:56:54 +0200

    feat(geom-api): add GroupAttribs, add docs

--- packages/geom-api/src/shape.ts
+++ packages/geom-api/src/shape.ts
@@ -3,18 +3,78 @@ import type { ReadonlyVec, Vec } from "@thi.ng/vectors";
 import type { SamplingOpts } from "./sample.js";
 
 export interface Attribs {
+	/**
+	 * Shape resampling options/resolution.
+	 */
 	__samples?: SamplingOpts | number;
+	/**
+	 * Control attribute to define the number of fractional digits for numeric
+	 * values in the serialized SVG string.
+	 *
+	 * @defaultValue 3
+	 */
+	__prec?: number;
+	/**
+	 * 2D rotation angle (in radians)
+	 */
 	rotate?: number;
+	/**
+	 * 3D X rotation angle (in radians)
+	 */
 	rotateX?: number;
+	/**
+	 * 3D Y rotation angle (in radians)
+	 */
 	rotateY?: number;
+	/**
+	 * 3D Z rotation angle (in radians)
+	 */
 	rotateZ?: number;
+	/**
+	 * Uniform or non-uniform scale factor.
+	 */
 	scale?: number | ReadonlyVec;
+	/**
+	 * Translation offset.
+	 */
 	translate?: ReadonlyVec;
+	/**
+	 * Transformation matrix (2x3 for 2D, 4x4 for 3D).
+	 */
 	transform?: ReadonlyVec;
+	/**
+	 * Fill color
+	 */
 	fill?: any;
+	/**
+	 * Stroke color
+	 */
 	stroke?: any;
+	/**
+	 * Stroke weight
+	 */
 	weight?: number;
 	[id: string]: any;
+}
+
+export interface GroupAttribs extends Attribs {
+	/**
+	 * Currently only used by thi.ng/hiccup-canvas. Canvas background color,
+	 * should only should be used for a root group.
+	 */
+	__background?: any;
+	/**
+	 * Only used by thi.ng/hiccup-canvas. Force clearing of the canvas before
+	 * drawing. Takes priority over {@link GroupAttribs.__background} and should
+	 * only should be used for a root group.
+	 *
+	 * @defaultValue false
+	 */
+	__clear?: boolean;
+	/**
+	 * Only used for SVG serialization. Inkscape payer name
+	 */
+	__inkscapeLayer?: string;
 }
 
 export interface IAttributed<T> {
