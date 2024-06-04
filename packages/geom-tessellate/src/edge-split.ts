91480c7aadb76ac2786336168f3433b4a1c680d8
Karsten Schmidt <k@thi.ng>
2024-06-04 14:01:33 +0200

    feat(geom-tessellate): add edgeSplitWithThreshold() tessellator
    - add docstrings

--- packages/geom-tessellate/src/edge-split.ts
+++ packages/geom-tessellate/src/edge-split.ts
@@ -7,15 +7,23 @@ import { push } from "@thi.ng/transducers/push";
 import { transduce } from "@thi.ng/transducers/transduce";
 import { wrapSides } from "@thi.ng/transducers/wrap-sides";
 import type { ReadonlyVec, Vec } from "@thi.ng/vectors";
-import { mixN } from "@thi.ng/vectors/mixn";
+import { addmN } from "@thi.ng/vectors/addmn";
+import { distSq } from "@thi.ng/vectors/distsq";
 
+/**
+ * Tessellates a polygon into sequence of triangles by splitting each boundary
+ * edge and constructing 2 triangles: `amc`, `mbc`, where `a` and `b` are edge
+ * endpoints, `m` is the edge centroid and `c` the face centroid.
+ *
+ * @param points
+ */
 export const edgeSplit: Tessellator = (points: ReadonlyVec[]) => {
 	const c = centroid(points);
 	return transduce(
 		comp(
 			partition<Vec>(2, 1),
 			mapcat(([a, b]) => {
-				const m = mixN([], a, b, 0.5);
+				const m = addmN([], a, b, 0.5);
 				return [
 					[a, m, c],
 					[m, b, c],
@@ -26,3 +34,39 @@ export const edgeSplit: Tessellator = (points: ReadonlyVec[]) => {
 		wrapSides(points, 0, 1)
 	);
 };
+
+/**
+ * Higher-order version of {@link edgeSplit}, with min distance `threshold`.
+ * Only splits edges (`a` -> `b`) if the edge length and the distance of the end
+ * points to the face centroid `c` is >= `threshold`.
+ *
+ * @param threshold
+ */
+export const edgeSplitWithThreshold =
+	(threshold: number): Tessellator =>
+	(points: ReadonlyVec[]) => {
+		const c = centroid(points);
+		const minD = threshold * threshold;
+		return transduce(
+			comp(
+				partition<Vec>(2, 1),
+				mapcat(([a, b]) => {
+					if (
+						distSq(a, b) >= minD &&
+						distSq(a, c) >= minD &&
+						distSq(b, c) >= minD
+					) {
+						const m = addmN([], a, b, 0.5);
+						return [
+							[a, m, c],
+							[m, b, c],
+						];
+					} else {
+						return [[a, b, c]];
+					}
+				})
+			),
+			push(),
+			wrapSides(points, 0, 1)
+		);
+	};
