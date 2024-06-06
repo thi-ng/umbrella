5b0b2ec4f9051a7eed2ea49c885c1f611b43f567
Karsten Schmidt <k@thi.ng>
2024-06-06 18:08:05 +0200

    feat(geom-api): add Tessellation type, update Tessellator
    BREAKING CHANGE: update Tessellator behavior & signature
    
    - Tessellators now collect/append results to a Tessellation object, consisting of
      a single point array and an array of face vertex IDs

--- packages/geom-api/src/tessel.ts
+++ packages/geom-api/src/tessel.ts
@@ -1,8 +1,20 @@
-import type { ReadonlyVec, Vec } from "@thi.ng/vectors";
+import type { Fn2 } from "@thi.ng/api";
+import type { ReadonlyVec } from "@thi.ng/vectors";
 
-/**
- * Tessellation function. Receives a point array representing a polygon
- * / cell and yields an array of point arrays, representing the
- * subdivided cells.
- */
-export type Tessellator = (points: ReadonlyVec[]) => Vec[][];
+export type Tessellator = Fn2<Tessellation, number[], Tessellation>;
+
+export interface Tessellation {
+	/**
+	 * Points referenced by {@link Tessellation.indices}.
+	 *
+	 * @remarks
+	 * If a {@link Tessellator} is creating new points as part of its
+	 * processing, these are to be appended to this array.
+	 */
+	points: ReadonlyVec[];
+	/**
+	 * Array of per-face point IDs/indices, referencing
+	 * {@link Tessellation.points}.
+	 */
+	indices: number[][];
+}
