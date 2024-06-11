7306b35c7cc14b7dcd6be0855ea85b3726292496
Karsten Schmidt <k@thi.ng>
2024-06-11 14:07:36 +0200

    feat(geom): add internal __ensurePCLike() helper

--- packages/geom/src/internal/pclike.ts
+++ packages/geom/src/internal/pclike.ts
@@ -1,6 +1,13 @@
+// thing:export
+import { isArray } from "@thi.ng/checks/is-array";
 import { assert } from "@thi.ng/errors/assert";
-import type { PCLikeConstructor } from "../api.js";
+import type { IShape, PCLike, PCLikeConstructor } from "../api.js";
 import { __argAttribs } from "./args.js";
+
+export const __ensurePCLike = (x: IShape): PCLike => {
+	assert(isArray((<any>x).points), "expected a PCLike shape");
+	return <PCLike>x;
+};
 
 export const __pclike = (ctor: PCLikeConstructor, args: any[]) => {
 	const attr = __argAttribs(args);
