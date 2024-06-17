25576caa7e1947aa2c28bf3ce24a36c282cc550a
Karsten Schmidt <k@thi.ng>
2024-06-17 17:48:45 +0200

    refactor(geom-subdiv-curve): update subdivide() signature
    BREAKING CHANGE: update subdivide() signature
    
    - only accept kernels as array
    - add `closed` arg/flag

--- packages/geom-subdiv-curve/src/subdivide.ts
+++ packages/geom-subdiv-curve/src/subdivide.ts
@@ -1,45 +1,41 @@
-import { isArray } from "@thi.ng/checks/is-array";
 import { comp } from "@thi.ng/transducers/comp";
 import { mapcatIndexed } from "@thi.ng/transducers/mapcat-indexed";
 import { partition } from "@thi.ng/transducers/partition";
 import { push } from "@thi.ng/transducers/push";
-import { repeat } from "@thi.ng/transducers/repeat";
 import { transduce } from "@thi.ng/transducers/transduce";
 import type { ReadonlyVec, Vec } from "@thi.ng/vectors";
 import type { SubdivKernel } from "./api.js";
 
 /**
- * http://algorithmicbotany.org/papers/subgpu.sig2003.pdf
+ * Takes an array of `points` and an array of subdivision `kernels`. Iterativel
+ * applies kernels in given order (each iteration operating on the result of the
+ * previous iteration). The `closed` flag (default: false) indicates if the
+ * points define a closed or open geometry (i.e. polygon vs. polyline).
  *
- * @param kernel - subdivision scheme(s)
- * @param pts - source points
- * @param iter - number of iterations
+ * @remarks
+ * Reference:
+ * - https://web.archive.org/web/20060816003547/https://algorithmicbotany.org/papers/subgpu.sig2003.pdf
+ * - https://www.csl.mtu.edu/cs3621/common/Subdivision.pdf
+ *
+ * @param points - source points
+ * @param kernels - subdivision scheme(s)
+ * @param closed
  */
 export function subdivide(
-	pts: ReadonlyVec[],
-	kernel: SubdivKernel,
-	iter?: number
-): ReadonlyVec[];
-export function subdivide(
-	pts: ReadonlyVec[],
-	kernels: SubdivKernel[]
-): ReadonlyVec[];
-export function subdivide(
-	pts: ReadonlyVec[],
-	kernels: SubdivKernel | SubdivKernel[],
-	iter = 1
-) {
-	kernels = isArray(kernels) ? kernels : [...repeat(kernels, iter)];
+	points: ReadonlyVec[],
+	kernels: SubdivKernel[],
+	closed = false
+): ReadonlyVec[] {
 	for (let { fn, pre, size } of kernels) {
-		const nump = pts.length;
-		pts = transduce<ReadonlyVec, ReadonlyVec, Vec[]>(
+		const nump = points.length;
+		points = transduce<ReadonlyVec, ReadonlyVec, Vec[]>(
 			comp(
 				partition(size, 1),
-				mapcatIndexed((i, pts) => fn(pts, i, nump))
+				mapcatIndexed((i, pts) => fn(pts, i, nump, closed))
 			),
 			push(),
-			pre ? pre(pts) : pts
+			pre ? pre(points, closed) : points
 		);
 	}
-	return pts;
+	return points;
 }
