2e0c1cae7759e5d4aaf01732f7cb7adae3a3c930
Karsten Schmidt <k@thi.ng>
2024-06-19 21:04:26 +0200

    feat(examples): update geom-classify-point to use canvas vs. SVG
    - update deps

--- examples/geom-classify-point/src/index.ts
+++ examples/geom-classify-point/src/index.ts
@@ -1,10 +1,6 @@
-import {
-	asSvg,
-	circle,
-	classifyPoint,
-	smoothPolygon,
-	svgDoc,
-} from "@thi.ng/geom";
+import { adaptiveCanvas2d } from "@thi.ng/canvas";
+import { circle, classifyPoint, group, smoothPolygon } from "@thi.ng/geom";
+import { draw } from "@thi.ng/hiccup-canvas";
 import { repeatedly } from "@thi.ng/transducers";
 import { randMinMax2 } from "@thi.ng/vectors";
 
@@ -12,22 +8,21 @@ const poly = smoothPolygon(
 	[...repeatedly(() => randMinMax2([], [0, 0], [500, 500]), 8)],
 	{
 		fill: "#ccc",
+		fillRule: "evenodd",
 		stroke: "#000",
 		weight: 3,
-		"fill-rule": "evenodd",
 	},
 	{ mode: "break" }
 );
 
-document.body.innerHTML = asSvg(
-	svgDoc(
-		{ stroke: "none" },
-		poly,
-		...repeatedly(() => {
-			const p = randMinMax2([], [0, 0], [500, 500]);
-			return circle(p, 4, {
-				fill: ["#f00", "#9f0", "#00f"][classifyPoint(poly, p, 5) + 1],
-			});
-		}, 1000)
-	)
-);
+const geo = group({ stroke: "none", scale: window.devicePixelRatio }, [
+	poly,
+	...repeatedly(() => {
+		const p = randMinMax2([], [0, 0], [500, 500]);
+		return circle(p, 3, {
+			fill: ["#f00", "#9f0", "#00f"][classifyPoint(poly, p, 5) + 1],
+		});
+	}, 1000),
+]);
+
+draw(adaptiveCanvas2d(500, 500, document.body).ctx, geo);
