19df48755e5a650dac6786127cc740cb491474b9
Karsten Schmidt <k@thi.ng>
2024-06-20 16:02:35 +0200

    refactor(color): minor dedupe lab/lch CSS serialization

--- packages/color/src/internal/css.ts
+++ packages/color/src/internal/css.ts
@@ -9,7 +9,7 @@ export const __labCss = (mode: string, src: ReadonlyColor, scale: number) => {
 	const a = FF(src[1] * scale);
 	const b = FF(src[2] * scale);
 	const alpha = __ensureAlpha(src[3]);
-	return `${mode}(${l} ${a} ${b}` + (alpha < 1 ? `/${FF(alpha)})` : ")");
+	return __css(mode, l, a, b, alpha);
 };
 
 export const __lchCss = (mode: string, src: ReadonlyColor, scaleC: number) => {
@@ -17,5 +17,8 @@ export const __lchCss = (mode: string, src: ReadonlyColor, scaleC: number) => {
 	const c = FF(clamp0(src[1]) * scaleC);
 	const h = FF(fract(src[2]) * 360);
 	const a = __ensureAlpha(src[3]);
-	return `${mode}(${l} ${c} ${h}` + (a < 1 ? `/${FF(a)})` : ")");
+	return __css(mode, l, c, h, a);
 };
+
+const __css = (mode: string, x: string, y: string, z: string, a: number) =>
+	`${mode}(${x} ${y} ${z}` + (a < 1 ? `/${FF(a)})` : ")");
