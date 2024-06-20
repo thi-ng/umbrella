df7aef2f5495b69f0bd6b47f79e2772620343c7f
Karsten Schmidt <k@thi.ng>
2024-06-20 15:50:27 +0200

    refactor(geom): update proximity() to accept optional distance fn

--- packages/geom/src/proximity.ts
+++ packages/geom/src/proximity.ts
@@ -1,16 +1,18 @@
-import type { IShape } from "./api.js";
 import type { ReadonlyVec } from "@thi.ng/vectors";
-import { dist } from "@thi.ng/vectors/dist";
+import { dist as $dist } from "@thi.ng/vectors/dist";
+import type { IShape } from "./api.js";
 import { closestPoint } from "./closest-point.js";
 
 /**
  * Computes {@link closestPoint} on `shape` to `p`, and if successful, returns
- * Eucledian distance between that point and `p`.
+ * the distance between that point and `p`, using optionally given `dist`ance
+ * function (by default uses Eucledian distance).
  *
  * @param shape
  * @param p
+ * @param dist
  */
-export const proximity = (shape: IShape, p: ReadonlyVec) => {
+export const proximity = (shape: IShape, p: ReadonlyVec, dist = $dist) => {
 	const q = closestPoint(shape, p);
 	return q ? dist(p, q) : undefined;
 };
