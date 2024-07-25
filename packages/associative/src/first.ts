8a53ee0f547c134c6c3fdcc3be78d5dbfed26b63
Karsten Schmidt <k@thi.ng>
2024-07-25 12:32:52 +0200

    fix(associative): add explicit return type for `first()`

--- packages/associative/src/first.ts
+++ packages/associative/src/first.ts
@@ -1,1 +1,9 @@
-export const first = <T>(x: Iterable<T>) => x[Symbol.iterator]().next().value;
+import type { Maybe } from "@thi.ng/api";
+
+/**
+ * Returns the first value (if any) from given iterable (e.g. a Set).
+ *
+ * @param x
+ */
+export const first = <T>(x: Iterable<T>): Maybe<T> =>
+	x[Symbol.iterator]().next().value;
