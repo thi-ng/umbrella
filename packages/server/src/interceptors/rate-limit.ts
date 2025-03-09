f342fde4700310bd7a60c00b901c9e783a1a1483
Karsten Schmidt <k@thi.ng>
2025-03-09 18:54:35 +0100

    feat(server): update RateLimiter to use leaky buckets
    - update `RateLimterOpts`
    - update/simplify `RateLimiter` interceptor
    - update deps

--- packages/server/src/interceptors/rate-limit.ts
+++ packages/server/src/interceptors/rate-limit.ts
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: Apache-2.0
 import type { Fn, Fn2, Maybe } from "@thi.ng/api";
-import { TLRUCache } from "@thi.ng/cache";
 import { isNumber } from "@thi.ng/checks";
+import { LeakyBucketMap } from "@thi.ng/leaky-bucket";
 import type { Interceptor, RequestCtx } from "../api.js";
 
+/**
+ * Configuration options for {@link RateLimiter}.
+ *
+ * References:
+ * - https://thi.ng/leaky-bucket
+ * - https://en.wikipedia.org/wiki/Leaky_bucket
+ */
 export interface RateLimitOpts<T extends RequestCtx = RequestCtx> {
 	/**
 	 * Function to produce a unique ID for rate limiting a client. By default
@@ -12,22 +19,43 @@ export interface RateLimitOpts<T extends RequestCtx = RequestCtx> {
 	 */
 	id?: Fn<T, Maybe<string>>;
 	/**
-	 * Function to compute the max number of allowed requests for given client
-	 * ID and configured {@link RateLimitOpts.period}. If given as number, that
-	 * limit will be enforced for all identified clients.
+	 * Max. number of concurrently active client IDs/buckets. A bucket is
+	 * created for each unique ID produced via {@link RateLimitOpts.id}.
 	 *
 	 * @remarks
-	 * Quotas are only computed whenever a new client ID is first encountered or
-	 * when its previous request records have expired from the cache (after
-	 * {@link RateLimitOpts.period} seconds).
-	 */
-	quota: number | Fn2<T, string, number>;
-	/**
-	 * Size of the time window (in secords) to which this rate limiter applies.
+	 * A bucket's counter is incremented for each related request, up to the
+	 * configured {@link RateLimitOpts.bucketCapacity}. Once a bucket's capacity
+	 * has been reached, any new requests are dropped by the interceptor. Bucket
+	 * counters are slowly decremented every
+	 * {@link RateLimiterOpts.leakInterval} milliseconds, thus slowly allowing
+	 * again new requests. Once a bucket's counter goes back to zero, the bucket
+	 * will be removed.
 	 *
-	 * @defaultValue 900
+	 * @defaultValue 1000
 	 */
-	period?: number;
+	maxBuckets: number;
+	/**
+	 * Function to compute the max number of allowed requests for given client
+	 * ID bucket. If given as number, that limit will be uniformly enforced for
+	 * all clients. If given as function, per-client quotas can be defined.
+	 *
+	 * @remarks
+	 * Capacities are only computed whenever a new client ID is first
+	 * encountered or when its existing bucket is empty again (also see
+	 * {@link RateLimitOpts.leakInterval}).
+	 *
+	 * @defaultValue 60
+	 */
+	bucketCapacity: number | Fn2<T, string, number>;
+	/**
+	 * Time interval (in milliseconds) at which all active client ID buckets are
+	 * decreasing their counters, slowly freeing again their capacities. Once a
+	 * bucket is empty again (i.e. its counter is back at zero), the bucket will
+	 * be automatically removed.
+	 *
+	 * @defaultValue 1000
+	 */
+	leakInterval: number;
 }
 
 /**
@@ -40,96 +68,40 @@ export const rateLimiter = <T extends RequestCtx = RequestCtx>(
 ) => new RateLimiter<T>(opts);
 
 /**
- * Configurable, sliding time window-based rate limiter interceptor.
+ * Configurable rate limiter interceptor using a counter-based leaky bucket
+ * algorithm to ensure both an average rate and allow temporary bursting (up to
+ * an implicitly defined limit).
+ *
+ *
  */
 export class RateLimiter<T extends RequestCtx = RequestCtx>
 	implements Interceptor<T>
 {
-	cache: TLRUCache<string, Timestamps>;
-	clientQuota: Fn2<T, string, number>;
+	buckets: LeakyBucketMap<string>;
+	capacity: Fn2<T, string, number>;
 	clientID: Fn<T, Maybe<string>>;
-	period: number;
 
-	constructor({ id, quota, period = 15 * 60 }: RateLimitOpts<T>) {
+	constructor({
+		id,
+		maxBuckets,
+		bucketCapacity,
+		leakInterval,
+	}: RateLimitOpts<T>) {
 		this.clientID = id ?? ((ctx) => ctx.req.socket.remoteAddress);
-		this.clientQuota = isNumber(quota) ? () => quota : quota;
-		this.period = (period ?? 15 * 60) * 1000;
-		this.cache = new TLRUCache(null, {
-			ttl: this.period,
-			autoExtend: true,
-		});
+		this.capacity = isNumber(bucketCapacity)
+			? () => bucketCapacity
+			: bucketCapacity;
+		this.buckets = new LeakyBucketMap({ leakInterval, maxBuckets });
 	}
 
 	pre(ctx: T) {
-		const now = Date.now();
 		const id = this.clientID(ctx);
 		if (!id) return true;
-		let timestamps = this.cache.get(id);
-		if (timestamps) {
-			if (timestamps.expire(now - this.period) >= timestamps.quota) {
-				ctx.res.rateLimit(
-					{},
-					`Try again @ ${new Date(
-						timestamps.peek() + this.period
-					).toISOString()}`
-				);
-				return false;
-			}
-		} else {
-			timestamps = new Timestamps(this.clientQuota(ctx, id));
-			this.cache.set(id, timestamps);
+		const cap = !this.buckets.has(id) ? this.capacity(ctx, id) : undefined;
+		if (!this.buckets.update(id, cap)) {
+			ctx.res.rateLimit({}, `Try again later.`);
+			return false;
 		}
-		timestamps.write(now);
-		return true;
-	}
-}
-
-/**
- * Ring-buffer based history of request timestamps (per client).
- *
- * @remarks
- * Based on thi.ng/buffers `FIFOBuffer` implementation.
- *
- * @internal
- */
-class Timestamps {
-	buf: number[] = [];
-	rpos = 0;
-	wpos = 0;
-	num = 0;
-
-	constructor(public quota: number) {}
-
-	/**
-	 * Removes any timestamps older than `threshold` from the buffer and returns
-	 * remaining number of timestamps.
-	 *
-	 * @param threshold
-	 */
-	expire(threshold: number) {
-		let { buf, rpos, num } = this;
-		const max = buf.length;
-		while (num && buf[rpos] < threshold) {
-			rpos = (rpos + 1) % max;
-			num--;
-		}
-		this.rpos = rpos;
-		return (this.num = num);
-	}
-
-	peek() {
-		return this.buf[this.rpos];
-	}
-
-	writable() {
-		return this.num < this.quota;
-	}
-
-	write(x: number) {
-		const { buf, wpos } = this;
-		buf[wpos] = x;
-		this.wpos = (wpos + 1) % this.quota;
-		this.num++;
 		return true;
 	}
 }
