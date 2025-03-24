d63a4949c27bb0efb589ea1ff1c421a981c11918
Karsten Schmidt <k@thi.ng>
2025-03-24 10:53:15 +0100

    feat(vectors): update `softMax()`, add temperature support

--- packages/vectors/src/softmax.ts
+++ packages/vectors/src/softmax.ts
@@ -5,12 +5,15 @@ import { exp } from "./exp.js";
 import { sum } from "./sum.js";
 
 /**
- * Computes softmax (aka normalized exp) of input vector `src` and writes
- * results to `out` (or if null, back into `src`).
+ * Computes softmax (aka normalized {@link exp}) of input vector `src` and
+ * writes results to `out` (or if null, back into `src`). The optional
+ * `temperature` (should be >= 1) can be used to adjust the emphasis/scaling of
+ * normalized values: Higher temperatures reduce the contrast ratio between
+ * min/max components.
  *
  * @remarks
- * Computes elementwise: `s[i] = exp(src[i]) / sum(exp(src))`. The result
- * vector's elements will sum to 1.0.
+ * Computes: `smax = exp(src / temp) / sum(exp(src / temp))`. The result
+ * vector's components will all be in the [0,1] range and sum to 1.0.
  *
  * This function is often used as the last activation function in a neural
  * network to normalize the output to a probability distribution over predicted
@@ -23,8 +26,16 @@ import { sum } from "./sum.js";
  *
  * @param out -
  * @param src -
+ * @param temperature -
  */
-export const softMax = (out: Vec | null, src: ReadonlyVec) => {
-	const e = exp([], src);
+export const softMax = (
+	out: Vec | null,
+	src: ReadonlyVec,
+	temperature?: number
+) => {
+	const e =
+		temperature !== undefined
+			? exp(null, divN([], src, temperature))
+			: exp([], src);
 	return divN(out || src, e, sum(e));
 };
