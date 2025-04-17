ed4ae3b4b4ab606a69bca2c3fbfa6707c93c7b71
Karsten Schmidt <k@thi.ng>
2025-04-17 14:39:37 +0200

    fix(vectors): update minor2/3/4()
    - update to use `minid2/3/4()` fns

--- packages/vectors/src/minor.ts
+++ packages/vectors/src/minor.ts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-import { max2id, max3id, max4id } from "@thi.ng/math/interval";
+import { min2id, min3id, min4id } from "@thi.ng/math/interval";
 import type { MultiVecOpRoV, VecOpRoV } from "./api.js";
 import { vop } from "./vop.js";
 
@@ -9,21 +9,21 @@ const { abs } = Math;
  * Returns index of minor component in 2D vector `v`, i.e. where `|v[i]|`
  * is the smallest.
  */
-export const minor2: VecOpRoV<number> = (a) => max2id(abs(a[0]), abs(a[1]));
+export const minor2: VecOpRoV<number> = (a) => min2id(abs(a[0]), abs(a[1]));
 
 /**
  * Returns index of minor component in 3D vector `v`, i.e. where `|v[i]|`
  * is the smallest.
  */
 export const minor3: VecOpRoV<number> = (a) =>
-	max3id(abs(a[0]), abs(a[1]), abs(a[2]));
+	min3id(abs(a[0]), abs(a[1]), abs(a[2]));
 
 /**
  * Returns index of minor component in nD vector `v`, i.e. where `|v[i]|`
  * is the smallest.
  */
 export const minor4: VecOpRoV<number> = (a) =>
-	max4id(abs(a[0]), abs(a[1]), abs(a[2]), abs(a[3]));
+	min4id(abs(a[0]), abs(a[1]), abs(a[2]), abs(a[3]));
 
 /**
  * Returns index of minor component in nD vector `v`, i.e. where `|v[i]|`
