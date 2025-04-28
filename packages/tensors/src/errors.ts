a0052762f785def7410d255032c668d151c23945
Karsten Schmidt <k@thi.ng>
2025-04-28 15:41:03 +0200

    feat(tensors): add matrix-vector multiply, add custom error type

---
+++ packages/tensors/src/errors.ts
@@ -0,0 +1,7 @@
+import { defError } from "@thi.ng/errors/deferror";
+
+export const IllegalShapeError = defError<number[]>(() => "illegal shape");
+
+export const illegalShape = (shape: number[]) => {
+	throw new IllegalShapeError(shape);
+};
---
+++ packages/tensors/src/mulv.ts
@@ -0,0 +1,37 @@
+import { illegalShape } from "./errors.js";
+import { tensor, type Tensor1, type Tensor2 } from "./tensor.js";
+
+export const mulV = (out: Tensor1 | null, a: Tensor2, b: Tensor1) => {
+	const {
+		data: adata,
+		offset: offa,
+		shape: [rowa, cola],
+		stride: [txa, tya],
+	} = a;
+	const {
+		data: bdata,
+		offset: offb,
+		shape: [rowb],
+		stride: [txb],
+	} = b;
+	if (cola !== rowb) illegalShape(b.shape);
+	if (out == null) {
+		out = <Tensor1>tensor(b.type, [rowa], { storage: b.storage });
+	}
+	const {
+		data: odata,
+		offset: offo,
+		shape: [rowo],
+		stride: [txo],
+	} = out;
+	if (rowo !== rowa) illegalShape(out.shape);
+	for (let i = 0; i < rowa; i++) {
+		const ia = offa + i * txa;
+		let sum = 0;
+		for (let j = 0; j < cola; j++) {
+			sum += adata[ia + j * tya] * bdata[offb + j * txb];
+		}
+		odata[offo + i * txo] = sum;
+	}
+	return out;
+};
