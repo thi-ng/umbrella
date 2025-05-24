025c7bafcb8c24851a99eefdcf6a5331c4cb1c19
Karsten Schmidt <k@thi.ng>
2025-05-24 15:35:20 +0200

    feat(tensors): add/update/rename convolution kernels

--- packages/tensors/src/kernels.ts
+++ packages/tensors/src/kernels.ts
@@ -6,15 +6,15 @@ import { constant, tensor } from "./tensor.js";
 export const SOBEL1 = tensor([-1, 0, 1]);
 
 /**
- * 2D Sobel convolution kernel (along X, aka inner axis).
+ * 2D Sobel convolution kernel (along outer axis).
  *
  * @remarks
- * Use `SOBEL2.transpose([1, 0])` for other axis.
+ * Use `SOBEL2.transpose([1, 0])` for inner axis.
  */
 export const SOBEL2 = tensor([
-	[-1, 0, 1],
-	[-2, 0, 2],
-	[-1, 0, 1],
+	[-1, -2, -1],
+	[0, 0, 0],
+	[1, 2, 1],
 ]);
 
 /**
@@ -41,6 +41,26 @@ export const SOBEL3 = tensor([
 	],
 ]);
 
+export const EDGE2_3x3 = tensor([
+	[-1, -1, -1],
+	[-1, 8, -1],
+	[-1, -1, -1],
+]);
+
+export const EDGE2_5x5 = tensor([
+	[-1, -1, -1, -1, -1],
+	[-1, -1, -1, -1, -1],
+	[-1, -1, 24, -1, -1],
+	[-1, -1, -1, -1, -1],
+	[-1, -1, -1, -1, -1],
+]);
+
+export const SHARPEN2_3x3 = tensor([
+	[0, -1, 0],
+	[-1, 5, -1],
+	[0, -1, 0],
+]);
+
 /**
  * Higher order 2D box blur convolution kernel. Returns 2D tensor of size
  * `2*r+1`.
@@ -55,20 +75,20 @@ export const BOX_BLUR2 = (r: number) => {
 /**
  * 2D 3x3 box blur convolution kernel
  */
-export const BOX_BLUR2_3 = BOX_BLUR2(1);
+export const BOX_BLUR2_3x3 = BOX_BLUR2(1);
 
 /**
  * 2D 5x5 box blur convolution kernel
  */
-export const BOX_BLUR2_5 = BOX_BLUR2(2);
+export const BOX_BLUR2_5x5 = BOX_BLUR2(2);
 
-export const GAUSSIAN_BLUR2_3 = tensor([
+export const GAUSSIAN_BLUR2_3x3 = tensor([
 	[1 / 16, 1 / 8, 1 / 16],
 	[1 / 8, 1 / 4, 1 / 8],
 	[1 / 16, 1 / 8, 1 / 16],
 ]);
 
-export const GAUSSIAN_BLUR2_5 = tensor([
+export const GAUSSIAN_BLUR2_5x5 = tensor([
 	[1 / 256, 1 / 64, 3 / 128, 1 / 64, 1 / 256],
 	[1 / 64, 1 / 16, 3 / 32, 1 / 16, 1 / 64],
 	[3 / 128, 3 / 32, 9 / 64, 3 / 32, 3 / 128],
@@ -82,7 +102,7 @@ export const GAUSSIAN_BLUR2_5 = tensor([
  *
  * @param r -
  */
-export const GAUSSIAN2 = (r: number) => {
+export const GAUSSIAN_BLUR2 = (r: number) => {
 	r |= 0;
 	const sigma = -1 / (2 * ((Math.SQRT2 * r) / 3) ** 2);
 	const res: number[] = [];
