2ab48c77d047794efc4c3eebc9b1a2102fa93374
Karsten Schmidt <k@thi.ng>
2025-06-24 14:26:33 +0200

    feat(pixel-dominant-colors): add dominantColorsMeanCut()/dominantColorsMedianCut()

---
+++ packages/pixel-dominant-colors/src/mean-cut.ts
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+import type { Fn, NumericArray } from "@thi.ng/api";
+import { computeCutWith } from "@thi.ng/k-means/mean-cut";
+import type { FloatBuffer } from "@thi.ng/pixel/float";
+import type { ReadonlyVec } from "@thi.ng/vectors";
+import { mean, vmean } from "@thi.ng/vectors/mean";
+import { vmedian } from "@thi.ng/vectors/median";
+import type { DominantColor, DominantColorOpts } from "./api.js";
+import { filterSamples } from "./utils.js";
+
+export const dominantColorsMeanCut = (
+	img: FloatBuffer | NumericArray[],
+	num: number,
+	opts?: Partial<DominantColorOpts>
+) => __dominantColors(vmean, img, num, opts);
+
+export const dominantColorsMedianCut = (
+	img: FloatBuffer | NumericArray[],
+	num: number,
+	opts?: Partial<DominantColorOpts>
+) => __dominantColors(vmedian, img, num, opts);
+
+/** @internal */
+const __dominantColors = (
+	cut: Fn<ReadonlyVec, number>,
+	img: FloatBuffer | NumericArray[],
+	num: number,
+	opts?: Partial<DominantColorOpts>
+) => {
+	const samples = opts?.filter ? filterSamples(opts.filter, img) : [...img];
+	return samples.length
+		? computeCutWith(cut, samples, samples[0].length, num)
+				.sort((a, b) => b.length - a.length)
+				.map(
+					(bin) =>
+						<DominantColor>{
+							color: mean([], bin),
+							area: bin.length / samples.length,
+						}
+				)
+		: [];
+};
