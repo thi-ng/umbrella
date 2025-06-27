838e41785dacfc42bc944fde7aba803a25b67f6b
Karsten Schmidt <k@thi.ng>
2025-06-27 15:09:51 +0200

    feat(args): add thi.ng logo header tpl

---
+++ packages/args/src/header.ts
@@ -0,0 +1,16 @@
+/**
+ * Re-usable logo header for various thi.ng CLI tools.
+ *
+ * @param name
+ * @param version
+ * @param desc
+ *
+ * @internal
+ */
+export const THING_HEADER = (name: string, version: string, desc: string) => `
+ █ █   █           │
+██ █               │
+ █ █ █ █   █ █ █ █ │ ${name} v${version}
+ █ █ █ █ █ █ █ █ █ │ ${desc}
+                 █ │
+               █ █ │`;
