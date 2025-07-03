11859112f7d7bf4232ac031f9d57ae856ecaba68
Karsten Schmidt <k@thi.ng>
2025-07-03 16:59:28 +0200

    feat(file-io): add deleteFiles() and async versions

--- packages/file-io/src/delete.ts
+++ packages/file-io/src/delete.ts
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 import type { ILogger } from "@thi.ng/logger";
 import { rmSync, unlinkSync } from "node:fs";
+import { unlink } from "node:fs/promises";
+import { maskedPath } from "./mask.js";
 
 /**
  * Deletes file at given path. If `dryRun` is true (default: false), the file
@@ -12,9 +14,62 @@ import { rmSync, unlinkSync } from "node:fs";
  * @param dryRun
  */
 export const deleteFile = (path: string, logger?: ILogger, dryRun = false) => {
-	logger?.info(`${dryRun ? "[dryrun] " : ""}deleting file: ${path}`);
+	logger?.info(
+		`${dryRun ? "[dryrun] " : ""}deleting file:`,
+		maskedPath(path)
+	);
 	if (dryRun) return;
 	unlinkSync(path);
+};
+
+export const deleteFileAsync = async (
+	path: string,
+	logger?: ILogger,
+	dryRun = false
+) => {
+	logger?.info(
+		`${dryRun ? "[dryrun] " : ""}deleting file:`,
+		maskedPath(path)
+	);
+	if (dryRun) return;
+	return unlink(path);
+};
+
+/**
+ * Deletes multiple files via {@link deleteFile}. Any errors are caught and
+ * logged, but ignored otherwise. If `dryRun` is true, files WON'T be deleted.
+ *
+ * @param paths
+ * @param logger
+ * @param dryRun
+ */
+export const deleteFiles = (
+	paths: Iterable<string>,
+	logger?: ILogger,
+	dryRun = false
+) => {
+	for (let path of paths) {
+		try {
+			deleteFile(path, logger, dryRun);
+		} catch (e) {
+			logger?.warn(
+				"error deleting file:",
+				maskedPath((<Error>e).message)
+			);
+		}
+	}
+};
+
+export const deleteFilesAsync = async (
+	paths: Iterable<string>,
+	logger?: ILogger,
+	dryRun = false
+) => {
+	const promises: Promise<void>[] = [];
+	for (let path of paths) {
+		promises.push(deleteFileAsync(path, logger, dryRun));
+	}
+	await Promise.allSettled(promises);
 };
 
 /**
@@ -26,7 +81,10 @@ export const deleteFile = (path: string, logger?: ILogger, dryRun = false) => {
  * @param dryRun
  */
 export const deleteDir = (path: string, logger?: ILogger, dryRun = false) => {
-	logger?.info(`${dryRun ? "[dryrun] " : ""}deleting file: ${path}`);
+	logger?.info(
+		`${dryRun ? "[dryrun] " : ""}deleting directory:`,
+		maskedPath(path)
+	);
 	if (dryRun) return;
 	rmSync(path, { recursive: true, force: true });
 };
