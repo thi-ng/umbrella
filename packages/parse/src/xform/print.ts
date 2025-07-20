f5485f96af6d5c766ff7a189bd0f34bf720de941
Karsten Schmidt <k@thi.ng>
2025-07-20 13:45:49 +0200

    docs(parse): fix #524, update code example

--- packages/parse/src/xform/print.ts
+++ packages/parse/src/xform/print.ts
@@ -42,9 +42,9 @@ export const xfPrint = (fn: Fn<string, void> = console.log) => {
  * import { ALPHA, defContext, lit, oneOrMore, print, seq } from "@thi.ng/parse";
  *
  * print(seq([lit("["), oneOrMore(ALPHA), lit("]")]))(defContext("[abc]"));
- * // seq: null
+ * // seq: undefined
  * //   lit: "["
- * //   repeat1: null
+ * //   repeat1: undefined
  * //     oneOf: "a"
  * //     oneOf: "b"
  * //     oneOf: "c"
