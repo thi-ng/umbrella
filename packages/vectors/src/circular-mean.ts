4642241c76fb7fc3c129afc7125edbf2e883404f
Karsten Schmidt <k@thi.ng>
2025-07-23 18:22:04 +0200

    feat(vectors): update circularMean(), add centerCircular(), circularVariance(), circularSD()

--- packages/vectors/src/circular-mean.ts
+++ packages/vectors/src/circular-mean.ts
@@ -1,35 +1,62 @@
-import { cossin } from "@thi.ng/math/angle";
-import type { ReadonlyVec } from "./api.js";
-import { cartesian2 } from "./cartesian.js";
+// SPDX-License-Identifier: Apache-2.0
+import { PI, TAU } from "@thi.ng/math/api";
+import type { ReadonlyVec, Vec } from "./api.js";
+import { cartesian2FromAngles } from "./cartesian.js";
+import { center } from "./center.js";
 import { headingXY } from "./heading.js";
 import { mean } from "./mean.js";
+import { sd, variance } from "./variance.js";
 
 /**
- * Computes circular/angular mean angle (in radians, `[0,TAU)` interval) of
- * given 2D polar coordinates.
+ * Converts given angles into polar coordinates and then computes their mean
+ * angle.
  *
  * @remarks
- * Also see {@link circularMeanFromAngles}, {@link cartesian2}, {@link polar2}.
+ * Also see {@link cartesian2FromAngles}, {@link circularVariance},
+ * {@link circularSD}.
  *
  * References:
  *
  * - https://en.wikipedia.org/wiki/Circular_mean
  *
- * @param pts
- */
-export const circularMean = (pts: ReadonlyVec[]) =>
-	headingXY(
-		mean(
-			[],
-			pts.map((p) => cartesian2([], p))
-		)
-	);
-
-/**
- * Converts given angles into polar coordinates and then computes their mean
- * angle using {@link circularMean}.
- *
  * @param angles
  */
-export const circularMeanFromAngles = (angles: number[]) =>
-	circularMean(angles.map((x) => cossin(x)));
+export const circularMean = (angles: ReadonlyVec) =>
+	headingXY(mean([], cartesian2FromAngles(angles)));
+
+/**
+ * Circular version of {@link center} for given vector of angles.
+ *
+ * @param out
+ * @param angles
+ */
+export const centerCircular = (out: Vec | null, angles: ReadonlyVec) => {
+	const mean = circularMean(angles);
+	!out && (out = []);
+	for (let i = 0, n = angles.length; i < n; i++) {
+		out[i] = (angles[i] - mean + PI) % TAU;
+	}
+	return center(null, out);
+};
+
+/**
+ * Computes variance of given angles (in radians). Functional composition of
+ * {@link centerCircular} and {@link variance}. Also see the latter for
+ * explanation of the `corrected` param.
+ *
+ * @param angles
+ * @param corrected
+ */
+export const circularVariance = (angles: ReadonlyVec, corrected?: boolean) =>
+	variance(centerCircular([], angles), true, corrected);
+
+/**
+ * Computes standard deviation of given angles (in radians). Functional
+ * composition of {@link centerCircular} and {@link sd}. Also see the latter for
+ * explanation of the `corrected` param.
+ *
+ * @param angles
+ * @param corrected
+ */
+export const circularSD = (angles: ReadonlyVec, corrected?: boolean) =>
+	sd(centerCircular([], angles), true, corrected);
