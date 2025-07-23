17da925baff39ac422b80406f036905f6b785027
Karsten Schmidt <k@thi.ng>
2025-07-23 18:21:10 +0200

    feat(vectors): update variance(), sd(), sdError(), add meanAD()

--- packages/vectors/src/variance.ts
+++ packages/vectors/src/variance.ts
@@ -1,24 +1,46 @@
 // SPDX-License-Identifier: Apache-2.0
+import { abs } from "./abs.js";
 import type { ReadonlyVec } from "./api.js";
 import { center } from "./center.js";
 import { magSq } from "./magsq.js";
+import { sum } from "./sum.js";
 
 /**
- * Computes variance of vector components in `a`. If `isCentered` is
- * false (default), the vector will first be {@link center}ed (copy) in order to
+ * Computes variance of vector components in `a`. If `isCentered` is false
+ * (default), the vector will first be {@link center}ed (copy) in order to
  * compute the result.
  *
  * @remarks
- * var(a) = magSq(a) / len(a). Returns 0 if `a` is empty.
+ * If `corrected` is true, applies Bessel's correction for computing the sample
+ * variance of an unknown population size: `var(a) = magSq(a) / (len(a)-1)`
  *
- * Reference:
- * https://en.wikipedia.org/wiki/Variance
+ * If `corected` is false, assumes the given vector represents the full
+ * population and computes uncorrected variance: `var(a) = magSq(a) / len(a)`
+ *
+ * Returns 0 if `len(a) < k` where `k=2` for corrected and `k=1` for
+ * uncorrected.
+ *
+ * For angular values, use {@link circularVariance}.
+ *
+ * References:
+ *
+ * - https://en.wikipedia.org/wiki/Variance
+ * - https://en.wikipedia.org/wiki/Bessel's_correction
  *
  * @param a -
  * @param isCentered -
+ * @param corrected -
  */
-export const variance = (a: ReadonlyVec, isCentered = false) =>
-	a.length > 0 ? magSq(isCentered ? a : center([], a)) / a.length : 0;
+export const variance = (
+	a: ReadonlyVec,
+	isCentered = false,
+	corrected = false
+) => {
+	const k = ~~corrected;
+	return a.length > k
+		? magSq(isCentered ? a : center([], a)) / (a.length - k)
+		: 0;
+};
 
 /**
  * Computes standard deviation of vector components in `a`. If `isCentered` is
@@ -26,18 +48,28 @@ export const variance = (a: ReadonlyVec, isCentered = false) =>
  * compute the result.
  *
  * @remarks
- * sd = sqrt(magSq(a) / (len(a) - 1)). Returns 0 if len(a) < 2
+ * If `corrected` is true, applies Bessel's correction for computing the sample
+ * SD of an unknown population size: `sd(a) = sqrt(magSq(a) / (len(a)-1))`
+ *
+ * If `corected` is false, assumes the given vector represents the full
+ * population and computes uncorrected SD: `sd(a) = sqrt(magSq(a) / len(a))`
+ *
+ * Returns 0 if `len(a) < k` where `k=2` for corrected and `k=1` for
+ * uncorrected.
+ *
+ * For angular values, use {@link circularSD}.
  *
  * Reference:
- * https://en.wikipedia.org/wiki/Standard_deviation
+ *
+ * - https://en.wikipedia.org/wiki/Standard_deviation
+ * - https://en.wikipedia.org/wiki/Bessel's_correction
  *
  * @param a -
  * @param isCentered -
+ * @param corrected -
  */
-export const sd = (a: ReadonlyVec, isCentered = false) =>
-	a.length > 1
-		? Math.sqrt(magSq(isCentered ? a : center([], a)) / (a.length - 1))
-		: 0;
+export const sd = (a: ReadonlyVec, isCentered = false, corrected = false) =>
+	Math.sqrt(variance(a, isCentered, corrected));
 
 /**
  * Computes the standard error of vector components in `a`. If `isCentered` is
@@ -45,6 +77,8 @@ export const sd = (a: ReadonlyVec, isCentered = false) =>
  * compute the result.
  *
  * @remarks
+ * Also see {@link sd} comments about role of `corrected` param.
+ *
  * sderr(a) = sd(a) / sqrt(len(a))
  *
  * Reference:
@@ -52,6 +86,28 @@ export const sd = (a: ReadonlyVec, isCentered = false) =>
  *
  * @param a -
  * @param isCentered -
+ * @param corrected -
  */
-export const sdError = (a: ReadonlyVec, isCentered = false) =>
-	a.length > 1 ? sd(a, isCentered) / Math.sqrt(a.length) : 0;
+export const sdError = (
+	a: ReadonlyVec,
+	isCentered = false,
+	corrected = false
+) => (a.length > 1 ? sd(a, isCentered, corrected) / Math.sqrt(a.length) : 0);
+
+/**
+ * Computes the mean absolute deviation of vector components in `a`. If
+ * `isCentered` is false (default), the vector will first be {@link center}ed
+ * (copy) in order to compute the result.
+ *
+ * @remarks
+ * Reference:
+ *
+ * - https://en.wikipedia.org/wiki/Average_absolute_deviation
+ *
+ * @param a
+ * @param isCentered
+ */
+export const meanAD = (a: ReadonlyVec, isCentered = false) =>
+	a.length > 1
+		? sum(isCentered ? abs([], a) : abs(null, center([], a))) / a.length
+		: 0;
