a62642583927fec2d3413f40fd4832e75cd162ff
Karsten Schmidt <k@thi.ng>
2025-08-07 12:14:11 +0200

    fix(checks): fix #540, update hasCrypto() & hasWASM() checks
    - use `self` vs `window` to include worker support

--- packages/checks/src/has-crypto.ts
+++ packages/checks/src/has-crypto.ts
@@ -1,3 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
 export const hasCrypto = () =>
-	typeof window !== "undefined" && window["crypto"] !== undefined;
+	(typeof self !== "undefined" && self["crypto"] !== undefined) ||
+	(typeof global !== "undefined" && global["crypto"] !== undefined);
