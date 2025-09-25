f6ae3be57cd68fa1371a53410c1afaab6f9173c0
Karsten Schmidt <k@thi.ng>
2025-09-25 11:50:50 +0200

    refactor(tangle): simplify CLI impl via cliApp() wrapper

--- packages/tangle/src/cli.ts
+++ packages/tangle/src/cli.ts
@@ -3,16 +3,13 @@
 import {
 	ARG_DRY_RUN,
 	ARG_VERBOSE,
-	ParseError,
 	THING_HEADER,
+	cliApp,
+	configureLogLevel,
 	flag,
-	parse,
-	usage,
-	type Args,
-	type UsageOpts,
+	type Command,
 } from "@thi.ng/args";
 import { readJSON, writeText } from "@thi.ng/file-io";
-import { ConsoleLogger } from "@thi.ng/logger";
 import { join } from "node:path";
 import type { TangleCtx } from "./api.js";
 import { tangleFile } from "./tangle.js";
@@ -22,12 +19,6 @@ interface CLIOpts {
 	noComments: boolean;
 	verbose: boolean;
 }
-
-const argOpts: Args<CLIOpts> = {
-	...ARG_DRY_RUN,
-	...ARG_VERBOSE,
-	noComments: flag({ default: false, desc: "don't generate comments" }),
-};
 
 export const PKG = readJSON(join(process.argv[2], "package.json"));
 
@@ -39,44 +30,50 @@ export const HEADER = THING_HEADER(
 	"Literate programming code block tangling"
 );
 
-const usageOpts: Partial<UsageOpts> = {
-	lineWidth: process.stdout.columns,
-	prefix: `${HEADER}
+const CMD: Command<CLIOpts, CLIOpts> = {
+	desc: "",
+	opts: {},
+	inputs: [1, Infinity],
+	fn: async ({ opts, inputs, logger }) => {
+		let ctx: Partial<TangleCtx> = {
+			logger,
+			opts: {
+				comments: opts.noComments !== true,
+			},
+		};
+
+		for (let file of inputs) {
+			ctx = tangleFile(file, ctx);
+		}
+		for (let out in ctx.outputs!) {
+			writeText(out, ctx.outputs![out], ctx.logger, opts.dryRun);
+		}
+	},
+};
+
+cliApp({
+	name: "tangle",
+	start: 3,
+	opts: {
+		...ARG_DRY_RUN,
+		...ARG_VERBOSE,
+		noComments: flag({ default: false, desc: "don't generate comments" }),
+	},
+	commands: { CMD },
+	single: true,
+	usage: {
+		lineWidth: process.stdout.columns,
+		prefix: `${HEADER}
 
 usage: ${APP_NAME} [OPTS] SOURCE-FILES(S) ...
        ${APP_NAME} --help
 
 `,
-	showGroupNames: true,
-	paramWidth: 20,
-};
-
-const showUsage = () => {
-	process.stderr.write(usage(argOpts, usageOpts));
-	process.exit(1);
-};
-
-try {
-	const result = parse(argOpts, process.argv, { start: 3, usageOpts });
-	if (!result) process.exit(1);
-
-	const { result: opts, rest } = result!;
-	if (!rest.length) showUsage();
-
-	let ctx: Partial<TangleCtx> = {
-		logger: new ConsoleLogger("tangle", opts.verbose ? "DEBUG" : "INFO"),
-		opts: {
-			comments: opts.noComments !== true,
-		},
-	};
-
-	for (let file of rest) {
-		ctx = tangleFile(file, ctx);
-	}
-	for (let out in ctx.outputs!) {
-		writeText(out, ctx.outputs![out], ctx.logger, opts.dryRun);
-	}
-} catch (e) {
-	if (!(e instanceof ParseError)) process.stderr.write((<Error>e).message);
-	process.exit(1);
-}
+		showGroupNames: true,
+		paramWidth: 20,
+	},
+	ctx: async (ctx) => {
+		configureLogLevel(ctx.logger, ctx.opts.verbose);
+		return ctx;
+	},
+});
