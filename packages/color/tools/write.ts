df320cc893a5430da9050379f886a21869ee9331
Karsten Schmidt <k@thi.ng>
2025-10-28 10:56:03 +0100

    refactor(tools): update writeSVG() helper

--- packages/color/tools/write.ts
+++ packages/color/tools/write.ts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
+import { writeText } from "@thi.ng/file-io";
 import { serialize } from "@thi.ng/hiccup";
 import { svg } from "@thi.ng/hiccup-svg";
-import { writeFileSync } from "fs";
 
 export const writeSVG = (path: string, attribs: any, ...body: any[]) =>
-	writeFileSync(path, serialize(svg(attribs, ...body)), "utf-8");
+	writeText(path, serialize(svg(attribs, ...body)));
