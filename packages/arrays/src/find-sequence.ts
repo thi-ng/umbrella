3a3364fcb1c578684c8d8b1a51acfce795844656
Karsten Schmidt <k@thi.ng>
2025-11-10 12:40:30 +0100

    feat(arrays): add `findLastSequence()`, add tests

--- packages/arrays/src/find-sequence.ts
+++ packages/arrays/src/find-sequence.ts
@@ -41,7 +41,7 @@ export function findSequence(
 	const m = buf.length;
 	const n = needle.length;
 	const max = m - n;
-	if (max < 0) return -1;
+	if (!n || max < 0) return -1;
 	while (start < m) {
 		const idx = buf.indexOf(needle[0], start);
 		if (idx < 0 || idx > max) return -1;
@@ -53,6 +53,44 @@ export function findSequence(
 			}
 		}
 		if (j === 0) return idx;
+	}
+	return -1;
+}
+
+/**
+ * Similar to {@link findSequence}, but searches backwards, starting form given
+ * `start` index (default: end of `buf` array). Returns index of `needle` in
+ * `buf` or -1 if unsuccessful.
+ *
+ * @param buf
+ * @param needle
+ * @param start
+ */
+export function findLastSequence<T>(
+	buf: Array<T>,
+	needle: ArrayLike<T>,
+	start?: number
+): number;
+export function findLastSequence(
+	buf: TypedArray,
+	needle: ArrayLike<number>,
+	start?: number
+): number;
+export function findLastSequence(
+	buf: any[] | TypedArray,
+	needle: ArrayLike<any>,
+	start = buf.length - 1
+) {
+	const m = buf.length;
+	const n = needle.length;
+	if (!n || n > m) return -1;
+	start = Math.min(start, m - n);
+	let j;
+	for (let i = start; i >= 0; i--) {
+		for (j = n; j-- > 0; ) {
+			if (buf[i + j] !== needle[j]) break;
+		}
+		if (j < 0) return i;
 	}
 	return -1;
 }
