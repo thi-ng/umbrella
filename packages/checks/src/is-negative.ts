ea6f8f8a50f786d5a104532a02332e43021a8537
Karsten Schmidt <k@thi.ng>
2025-11-27 10:30:34 +0100

    feat(checks): add integer predicates
    - add `isNegativeInt()` / `isNonPositiveInt()`
    - add `isPositiveInt()` / `isNonNegativeInt()`

--- packages/checks/src/is-negative.ts
+++ packages/checks/src/is-negative.ts
@@ -1,3 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 export const isNegative = (x: any): x is number =>
 	typeof x === "number" && x < 0;
+
+export const isNegativeInt = (x: any): x is number =>
+	Number.isInteger(x) && x < 0;
+
+export const isNonPositiveInt = (x: any): x is number =>
+	Number.isInteger(x) && x <= 0;
--- packages/checks/src/is-positive.ts
+++ packages/checks/src/is-positive.ts
@@ -1,3 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 export const isPositive = (x: any): x is number =>
 	typeof x === "number" && x > 0;
+
+export const isPositiveInt = (x: any): x is number =>
+	Number.isInteger(x) && x > 0;
+
+export const isNonNegativeInt = (x: any): x is number =>
+	Number.isInteger(x) && x >= 0;
