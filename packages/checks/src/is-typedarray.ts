43388f3dbb39e8472fdc11ce6db457c74aa44728
Karsten Schmidt <k@thi.ng>
2025-12-03 13:25:03 +0100

    feat(checks): add more granular typed array checks
    - add `isIntArray()`, `isUintArray()`, `isFloatArray()`

--- packages/checks/src/is-typedarray.ts
+++ packages/checks/src/is-typedarray.ts
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
-export type TypedArray =
-	| Float32Array
-	| Float64Array
-	| Int8Array
-	| Int16Array
-	| Int32Array
+export type TypedArray = IntArray | UintArray | FloatArray;
+
+// TODO add Float16Array once widely supported
+export type FloatArray = Float32Array | Float64Array;
+
+// TODO add bigint array types
+export type IntArray = Int8Array | Int16Array | Int32Array;
+
+export type UintArray =
 	| Uint8Array
 	| Uint8ClampedArray
 	| Uint16Array
@@ -12,12 +15,28 @@ export type TypedArray =
 
 export const isTypedArray = (x: any): x is TypedArray =>
 	!!x &&
-	(x instanceof Float32Array ||
-		x instanceof Float64Array ||
-		x instanceof Uint32Array ||
-		x instanceof Int32Array ||
-		x instanceof Uint8Array ||
+	(x instanceof Uint8Array ||
 		x instanceof Int8Array ||
 		x instanceof Uint16Array ||
 		x instanceof Int16Array ||
+		x instanceof Uint32Array ||
+		x instanceof Int32Array ||
+		x instanceof Float32Array ||
+		x instanceof Float64Array ||
 		x instanceof Uint8ClampedArray);
+
+export const isIntArray = (x: any): x is IntArray =>
+	!!x &&
+	(x instanceof Int8Array ||
+		x instanceof Int16Array ||
+		x instanceof Int32Array);
+
+export const isUintArray = (x: any): x is UintArray =>
+	!!x &&
+	(x instanceof Uint32Array ||
+		x instanceof Uint16Array ||
+		x instanceof Uint8Array ||
+		x instanceof Uint8ClampedArray);
+
+export const isFloatArray = (x: any): x is FloatArray =>
+	!!x && (x instanceof Float32Array || x instanceof Float64Array);
