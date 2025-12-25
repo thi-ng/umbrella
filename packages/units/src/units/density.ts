8900f8bd66cfff9db95263b1a248160b9cf6a7b9
Karsten Schmidt <k@thi.ng>
2025-12-25 15:46:01 +0100

    feat(units): add `gsm`/`g_m2` presets

--- packages/units/src/units/density.ts
+++ packages/units/src/units/density.ts
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: Apache-2.0
-import { inch } from "./length.js";
-import { kg } from "./mass.js";
 import { defUnit, div, reciprocal } from "../unit.js";
+import { m2 } from "./area.js";
+import { inch } from "./length.js";
+import { g, kg } from "./mass.js";
 import { m3 } from "./volume.js";
+
+export const g_m2 = defUnit("g/m2", "gram per square meter", div(g, m2));
+
+/**
+ * Alias for {@link g_m2}
+ */
+export const gsm = defUnit("gsm", "gram per square meter", div(g, m2));
 
 export const kg_m3 = defUnit("kg/m3", "kilogram per cubic meter", div(kg, m3));
 
