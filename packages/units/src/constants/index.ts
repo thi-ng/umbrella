7c7921e7336910fab8f28ffa5cbc33ab50018888
Karsten Schmidt <k@thi.ng>
2025-12-25 15:20:09 +0100

    refactor(units): add/update pkg exports

---
+++ packages/units/src/constants/index.ts
@@ -0,0 +1,4 @@
+export * from "./densities.js";
+export * from "./earth.js";
+export * from "./paper-sizes.js";
+export * from "./velocities.js";
--- packages/units/src/index.ts
+++ packages/units/src/index.ts
@@ -1,29 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 export * from "./api.js";
+export * from "./dsl.js";
 export * from "./unit.js";
 
-export * from "./units/accel.js";
-export * from "./units/angle.js";
-export * from "./units/area.js";
-export * from "./units/data.js";
-export * from "./units/density.js";
-export * from "./units/electric.js";
-export * from "./units/energy.js";
-export * from "./units/force.js";
-export * from "./units/frequency.js";
-export * from "./units/length.js";
-export * from "./units/luminous.js";
-export * from "./units/mass.js";
-export * from "./units/parts.js";
-export * from "./units/power.js";
-export * from "./units/pressure.js";
-export * from "./units/substance.js";
-export * from "./units/temperature.js";
-export * from "./units/time.js";
-export * from "./units/velocity.js";
-export * from "./units/volume.js";
-
-export * from "./constants/densities.js";
-export * from "./constants/earth.js";
-export * from "./constants/paper-sizes.js";
-export * from "./constants/velocities.js";
+export * from "./constants/index.js";
+export * from "./units/index.js";
