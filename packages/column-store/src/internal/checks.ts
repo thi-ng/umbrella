3c430e0ac8225cfe95056138833539dec68e759f
Karsten Schmidt <k@thi.ng>
2026-02-06 10:29:15 +0100

    fix(column-store): update column value validators

--- packages/column-store/src/internal/checks.ts
+++ packages/column-store/src/internal/checks.ts
@@ -6,14 +6,14 @@ import type { ColumnSpec } from "../api.js";
 
 /** @internal */
 export const __validateValue = (spec: ColumnSpec, x: any) =>
-	(x == null && spec.cardinality[0] === 0) ||
+	(x == null && (spec.cardinality[0] === 0 || spec.default != null)) ||
 	(spec.type === "str" ? isString(x) : isNumber(x));
 
 /** @internal */
 export const __validateArrayValue = (spec: ColumnSpec, x: any) => {
 	const [min, max] = spec.cardinality;
 	return (
-		(x == null && min === 0) ||
+		(x == null && (min === 0 || spec.default != null)) ||
 		(isArray(x) &&
 			x.length >= min &&
 			x.length <= max &&
