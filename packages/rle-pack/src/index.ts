28d6a8fcb689aa0e0f03346b3597ef0bb3a72104
Karsten Schmidt <k@thi.ng>
2026-02-19 14:26:53 +0100

    chore: add SPDX headers

--- packages/rle-pack/src/index.ts
+++ packages/rle-pack/src/index.ts
@@ -1,2 +1,3 @@
+// SPDX-License-Identifier: Apache-2.0
 export * from "./binary.js";
 export * from "./simple.js";
--- packages/tensors/src/histogram.ts
+++ packages/tensors/src/histogram.ts
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0
 import type { Range1_24 } from "@thi.ng/api";
 import type { ITensor, ITensor1 } from "./api.js";
 import { cdf } from "./cdf.js";
