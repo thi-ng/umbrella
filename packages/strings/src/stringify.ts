78690d4a1fbf441e768ef6a548e1e4e66d09c68d
Karsten Schmidt <k@thi.ng>
2026-03-11 10:47:18 +0100

    feat(strings): add Error & RegExp support for `stringify()`

--- packages/strings/src/stringify.ts
+++ packages/strings/src/stringify.ts
@@ -8,6 +8,12 @@ import type { Stringer } from "./api.js";
  * `String(x)`. If `indent` is given, it will be used for `JSON.stringify(x,
  * null, indent)`
  *
+ * @remarks
+ * Special treatment for the following types:
+ *
+ * - `Error`: uses `.stack` or `.message` string properties
+ * - `RegExp`: use `.source` string property
+ *
  * @example
  * ```ts tangle:../export/stringify.ts
  * import { stringify } from "@thi.ng/strings";
@@ -26,6 +32,18 @@ import type { Stringer } from "./api.js";
  *   stringify()({ a: "hello" })
  * );
  * // { "a": "hello" }
+ *
+ * console.log(
+ *   stringify()(/\/abc/)
+ * );
+ * // "\\/abc"
+ *
+ * try {
+ *   throw new Error("eek");
+ * } catch(e) {
+ *   console.log(stringify()(e));
+ * }
+ * // "Error: eek\n    at <...>/stringify.ts:22:13\n    at moduleEvaluation (native:1:11)..."
  * ```
  *
  * @param all -
@@ -35,5 +53,13 @@ export const stringify =
 	(all = false, indent?: number): Stringer<any> =>
 	(x) =>
 		all || (typeof x !== "string" && typeof x !== "number")
-			? JSON.stringify(x, null, indent)
+			? JSON.stringify(
+					x instanceof Error
+						? (x.stack ?? x.message)
+						: x instanceof RegExp
+							? x.source
+							: x,
+					null,
+					indent,
+				)
 			: String(x);
