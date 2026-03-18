ea28608d2045d8bd7901c850b8708d51a5c3fc93
Karsten Schmidt <k@thi.ng>
2026-03-18 22:29:55 +0100

    feat(rstream): add `isSubscriber()`, `isSubscriptionLike()` helper predicates

--- packages/rstream/src/checks.ts
+++ packages/rstream/src/checks.ts
@@ -1,9 +1,20 @@
 // SPDX-License-Identifier: Apache-2.0
 import { implementsFunction } from "@thi.ng/checks/implements-function";
-import type { CloseMode, ISubscribable } from "./api.js";
+import type {
+	CloseMode,
+	ISubscribable,
+	ISubscriber,
+	ISubscription,
+} from "./api.js";
+
+export const isSubscriber = (x: any): x is ISubscriber<any> =>
+	implementsFunction(x, "next");
 
 export const isSubscribable = (x: any): x is ISubscribable<any> =>
 	implementsFunction(x, "subscribe");
+
+export const isSubscriptionLike = (x: any): x is ISubscription<any, any> =>
+	isSubscriber(x) && isSubscribable(x);
 
 /**
  * Returns true if mode is FIRST, or if mode is LAST *and* `num = 0`.
